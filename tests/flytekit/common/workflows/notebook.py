641f75a51df931ae9cf4a9feb1052d1345fb818f
Anmol Khurana <akhurana@lyft.com>
2019-10-29 23:17:49 +0530

    PR comments

--- tests/flytekit/common/workflows/notebook.py
+++ tests/flytekit/common/workflows/notebook.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 
 from flytekit.sdk.types import Types
 from flytekit.sdk.workflow import workflow_class, Input
-from flytekit.common.tasks.notebook import python_notebook, spark_notebook
+from flytekit.contrib.notebook import python_notebook, spark_notebook
 
 interactive_python = python_notebook(notebook_path="../../../../notebooks/python-notebook.ipynb",
                                           inputs={"pi": Types.Float},
@@ -23,4 +23,3 @@ class FlyteNotebookSparkWorkflow(object):
     partitions = Input(Types.Integer, default=10)
     out1 = interactive_spark(partitions=partitions)
     out2 = interactive_python(pi=out1.outputs.pi)
-    
