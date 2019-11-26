7e7ff58290e643ef270cc95d37784017da2349a7
Yee Hing Tong <ytong@lyft.com>
2019-11-25 16:45:22 -0800

    unit tests

--- tests/flytekit/unit/cli/auth/test_auth.py
+++ tests/flytekit/unit/cli/auth/test_auth.py
@@ -1,18 +1,9 @@
-from flytekit.clis.auth.discovery import DiscoveryClient
-from flytekit.clis.auth.auth import AuthorizationClient as _AuthorizationClient
-from flytekit.configuration.creds import (
-    DISCOVERY_ENDPOINT as _DISCOVERY_ENDPOINT,
-    REDIRECT_URI as _REDIRECT_URI,
-    CLIENT_ID as _CLIENT_ID
-)
-from flytekit.configuration.platform import URL as _URL
+from __future__ import absolute_import
+
+from flytekit.clis.auth import auth as _auth
 
 
-def test_discovery_client():
-    discovery_endpoint = _DISCOVERY_ENDPOINT.get()
-    discovery_client = DiscoveryClient(discovery_url=discovery_endpoint)
-    authorization_endpoints = discovery_client.get_authorization_endpoints()
-    print("///////////////////////////////////////|||||||||||||||||||||||||||||||||||||||||")
-    print(authorization_endpoints.token_endpoint)
-    token_endpoint = authorization_endpoints.token_endpoint
-    scope = 'svc'
+def test_generate_code_verifier():
+    verifier = _auth._generate_code_verifier()
+    # TODO: Write test later
+    assert verifier is not None
