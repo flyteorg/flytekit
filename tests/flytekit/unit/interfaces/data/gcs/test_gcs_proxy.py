ec7616e9ce0d526a6eefe65046c85435d1ac9475
Hongxin Liang <honnix@spotify.com>
2020-03-02 13:26:44 +0100

    add unit test for gcs_proxy

---
+++ tests/flytekit/unit/interfaces/data/gcs/test_gcs_proxy.py
@@ -0,0 +1,43 @@
+from __future__ import absolute_import
+
+import os as _os
+
+import mock as _mock
+import pytest as _pytest
+from flytekit.interfaces.data.gcs import gcs_proxy as _gcs_proxy
+
+
+@_pytest.fixture
+def gcs_proxy():
+    return _gcs_proxy.GCSProxy()
+
+
+@_mock.patch("flytekit.interfaces.data.gcs.gcs_proxy._update_cmd_config_and_execute")
+def test_upload_directory(mock_update_cmd_config_and_execute, gcs_proxy):
+    local_path, remote_path = "/foo/*", "gs://bar/0/"
+    gcs_proxy.upload_directory(local_path, remote_path)
+    mock_update_cmd_config_and_execute.assert_called_once_with(
+        ["gsutil", "cp", "-r", local_path, remote_path]
+    )
+
+
+@_mock.patch("flytekit.interfaces.data.gcs.gcs_proxy._update_cmd_config_and_execute")
+def test_upload_directory_padding_wildcard_for_local_path(
+    mock_update_cmd_config_and_execute, gcs_proxy
+):
+    local_path, remote_path = "/foo", "gs://bar/0/"
+    gcs_proxy.upload_directory(local_path, remote_path)
+    mock_update_cmd_config_and_execute.assert_called_once_with(
+        ["gsutil", "cp", "-r", _os.path.join(local_path, "*"), remote_path]
+    )
+
+
+@_mock.patch("flytekit.interfaces.data.gcs.gcs_proxy._update_cmd_config_and_execute")
+def test_upload_directory_padding_slash_for_remote_path(
+    mock_update_cmd_config_and_execute, gcs_proxy
+):
+    local_path, remote_path = "/foo/*", "gs://bar/0"
+    gcs_proxy.upload_directory(local_path, remote_path)
+    mock_update_cmd_config_and_execute.assert_called_once_with(
+        ["gsutil", "cp", "-r", local_path, remote_path + "/"]
+    )
