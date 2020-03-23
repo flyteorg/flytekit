0549e22577386feb0dc9dd991be4ac4424066a68
Miguel Toledo <mtoledo@lyft.com>
2020-03-23 15:23:42 -0700

    add metadata_defaults to workflow constructor

--- tests/flytekit/unit/common_tests/test_workflow_promote.py
+++ tests/flytekit/unit/common_tests/test_workflow_promote.py
@@ -114,57 +114,57 @@ def get_workflow_template():
 # Commenting these tests out for now until we can find a way to ensure
 # these tests pass on all flyteidl changes.
 
-# @_patch("flytekit.common.tasks.task.SdkTask.fetch")
-# def test_basic_workflow_promote(mock_task_fetch):
-#     # This section defines a sample workflow from a user
-#     @_sdk_tasks.inputs(a=_Types.Integer)
-#     @_sdk_tasks.outputs(b=_Types.Integer, c=_Types.Integer)
-#     @_sdk_tasks.python_task()
-#     def demo_task_for_promote(wf_params, a, b, c):
-#         b.set(a + 1)
-#         c.set(a + 2)
+@_patch("flytekit.common.tasks.task.SdkTask.fetch")
+def test_basic_workflow_promote(mock_task_fetch):
+    # This section defines a sample workflow from a user
+    @_sdk_tasks.inputs(a=_Types.Integer)
+    @_sdk_tasks.outputs(b=_Types.Integer, c=_Types.Integer)
+    @_sdk_tasks.python_task()
+    def demo_task_for_promote(wf_params, a, b, c):
+        b.set(a + 1)
+        c.set(a + 2)
 
-#     @_sdk_workflow.workflow_class()
-#     class TestPromoteExampleWf(object):
-#         wf_input = _sdk_workflow.Input(_Types.Integer, required=True)
-#         my_task_node = demo_task_for_promote(a=wf_input)
-#         wf_output_b = _sdk_workflow.Output(my_task_node.outputs.b, sdk_type=_Types.Integer)
-#         wf_output_c = _sdk_workflow.Output(my_task_node.outputs.c, sdk_type=_Types.Integer)
+    @_sdk_workflow.workflow_class()
+    class TestPromoteExampleWf(object):
+        wf_input = _sdk_workflow.Input(_Types.Integer, required=True)
+        my_task_node = demo_task_for_promote(a=wf_input)
+        wf_output_b = _sdk_workflow.Output(my_task_node.outputs.b, sdk_type=_Types.Integer)
+        wf_output_c = _sdk_workflow.Output(my_task_node.outputs.c, sdk_type=_Types.Integer)
 
-#     # This section uses the TaskTemplate stored in Admin to promote back to an Sdk Workflow
-#     int_type = _types.LiteralType(_types.SimpleType.INTEGER)
-#     task_interface = _interface.TypedInterface(
-#         # inputs
-#         {'a': _interface.Variable(int_type, "description1")},
-#         # outputs
-#         {
-#             'b': _interface.Variable(int_type, "description2"),
-#             'c': _interface.Variable(int_type, "description3")
-#         }
-#     )
-#     # Since the promotion of a workflow requires retrieving the task from Admin, we mock the SdkTask to return
-#     task_template = _task_model.TaskTemplate(
-#         _identifier.Identifier(_identifier.ResourceType.TASK, "project", "domain",
-#                                "tests.flytekit.unit.common_tests.test_workflow_promote.demo_task_for_promote",
-#                                "version"),
-#         "python_container",
-#         get_sample_task_metadata(),
-#         task_interface,
-#         custom={},
-#         container=get_sample_container()
-#     )
-#     sdk_promoted_task = _task.SdkTask.promote_from_model(task_template)
-#     mock_task_fetch.return_value = sdk_promoted_task
-#     workflow_template = get_workflow_template()
-#     promoted_wf = _workflow_common.SdkWorkflow.promote_from_model(workflow_template)
+    # This section uses the TaskTemplate stored in Admin to promote back to an Sdk Workflow
+    int_type = _types.LiteralType(_types.SimpleType.INTEGER)
+    task_interface = _interface.TypedInterface(
+        # inputs
+        {'a': _interface.Variable(int_type, "description1")},
+        # outputs
+        {
+            'b': _interface.Variable(int_type, "description2"),
+            'c': _interface.Variable(int_type, "description3")
+        }
+    )
+    # Since the promotion of a workflow requires retrieving the task from Admin, we mock the SdkTask to return
+    task_template = _task_model.TaskTemplate(
+        _identifier.Identifier(_identifier.ResourceType.TASK, "project", "domain",
+                               "tests.flytekit.unit.common_tests.test_workflow_promote.demo_task_for_promote",
+                               "version"),
+        "python_container",
+        get_sample_task_metadata(),
+        task_interface,
+        custom={},
+        container=get_sample_container()
+    )
+    sdk_promoted_task = _task.SdkTask.promote_from_model(task_template)
+    mock_task_fetch.return_value = sdk_promoted_task
+    workflow_template = get_workflow_template()
+    promoted_wf = _workflow_common.SdkWorkflow.promote_from_model(workflow_template)
 
-#     assert promoted_wf.interface.inputs["wf_input"] == TestPromoteExampleWf.interface.inputs["wf_input"]
-#     assert promoted_wf.interface.outputs["wf_output_b"] == TestPromoteExampleWf.interface.outputs["wf_output_b"]
-#     assert promoted_wf.interface.outputs["wf_output_c"] == TestPromoteExampleWf.interface.outputs["wf_output_c"]
+    assert promoted_wf.interface.inputs["wf_input"] == TestPromoteExampleWf.interface.inputs["wf_input"]
+    assert promoted_wf.interface.outputs["wf_output_b"] == TestPromoteExampleWf.interface.outputs["wf_output_b"]
+    assert promoted_wf.interface.outputs["wf_output_c"] == TestPromoteExampleWf.interface.outputs["wf_output_c"]
 
-#     assert len(promoted_wf.nodes) == 1
-#     assert len(TestPromoteExampleWf.nodes) == 1
-#     assert promoted_wf.nodes[0].inputs[0] == TestPromoteExampleWf.nodes[0].inputs[0]
+    assert len(promoted_wf.nodes) == 1
+    assert len(TestPromoteExampleWf.nodes) == 1
+    assert promoted_wf.nodes[0].inputs[0] == TestPromoteExampleWf.nodes[0].inputs[0]
 
 
 def get_compiled_workflow_closure():
