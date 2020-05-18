d6d9279d62f6a01011ca8889dfb9e525c00bb86b
Ketan Umare <kumare@lyft.com>
2020-05-17 17:02:39 -0700

    Updated version of flyteidl

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         ]
     },
     install_requires=[
-        "flyteidl>=0.17.29,<1.0.0",
+        "flyteidl>=0.17.31,<1.0.0",
         "click>=6.6,<8.0",
         "croniter>=0.3.20,<4.0.0",
         "deprecation>=2.0,<3.0",
