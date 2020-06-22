a38657d48f080f9ac2a3620c5d581761687a1d41
Chang-Hong Hsu <changhonghsu@lyft.com>
2020-06-22 15:02:42 -0700

    adding custom training job sdk type

--- flytekit/common/tasks/sagemaker/training_job_task.py
+++ flytekit/common/tasks/sagemaker/training_job_task.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 from flytekit import __version__
-from flytekit.common.tasks import task as _sdk_task
+from flytekit.common.tasks import task as _sdk_task, sdk_runnable as _sdk_runnable
 from flytekit.sdk import types as _sdk_types
 from flytekit.models import task as _task_models
 from flytekit.models import interface as _interface_model
@@ -69,3 +69,72 @@ class SdkSimpleTrainingJobTask(_sdk_task.SdkTask):
             ),
             custom=MessageToDict(training_job),
         )
+
+
+class SdkCustomTrainingJobTask(_sdk_runnable.SdkRunnableTask):
+    def __init__(
+            self,
+            task_function,
+            task_type,
+            cache_version,
+            retries,
+            interruptible,
+            deprecated,
+            cacheable,
+            training_job_config,
+            algorithm_specification,
+            environment,
+    ):
+
+        # Use the training job model as a measure of type checking
+        training_job = _training_job_models.TrainingJob(
+            algorithm_specification=algorithm_specification,
+            training_job_config=training_job_config,
+        ).to_flyte_idl()
+
+        # Setting flyte-level timeout to 0, and let SageMaker takes the StoppingCondition and terminate the training
+        # job gracefully
+        timeout = _datetime.timedelta(seconds=0)
+
+        super(SdkCustomTrainingJobTask, self).__init__(
+            task_function=task_function,
+            task_type=task_type,
+            discovery_version=cache_version,
+            retries=retries,
+            interruptible=interruptible,
+            deprecated=deprecated,
+            storage_request="",
+            cpu_request="",
+            gpu_request="",
+            memory_request="",
+            storage_limit="",
+            cpu_limit="",
+            gpu_limit="",
+            memory_limit="",
+            discoverable=cacheable,
+            timeout=timeout,
+            environment=environment,
+            custom=MessageToDict(training_job),
+        ),
+        self.add_inputs(
+            {
+                "static_hyperparameters": _interface_model.Variable(
+                    _sdk_types.Types.Generic.to_flyte_literal_type(), ""
+                ),
+                "train": _interface_model.Variable(
+                    _sdk_types.Types.MultiPartCSV.to_flyte_literal_type(), ""
+                ),
+                "validation": _interface_model.Variable(
+                    _sdk_types.Types.MultiPartCSV.to_flyte_literal_type(), ""
+                )
+            },
+        )
+        self.add_outputs(
+            {
+                "model": _interface_model.Variable(
+                    _sdk_types.Types.Blob.to_flyte_literal_type(), ""
+                )
+            }
+        )
+
+
