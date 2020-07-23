dcc33cd1622ef8601a5966adc463ad88fdf3c64c
Chang-Hong Hsu <changhonghsu@lyft.com>
2020-07-23 13:26:39 -0700

    Revert "failing the unit test"
    This reverts commit e45ccf7eb7ac74a8ba07e6f57ba038668f176c47.

--- flytekit/common/tasks/sagemaker/training_job_task.py
+++ flytekit/common/tasks/sagemaker/training_job_task.py
@@ -19,51 +19,24 @@ from flytekit.models import literals as _literal_models
 from flytekit.common.constants import SdkTaskType
 
 
-def _dummy_training_job_task_function(
-        _,
-        train,
-        validation,
-        static_hyperparameters,
-        stopping_condition,
-        model,
-):
-    pass
-
-
-class SdkRunnableTrainingJobContainer(_sdk_runnable.SdkRunnableContainer):
-    @property
-    def args(self):
-        """
-        Override args to remove the injection of command prefixes
-        :rtype: list[Text]
-        """
-        return self._args
-
-
-class _SdkTrainingJobTask(_sdk_runnable.SdkRunnableTask):
+class SdkSimpleTrainingJobTask(_sdk_task.SdkTask):
     def __init__(
             self,
-            task_function: Callable,
             training_job_config: _training_job_models.TrainingJobConfig,
             algorithm_specification: _training_job_models.AlgorithmSpecification,
-            cache_version: str = "",
-            retries: int = 0,
             interruptible: bool = False,
-            deprecated: str = "",
+            retries: int = 0,
             cacheable: bool = False,
-            environment: Dict[str, str] = None,
+            cache_version: str = "",
     ):
         """
 
-        :param task_function:
         :param training_job_config:
         :param algorithm_specification:
-        :param cache_version:
-        :param retries:
         :param interruptible:
-        :param deprecated:
+        :param retries:
         :param cacheable:
-        :param environment:
+        :param cache_version:
         """
         # Use the training job model as a measure of type checking
         self._training_job_model = _training_job_models.TrainingJob(
@@ -75,138 +48,7 @@ class _SdkTrainingJobTask(_sdk_runnable.SdkRunnableTask):
         # job gracefully
         timeout = _datetime.timedelta(seconds=0)
 
-        super(_SdkTrainingJobTask, self).__init__(
-            task_function=task_function,
-            task_type=SdkTaskType.SAGEMAKER_TRAINING_JOB_TASK,
-            discovery_version=cache_version,
-            retries=retries,
-            interruptible=interruptible,
-            deprecated=deprecated,
-            storage_request="",
-            cpu_request="",
-            gpu_request="",
-            memory_request="",
-            storage_limit="",
-            cpu_limit="",
-            gpu_limit="",
-            memory_limit="",
-            discoverable=cacheable,
-            timeout=timeout,
-            environment=environment,
-            custom=MessageToDict(self._training_job_model.to_flyte_idl()),
-        )
-
-        self.add_inputs(
-            {
-                "static_hyperparameters": _interface_model.Variable(
-                    type=_idl_types.LiteralType(simple=_idl_types.SimpleType.STRUCT),
-                    description="",
-                ),
-                "train": _interface_model.Variable(
-                    type=_idl_types.LiteralType(
-                        blob=_core_types.BlobType(
-                            format="csv",
-                            dimensionality=_core_types.BlobType.BlobDimensionality.MULTIPART
-                        ),
-                    ),
-                    description="",
-                ),
-                "validation": _interface_model.Variable(
-                    type=_idl_types.LiteralType(
-                        blob=_core_types.BlobType(
-                            format="csv",
-                            dimensionality=_core_types.BlobType.BlobDimensionality.MULTIPART
-                        ),
-                    ),
-                    description="",
-                ),
-                "stopping_condition": _interface_model.Variable(
-                    _sdk_types.Types.Proto(_training_job_pb2.StoppingCondition).to_flyte_literal_type(), ""
-                )
-            },
-        )
-        self.add_outputs(
-            {
-                "model": _interface_model.Variable(
-                    type=_idl_types.LiteralType(
-                        blob=_core_types.BlobType(
-                            format="",
-                            dimensionality=_core_types.BlobType.BlobDimensionality.SINGLE
-                        )
-                    ),
-                    description=""
-                )
-            }
-        )
-
-    def _get_container_definition(
-            self,
-            **kwargs
-    ):
-        """
-        :rtype: SdkRunnableTrainingJobContainer
-        """
-        return super(_SdkTrainingJobTask, self)._get_container_definition(cls=SdkRunnableTrainingJobContainer, **kwargs)
-
-
-class SdkSimpleTrainingJobTask(_SdkTrainingJobTask):
-    def __init__(
-            self,
-            training_job_config: _training_job_models.TrainingJobConfig,
-            algorithm_specification: _training_job_models.AlgorithmSpecification,
-            interruptible: bool = False,
-            retries: int = 0,
-            cacheable: bool = False,
-            cache_version: str = "",
-            deprecated: str = "",
-    ):
         super(SdkSimpleTrainingJobTask, self).__init__(
-            task_function=_dummy_training_job_task_function,
-            training_job_config=training_job_config,
-            algorithm_specification=algorithm_specification,
-            cache_version=cache_version,
-            retries=retries,
-            interruptible=interruptible,
-            deprecated=deprecated,
-            cacheable=cacheable,
-            environment=None,
-        )
-
-    @property
-    def training_job_model(self) -> _training_job_models.TrainingJob:
-        return self._training_job_model
-
-
-class SdkSimpleTrainingJobTask2(_sdk_task.SdkTask):
-    def __init__(
-            self,
-            training_job_config: _training_job_models.TrainingJobConfig,
-            algorithm_specification: _training_job_models.AlgorithmSpecification,
-            interruptible: bool = False,
-            retries: int = 0,
-            cacheable: bool = False,
-            cache_version: str = "",
-    ):
-        """
-
-        :param training_job_config:
-        :param algorithm_specification:
-        :param interruptible:
-        :param retries:
-        :param cacheable:
-        :param cache_version:
-        """
-        # Use the training job model as a measure of type checking
-        self._training_job_model = _training_job_models.TrainingJob(
-            algorithm_specification=algorithm_specification,
-            training_job_config=training_job_config,
-        )
-
-        # Setting flyte-level timeout to 0, and let SageMaker takes the StoppingCondition and terminate the training
-        # job gracefully
-        timeout = _datetime.timedelta(seconds=0)
-
-        super(SdkSimpleTrainingJobTask2, self).__init__(
             type=SdkTaskType.SAGEMAKER_TRAINING_JOB_TASK,
             metadata=_task_models.TaskMetadata(
                 runtime=_task_models.RuntimeMetadata(
@@ -324,8 +166,7 @@ class SdkCustomTrainingJobTask(_sdk_runnable.SdkRunnableTask):
             timeout=timeout,
             environment=environment,
             custom=MessageToDict(training_job),
-        )
-
+        ),
         self.add_inputs(
             {
                 "static_hyperparameters": _interface_model.Variable(
