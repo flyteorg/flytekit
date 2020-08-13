80d0d34c3f7c2246eb500d1a8b24f61da0ea56b4
Jeev B <jeev.balakrishnan@freenome.com>
2020-08-13 09:42:27 -0700

    urllib3 upper version bound

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         "six>=1.9.0,<2.0.0",
         "sortedcontainers>=1.5.9<3.0.0",
         "statsd>=3.0.0,<4.0.0",
-        "urllib3>=1.22,<1.26",
+        "urllib3>=1.22,<2.0.0",
         "wrapt>=1.0.0,<2.0.0",
         "papermill>=1.2.0",
     ],
