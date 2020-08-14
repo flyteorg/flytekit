6c08e622fbd55c9643086ca5241b2605705aefed
Yee Hing Tong <ytong@lyft.com>
2020-08-14 15:36:35 -0700

    add back test

--- tests/flytekit/unit/models/test_launch_plan.py
+++ tests/flytekit/unit/models/test_launch_plan.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
-from flytekit.models import interface, launch_plan, schedule, types
+from flytekit.models import common, interface, launch_plan, literals, schedule, types
+from flytekit.models.core import identifier
 
 
 def test_metadata():
@@ -34,3 +35,53 @@ def test_lp_closure():
     assert obj == obj2
     assert obj2.expected_inputs == parameter_map
     assert obj2.expected_outputs == variable_map
+
+
+def test_launch_plan_spec():
+    identifier_model = identifier.Identifier(identifier.ResourceType.TASK, "project", "domain", "name", "version")
+
+    s = schedule.Schedule("asdf", "1 3 4 5 6 7")
+    launch_plan_metadata_model = launch_plan.LaunchPlanMetadata(schedule=s, notifications=[])
+
+    v = interface.Variable(types.LiteralType(simple=types.SimpleType.BOOLEAN), "asdf asdf asdf")
+    p = interface.Parameter(var=v)
+    parameter_map = interface.ParameterMap({"ppp": p})
+
+    fixed_inputs = literals.LiteralMap(
+        {"a": literals.Literal(scalar=literals.Scalar(primitive=literals.Primitive(integer=1)))}
+    )
+
+    labels_model = common.Labels({})
+    annotations_model = common.Annotations({"my": "annotation"})
+
+    auth_role_model = common.AuthRole(assumable_iam_role="my:iam:role")
+    raw_data_output_config = common.RawOutputDataConfig("s3://bucket")
+    empty_raw_data_output_config = common.RawOutputDataConfig("")
+
+    lp_spec_raw_output_prefixed = launch_plan.LaunchPlanSpec(
+        identifier_model,
+        launch_plan_metadata_model,
+        parameter_map,
+        fixed_inputs,
+        labels_model,
+        annotations_model,
+        auth_role_model,
+        raw_data_output_config,
+    )
+
+    obj2 = launch_plan.LaunchPlanSpec.from_flyte_idl(lp_spec_raw_output_prefixed.to_flyte_idl())
+    assert obj2 == lp_spec_raw_output_prefixed
+
+    lp_spec_no_prefix = launch_plan.LaunchPlanSpec(
+        identifier_model,
+        launch_plan_metadata_model,
+        parameter_map,
+        fixed_inputs,
+        labels_model,
+        annotations_model,
+        auth_role_model,
+        empty_raw_data_output_config,
+    )
+
+    obj2 = launch_plan.LaunchPlanSpec.from_flyte_idl(lp_spec_no_prefix.to_flyte_idl())
+    assert obj2 == lp_spec_no_prefix
