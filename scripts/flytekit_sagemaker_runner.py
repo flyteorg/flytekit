6cd96a663ae273c6f71bb281c0a4db0b1db59298
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-08-14 20:51:02 -0700

    fixes

--- scripts/flytekit_sagemaker_runner.py
+++ scripts/flytekit_sagemaker_runner.py
@@ -14,11 +14,13 @@ subprocess_cmd = args.flyte_sagmaker_cmd.split('+')
 
 flyte_sagemaker_cmd_parser = argparse.ArgumentParser(description="Parse pyflyte execute command to replace output prefix location.")
 flyte_sagemaker_cmd_parser.add_argument('--output-prefix', dest='output_prefix')
-args, unknowns = parser.parse_known_args(args=subprocess_cmd)
+args, unknowns = flyte_sagemaker_cmd_parser.parse_known_args(args=subprocess_cmd)
 args.output_prefix = "{}/{}".format(args.output_prefix, environ.get("TRAINING_JOB_NAME"))
-subprocess_cmd = " ".join(unknowns.expand(["--output-prefix", args.output_prefix]))
+unknowns.extend(["--output-prefix", args.output_prefix])
 
-logging.info("Launching a subprocess with: {}".format(subprocess_cmd))
+logging.info("Launching a subprocess with: {}".format(unknowns))
 
 # Launching a subprocess with the selected entrypoint script and the rest of the arguments
+subprocess_cmd = []
+subprocess_cmd.extend(unknowns)
 subprocess.run(subprocess_cmd)
