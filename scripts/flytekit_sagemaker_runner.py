3935d231c404844ec82db889ecf4fa6a779ce370
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-08-14 19:20:57 -0700

    typo

--- scripts/flytekit_sagemaker_runner.py
+++ scripts/flytekit_sagemaker_runner.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 import argparse
 import logging
 import subprocess
+from os import environ
 
 parser = argparse.ArgumentParser(description="Running sagemaker task")
 parser.add_argument('--__FLYTE_SAGEMAKER_CMD__', dest='flyte_sagmaker_cmd',
@@ -11,7 +12,7 @@ args, unknowns = parser.parse_known_args()
 # Extending the command with the rest of the command-line arguments
 subprocess_cmd = args.flyte_sagmaker_cmd.split()
 
-flyte_sagemaker_cmd_parser = argparse.AgumentParser(description="Parse pyflyte execute command to replace output prefix location.")
+flyte_sagemaker_cmd_parser = argparse.ArgumentParser(description="Parse pyflyte execute command to replace output prefix location.")
 flyte_sagemaker_cmd_parser.add_argument('--output-prefix', dest='output_prefix')
 args, unknowns = parser.parse_known_args(args=subprocess_cmd)
 args.output_prefix = "{}/{}".format(args.output_prefix, environ.get("TRAINING_JOB_NAME"))
