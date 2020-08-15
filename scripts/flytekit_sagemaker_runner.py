1c797f48fc5783532b6f72853af35ca0b732e0bd
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-08-14 18:02:49 -0700

    add training job name to the output location

--- scripts/flytekit_sagemaker_runner.py
+++ scripts/flytekit_sagemaker_runner.py
@@ -10,6 +10,13 @@ args, unknowns = parser.parse_known_args()
 
 # Extending the command with the rest of the command-line arguments
 subprocess_cmd = args.flyte_sagmaker_cmd.split()
+
+flyte_sagemaker_cmd_parser = argparse.AgumentParser(description="Parse pyflyte execute command to replace output prefix location.")
+flyte_sagemaker_cmd_parser.add_argument('--output-prefix', dest='output_prefix')
+args, unknowns = parser.parse_known_args(args=subprocess_cmd)
+args.output_prefix = "{}/{}".format(args.output_prefix, environ.get("TRAINING_JOB_NAME"))
+subprocess_cmd = " ".join(unknowns.expand(["--output-prefix", args.output_prefix]))
+
 logging.info("Launching a subprocess with: {}".format(subprocess_cmd))
 
 # Launching a subprocess with the selected entrypoint script and the rest of the arguments
