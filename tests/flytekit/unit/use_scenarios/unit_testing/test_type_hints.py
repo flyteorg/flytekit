8e274da34e8f48b36ae0f85870bc9359864d0159
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-08-19 08:32:17 -0700

    Add file reading options

--- tests/flytekit/unit/use_scenarios/unit_testing/test_type_hints.py
+++ tests/flytekit/unit/use_scenarios/unit_testing/test_type_hints.py
@@ -47,17 +47,27 @@ def test_single_output():
 
 @flyte_test
 def test_read_file_known_location():
+    # Option 1
+    # users receive a file handle as the parameter to their task
     @python_task
     def my_task(ctx, fh: typing.BinaryIO):
         lines = fh.readlines()
         # assert 
     
+    # Option 1.1
+    # To call the task for unit testing, users need to open a file and pass the handle
     with open('/mytest', mode='rb') as fh:
         assert my_task.unit_test(fh=fh) == {'output': 'Hello world'}
 
 
+    # Option 1.2
+    # Users pass a Path-Like object that flyte knows how to interpret and open or youo
     assert my_task.unit_test(fh=CustomPathLike('/mytest', format="csv")) == {}
     
+    # Option 2
+    # Users receive a Path-Like type as a parameter to their function (much like how Blobs work today)
+    # Option 2.1
+    # Users will have to call downlooad then open
     @python_task
     def my_task(ctx, file_path: CustomPathLike):
         file_path.download()
@@ -67,7 +77,8 @@ def test_read_file_known_location():
 
     assert my_task.unit_test(file_path=CustomPathLike('s3://bucket-my/mytest')) == {}
 
-
+    # Option 2.2
+    # Provide a custom open() that encapsulates download and open actions
     @python_task
     def my_task(ctx, file_path: CustomPathLike):
         with file_path.open() as fh:
@@ -75,6 +86,8 @@ def test_read_file_known_location():
             # assert 
 
 
+    # Option 2.3
+    # Flyte always downloads the file and users can use the regular open() function to read it.
     @python_task
     def my_task(ctx, file_path: CustomPathLike):
         with open(file_path.local_path, mode='rb') as fh:
