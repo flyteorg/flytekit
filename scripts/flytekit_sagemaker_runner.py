ea19ce5e8bf8adb8fa52b740f83bc40344f7cdd2
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-08-28 06:46:54 -0700

    Add example to runner script

--- scripts/flytekit_sagemaker_runner.py
+++ scripts/flytekit_sagemaker_runner.py
@@ -10,6 +10,12 @@ FLYTE_ENV_VAR_PREFIX = f"{FLYTE_ARG_PREFIX}_ENV_VAR_"
 FLYTE_CMD_PREFIX = f"{FLYTE_ARG_PREFIX}_CMD_"
 FLYTE_ARG_SUFFIX = "__"
 
+# An example for a valid command:
+# --__FLYTE_ENV_VAR_env1=val1__ --__FLYTE_ENV_VAR_env2=val2__ --__FLYTE_CMD_service_venv__
+# --__FLYTE_CMD_pyflyte-execute__ --__FLYTE_CMD_--task-module__ --__FLYTE_CMD_blah__ --__FLYTE_CMD_--task-name__
+# --__FLYTE_CMD_bloh__ --__FLYTE_CMD_--output-prefix__ --__FLYTE_CMD_s3://fake-bucket__ --__FLYTE_CMD_--inputs__
+# --__FLYTE_CMD_s3://fake-bucket__
+
 parser = argparse.ArgumentParser(description="Running sagemaker task")
 args, unknowns = parser.parse_known_args()
 
