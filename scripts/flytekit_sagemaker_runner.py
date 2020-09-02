1b8048cfdb58fa12aa913b61eb231d05a367fea7
Chang-Hong Hsu <changhonghsu@lyft.com>
2020-09-01 20:55:10 -0700

    refactor runner script and add a unit test

--- scripts/flytekit_sagemaker_runner.py
+++ scripts/flytekit_sagemaker_runner.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import argparse
 import logging
 import subprocess
+import sys
 from os import environ
 
 logging.basicConfig(level=logging.DEBUG)
@@ -24,45 +25,64 @@ FLYTE_ARG_SUFFIX = "__"
 # --__FLYTE_CMD_8_--inputs__ __FLYTE_CMD_DUMMY_VALUE__
 # --__FLYTE_CMD_9_s3://fake-bucket__ __FLYTE_CMD_DUMMY_VALUE__
 
-parser = argparse.ArgumentParser(description="Running sagemaker task")
-args, unknowns = parser.parse_known_args()
 
-# Parse the command line and env vars
-flyte_cmd = []
-env_vars = {}
-i = 0
+def parse_args(cli_args):
+    parser = argparse.ArgumentParser(description="Running sagemaker task")
+    args, unknowns = parser.parse_known_args(cli_args)
 
-while i < len(unknowns):
-    unknown = unknowns[i]
-    logging.info(f"Processing argument {unknown}")
-    if unknown.startswith(FLYTE_CMD_PREFIX) and unknown.endswith(FLYTE_ARG_SUFFIX):
-        processed = unknown[len(FLYTE_CMD_PREFIX) :][: -len(FLYTE_ARG_SUFFIX)]
-        # Parse the format `1_--task-module`
-        parts = processed.split("_", maxsplit=1)
-        flyte_cmd.append((parts[0], parts[1]))
-        i += 1
-    elif unknown.startswith(FLYTE_ENV_VAR_PREFIX) and unknown.endswith(FLYTE_ARG_SUFFIX):
-        processed = unknown[len(FLYTE_ENV_VAR_PREFIX) :][: -len(FLYTE_ARG_SUFFIX)]
-        i += 1
-        if unknowns[i].startswith(FLYTE_ARG_PREFIX) is False:
-            env_vars[processed] = unknowns[i]
+    # Parse the command line and env vars
+    flyte_cmd = []
+    env_vars = {}
+    i = 0
+
+    while i < len(unknowns):
+        unknown = unknowns[i]
+        logging.info(f"Processing argument {unknown}")
+        if unknown.startswith(FLYTE_CMD_PREFIX) and unknown.endswith(FLYTE_ARG_SUFFIX):
+            processed = unknown[len(FLYTE_CMD_PREFIX) :][: -len(FLYTE_ARG_SUFFIX)]
+            # Parse the format `1_--task-module`
+            parts = processed.split("_", maxsplit=1)
+            flyte_cmd.append((parts[0], parts[1]))
             i += 1
-    else:
-        # To prevent SageMaker from ignoring our __FLYTE_CMD_*__ hyperparameters, we need to set a dummy value
-        # which serves as a placeholder for each of them. The dummy value placeholder `__FLYTE_CMD_DUMMY_VALUE__`
-        # falls into this branch and will be ignored
-        i += 1
+        elif unknown.startswith(FLYTE_ENV_VAR_PREFIX) and unknown.endswith(FLYTE_ARG_SUFFIX):
+            processed = unknown[len(FLYTE_ENV_VAR_PREFIX) :][: -len(FLYTE_ARG_SUFFIX)]
+            i += 1
+            if unknowns[i].startswith(FLYTE_ARG_PREFIX) is False:
+                env_vars[processed] = unknowns[i]
+                i += 1
+        else:
+            # To prevent SageMaker from ignoring our __FLYTE_CMD_*__ hyperparameters, we need to set a dummy value
+            # which serves as a placeholder for each of them. The dummy value placeholder `__FLYTE_CMD_DUMMY_VALUE__`
+            # falls into this branch and will be ignored
+            i += 1
 
-# Order the cmd using the index (the first element in each tuple)
-flyte_cmd.sort(key=lambda x: x[0])
-flyte_cmd = [x[1] for x in flyte_cmd]
+    return flyte_cmd, env_vars
 
-logging.info(f"Cmd:{flyte_cmd}")
-logging.info(f"Env vars:{env_vars}")
 
-for key, val in env_vars.items():
-    environ[key] = val
+def sort_flyte_cmd(flyte_cmd):
+    # Order the cmd using the index (the first element in each tuple)
+    flyte_cmd.sort(key=lambda x: x[0])
+    flyte_cmd = [x[1] for x in flyte_cmd]
+    return flyte_cmd
 
-# Launching a subprocess with the selected entrypoint script and the rest of the arguments
-logging.info(f"Launching command: {flyte_cmd}")
-subprocess.run(flyte_cmd)
+
+def set_env_vars(env_vars):
+    for key, val in env_vars.items():
+        environ[key] = val
+
+
+def run(cli_args):
+    flyte_cmd, env_vars = parse_args(cli_args)
+    flyte_cmd = sort_flyte_cmd(flyte_cmd)
+    set_env_vars(env_vars)
+
+    logging.info(f"Cmd:{flyte_cmd}")
+    logging.info(f"Env vars:{env_vars}")
+
+    # Launching a subprocess with the selected entrypoint script and the rest of the arguments
+    logging.info(f"Launching command: {flyte_cmd}")
+    subprocess.run(flyte_cmd)
+
+
+if __name__ == "__main__":
+    run(sys.argv)
