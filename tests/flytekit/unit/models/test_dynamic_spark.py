889d0d6f98f1b3d834cc6f17fd26bd2bf5fd7b4d
Ketan Umare <kumare@lyft.com>
2020-09-29 17:00:16 -0700

    lint fix

--- tests/flytekit/unit/models/test_dynamic_spark.py
+++ tests/flytekit/unit/models/test_dynamic_spark.py
@@ -1,7 +1,6 @@
 from configuration import TemporaryConfiguration
 from flytekit.common import constants as _sdk_constants
 from flytekit.sdk import tasks as _tasks
-from flytekit.sdk import workflow as _workflow
 from flytekit.sdk.types import Types as _Types
 
 
