4807d5ef4bae9d72856adc563f0266c6aa9bac5c
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-10-15 14:45:00 +0200

    lint

--- flytekit/common/tasks/sagemaker/types.py
+++ flytekit/common/tasks/sagemaker/types.py
@@ -1,4 +1,5 @@
-from flytekit.models.sagemaker import parameter_ranges as _parameter_range_models, hpo_job as _hpo_models
+from flytekit.models.sagemaker import hpo_job as _hpo_models
+from flytekit.models.sagemaker import parameter_ranges as _parameter_range_models
 from flytekit.sdk import types as _sdk_types
 
 HyperparameterTuningJobConfig = _sdk_types.Types.GenericProto(_hpo_models.HyperparameterTuningJobConfig)
