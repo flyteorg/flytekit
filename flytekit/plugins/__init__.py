7dcf46c8d3badab53524abbd64138f568a501b36
Chang-Hong Hsu <changhonghsu@lyft.com>
2020-10-24 18:57:36 -0700

    fix incorrect hyphen

--- flytekit/plugins/__init__.py
+++ flytekit/plugins/__init__.py
@@ -15,7 +15,7 @@ pandas = _lazy_loader.lazy_load_module("pandas")  # type: _lazy_loader._LazyLoad
 hmsclient = _lazy_loader.lazy_load_module("hmsclient")  # type: _lazy_loader._LazyLoadModule
 type(hmsclient).add_sub_module("genthrift.hive_metastore.ttypes")
 
-sagemaker_training = _lazy_loader.lazy_load_module("sagemaker-training")  # type: _lazy_loader._LazyLoadModule
+sagemaker_training = _lazy_loader.lazy_load_module("sagemaker_training")  # type: _lazy_loader._LazyLoadModule
 
 _lazy_loader.LazyLoadPlugin("spark", ["pyspark>=2.4.0,<3.0.0"], [pyspark])
 
