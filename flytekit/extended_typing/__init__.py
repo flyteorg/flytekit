54923ac93d3bcd43e2edb1944553307c442472bd
Yee Hing Tong <ytong@lyft.com>
2020-11-10 11:31:37 -0800

    init

--- flytekit/extended_typing/__init__.py
+++ flytekit/extended_typing/__init__.py
@@ -1,1 +1,10 @@
 from flytekit.extended_typing.flyte_file import FlyteFile
+from flytekit.extended_typing.schema import (
+    FlyteSchema,
+    PandasSchemaReader,
+    PandasSchemaWriter,
+    Schema,
+    SchemaFormat,
+    SchemaOpenMode,
+    SchemaType,
+)
