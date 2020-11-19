f048d0bcca1ac987176baf2f405635b527a4a278
Yee Hing Tong <ytong@lyft.com>
2020-11-18 16:41:03 -0800

    wip

--- tests/flytekit/unit/annotated/test_type_hints.py
+++ tests/flytekit/unit/annotated/test_type_hints.py
@@ -1055,40 +1055,17 @@ def test_dict_wf_with_conversion():
 
 
 def test_fhdsa():
-    my_schema = None  # tddype: our new schema
+    my_schema = None
 
     hive_task = HiveQueryTask(
-        task_inputs=kwtypes(ds=str),
-        statement="SELECT * from unnest(sequence(1, {{ .Inputs.ds }}))",
-        output_schema=my_schema,
-        remote_location_prefix="s3://modelbuilder/blah/",
-        staging_query="",
+        task_inputs=kwtypes(my_schema=FlyteSchema, ds=str),
+        statement="""
+            set engine=tez;
+            insert overwrite directory '{{ .raw_output_data }}' stored as parquet  -- will be unique per retry
+            select *
+            from blah
+            where ds = '{{ .Inputs.ds }}' and uri = '{{ .inputs.my_schema }}'
+        """,
+        output_schema=my_schema,  # the schema literal's backend uri will be equal to the value of .raw_output_data
     )
-
-    def wf():
-        results = hive_task(ds="2020-01-11")
-
-
-    def create_data_task() -> FlyteSchema:
-        ...
-
-
-    # https://sql.lyft.net/queries/22952389
-    write_hive_task = HivePartitionUpdateTask()
-
-
-    def wf():
-        s = create_data_task()
-        results = write_hive_task(required_schema=s, table_name="table", paritions = {
-            'ds': 2020-08-12
-        })
-
-
-
-
-
-
-
-
-
 
