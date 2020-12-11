d5684f2daada3eae78a8ea26998a37a59952e3c4
Honnix <honnix@users.noreply.github.com>
2020-12-11 20:36:29 +0100

    List projects with filter and sort_by (#272)
    * List projects with filter and sort_by
    
    * Bump version

--- tests/flytekit/unit/cli/test_flyte_cli.py
+++ tests/flytekit/unit/cli/test_flyte_cli.py
@@ -6,6 +6,8 @@ from flytekit.clis.flyte_cli import main as _main
 from flytekit.common.exceptions.user import FlyteAssertion
 from flytekit.common.types import primitives
 from flytekit.configuration import TemporaryConfiguration
+from flytekit.models import filters as _filters
+from flytekit.models.admin import common as _admin_common
 from flytekit.models.core import identifier as _core_identifier
 from flytekit.models.project import Project as _Project
 from flytekit.sdk.tasks import inputs, outputs, python_task
@@ -62,6 +64,22 @@ def _identity_dummy(a, b):
 def test__extract_files_pair_iterator():
     results = _main._extract_files([1, 2, 3, 4])
     assert [(1, 2), (3, 4)] == results
+
+
+@_mock.patch("flytekit.clis.flyte_cli.main._friendly_client.SynchronousFlyteClient")
+def test_list_projects(mock_client):
+    mock_client().list_projects_paginated.return_value = ([], "")
+    runner = _CliRunner()
+    result = runner.invoke(
+        _main._flyte_cli, ["list-projects", "-h", "a.b.com", "-i", "--filter", "ne(state,-1)", "--sort-by", "asc(name)"]
+    )
+    assert result.exit_code == 0
+    mock_client().list_projects_paginated.assert_called_with(
+        limit=100,
+        token="",
+        filters=[_filters.Filter.from_python_std("ne(state,-1)")],
+        sort_by=_admin_common.Sort.from_python_std("asc(name)"),
+    )
 
 
 @_mock.patch("flytekit.clis.flyte_cli.main._friendly_client.SynchronousFlyteClient")
--- tests/flytekit/unit/clients/test_raw.py
+++ tests/flytekit/unit/clients/test_raw.py
@@ -44,3 +44,12 @@ def test_update_project(mock_channel, mock_admin):
     project = _project_pb2.Project(id="foo", name="name", description="description", state=_project_pb2.Project.ACTIVE)
     client.update_project(project)
     mock_admin.AdminServiceStub().UpdateProject.assert_called_with(project)
+
+
+@mock.patch("flytekit.clients.raw._admin_service")
+@mock.patch("flytekit.clients.raw._insecure_channel")
+def test_list_projects_paginated(mock_channel, mock_admin):
+    client = _RawSynchronousFlyteClient(url="a.b.com", insecure=True)
+    project_list_request = _project_pb2.ProjectListRequest(limit=100, token="", filters=None, sort_by=None)
+    client.list_projects(project_list_request)
+    mock_admin.AdminServiceStub().ListProjects.assert_called_with(project_list_request, metadata=None)
