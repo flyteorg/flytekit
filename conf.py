19f0a808457c03204bfc6552d027807b4d74b28c
Haytham Abuelfutuh <haytham@afutuh.com>
2020-12-11 14:51:36 -0800

    theme

--- conf.py
+++ conf.py
@@ -28,6 +28,7 @@ release = '0.16.0'
 import sys
 import os
 import flytekit
+import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('./flytekit'))
@@ -49,6 +50,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.extlinks',
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -67,7 +69,7 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
