cac0b253a5dcbda84d16a1dcba9cd7ef0cc243a7
wild-endeavor <wild-endeavor@users.noreply.github.com>
2020-12-16 17:22:54 -0800

    fill in

--- flytekit/annotated/reference.py
+++ flytekit/annotated/reference.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+from typing import Dict, Type
 from flytekit.annotated.launch_plan import ReferenceLaunchPlan
+from flytekit.annotated.workflow import ReferenceWorkflow
+from flytekit.annotated.reference_task import ReferenceTask
 from flytekit.models.core import identifier as _identifier_model
 
 
@@ -14,8 +17,8 @@ def get_reference_entity(
     outputs: Dict[str, Type],
 ):
     if resource_type == _identifier_model.ResourceType.TASK:
-        ...
+        return ReferenceTask(project, domain, name, version, inputs, outputs)
     elif resource_type == _identifier_model.ResourceType.WORKFLOW:
-        ...
+        return ReferenceWorkflow(project, domain, name, version, inputs, outputs)
     elif resource_type == _identifier_model.ResourceType.LAUNCH_PLAN:
         return ReferenceLaunchPlan(project, domain, name, version, inputs, outputs)
