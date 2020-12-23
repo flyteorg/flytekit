fb74fe64025ffc4ea32e580544afd2571b3a6297
Chang-Hong Hsu <changhonghsu@lyft.com>
2020-12-23 15:54:22 -0800

    bump version

--- flytekit/__init__.py
+++ flytekit/__init__.py
@@ -2,7 +2,7 @@ import logging as _logging
 
 import flytekit.plugins  # noqa: F401
 
-__version__ = "0.15.3"
+__version__ = "0.15.4"
 logger = _logging.getLogger("flytekit")
 
 # create console handler and set level to debug
