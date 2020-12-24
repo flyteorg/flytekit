eb18d8665519a51d4208582e5b7a6d72837676dd
Ketan Umare <ketan.umare@gmail.com>
2020-12-24 12:27:15 -0800

    More docs

--- flytekit/taskplugins/hive/task.py
+++ flytekit/taskplugins/hive/task.py
@@ -13,6 +13,15 @@ from flytekit.types.schema import FlyteSchema
 
 @dataclass
 class HiveConfig(object):
+    """
+    HiveConfig should be used to configure a Hive Task. Config values are statically defined during the construction
+    of a task, and are not parameterized.
+    Note: A separate story is in progress to dynamically alter configuration for an execution launch.
+
+    Args:
+        cluster_label: A string value that helps in identifying the cluster label.
+        tags: Any tags that should be associated with the remote execution request.
+    """
     cluster_label: str
     tags: Optional[List[str]] = None
 
@@ -23,7 +32,7 @@ class HiveConfig(object):
 
 class HiveTask(SQLTask[HiveConfig]):
     """
-    Hive Task
+    This is the simplest form of a Hive Task, that can be used even for tasks that do not produce any output.
     """
 
     def __init__(
@@ -37,6 +46,16 @@ class HiveTask(SQLTask[HiveConfig]):
         *args,
         **kwargs,
     ):
+        """
+        Args:
+            name: Name of this task, should be unique in the project
+            config: Type HiveConfig object
+            inputs: Name and type of inputs specified as an ordered dictionary
+            query_template: The actual query to run. We use Flyte's Golang templating format for Query templating.
+                            Refer to the templating documentation
+            metadata: Any additional metadata for the task - like retries, timeouts etc
+            output_schema_type: If some data is produced by this query, then you can specify the output schema type
+        """
         outputs = None
         if output_schema_type is not None:
             outputs = {
@@ -100,6 +119,12 @@ class HiveSelectTask(HiveTask):
         *args,
         **kwargs,
     ):
+        """
+            Args:
+                select_query: Singular query that returns a Tabular dataset
+                stage_query: optional query that should be executed before the actual ``select_query``. This can usually
+                            be used for setting memory or the an alternate execution engine like :ref:`tez<https://tez.apache.org/>`_/
+        """
         query_template = HiveSelectTask._HIVE_QUERY_FORMATTER.format(
             stage_query_str=stage_query or "", select_query_str=select_query.strip().strip(";")
         )
