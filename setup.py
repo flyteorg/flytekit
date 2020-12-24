74ec974d1b4fbe7c6e6f1204f9fb3258365c19f0
Katrina Rogan <katrogan9@gmail.com>
2020-12-23 16:56:25 -0800

    derp

--- setup.py
+++ setup.py
@@ -1,13 +1,43 @@
 from setuptools import find_packages, setup  # noqa
 
-import flytekit  # noqa
-from flytekit.tools.lazy_loader import LazyLoadPlugin  # noqa
+# from flytekit.tools.lazy_loader import LazyLoadPlugin  # noqa
+# extras_require = LazyLoadPlugin.get_extras_require()
+extras_require = {
+    "spark": ["pyspark>=2.4.0,<3.0.0"],
+    "spark3": ["pyspark>=3.0.0"],
+    "sidecar": ["k8s-proto>=0.0.3,<1.0.0"],
+    "schema": ["numpy>=1.14.0,<2.0.0", "pandas>=0.22.0,<2.0.0", "pyarrow>=0.11.0,<1.0.0"],
+    "hive_sensor": ["hmsclient>=0.0.1,<1.0.0"],
+    "all-spark2.4": [
+        "pyspark>=2.4.0,<3.0.0",
+        "k8s-proto>=0.0.3,<1.0.0",
+        "numpy>=1.14.0,<2.0.0",
+        "pandas>=0.22.0,<2.0.0",
+        "pyarrow>=0.11.0,<1.0.0",
+        "hmsclient>=0.0.1,<1.0.0",
+    ],
+    "all-spark3": [
+        "pyspark>=3.0.0",
+        "k8s-proto>=0.0.3,<1.0.0",
+        "numpy>=1.14.0,<2.0.0",
+        "pandas>=0.22.0,<2.0.0",
+        "pyarrow>=0.11.0,<1.0.0",
+        "hmsclient>=0.0.1,<1.0.0",
+    ],
+    "all": [
+        "pyspark>=2.4.0,<3.0.0",
+        "k8s-proto>=0.0.3,<1.0.0",
+        "numpy>=1.14.0,<2.0.0",
+        "pandas>=0.22.0,<2.0.0",
+        "pyarrow>=0.11.0,<1.0.0",
+        "hmsclient>=0.0.1,<1.0.0",
+    ],
+}
 
-extras_require = LazyLoadPlugin.get_extras_require()
 
 setup(
     name="flytekit",
-    version=flytekit.__version__,
+    version="0.16.0a2",
     maintainer="Lyft",
     maintainer_email="flyte-eng@lyft.com",
     packages=find_packages(exclude=["tests*"]),
@@ -18,13 +48,12 @@ setup(
     entry_points={
         "console_scripts": [
             "pyflyte-execute=flytekit.bin.entrypoint:execute_task_cmd",
-            "pyflyte-fast-execute=flytekit.bin.entrypoint:fast_execute_task_cmd",
             "pyflyte=flytekit.clis.sdk_in_container.pyflyte:main",
             "flyte-cli=flytekit.clis.flyte_cli.main:_flyte_cli",
         ]
     },
     install_requires=[
-        "flyteidl>=0.18.11,<1.0.0",
+        "flyteidl>=0.18.9,<1.0.0",
         "click>=6.6,<8.0",
         "croniter>=0.3.20,<4.0.0",
         "deprecated>=1.0,<2.0",
@@ -40,14 +69,13 @@ setup(
         "six>=1.9.0,<2.0.0",
         "sortedcontainers>=1.5.9<3.0.0",
         "statsd>=3.0.0,<4.0.0",
-        "urllib3>=1.22,<1.26",
+        "urllib3>=1.22,<1.26.0",
         "wrapt>=1.0.0,<2.0.0",
         "papermill>=1.2.0",
         "ipykernel>=5.0.0",
         "black==19.10b0",
         "retry==0.9.2",
-        "natsort>=7.0.1",
-        "dirhash>=0.2.1",
+        "dataclasses-json>=0.5.2",
     ],
     extras_require=extras_require,
     scripts=[
@@ -58,12 +86,11 @@ setup(
         "scripts/flytekit_sagemaker_runner.py",
     ],
     license="apache2",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries",
