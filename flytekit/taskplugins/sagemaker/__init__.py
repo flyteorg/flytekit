0c0c85a806cae5f8b640a1f458c7d89e03fe05a4
Ketan Umare <ketan.umare@gmail.com>
2021-01-05 13:33:48 -0800

    Some fixes while writing examples

--- flytekit/taskplugins/sagemaker/__init__.py
+++ flytekit/taskplugins/sagemaker/__init__.py
@@ -3,18 +3,24 @@ from flytekit.models.sagemaker.hpo_job import (
     HyperparameterTuningObjective,
     HyperparameterTuningObjectiveType,
     TrainingJobEarlyStoppingType,
+    HyperparameterTuningStrategy,
 )
+
 from flytekit.models.sagemaker.parameter_ranges import (
     CategoricalParameterRange,
     ContinuousParameterRange,
     IntegerParameterRange,
     ParameterRangeOneOf,
+    HyperparameterScalingType,
 )
+
 from flytekit.models.sagemaker.training_job import (
     AlgorithmName,
     AlgorithmSpecification,
     DistributedProtocol,
     TrainingJobResourceConfig,
+    InputContentType,
+    InputMode,
 )
 
 from .hpo import HPOJob, SagemakerHPOTask
