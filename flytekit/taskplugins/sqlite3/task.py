ec90d17483960e79223e24b82762bb1243626ba9
Ketan Umare <ketan.umare@gmail.com>
2021-01-12 22:02:24 -0800

    wip

---
+++ flytekit/taskplugins/sqlite3/task.py
@@ -0,0 +1,134 @@
+import contextlib
+import os
+import shutil
+import sqlite3
+import tempfile
+import typing
+from dataclasses import dataclass
+from enum import Enum
+
+from flytekit import FlyteContext, TaskMetadata, kwtypes
+from flytekit.annotated.base_sql_task import SQLTask
+from flytekit.plugins import pandas as pd
+from flytekit.types.file import FlyteFile
+from flytekit.types.schema import FlyteSchema
+
+
+def unarchive_file(local_path: str, to_dir: str):
+    """
+    Unarchive given archive and returns the unarchived file name. It is expected that only one file is unarchived.
+    More than one file or 0 files will result in a ``RuntimeError``
+    """
+    archive_dir = os.path.join(to_dir, "_arch")
+    shutil.unpack_archive(local_path, archive_dir)
+    # file gets uncompressed into to_dir/_arch/*.*
+    files = os.listdir(archive_dir)
+    if not files or len(files) == 0 or len(files) > 1:
+        raise RuntimeError(f"Uncompressed archive should contain only one file - found {files}!")
+    return os.path.join(archive_dir, files[0])
+
+
+@dataclass
+class SQLite3Config(object):
+    """
+    Use this configuration to configure if sqlite3 files are passed in dynamically into the task or
+    to configure a path to a static sqlite3 compatible database file that is available either
+      - within the container
+      - or from a publicly downloadable source
+
+    If the Mode is marked dynamic, then a variable called ``sqlite: FlyteFile[".sqlite"]`` will be automatically added
+    to the input interface of the task. Failing to supply this at runtime will raise an Assertion
+
+        Args:
+            sqlite_db_mode: STATIC should be use for constant locations of file, dynamic to allow dynamically provided
+                            sqlite3 file locations. The file will have to comply with FlyteFile[.db|.zip|.tar.gz|.bz2]
+                            format
+            static_file_path: default FlyteFile that will be downloaded on execute
+            compressed: Boolean that indicates if the given file is a compressed archive. Supported file types are
+                            [zip, tar, gztar, bztar, xztar]
+    """
+
+    class Mode(Enum):
+        STATIC = "static"
+        """
+        STATIC refers to a constant, statically configured reference.
+        """
+        DYNAMIC = "dynamic"
+        """
+        Dynamic refers to SQLite files will be dynamically injected into the task.
+        """
+
+    sqlite_db_mode: Mode = Mode.DYNAMIC
+    static_file_uri: typing.Optional[str] = None
+    compressed: bool = False
+
+
+class SQLite3Task(SQLTask[SQLite3Config]):
+    """
+    Makes it possible to run client side SQLite3 queries that optionally return a FlyteSchema object
+
+    TODO: How should we use pre-built containers for running portable tasks like this. Should this always be a
+          referenced task type?
+    """
+
+    _SQLITE_TASK_TYPE = "sqlite"
+    _SQLITE_INPUT_FILE = "sqlite"
+
+    def __init__(
+        self,
+        name: str,
+        query_template: str,
+        metadata: typing.Optional[TaskMetadata] = None,
+        inputs: typing.Optional[typing.Dict[str, typing.Type]] = None,
+        task_config: typing.Optional[SQLite3Config] = None,
+        output_schema_type: typing.Optional[typing.Type[FlyteSchema]] = None,
+        *args,
+        **kwargs,
+    ):
+        if not inputs:
+            inputs = {}
+        if task_config is not None and task_config.sqlite_db_mode == SQLite3Config.Mode.STATIC:
+            if task_config.static_file_uri is None:
+                raise ValueError("SQLite DB mode configured to be static, but, the static_file_path was not specified.")
+        else:
+            if self._SQLITE_INPUT_FILE in inputs:
+                raise ValueError(f"Input name `{self._SQLITE_INPUT_FILE}` is reserved as an auto-injected input")
+            inputs[self._SQLITE_INPUT_FILE] = FlyteFile
+        if output_schema_type is None:
+            output_schema_type = FlyteSchema
+        outputs = kwtypes(results=output_schema_type)
+        super().__init__(
+            task_type=self._SQLITE_TASK_TYPE,
+            name=name,
+            query_template=query_template,
+            inputs=inputs,
+            metadata=metadata,
+            outputs=outputs,
+            task_config=task_config,
+            *args,
+            **kwargs,
+        )
+
+    def execute(self, **kwargs) -> typing.Any:
+        with tempfile.TemporaryDirectory() as temp_dir:
+            if self.task_config is not None and self.task_config.sqlite_db_mode == SQLite3Config.Mode.STATIC:
+                ctx = FlyteContext.current_context()
+                file_ext = os.path.basename(self.task_config.static_file_uri)
+                local_path = os.path.join(temp_dir, file_ext)
+                ctx.file_access.download(self.task_config.static_file_uri, local_path)
+                if self.task_config.compressed:
+                    local_path = unarchive_file(local_path, temp_dir)
+            else:
+                if self._SQLITE_INPUT_FILE not in kwargs:
+                    raise AssertionError("``sqlite: FlyteFile`` is a required argument")
+                in_file = kwargs[self._SQLITE_INPUT_FILE]
+                in_file.download()
+                local_path = in_file.path
+                del kwargs[self._SQLITE_INPUT_FILE]
+                if self.task_config.compressed:
+                    local_path = unarchive_file(local_path, temp_dir)
+
+            print(f"Connecting to db {local_path}")
+            with contextlib.closing(sqlite3.connect(local_path)) as con:
+                df = pd.read_sql_query(self.get_query(**kwargs), con)
+                return df
---
+++ tests/flytekit/unit/taskplugins/sqlite3/test_task.py
@@ -0,0 +1,52 @@
+import pytest
+
+from flytekit import kwtypes, task, workflow
+from flytekit.plugins import pandas
+from flytekit.taskplugins.sqlite3.task import SQLite3Config, SQLite3Task
+
+# https://www.sqlitetutorial.net/sqlite-sample-database/
+from flytekit.types.file import FlyteFile
+
+EXAMPLE_DB = "https://cdn.sqlitetutorial.net/wp-content/uploads/2018/03/chinook.zip"
+
+
+def test_task_static():
+    tk = SQLite3Task(
+        "test",
+        query_template="select * from tracks",
+        task_config=SQLite3Config(
+            sqlite_db_mode=SQLite3Config.Mode.STATIC, static_file_uri=EXAMPLE_DB, compressed=True,
+        ),
+    )
+    df = tk()
+    assert df is not None
+
+
+def test_task_dynamic():
+    tk = SQLite3Task("test", query_template="select * from tracks", task_config=SQLite3Config(compressed=True))
+    with pytest.raises(AssertionError):
+        tk()
+
+    df = tk(sqlite=FlyteFile.from_path(EXAMPLE_DB))
+    print(df)
+
+
+def test_workflow():
+    @task
+    def my_task(df: pandas.DataFrame) -> int:
+        return len(df[df.columns[0]])
+
+    @workflow
+    def wf(limit: int) -> int:
+        return my_task(
+            df=SQLite3Task(
+                "test",
+                query_template="select * from tracks limit {{.inputs.limit}}",
+                inputs=kwtypes(limit=int),
+                task_config=SQLite3Config(
+                    sqlite_db_mode=SQLite3Config.Mode.STATIC, static_file_uri=EXAMPLE_DB, compressed=True,
+                ),
+            )(limit=limit)
+        )
+
+    assert wf(limit=5) == 5
