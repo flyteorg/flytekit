1402f0c5ec2d7fdc33d8d6090aabb1ee8947bb63
Katrina Rogan <katrogan9@gmail.com>
2021-01-15 17:52:05 -0800

    comments

--- flytekit/remote/execution.py
+++ flytekit/remote/execution.py
@@ -1,21 +1,24 @@
 from dataclasses import dataclass
-from typing import Any, Dict, Union, List
+from typing import Any, Dict, List, Union
 
 from flytekit import ContainerTask
+from flytekit.annotated import context_manager as flyte_context
 from flytekit.annotated.context_manager import SerializationSettings
 from flytekit.annotated.promise import translate_inputs_to_literals
-from flytekit.annotated.reference_entity import ReferenceEntity
-from flytekit.common.core.identifier import WorkflowExecutionIdentifier
+from flytekit.annotated.reference_entity import ReferenceEntity, TaskReference
+from flytekit.clients.friendly import SynchronousFlyteClient
+from flytekit.common.core.identifier import WorkflowExecutionIdentifier, Identifier
 from flytekit.common.exceptions import user as user_exceptions
 from flytekit.common.translator import get_serializable
+from flytekit.common.types.helpers import pack_python_std_map_to_literal_map, get_sdk_type_from_literal_type
 from flytekit.common.workflow_execution import SdkWorkflowExecution
-from flytekit.models.common import Notification, Annotations, Labels, AuthRole
-from flytekit.models.execution import ExecutionSpec, ExecutionMetadata, NotificationList
-from flytekit.clients.friendly import SynchronousFlyteClient
-from flytekit.models.literals import LiteralMap
-from flytekit.annotated import context_manager as flyte_context
-from flytekit.models.task import TaskSpec
 from flytekit.configuration import auth as auth_config
+from flytekit.configuration import platform as platform_config
+from flytekit.models.common import Annotations, AuthRole, Labels, Notification
+from flytekit.models.core.identifier import ResourceType
+from flytekit.models.execution import ExecutionMetadata, ExecutionSpec, NotificationList
+from flytekit.models.literals import LiteralMap
+from flytekit.models.task import TaskSpec
 
 
 @dataclass
@@ -31,14 +34,17 @@ class ExecutionParameters(object):
 
 
 def execute(
-    entity: Union[ReferenceEntity, ContainerTask],
-    client: SynchronousFlyteClient,
+    entity: Union[Identifier, ContainerTask],
     project: str,
     domain: str,
     inputs: Dict[str, Any] = None,
+    client: SynchronousFlyteClient = None,
     exec_parameters: ExecutionParameters = None,
     serialization_settings: SerializationSettings = None,
 ) -> SdkWorkflowExecution:
+
+    if client is None:
+        client = SynchronousFlyteClient(platform_config.URL.get, insecure=platform_config.INSECURE.get())
 
     # If serialization settings are supplied, first serialize and register the entity before attempting to launch
     # an execution with it.
@@ -51,55 +57,66 @@ def execute(
             reference_identifier = sdk_task.id
             try:
                 client.create_task(task_identifer=sdk_task.id, task_spec=TaskSpec(sdk_task))
+                literals = translate_inputs_to_literals(
+                    ctx, input_kwargs=inputs, interface=entity.interface, native_input_types=entity.get_input_types()
+                )
+
+                input_literal_map = LiteralMap(literals=literals)
             except user_exceptions.FlyteEntityAlreadyExistsException:
                 pass
     else:
-        reference_identifier = entity.id
+        reference_identifier = entity
+        if entity.resource_type == ResourceType.TASK:
+            task_model = client.get_task(entity)
+            input_literal_map = pack_python_std_map_to_literal_map(
+                inputs,
+                {
+                    k: get_sdk_type_from_literal_type(v.type)
+                    for k, v in task_model.closure.compiled_task.template.interface.inputs.items()
+                },
+            )
+        elif entity.resource_type == ResourceType.LAUNCH_PLAN:
+            lp_model = client.get_launch_plan(entity)
+            # TODO: fetch reference workflow and use that interface.
 
-    with flyte_context.FlyteContext.current_context():
-        literals = translate_inputs_to_literals(
-            ctx, input_kwargs=inputs, interface=entity.interface, native_input_types=entity.get_input_types()
+    exec_parameters = exec_parameters if exec_parameters is not None else ExecutionParameters()
+    disable_all = exec_parameters.notifications == []
+    if disable_all:
+        notifications = None
+    else:
+        notifications = NotificationList(exec_parameters.notifications or [])
+        disable_all = None
+
+    if exec_parameters.auth_role is not None:
+        auth_role = exec_parameters.auth_role
+    else:
+        assumable_iam_role = auth_config.ASSUMABLE_IAM_ROLE.get()
+        kubernetes_service_account = auth_config.KUBERNETES_SERVICE_ACCOUNT.get()
+        auth_role = AuthRole(
+            assumable_iam_role=assumable_iam_role, kubernetes_service_account=kubernetes_service_account
         )
-        input_literal_map = LiteralMap(literals=literals)
 
-        exec_parameters = exec_parameters if exec_parameters is not None else ExecutionParameters()
-        disable_all = exec_parameters.notifications == []
-        if disable_all:
-            notifications = None
-        else:
-            notifications = NotificationList(exec_parameters.notifications or [])
-            disable_all = None
-
-        if exec_parameters.auth_role is not None:
-            auth_role = exec_parameters.auth_role
-        else:
-            assumable_iam_role = auth_config.ASSUMABLE_IAM_ROLE.get()
-            kubernetes_service_account = auth_config.KUBERNETES_SERVICE_ACCOUNT.get()
-            auth_role = AuthRole(
-                assumable_iam_role=assumable_iam_role, kubernetes_service_account=kubernetes_service_account
-            )
-
-        try:
-            complete_exec_id = client.create_execution(
-                project,
-                domain,
-                exec_parameters.name,
-                ExecutionSpec(
-                    reference_identifier,
-                    # TODO: Detect nesting
-                    # Note: the principal can be derived from the authenticated user context.
-                    ExecutionMetadata(
-                        ExecutionMetadata.ExecutionMode.MANUAL, principal=exec_parameters.principal, nesting=0
-                    ),
-                    notifications=notifications,
-                    disable_all=disable_all,
-                    labels=exec_parameters.labels,
-                    annotations=exec_parameters.annotations,
-                    auth_role=auth_role,
+    try:
+        complete_exec_id = client.create_execution(
+            project,
+            domain,
+            exec_parameters.name,
+            ExecutionSpec(
+                reference_identifier,
+                # TODO: Detect nesting
+                # Note: the principal can be derived from the authenticated user context.
+                ExecutionMetadata(
+                    ExecutionMetadata.ExecutionMode.MANUAL, principal=exec_parameters.principal, nesting=0
                 ),
-                input_literal_map,
-            )
-        except user_exceptions.FlyteEntityAlreadyExistsException:
-            complete_exec_id = WorkflowExecutionIdentifier(project, domain, exec_parameters.name)
-        execution = client.get_execution(complete_exec_id)
-        return SdkWorkflowExecution.promote_from_model(execution)
+                notifications=notifications,
+                disable_all=disable_all,
+                labels=exec_parameters.labels,
+                annotations=exec_parameters.annotations,
+                auth_role=auth_role,
+            ),
+            input_literal_map,
+        )
+    except user_exceptions.FlyteEntityAlreadyExistsException:
+        complete_exec_id = WorkflowExecutionIdentifier(project, domain, exec_parameters.name)
+    execution = client.get_execution(complete_exec_id)
+    return SdkWorkflowExecution.promote_from_model(execution)
