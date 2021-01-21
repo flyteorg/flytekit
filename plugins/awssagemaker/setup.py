f86f18431a99cbbe3ad2d71aeadb3cb602a038cd
Katrina Rogan <katrogan9@gmail.com>
2021-01-21 12:57:36 -0800

    copy over install scripts

--- plugins/awssagemaker/setup.py
+++ plugins/awssagemaker/setup.py
@@ -29,5 +29,4 @@ setup(
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    scripts=["scripts/flytekit_sagemaker_runner.py"],
 )
