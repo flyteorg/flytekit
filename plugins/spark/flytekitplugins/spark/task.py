d47833094621da65531d384d5419c41a57f923aa
Katrina Rogan <katrogan9@gmail.com>
2021-01-22 11:22:06 -0800

    Use entrypoint path in spark custom

--- plugins/spark/flytekitplugins/spark/task.py
+++ plugins/spark/flytekitplugins/spark/task.py
@@ -76,11 +76,15 @@ class PysparkFunctionTask(PythonFunctionTask[Spark]):
         )
 
     def get_custom(self, settings: SerializationSettings) -> Dict[str, Any]:
+
         from flytekit.bin import entrypoint as _entrypoint
 
         spark_exec_path = os.path.abspath(_entrypoint.__file__)
         if spark_exec_path.endswith(".pyc"):
             spark_exec_path = spark_exec_path[:-1]
+
+        if settings.entrypoint_settings.path is not None:
+            spark_exec_path = settings.entrypoint_settings.path
 
         job = _task_model.SparkJob(
             spark_conf=self.task_config.spark_conf,
