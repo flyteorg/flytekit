5bbe30589d4f246828bf993792437e65e6c8ea23
wild-endeavor <wild-endeavor@users.noreply.github.com>
2021-01-25 18:39:21 -0800

    shellcheck

--- plugins/run_all_plugins.sh
+++ plugins/run_all_plugins.sh
@@ -7,14 +7,12 @@
 # of plugins and run command supplied if a setup.py exists in that directory (implying a plugin).
 
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 shopt -s dotglob
-find * -prune -type d | while IFS= read -r d; do
+find ./* -prune -type d | while IFS= read -r d; do
     if [ -f "$d/setup.py" ]; then
         echo "Running your command in $d..."
-        cd $d;
-        $@;
-        cd -;
+        cd $d || exit;
+        "$@;"
+        cd - || exit;
     fi
 done
