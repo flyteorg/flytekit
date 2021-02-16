80835d1abd9c62e52f17f753704c501d4c653179
Miguel Toledo <mtoledo@lyft.com>
2021-02-16 12:50:48 -0500

    Fix executor path and application file parameters for Spark Tasks (#381)
    * rm /bin/python
    
    * add /bin/python3 to in container env
    
    * introduce separate variable for python interpreter
    
    * temporarily revert back to old way of flytekit_virtual_env path
    
    * use dirname not full file
    
    * rm comment and unnecessary code
    
    * upd serialize all comment

--- flytekit/clis/sdk_in_container/serialize.py
+++ flytekit/clis/sdk_in_container/serialize.py
@@ -8,6 +8,7 @@ from typing import List
 
 import click
 
+import flytekit as _flytekit
 from flytekit.clis.sdk_in_container.constants import CTX_PACKAGES
 from flytekit.common import utils as _utils
 from flytekit.common.core import identifier as _identifier
@@ -43,6 +44,7 @@ CTX_IMAGE = "image"
 CTX_LOCAL_SRC_ROOT = "local_source_root"
 CTX_CONFIG_FILE_LOC = "config_file_loc"
 CTX_FLYTEKIT_VIRTUALENV_ROOT = "flytekit_virtualenv_root"
+CTX_PYTHON_INTERPRETER = "python_interpreter"
 
 
 class SerializationMode(_Enum):
@@ -95,25 +97,27 @@ def serialize_all(
     image: str = None,
     config_path: str = None,
     flytekit_virtualenv_root: str = None,
+    python_interpreter: str = None,
 ):
     """
-    In order to register, we have to comply with Admin's endpoints. Those endpoints take the following objects. These
-    flyteidl.admin.launch_plan_pb2.LaunchPlanSpec
-    flyteidl.admin.workflow_pb2.WorkflowSpec
-    flyteidl.admin.task_pb2.TaskSpec
+      This function will write to the folder specified the following protobuf types ::
+          flyteidl.admin.launch_plan_pb2.LaunchPlan
+          flyteidl.admin.workflow_pb2.WorkflowSpec
+          flyteidl.admin.task_pb2.TaskSpec
 
-    However, if we were to merely call .to_flyte_idl() on all the discovered entities, what we would get are:
-    flyteidl.admin.launch_plan_pb2.LaunchPlanSpec
-    flyteidl.core.workflow_pb2.WorkflowTemplate
-    flyteidl.core.tasks_pb2.TaskTemplate
+      These can be inspected by calling (in the launch plan case) ::
+          flyte-cli parse-proto -f filename.pb -p flyteidl.admin.launch_plan_pb2.LaunchPlan
 
-    For Workflows and Tasks therefore, there is special logic in the serialize function that translates these objects.
-
-    :param list[Text] pkgs:
-    :param Text folder:
-
-    :return:
-    """
+      See :py:class:`flytekit.models.core.identifier.ResourceType` to match the trailing index in the file name with the
+      entity type.
+      :param pkgs: Dot-delimited Python packages/subpackages to look into for serialization.
+      :param local_source_root: Where to start looking for the code.
+      :param folder: Where to write the output protobuf files
+      :param mode: Regular vs fast
+      :param image: The fully qualified and versioned default image to use
+      :param config_path: Path to the config file, if any, to be used during serialization
+      :param flytekit_virtualenv_root: The full path of the virtual env in the container.
+      """
 
     # m = module (i.e. python file)
     # k = value of dir(m), type str
@@ -132,6 +136,7 @@ def serialize_all(
         image_config=flyte_context.get_image_config(img_name=image),
         env=env,
         flytekit_virtualenv_root=flytekit_virtualenv_root,
+        python_interpreter=python_interpreter,
         entrypoint_settings=flyte_context.EntrypointSettings(
             path=_os.path.join(flytekit_virtualenv_root, _DEFAULT_FLYTEKIT_RELATIVE_ENTRYPOINT_LOC)
         ),
@@ -260,10 +265,18 @@ def serialize(ctx, image, local_source_root, in_container_config_path, in_contai
             if in_container_virtualenv_root is not None
             else _DEFAULT_FLYTEKIT_VIRTUALENV_ROOT
         )
+
+        # append python3
+        ctx.obj[CTX_PYTHON_INTERPRETER] = ctx.obj[CTX_FLYTEKIT_VIRTUALENV_ROOT] + "/bin/python3"
     else:
         # For in container serialize we make sure to never accept an override the entrypoint path and determine it here
         # instead.
-        ctx.obj[CTX_FLYTEKIT_VIRTUALENV_ROOT] = sys.executable
+        entrypoint_path = _os.path.abspath(_flytekit.__file__)
+        if entrypoint_path.endswith(".pyc"):
+            entrypoint_path = entrypoint_path[:-1]
+
+        ctx.obj[CTX_FLYTEKIT_VIRTUALENV_ROOT] = _os.path.dirname(entrypoint_path)
+        ctx.obj[CTX_PYTHON_INTERPRETER] = sys.executable
 
 
 @click.command("tasks")
