51f822c0075f5fb417b00216f329037a40958fa6
wild-endeavor <wild-endeavor@users.noreply.github.com>
2021-02-17 09:07:20 -0800

    wip

--- flytekit/core/workflow.py
+++ flytekit/core/workflow.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import inspect
-import typing
 from dataclasses import dataclass
 from enum import Enum
 from typing import Any, Callable, Dict, List, Optional, Tuple, Type, Union
