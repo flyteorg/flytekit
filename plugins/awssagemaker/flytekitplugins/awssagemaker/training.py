2e4e1f8186a3fc0b569d4f4618890381fba4b0ac
wild-endeavor <wild-endeavor@users.noreply.github.com>
2021-02-17 09:27:46 -0800

    nit

--- plugins/awssagemaker/flytekitplugins/awssagemaker/training.py
+++ plugins/awssagemaker/flytekitplugins/awssagemaker/training.py
@@ -7,15 +7,8 @@ from flytekitplugins.awssagemaker.distributed_training import DistributedTrainin
 from google.protobuf.json_format import MessageToDict
 
 import flytekit
-from flytekit import FlyteContext, PythonFunctionTask, kwtypes, ExecutionParameters
-from flytekit.extend import (
-    ExecutionState,
-    IgnoreOutputs,
-    Interface,
-    PythonTask,
-    SerializationSettings,
-    TaskPlugins,
-)
+from flytekit import ExecutionParameters, FlyteContext, PythonFunctionTask, kwtypes
+from flytekit.extend import ExecutionState, IgnoreOutputs, Interface, PythonTask, SerializationSettings, TaskPlugins
 from flytekit.models.sagemaker import training_job as _training_job_models
 from flytekit.types.directory.types import FlyteDirectory
 from flytekit.types.file import FlyteFile
