1bbecb043e5b1e0516f7a5b0f6f67f4402af627b
wild-endeavor <wild-endeavor@users.noreply.github.com>
2021-02-26 14:11:18 -0800

    wip
    Signed-off-by: wild-endeavor <wild-endeavor@users.noreply.github.com>

--- tests/flytekit/unit/common_tests/tasks/ttt.py
+++
@@ -1,9 +0,0 @@
-from flytekit.common.types import primitives
-from flytekit.sdk.tasks import inputs, outputs, python_task
-
-
-@inputs(a=primitives.Integer)
-@outputs(b=primitives.Integer)
-@python_task
-def my_task(wf_params, a, b):
-    b.set(a + 1)
