1e8c8f583d8af399bfd01feba19d77f4479fd133
wild-endeavor <wild-endeavor@users.noreply.github.com>
2021-03-05 09:40:37 -0800

    fixing test

--- tests/flytekit/unit/extras/sqlite3/test_sql_tracker.py
+++ tests/flytekit/unit/extras/sqlite3/test_sql_tracker.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from flytekit.common.translator import get_serializable
 from flytekit.core import context_manager
 from flytekit.core.context_manager import Image, ImageConfig
@@ -17,11 +19,10 @@ def test_sql_command():
         env=None,
         image_config=ImageConfig(default_image=default_img, images=[default_img]),
     )
-    srz_t = get_serializable(serialization_settings, not_tk)
-    assert srz_t.container.args[-8:] == [
+    srz_t = get_serializable(OrderedDict(), serialization_settings, not_tk)
+    assert srz_t.container.args[-7:] == [
         "--resolver",
         "flytekit.core.python_auto_container.default_task_resolver",
-        "--resolver-args",
         "--",
         "task-module",
         "tests.flytekit.unit.extras.sqlite3.test_task",
