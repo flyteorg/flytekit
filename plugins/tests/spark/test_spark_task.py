d4e4f0cd9ba84c600c560e36fc5ee3ed0605c957
Rubén Barragán <RubenBarragan@users.noreply.github.com>
2021-04-07 14:18:30 -0700

    Propagate required envs to executors and fix bug to set configs in Spark task (#444)

--- plugins/tests/spark/test_spark_task.py
+++ plugins/tests/spark/test_spark_task.py
@@ -1,7 +1,9 @@
 from flytekitplugins.spark import Spark
+from flytekitplugins.spark.task import new_spark_session
 
 import flytekit
 from flytekit import task
+from flytekit.common.tasks.sdk_runnable import ExecutionParameters
 from flytekit.extend import Image, ImageConfig, SerializationSettings
 
 
@@ -26,3 +28,28 @@ def test_spark_task():
 
     retrieved_settings = my_spark.get_custom(settings)
     assert retrieved_settings["sparkConf"] == {"spark": "1"}
+
+    pb = ExecutionParameters.new_builder()
+    pb.working_dir = "/tmp"
+    pb.execution_id = "ex:local:local:local"
+    p = pb.build()
+    new_p = my_spark.pre_execute(p)
+    assert new_p is not None
+    assert new_p.has_attr("SPARK_SESSION")
+
+    assert my_spark.sess is not None
+    configs = my_spark.sess.sparkContext.getConf().getAll()
+    assert ("spark", "1") in configs
+    assert ("spark.app.name", "FlyteSpark: ex:local:local:local") in configs
+
+
+def test_new_spark_session():
+    name = "SessionName"
+    spark_conf = {"spark1": "1", "spark2": "2"}
+    new_sess = new_spark_session(name, spark_conf)
+    configs = new_sess.sparkContext.getConf().getAll()
+    assert new_sess is not None
+    assert ("spark.driver.bindAddress", "127.0.0.1") in configs
+    assert ("spark.master", "local[*]") in configs
+    assert ("spark1", "1") in configs
+    assert ("spark2", "2") in configs
