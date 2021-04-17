f31d51c9f1510a509fae5663944fc42b1bec9aa2
Max Hoffman <max@dolthub.com>
2021-04-16 20:45:04 -0700

    Fix dependencies
    Signed-off-by: Max Hoffman <max@dolthub.com>

--- setup.py
+++ setup.py
@@ -83,7 +83,6 @@ setup(
         "natsort>=7.0.1",
         "dirhash>=0.2.1",
         "docker-image-py>=0.1.10",
-        "sqlalchemy>=1.4.7",
     ],
     extras_require=extras_require,
     scripts=[
