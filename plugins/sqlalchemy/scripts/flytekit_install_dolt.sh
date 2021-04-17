49a028738afe986c1dafc878719811ee703cd147
Max Hoffman <max@dolthub.com>
2021-04-16 21:15:15 -0700

    Move dolt install
    Signed-off-by: Max Hoffman <max@dolthub.com>

--- plugins/sqlalchemy/scripts/flytekit_install_dolt.sh
+++ plugins/sqlalchemy/scripts/flytekit_install_dolt.sh
@@ -9,4 +9,8 @@ set -eox pipefail
 
 apt-get update -y \
     && apt-get install curl \
-    && sudo bash -c 'curl -L https://github.com/dolthub/dolt/releases/latest/download/install.sh | sudo bash'
+    && sudo bash -c 'curl -L https://github.com/dolthub/dolt/releases/latest/download/install.sh | sudo bash' \
+    && dolt config --global --add user.email bojack@horseman.com \
+    && dolt config --global --add user.name "Bojack Horseman" \
+    && dolt config --global --add metrics.host eventsapi.awsdev.ld-corp.com \
+    && dolt config --global --add metrics.port 443
