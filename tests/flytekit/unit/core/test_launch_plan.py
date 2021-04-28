0f3fd01f5a65bbcfc797e17018767bf3ea79b003
Samhita Alla <aallasamhita@gmail.com>
2021-04-28 12:09:36 +0530

    Added Unit Test
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- tests/flytekit/unit/core/test_launch_plan.py
+++ tests/flytekit/unit/core/test_launch_plan.py
@@ -2,9 +2,12 @@ import typing
 
 import pytest
 
-from flytekit.core import launch_plan
+from flytekit.core import launch_plan, notification
+from flytekit.core.schedule import CronSchedule
 from flytekit.core.task import task
 from flytekit.core.workflow import workflow
+from flytekit.models.common import AuthRole
+from flytekit.models.core import execution as _execution_model
 
 
 def test_lp():
@@ -12,10 +15,6 @@ def test_lp():
     def t1(a: int) -> typing.NamedTuple("OutputsBC", t1_int_output=int, c=str):
         a = a + 2
         return a, "world-" + str(a)
-
-    @task
-    def t2(a: str, b: str) -> str:
-        return b + a
 
     @workflow
     def wf(a: int) -> (str, str):
@@ -37,3 +36,62 @@ def test_lp():
 
     lp_with_defaults = launch_plan.LaunchPlan.create("get_or_create2", wf, default_inputs={"a": 3})
     assert lp_with_defaults.parameters.parameters["a"].default.scalar.primitive.integer == 3
+
+
+def test_lp_all_parameters():
+    @task
+    def t1(a: int) -> typing.NamedTuple("OutputsBC", t1_int_output=int, c=str):
+        a = a + 2
+        return a, "world-" + str(a)
+
+    @task
+    def t2(a: str, b: str, c: str) -> str:
+        return b + a + c
+
+    @workflow
+    def wf(a: int, c: str) -> str:
+        x, y = t1(a=a)
+        u = t2(a=x, b=y, c=c)
+        return u
+
+    obj = CronSchedule("* * ? * * *", kickoff_time_input_arg="abc")
+    obj1 = CronSchedule("10 * ? * * *", kickoff_time_input_arg="abc")
+    slack_notif = notification.Slack(
+        phases=[_execution_model.WorkflowExecutionPhase.SUCCEEDED], recipients_email=["my-team@slack.com"]
+    )
+    auth_role_model = AuthRole(assumable_iam_role="my:iam:role")
+
+    lp = launch_plan.LaunchPlan.get_or_create(
+        workflow=wf,
+        name="get_or_create",
+        default_inputs={"a": 3},
+        fixed_inputs={"c": "4"},
+        schedule=obj,
+        notifications=slack_notif,
+        auth_role=auth_role_model,
+    )
+    lp2 = launch_plan.LaunchPlan.get_or_create(
+        workflow=wf,
+        name="get_or_create",
+        default_inputs={"a": 3},
+        fixed_inputs={"c": "4"},
+        schedule=obj,
+        notifications=slack_notif,
+        auth_role=auth_role_model,
+    )
+
+    assert lp is lp2
+
+    # Check for assertion error when a different scheduler is used
+    lp3 = launch_plan.LaunchPlan.get_or_create(
+        workflow=wf,
+        name="get_or_create",
+        default_inputs={"a": 3},
+        fixed_inputs={"c": "4"},
+        schedule=obj1,
+        notifications=slack_notif,
+        auth_role=auth_role_model,
+    )
+
+    with pytest.raises(AssertionError):
+        assert lp is lp3
