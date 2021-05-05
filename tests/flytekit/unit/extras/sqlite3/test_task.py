86701cbc1db71b5020c8abd35a30e924a037c193
wild-endeavor <wild-endeavor@users.noreply.github.com>
2021-05-05 14:39:55 -0700

    wip
    Signed-off-by: wild-endeavor <wild-endeavor@users.noreply.github.com>

--- tests/flytekit/unit/extras/sqlite3/test_task.py
+++ tests/flytekit/unit/extras/sqlite3/test_task.py
@@ -8,23 +8,6 @@ from flytekit.types.schema import FlyteSchema
 
 EXAMPLE_DB = "https://cdn.sqlitetutorial.net/wp-content/uploads/2018/03/chinook.zip"
 
-# This task belongs to test_task_static but is intentionally here to help test tracking
-tk = SQLite3Task(
-    "test",
-    query_template="select * from tracks",
-    task_config=SQLite3Config(
-        uri=EXAMPLE_DB,
-        compressed=True,
-    ),
-)
-
-
-def test_task_static():
-    assert tk.output_columns is None
-
-    df = tk()
-    assert df is not None
-
 
 def test_task_schema():
     sql_task = SQLite3Task(
@@ -63,3 +46,36 @@ def test_workflow():
         return my_task(df=sql_task(limit=limit))
 
     assert wf(limit=5) == 5
+
+
+def test_task_serialization():
+    sql_task = SQLite3Task(
+        "test",
+        query_template="select TrackId, Name from tracks limit {{.inputs.limit}}",
+        inputs=kwtypes(limit=int),
+        output_schema_type=FlyteSchema[kwtypes(TrackId=int, Name=str)],
+        task_config=SQLite3Config(
+            uri=EXAMPLE_DB,
+            compressed=True,
+        ),
+    )
+
+    tt = sql_task.serialize_to_model(sql_task.SERIALIZE_SETTINGS)
+
+    assert tt.container.args == [
+        "pyflyte-execute",
+        "--inputs",
+        "{{.input}}",
+        "--output-prefix",
+        "{{.outputPrefix}}",
+        "--raw-output-data-prefix",
+        "{{.rawOutputDataPrefix}}",
+        "--resolver",
+        "flytekit.core.python_customized_container_task.default_task_template_resolver",
+        "--",
+        "{{.taskTemplatePath}}",
+        "flytekit.extras.sqlite3.task.SQLite3TaskExecutor",
+    ]
+
+    assert tt.custom["query_template"] == "select TrackId, Name from tracks limit {{.inputs.limit}}"
+    assert tt.container.image != ""
