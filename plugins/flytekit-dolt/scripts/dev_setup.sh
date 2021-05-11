cb096192d803c13076963c7f818f18f2ee77a7e9
Max Hoffman <max@dolthub.com>
2021-05-11 15:08:06 -0700

    last of the bash linting errors
    Signed-off-by: Max Hoffman <max@dolthub.com>

--- plugins/flytekit-dolt/scripts/dev_setup.sh
+++ plugins/flytekit-dolt/scripts/dev_setup.sh
@@ -5,10 +5,10 @@ set -eox pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # hacky install.sh that doesn't use sudo and installs bins to INSTALL_PATH
-$SCRIPT_DIR/install.sh
+"$SCRIPT_DIR"/install.sh
 
 # set configurations
-$INSTALL_PATH/dolt config --global --add user.name Bojack Horseman
-$INSTALL_PATH/dolt config --global --add user.email bojack@horseman.com
-$INSTALL_PATH/dolt config --global --add metrics.host eventsapi.awsdev.ld-corp.com
-$INSTALL_PATH/dolt config --global --add  metrics.port 443
+"$INSTALL_PATH"/dolt config --global --add user.name Bojack Horseman
+"$INSTALL_PATH"/dolt config --global --add user.email bojack@horseman.com
+"$INSTALL_PATH"dolt config --global --add metrics.host eventsapi.awsdev.ld-corp.com
+"$INSTALL_PATH"/dolt config --global --add  metrics.port 443
