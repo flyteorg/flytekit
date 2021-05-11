2236ddd54cd43821ccc70295f3a80f11e4c2e5d9
Max Hoffman <max@dolthub.com>
2021-05-11 15:22:38 -0700

    Subtle config bug in CI that doesn't show up locally
    Signed-off-by: Max Hoffman <max@dolthub.com>

--- plugins/flytekit-dolt/scripts/dev_setup.sh
+++ plugins/flytekit-dolt/scripts/dev_setup.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 "$SCRIPT_DIR"/install.sh
 
 # set configurations
-"$INSTALL_PATH"/dolt config --global --add user.name Bojack Horseman
+"$INSTALL_PATH"/dolt config --global --add user.name "Bojack Horseman"
 "$INSTALL_PATH"/dolt config --global --add user.email bojack@horseman.com
 "$INSTALL_PATH"dolt config --global --add metrics.host eventsapi.awsdev.ld-corp.com
 "$INSTALL_PATH"/dolt config --global --add  metrics.port 443
