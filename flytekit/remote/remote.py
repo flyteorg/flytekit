1fc1731704e04c52ebe9eb308d92b6e308d359ef
cosmicBboy <niels.bantilan@gmail.com>
2021-06-29 16:44:06 -0400

    implement fetch_* methods in FlyteRemote
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- flytekit/remote/remote.py
+++ flytekit/remote/remote.py
@@ -1,33 +1,112 @@
 """Module defining main Flyte backend entrypoint."""
 
-from functools import singledispatchmethod
+import typing
 
+try:
+    from functools import singledispatchmethod
+except ImportError:
+    from singledispatchmethod import singledispatchmethod
+
+from flytekit.common.exceptions import scopes as exception_scopes
+from flytekit.common.exceptions import user as user_exceptions
 from flytekit.core.launch_plan import LaunchPlan
 from flytekit.core.python_function_task import PythonFunctionTask
 from flytekit.core.workflow import PythonFunctionWorkflow
+from flytekit.engines.flyte.engine import get_client
+from flytekit.models.admin.common import Sort
+from flytekit.models.common import NamedEntityIdentifier
+from flytekit.models.core.identifier import ResourceType
+from flytekit.remote.identifier import Identifier, WorkflowExecutionIdentifier
 from flytekit.remote.launch_plan import FlyteLaunchPlan
 from flytekit.remote.tasks.task import FlyteTask
 from flytekit.remote.workflow import FlyteWorkflow
 from flytekit.remote.workflow_execution import FlyteWorkflowExecution
 
 
+def _get_latest_version(client_method: typing.Callable, project: str, domain: str, name: str):
+    named_entity = NamedEntityIdentifier(project, domain, name)
+    entity_list, _ = client_method(
+        named_entity,
+        limit=1,
+        sort_by=Sort("created_at", Sort.Direction.DESCENDING),
+    )
+    admin_entity = None if not entity_list else entity_list[0]
+    if not admin_entity:
+        raise user_exceptions.FlyteEntityNotExistException("Named entity {} not found".format(named_entity))
+    return admin_entity.id.version
+
+
 class FlyteRemote(object):
-    """Entrypoint class for accessing Flyte remote backend."""
+    """Main entrypoint for programmatically accessing Flyte remote backend."""
 
     def __init__(self):
+        # TODO: figure out what config/metadata needs to be loaded into the FlyteRemote object at initialization
         pass
 
-    def fetch_task(self, project, domain, name, version=None) -> FlyteTask:
-        pass
+    @exception_scopes.system_entry_point
+    def fetch_task(self, project: str, domain: str, name: str, version: str = None) -> FlyteTask:
+        client = get_client()
+        task_id = Identifier(
+            ResourceType.TASK,
+            project,
+            domain,
+            name,
+            version if version is not None else _get_latest_version(client.list_tasks_paginated, project, domain, name),
+        )
+        admin_task = client.get_task(task_id)
+        flyte_task = FlyteTask.promote_from_model(admin_task.closure.compiled_task.template)
+        flyte_task._id = task_id
+        return flyte_task
 
-    def fetch_workflow(self, project, domain, name, version=None) -> FlyteWorkflow:
-        pass
+    @exception_scopes.system_entry_point
+    def fetch_workflow(self, project: str, domain: str, name: str, version: str = None) -> FlyteWorkflow:
+        client = get_client()
+        workflow_id = Identifier(
+            ResourceType.WORKFLOW,
+            project,
+            domain,
+            name,
+            version
+            if version is not None
+            else _get_latest_version(client.list_workflows_paginated, project, domain, name),
+        )
+        admin_workflow = client.get_workflow(workflow_id)
+        compiled_wf = admin_workflow.closure.compiled_workflow
+        flyte_workflow = FlyteWorkflow.promote_from_model(
+            base_model=compiled_wf.primary.template,
+            sub_workflows={sw.template.id: sw.template for sw in compiled_wf.sub_workflows},
+            tasks={t.template.id: t.template for t in compiled_wf.tasks},
+        )
+        flyte_workflow._id = workflow_id
+        return flyte_workflow
 
-    def fetch_launchplan(self, project, domain, name, version=None) -> FlyteLaunchPlan:
-        pass
+    @exception_scopes.system_entry_point
+    def fetch_launch_plan(self, project: str, domain: str, name: str, version: str = None) -> FlyteLaunchPlan:
+        client = get_client()
+        launch_plan_id = Identifier(
+            ResourceType.LAUNCH_PLAN,
+            project,
+            domain,
+            name,
+            version
+            if version is not None
+            else _get_latest_version(client.list_launch_plans_paginated, project, domain, name),
+        )
 
-    def fetch_workflow_execution(self, project, domain, name) -> FlyteWorkflowExecution:
-        pass
+        admin_launch_plan = client.get_launch_plan(launch_plan_id)
+        flyte_launch_plan = FlyteLaunchPlan.promote_from_model(admin_launch_plan.spec)
+        flyte_launch_plan._id = launch_plan_id
+
+        wf_id = flyte_launch_plan.workflow_id
+        workflow = self.fetch_workflow(wf_id.project, wf_id.domain, wf_id.name, wf_id.version)
+        flyte_launch_plan._interface = workflow.interface
+        return flyte_launch_plan
+
+    @exception_scopes.system_entry_point
+    def fetch_workflow_execution(self, project: str, domain: str, name: str) -> FlyteWorkflowExecution:
+        return FlyteWorkflowExecution.promote_from_model(
+            get_client().get_execution(WorkflowExecutionIdentifier(project, domain, name))
+        )
 
     #####################
     # Register Entities #
---
+++ tests/flytekit/unit/remote/test_remote.py
@@ -0,0 +1,81 @@
+import pytest
+from mock import MagicMock, patch
+
+from flytekit.models.admin.workflow import Workflow
+from flytekit.models.core.identifier import Identifier, ResourceType, WorkflowExecutionIdentifier
+from flytekit.models.execution import Execution
+from flytekit.models.launch_plan import LaunchPlan
+from flytekit.models.task import Task
+from flytekit.remote.remote import FlyteRemote
+
+
+@patch("flytekit.engines.flyte.engine._FlyteClientManager")
+@patch("flytekit.configuration.platform.URL")
+@pytest.mark.parametrize(
+    "entity_cls,resource_type",
+    [
+        [Workflow, ResourceType.WORKFLOW],
+        [Task, ResourceType.TASK],
+        [LaunchPlan, ResourceType.LAUNCH_PLAN],
+    ],
+)
+def test_remote_fetch_entities_task_workflow_launchplan(
+    mock_url, mock_client_manager, entity_cls, resource_type,
+):
+    mock_url.get.return_value = "localhost"
+    admin_entities = [
+        entity_cls(
+            Identifier(resource_type, "p1", "d1", "n1", version),
+            *([MagicMock()] if resource_type != ResourceType.LAUNCH_PLAN else [MagicMock(), MagicMock()]),
+        )
+        for version in ["latest", "old"]
+    ]
+
+    client_method = {
+        ResourceType.WORKFLOW: "list_workflows_paginated",
+        ResourceType.TASK: "list_tasks_paginated",
+        ResourceType.LAUNCH_PLAN: "list_launch_plans_paginated",
+    }[resource_type]
+
+    remote_method = {
+        ResourceType.WORKFLOW: "fetch_workflow",
+        ResourceType.TASK: "fetch_task",
+        ResourceType.LAUNCH_PLAN: "fetch_launch_plan",
+    }[resource_type]
+
+    mock_client = MagicMock()
+    getattr(mock_client, client_method).return_value = admin_entities, ""
+    mock_client_manager.return_value.client = mock_client
+
+    remote = FlyteRemote()
+    fetch_method = getattr(remote, remote_method)
+    flyte_workflow_latest = fetch_method("p1", "d1", "n1", "latest")
+    flyte_workflow_latest_implicit = fetch_method("p1", "d1", "n1")
+    flyte_workflow_old = fetch_method("p1", "d1", "old")
+
+    assert flyte_workflow_latest.entity_type_text == {
+        ResourceType.WORKFLOW: "Workflow",
+        ResourceType.TASK: "Task",
+        ResourceType.LAUNCH_PLAN: "Launch Plan",
+    }[resource_type]
+    assert flyte_workflow_latest.id == admin_entities[0].id
+    assert flyte_workflow_latest.id == flyte_workflow_latest_implicit.id
+    assert flyte_workflow_latest.id != flyte_workflow_old.id
+
+
+@patch("flytekit.engines.flyte.engine._FlyteClientManager")
+@patch("flytekit.configuration.platform.URL")
+def test_remote_fetch_workflow_execution(mock_url, mock_client_manager):
+    mock_url.get.return_value = "localhost"
+    admin_workflow_execution = Execution(
+        id=WorkflowExecutionIdentifier("p1", "d1", "n1"),
+        spec=MagicMock(),
+        closure=MagicMock(),
+    )
+    mock_client = MagicMock()
+    mock_client.get_execution.return_value = admin_workflow_execution
+    mock_client_manager.return_value.client = mock_client
+
+    remote = FlyteRemote()
+    flyte_workflow_execution = remote.fetch_workflow_execution("p1", "d1", "n1")
+    assert flyte_workflow_execution.id == admin_workflow_execution.id
