fa65a0d028f0c9a5eebdef61b4f0578c1e6c3d7b
Haytham Abuelfutuh <haytham@afutuh.com>
2021-06-30 12:47:37 -0700

    Allow statsd tags to be disabled through config
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- flytekit/configuration/statsd.py
+++ flytekit/configuration/statsd.py
@@ -3,3 +3,4 @@ from flytekit.configuration import common as _common_config
 HOST = _common_config.FlyteStringConfigurationEntry("statsd", "host", default="localhost")
 PORT = _common_config.FlyteIntegerConfigurationEntry("statsd", "port", default=8125)
 DISABLED = _common_config.FlyteBoolConfigurationEntry("statsd", "disabled", default=False)
+DISABLE_TAGS = _common_config.FlyteBoolConfigurationEntry("statsd", "disable_tagging", default=False)
