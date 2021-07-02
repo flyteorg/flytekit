41ea142298a99824d2c6ea0ef6477030719368c2
Haytham Abuelfutuh <haytham@afutuh.com>
2021-07-02 11:09:23 -0700

    Add comment
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- flytekit/configuration/statsd.py
+++ flytekit/configuration/statsd.py
@@ -1,5 +1,8 @@
 from flytekit.configuration import common as _common_config
 
+# StatsD Config flags should ideally be controlled at the platform level and not through flytekit's config file.
+# They are meant to allow administrators to control certain behavior according to how the system is configured.
+
 HOST = _common_config.FlyteStringConfigurationEntry("statsd", "host", default="localhost")
 PORT = _common_config.FlyteIntegerConfigurationEntry("statsd", "port", default=8125)
 DISABLED = _common_config.FlyteBoolConfigurationEntry("statsd", "disabled", default=False)
