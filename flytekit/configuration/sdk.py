54576b3cedbe47518783b26916479c588f438573
Niels Bantilan <niels.bantilan@gmail.com>
2021-07-07 15:55:19 -0400

    local sandbox tmp dir supports windows (#542)
    * local sandbox tmp dir supports windows
    
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>
    
    * use tempfile
    
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- flytekit/configuration/sdk.py
+++ flytekit/configuration/sdk.py
@@ -1,3 +1,5 @@
+import tempfile
+
 from flytekit.configuration import common as _config_common
 
 WORKFLOW_PACKAGES = _config_common.FlyteStringListConfigurationEntry("sdk", "workflow_packages", default=[])
@@ -20,7 +22,11 @@ This is a comma-delimited list of package strings, in order, for resolving type 
 TODO: Explain how this would be used to extend the SDK
 """
 
-LOCAL_SANDBOX = _config_common.FlyteStringConfigurationEntry("sdk", "local_sandbox", default="/tmp/flyte")
+LOCAL_SANDBOX = _config_common.FlyteStringConfigurationEntry(
+    "sdk",
+    "local_sandbox",
+    default=tempfile.mkdtemp(prefix="flyte"),
+)
 """
 This is the path where SDK will place files during local executions and testing.  The SDK will not automatically
 clean up data in these directories.
