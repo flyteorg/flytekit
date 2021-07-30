66964504fe9ca284db97b062499ed74f579df386
Chao-Han Tsai <milton0825@gmail.com>
2021-07-29 17:12:55 -0700

    add tests
    Signed-off-by: Chao-Han Tsai <milton0825@gmail.com>

---
+++ tests/flytekit/unit/core/test_python_auto_container.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from flytekit.core.context_manager import SerializationSettings
+from flytekit.core.python_auto_container import PythonAutoContainerTask
+import mock
+
+
+@mock.patch('flytekit.core.python_auto_container.SDK_PYTHON_VENV')
+def test_get_default_command_prefix_sdk_python_venv(mock_venv):
+
+    mock_venv.get.return_value = ["service_venv"]
+
+    task = PythonAutoContainerTask(
+        "test", None,
+    )
+
+    task._task_resolver._location = 'location'
+    task._task_resolver.loader_args = lambda a, b: ['a', 'b']
+
+    cmd: List[str] = task.get_default_command(SerializationSettings('p', 'd', 'v', None))
+    assert cmd[0] == "service_venv"
