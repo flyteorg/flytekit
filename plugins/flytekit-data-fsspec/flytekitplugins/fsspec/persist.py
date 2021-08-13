055a1ebcfe178c989fe0767968d80d239517c070
Ketan Umare <ketan.umare@gmail.com>
2021-08-13 15:17:28 -0700

    FSSpec debugging and defaults
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

--- plugins/flytekit-data-fsspec/flytekitplugins/fsspec/persist.py
+++ plugins/flytekit-data-fsspec/flytekitplugins/fsspec/persist.py
@@ -11,18 +11,25 @@ class FSSpecPersistence(DataPersistence):
 
     @staticmethod
     def _get_filesystem(path: str) -> fsspec.AbstractFileSystem:
-        protocol = split_protocol(path)
+        protocol, _ = split_protocol(path)
+        if protocol is None and path.startswith("/"):
+            print("Setting protocol to file")
+            protocol = "file"
+        print(f"Protocol: {protocol}")
         return fsspec.filesystem(protocol)
 
     def exists(self, path: str) -> bool:
+        print("FSSPEC Exists")
         fs = self._get_filesystem(path)
         return fs.exists(path)
 
     def get(self, from_path: str, to_path: str, recursive: bool = False):
+        print(f"FSSPEC get - {from_path} {to_path}")
         fs = self._get_filesystem(from_path)
         return fs.get(from_path, to_path, recursive=recursive)
 
     def put(self, from_path: str, to_path: str, recursive: bool = False):
+        print(f"FSSPEC put - {from_path} {to_path}")
         fs = self._get_filesystem(to_path)
         return fs.put(from_path, to_path, recursive=recursive)
 
@@ -38,6 +45,7 @@ class FSSpecPersistence(DataPersistence):
 
 def _register():
     print("Registering fsspec known implementations and overriding all default implementations for persistence.")
+    DataPersistencePlugins.register_plugin("/", FSSpecPersistence, force=True)
     for k, v in known_implementations.items():
         DataPersistencePlugins.register_plugin(f"{k}://", FSSpecPersistence, force=True)
 
