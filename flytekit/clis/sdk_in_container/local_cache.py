9deff96fb0f09b61daa416fcf0cd190b1cbeaa3e
Eduardo Apolinario <curupa@gmail.com>
2021-08-13 14:23:57 -0700

    Linting.
    Signed-off-by: Eduardo Apolinario <curupa@gmail.com>

--- flytekit/clis/sdk_in_container/local_cache.py
+++ flytekit/clis/sdk_in_container/local_cache.py
@@ -1,4 +1,5 @@
 import click
+
 from flytekit.core.local_cache import LocalCache
 
 
@@ -16,5 +17,6 @@ def clear_local_cache():
     This command will remove all stored objects from local cache.
     """
     LocalCache.clear()
+
 
 local_cache.add_command(clear_local_cache)
