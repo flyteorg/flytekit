ad0946d2a3750822b292569470117e459b0be18d
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2021-08-23 08:30:55 -0700

    exception type
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- tests/flytekit/unit/core/test_flyte_file.py
+++ tests/flytekit/unit/core/test_flyte_file.py
@@ -354,7 +354,7 @@ def test_returning_folder_instead_of_file():
     def wf1() -> FlyteFile:
         return t1()
 
-    with pytest.raises(AssertionError):
+    with pytest.raises(ValueError):
         wf1()
 
     @task
@@ -366,7 +366,7 @@ def test_returning_folder_instead_of_file():
     def wf2() -> FlyteFile:
         return t2()
 
-    with pytest.raises(AssertionError):
+    with pytest.raises(ValueError):
         wf2()
 
 
