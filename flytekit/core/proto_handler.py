f96528d5e2bcfffed4c6fec2433cdd3c7d92e801
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2021-08-26 16:07:45 -0700

    exc
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- flytekit/core/proto_handler.py
+++ flytekit/core/proto_handler.py
@@ -1,8 +1,6 @@
 import importlib
 import typing
 
-from flytekit.common.exceptions import user as user_exceptions
-
 T = typing.TypeVar("T")
 
 
@@ -12,20 +10,17 @@ def load_type_from_tag(tag: str) -> typing.Type[T]:
     """
 
     if "." not in tag:
-        raise user_exceptions.FlyteValueException(
-            tag,
-            "Protobuf tag must include at least one '.' to delineate package and object name.",
+        raise ValueError(
+            f"Protobuf tag must include at least one '.' to delineate package and object name got {tag}",
         )
 
     module, name = tag.rsplit(".", 1)
     try:
         pb_module = importlib.import_module(module)
     except ImportError:
-        raise user_exceptions.FlyteAssertion(
-            "Could not resolve the protobuf definition @ {}.  Is the protobuf library installed?".format(module)
-        )
+        raise ValueError(f"Could not resolve the protobuf definition @ {module}.  Is the protobuf library installed?")
 
     if not hasattr(pb_module, name):
-        raise user_exceptions.FlyteAssertion("Could not find the protobuf named: {} @ {}.".format(name, module))
+        raise ValueError(f"Could not find the protobuf named: {name} @ {module}.")
 
     return getattr(pb_module, name)
