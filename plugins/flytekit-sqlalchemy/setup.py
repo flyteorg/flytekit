11a2998dbd543285382aa849b876d3ddc9e276f3
Eduardo Apolinario <curupa@gmail.com>
2021-08-31 16:37:24 -0700

    One more try to get flyteidl-0.20.1 after plugins are installed
    Signed-off-by: Eduardo Apolinario <curupa@gmail.com>

--- plugins/flytekit-sqlalchemy/setup.py
+++ plugins/flytekit-sqlalchemy/setup.py
@@ -4,7 +4,7 @@ PLUGIN_NAME = "sqlalchemy"
 
 microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
 
-plugin_requires = ["flytekit>=0.17.0,<1.0.0", "sqlalchemy>=1.4.7"]
+plugin_requires = ["flytekit>=0.22.0b6,<1.0.0", "sqlalchemy>=1.4.7"]
 
 __version__ = "0.0.0+develop"
 
