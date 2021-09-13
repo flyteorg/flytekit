84002d301a410b40144733280efbfd1d044b1cc4
Tatyana Vlasova <tatyana.vlasova@intel.com>
2021-09-13 17:58:32 +0300

    Changes to be committed: The schema.py fixed - ParquetIO and FastParquetIO reimplemented before currently removed. Fails connected with linters are fixed.
    Signed-off-by: Tatyana Vlasova <tatyana.vlasova@intel.com>

--- plugins/flytekit-modin/flytekitplugins/modin/schema.py
+++ plugins/flytekit-modin/flytekitplugins/modin/schema.py
@@ -2,107 +2,63 @@ import os
 import typing
 from typing import Type
 
+import modin
+from modin import pandas
+
 from flytekit import FlyteContext
-from flytekit.configuration import sdk
 from flytekit.extend import T, TypeEngine, TypeTransformer
 from flytekit.models.literals import Literal, Scalar, Schema
 from flytekit.models.types import LiteralType, SchemaType
-from flytekit.types.schema import LocalIOSchemaReader, LocalIOSchemaWriter, SchemaEngine, SchemaHandler
-from flytekit.types.schema import SchemaFormat
+from flytekit.types.schema import LocalIOSchemaReader, LocalIOSchemaWriter, SchemaEngine, SchemaFormat, SchemaHandler
 from flytekit.types.schema.types import FlyteSchemaTransformer
 
-from modin import pandas
 
-
-class ParquetIO(object):
-    PARQUET_ENGINE = "pyarrow"
-
-    def _read(self, chunk: os.PathLike, columns: typing.List[str], **kwargs) -> pandas.DataFrame:
-        return pandas.read_parquet(chunk, columns=columns, engine=self.PARQUET_ENGINE, **kwargs)
-
-    def read(self, *files: os.PathLike, columns: typing.List[str] = None, **kwargs) -> pandas.DataFrame:
-        frames = [self._read(chunk=f, columns=columns, **kwargs) for f in files if os.path.getsize(f) > 0]
-        if len(frames) == 1:
-            return frames[0]
-        elif len(frames) > 1:
-            return pandas.concat(frames, copy=True)
-        return pandas.DataFrame()
-
-    def write(
-        self,
-        df: pandas.DataFrame,
-        to_file: os.PathLike,
-        coerce_timestamps: str = "us",
-        allow_truncated_timestamps: bool = False,
-        **kwargs,
-    ):
-        """
-        Writes data frame as a chunk to the local directory owned by the Schema object.  Will later be uploaded to s3.
-        :param df: data frame to write as parquet
-        :param to_file: Sink file to write the dataframe to
-        :param coerce_timestamps: format to store timestamp in parquet. 'us', 'ms', 's' are allowed values.
-            Note: if your timestamps will lose data due to the coercion, your write will fail!  Nanoseconds are
-            problematic in the Parquet format and will not work. See allow_truncated_timestamps.
-        :param allow_truncated_timestamps: default False. Allow truncation when coercing timestamps to a coarser
-            resolution.
-        """
-        df.to_parquet(
-            to_file,
-            coerce_timestamps=coerce_timestamps,
-            allow_truncated_timestamps=allow_truncated_timestamps,
-            **kwargs,
-        )
-
-
-class FastParquetIO(ParquetIO):
-    PARQUET_ENGINE = "fastparquet"
-
-    def _read(self, chunk: os.PathLike, columns: typing.List[str], **kwargs) -> pandas.DataFrame:
-        from fastparquet import ParquetFile as _ParquetFile
-        from fastparquet import thrift_structures as _ts
-
-        df = pandas.read_parquet(chunk, columns=columns, engine=self.PARQUET_ENGINE, index=False)
-        df_column_types = df.dtypes
-        pf = _ParquetFile(chunk)
-        schema_column_dtypes = {l.name: l.type for l in list(pf.schema.schema_elements)}
-
-        for idx in df_column_types[df_column_types == "float16"].index.tolist():
-            # A hacky way to get the string representations of the column types of a parquet schema
-            # Reference:
-            # https://github.com/dask/fastparquet/blob/f4ecc67f50e7bf98b2d0099c9589c615ea4b06aa/fastparquet/schema.py
-            if _ts.parquet_thrift.Type._VALUES_TO_NAMES[schema_column_dtypes[idx]] == "BOOLEAN":
-                df[idx] = df[idx].astype("object")
-                df[idx].replace({0: False, 1: True, pandas.np.nan: None}, inplace=True)
-        return df
-
-
-_PARQUETIO_ENGINES: typing.Dict[str, ParquetIO] = {
-    ParquetIO.PARQUET_ENGINE: ParquetIO(),
-    FastParquetIO.PARQUET_ENGINE: FastParquetIO(),
-}
-
-
-class ModinPandasSchemaReader(LocalIOSchemaReader[pandas.DataFrame]):
-    def __init__(self, local_dir: os.PathLike, cols: typing.Optional[typing.Dict[str, type]], fmt: SchemaFormat):
-        super().__init__(local_dir, cols, fmt)
-        self._parquet_engine = _PARQUETIO_ENGINES[sdk.PARQUET_ENGINE.get()]
-
-    def _read(self, *path: os.PathLike, **kwargs) -> pandas.DataFrame:
-        return self._parquet_engine.read(*path, columns=self.column_names, **kwargs)
-
-
-class ModinPandasSchemaWriter(LocalIOSchemaWriter[pandas.DataFrame]):
-    def __init__(self, local_dir: os.PathLike, cols: typing.Optional[typing.Dict[str, type]], fmt: SchemaFormat):
-        super().__init__(local_dir, cols, fmt)
-        self._parquet_engine = _PARQUETIO_ENGINES[sdk.PARQUET_ENGINE.get()]
-
-    def _write(self, df: T, path: os.PathLike, **kwargs):
-        return self._parquet_engine.write(df, to_file=path, **kwargs)
-
-
-class ModinPandasDataFrameTransformer(TypeTransformer[pandas.DataFrame]):
+class ModinPandasSchemaReader(LocalIOSchemaReader[modin.pandas.DataFrame]):
     """
-    Transforms a pd.DataFrame to Schema without column types.
+    Implements how ModinPandasDataFrame should be read using the ``open`` method of FlyteSchema
+    """
+
+    def __init__(
+        self,
+        from_path: str,
+        cols: typing.Optional[typing.Dict[str, type]],
+        fmt: SchemaFormat,
+    ):
+        super().__init__(from_path, cols, fmt)
+
+    def all(self, **kwargs) -> modin.pandas.DataFrame:
+        if self._fmt == SchemaFormat.PARQUET:
+            return modin.pandas.read_parquet(self.from_path + "/00000")
+        raise AssertionError("Only Parquet type files are supported for modin pandas dataframe currently")
+
+
+class ModinPandasSchemaWriter(LocalIOSchemaWriter[modin.pandas.DataFrame]):
+    """
+    Implements how ModinPandasDataFrame should be written to using ``open`` method of FlyteSchema
+    """
+
+    def __init__(
+        self,
+        to_path: os.PathLike,
+        cols: typing.Optional[typing.Dict[str, type]],
+        fmt: SchemaFormat,
+    ):
+        super().__init__(str(to_path), cols, fmt)
+
+    def write(self, *dfs: modin.pandas.DataFrame, **kwargs):
+        if dfs is None or len(dfs) == 0:
+            return
+        if len(dfs) > 1:
+            raise AssertionError("Only a single modin.pandas.DataFrame can be written per variable currently")
+        if self._fmt == SchemaFormat.PARQUET:
+            dfs[0].to_parquet(self.to_path + "/00000")
+            return
+        raise AssertionError("Only Parquet type files are supported for modin.pandas dataframe currently")
+
+
+class ModinPandasDataFrameTransformer(TypeTransformer[modin.pandas.DataFrame]):
+    """
+    Transforms ModinPandas DataFrame's to and from a Schema (typed/untyped)
     """
 
     _SUPPORTED_TYPES: typing.Dict[
@@ -110,43 +66,51 @@ class ModinPandasDataFrameTransformer(TypeTransformer[pandas.DataFrame]):
     ] = FlyteSchemaTransformer._SUPPORTED_TYPES
 
     def __init__(self):
-        super().__init__("ModinPandasDataFrame<->GenericSchema", pandas.DataFrame)
-        self._parquet_engine = _PARQUETIO_ENGINES[sdk.PARQUET_ENGINE.get()]
+        super().__init__("modin.pandas-df-transformer", pandas.DataFrame)
 
     @staticmethod
     def _get_schema_type() -> SchemaType:
         return SchemaType(columns=[])
 
-    def get_literal_type(self, t: Type[pandas.DataFrame]) -> LiteralType:
+    def get_literal_type(self, t: Type[modin.pandas.DataFrame]) -> LiteralType:
         return LiteralType(schema=self._get_schema_type())
 
     def to_literal(
         self,
         ctx: FlyteContext,
-        python_val: pandas.DataFrame,
-        python_type: Type[pandas.DataFrame],
+        python_val: modin.pandas.DataFrame,
+        python_type: Type[modin.pandas.DataFrame],
         expected: LiteralType,
     ) -> Literal:
         local_dir = ctx.file_access.get_random_local_directory()
-        w = ModinPandasSchemaWriter(local_dir=local_dir, cols=None, fmt=SchemaFormat.PARQUET)
+        print(local_dir)
+        w = ModinPandasSchemaWriter(to_path=local_dir, cols=None, fmt=SchemaFormat.PARQUET)
         w.write(python_val)
         remote_path = ctx.file_access.get_random_remote_directory()
         ctx.file_access.put_data(local_dir, remote_path, is_multipart=True)
         return Literal(scalar=Scalar(schema=Schema(remote_path, self._get_schema_type())))
 
     def to_python_value(
-        self, ctx: FlyteContext, lv: Literal, expected_python_type: Type[pandas.DataFrame]
-    ) -> pandas.DataFrame:
+        self,
+        ctx: FlyteContext,
+        lv: Literal,
+        expected_python_type: Type[modin.pandas.DataFrame],
+    ) -> T:
         if not (lv and lv.scalar and lv.scalar.schema):
-            return pandas.DataFrame()
+            return modin.pandas.DataFrame()
         local_dir = ctx.file_access.get_random_local_directory()
         ctx.file_access.download_directory(lv.scalar.schema.uri, local_dir)
-        r = ModinPandasSchemaReader(local_dir=local_dir, cols=None, fmt=SchemaFormat.PARQUET)
+        r = ModinPandasSchemaReader(from_path=local_dir, cols=None, fmt=SchemaFormat.PARQUET)
         return r.all()
 
 
 SchemaEngine.register_handler(
-    SchemaHandler("ModinPandas.Dataframe-Schema", pandas.DataFrame, ModinPandasSchemaReader, ModinPandasSchemaWriter)
+    SchemaHandler(
+        "modin.pandas.Dataframe-Schema",
+        modin.pandas.DataFrame,
+        ModinPandasSchemaReader,
+        ModinPandasSchemaWriter,
+    )
 )
 
 TypeEngine.register(ModinPandasDataFrameTransformer())
