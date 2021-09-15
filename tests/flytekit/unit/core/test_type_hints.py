dd150aae8a97b0abd730acc5946313a887029f35
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2021-09-14 20:15:26 -0700

    Add expected value for test_dict3
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- tests/flytekit/unit/core/test_type_hints.py
+++ tests/flytekit/unit/core/test_type_hints.py
@@ -1335,4 +1335,6 @@ def test_guess_dict3():
 
     ctx = context_manager.FlyteContextManager.current_context()
     output_lm = t2.dispatch_execute(ctx, _literal_models.LiteralMap(literals={}))
-    assert output_lm.literals["o0"].scalar.generic is not None
+    expected_struct = Struct()
+    expected_struct.update({"k1": "v1", "k2": 3, "4": {"one": [1, "two", [3]]}})
+    assert output_lm.literals["o0"].scalar.generic == expected_struct
