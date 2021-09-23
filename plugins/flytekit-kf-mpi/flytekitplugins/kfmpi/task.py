74d08998f747f0c06676bdcff54effff420d9b71
Katrina Rogan <katroganGH@gmail.com>
2021-09-23 13:39:06 -0700

    Read replica resources from task decorator
    Signed-off-by: Katrina Rogan <katroganGH@gmail.com>

--- plugins/flytekit-kf-mpi/flytekitplugins/kfmpi/task.py
+++ plugins/flytekit-kf-mpi/flytekitplugins/kfmpi/task.py
@@ -25,19 +25,11 @@ class MPIJob(object):
         num_launcher_replicas: Number of launcher server replicas to use
 
         slots: Number of slots per worker used in hostfile
-
-        per_replica_requests: [optional] lower-bound resources for each replica spawned for this job
-        (i.e. both for (main)master and workers).  Default is set by platform-level configuration.
-
-        per_replica_limits: [optional] upper-bound resources for each replica spawned for this job. If not specified
-        the scheduled resource may not have all the resources
     """
 
     slots: int
     num_launcher_replicas: int = 1
     num_workers: int = 1
-    per_replica_requests: Optional[Resources] = None
-    per_replica_limits: Optional[Resources] = None
 
 
 class MPIFunctionTask(PythonFunctionTask[MPIJob]):
@@ -73,7 +65,7 @@ class MPIFunctionTask(PythonFunctionTask[MPIJob]):
             task_config=task_config,
             task_function=task_function,
             task_type=self._MPI_JOB_TASK_TYPE,
-            **{**kwargs, "requests": task_config.per_replica_requests, "limits": task_config.per_replica_limits}
+            **kwargs,
         )
 
     def get_command(self, settings: SerializationSettings) -> List[str]:
