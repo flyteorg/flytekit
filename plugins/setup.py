868516a74198ae13251b7903f77838776f065c8f
Tat-V <61228633+Tat-V@users.noreply.github.com>
2021-10-12 20:48:55 +0300

    Modin (#637)
    Signed-off-by: Tatyana Vlasova <tatyana.vlasova@intel.com>
    Signed-off-by: Anton Malakhov <Anton.Malakhov@intel.com>
    Co-authored-by: Tatyana Vlasova <tatyana.vlasova@intel.com>
    Co-authored-by: Anton Malakhov <Anton.Malakhov@intel.com>

--- plugins/setup.py
+++ plugins/setup.py
@@ -21,6 +21,7 @@ SOURCES = {
     "flytekitplugins-snowflake": "flytekit-snowflake",
     "flytekitplugins-spark": "flytekit-spark",
     "flytekitplugins-sqlalchemy": "flytekit-sqlalchemy",
+    "flytekitplugins-modin": "flytekit-modin",
 }
 
 
