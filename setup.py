805f51f1eac04c19ce5c418971dfd30c351ff25a
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2021-10-15 13:18:13 -0700

    Add python 3.9 to the list of supported languages
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -110,6 +110,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Software Development",
