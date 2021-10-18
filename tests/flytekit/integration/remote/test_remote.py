ae75501b70579961d3f2aeae7a6965d3538e7cbe
Kevin Su <pingsutw@apache.org>
2021-10-18 23:50:33 +0800

    Fixed lint
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- tests/flytekit/integration/remote/test_remote.py
+++ tests/flytekit/integration/remote/test_remote.py
@@ -4,6 +4,7 @@ import os
 import pathlib
 import time
 import typing
+import uuid
 
 import joblib
 import pytest
@@ -287,13 +288,15 @@ def test_execute_joblib_workflow(flyteclient, flyte_workflows_register, flyte_re
 def test_execute_with_default_launch_plan(flyteclient, flyte_remote_env):
     from mock_flyte_repo.workflows.basic.list_float_wf import my_wf
 
+    my_wf._name = my_wf.name.replace("mock_flyte_repo.", "")
     remote = FlyteRemote.from_config(PROJECT, "development")
     xs: typing.List[float] = [42.24, 999.1, 0.0001]
-    execution = remote.execute(my_wf, inputs={"xs": xs}, wait=True)
+    version = uuid.uuid4().hex[:30] + str(int(time.time()))
+    execution = remote.execute(my_wf, version=version, inputs={"xs": xs}, wait=True)
     assert execution.outputs["o0"] == "[42.24, 999.1, 0.0001]"
 
 
 def test_fetch_not_exist_launch_plan(flyteclient):
     remote = FlyteRemote.from_config(PROJECT, "development")
     with pytest.raises(FlyteEntityNotExistException):
-        remote.fetch_launch_plan(name="workflows.basic.list_float_wf.my_wf", version=f"v{VERSION}")
+        remote.fetch_launch_plan(name="workflows.basic.list_float_wf.fake_wf", version=f"v{VERSION}")
