c25aafeb1354d1a49b7d183459042276ad420ed7
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2021-10-25 11:26:43 -0700

    add test
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- tests/flytekit/unit/core/test_flyte_pickle.py
+++ tests/flytekit/unit/core/test_flyte_pickle.py
@@ -1,8 +1,23 @@
+from collections import OrderedDict
+from typing import List
+
+from flytekit.common.translator import get_serializable
 from flytekit.core import context_manager
+from flytekit.core.context_manager import Image, ImageConfig
+from flytekit.core.task import task
 from flytekit.models.core.types import BlobType
 from flytekit.models.literals import BlobMetadata
 from flytekit.models.types import LiteralType
 from flytekit.types.pickle.pickle import FlytePickle, FlytePickleTransformer
+
+default_img = Image(name="default", fqn="test", tag="tag")
+serialization_settings = context_manager.SerializationSettings(
+    project="project",
+    domain="domain",
+    version="version",
+    env=None,
+    image_config=ImageConfig(default_image=default_img, images=[default_img]),
+)
 
 
 def test_to_python_value_and_literal():
@@ -32,3 +47,16 @@ def test_get_literal_type():
             format=FlytePickleTransformer.PYTHON_PICKLE_FORMAT, dimensionality=BlobType.BlobDimensionality.SINGLE
         )
     )
+
+
+def test_nested():
+    class Foo(object):
+        def __init__(self, number: int):
+            self.number = number
+
+    @task
+    def t1(a: int) -> List[List[Foo]]:
+        return [[Foo(number=a)]]
+
+    task_spec = get_serializable(OrderedDict(), serialization_settings, t1)
+    print(task_spec)
