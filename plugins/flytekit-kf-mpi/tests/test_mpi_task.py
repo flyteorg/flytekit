895cdede5a85882b3a2272b0f72fc568446d61c2
Yuvraj <code@evalsocket.dev>
2021-10-27 12:33:49 +0530

    Added mpi plugin (#595)
    * Added mpi plugin
    
    Signed-off-by: Yuvraj <code@evalsocket.dev>
    
    Co-authored-by: Ketan Umare <ketan.umare@gmail.com>

---
+++ plugins/flytekit-kf-mpi/tests/test_mpi_task.py
@@ -0,0 +1,45 @@
+from flytekitplugins.kfmpi.task import MPIJob, MPIJobModel
+
+from flytekit import Resources, task
+from flytekit.core.context_manager import EntrypointSettings
+from flytekit.extend import Image, ImageConfig, SerializationSettings
+
+
+def test_mpi_model_task():
+    job = MPIJobModel(
+        num_workers=1,
+        num_launcher_replicas=1,
+        slots=1,
+    )
+    assert job.num_workers == 1
+    assert job.num_launcher_replicas == 1
+    assert job.slots == 1
+    assert job.from_flyte_idl(job.to_flyte_idl())
+
+
+def test_mpi_task():
+    @task(
+        task_config=MPIJob(num_workers=10, num_launcher_replicas=10, slots=1),
+        requests=Resources(cpu="1"),
+        cache=True,
+        cache_version="1",
+    )
+    def my_mpi_task(x: int, y: str) -> int:
+        return x
+
+    assert my_mpi_task(x=10, y="hello") == 10
+
+    assert my_mpi_task.task_config is not None
+
+    default_img = Image(name="default", fqn="test", tag="tag")
+    settings = SerializationSettings(
+        project="project",
+        domain="domain",
+        version="version",
+        env={"FOO": "baz"},
+        image_config=ImageConfig(default_image=default_img, images=[default_img]),
+        entrypoint_settings=EntrypointSettings(path="/etc/my-entrypoint", command="my-entrypoint"),
+    )
+
+    assert my_mpi_task.get_custom(settings) == {"numLauncherReplicas": 10, "numWorkers": 10, "slots": 1}
+    assert my_mpi_task.task_type == "mpi"
