d2130cc3a7cb4fd8ddc823c07f7ea98e539cc3af
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2021-11-04 15:34:21 -0700

    Add example sub-command
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- flytekit/clis/sdk_in_container/init.py
+++ flytekit/clis/sdk_in_container/init.py
@@ -3,13 +3,32 @@ from cookiecutter.main import cookiecutter
 from cookiecutter.prompt import read_user_variable
 
 
-@click.command("init")
+@click.group("init")
 def init():
     """
-    Create a flyte-ready project.
+    Create flyte-ready projects.
     """
     # TODO: Add `interactive` boolean flag to allow for programmatic access to this subcommand.
+    pass
 
+
+@click.command("example")
+def generate_simple_example():
+    """
+    This command creates a directory containing the minimal code necessary to start a Flyte-ready project.
+    The generated directory structure for the default values resemble:
+        flyte_example
+        ├── Dockerfile
+        ├── flyte.config
+        ├── LICENSE
+        ├── myapp
+        │   ├── __init__.py
+        │   └── workflows
+        │       ├── example.py
+        │       └── __init__.py
+        ├── README.md
+        └── requirements.txt
+    """
     print("What is the name of your project? This is the name of the directory that will be created.")
     project_name = read_user_variable("project_name", "flyte_example")
     print("What should we call your application? This serves as the top level package where your workflows will live.")
@@ -23,7 +42,6 @@ def init():
         "workflow": workflow_name,
     }
     cookiecutter(
-        # TODO move this to the main repo. Possibly maintain a branch before cutting over the documentation.
         "https://github.com/flyteorg/flytekit-python-template.git",
         # TODO: remove this once we make the transition to cookie-cutter official.
         checkout="cookie-cutter",
@@ -37,3 +55,6 @@ def init():
         # a tensorflow-based example.
         directory="simple-example",
     )
+
+
+init.add_command(generate_simple_example)
