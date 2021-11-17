72bb4011860471cfed235914943f2a870f517e81
Kevin Su <pingsutw@apache.org>
2021-11-17 21:46:04 +0800

    Fixed lint
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/types/structured/arrow.py
+++ flytekit/types/structured/arrow.py
@@ -1,13 +1,10 @@
 import os
-import re
 import typing
 
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-from google.cloud import bigquery, bigquery_storage
 from google.cloud.bigquery import LoadJob, LoadJobConfig
-from google.cloud.bigquery_storage_v1 import types
 
 from flytekit.types.structured.structured_dataset import (
     FLYTE_DATASET_TRANSFORMER,
@@ -28,9 +25,9 @@ class ArrowToParquetPersistenceHandlers(DatasetPersistenceHandler):
 
 class ArrowToBQPersistenceHandlers(DatasetPersistenceHandler):
     def persist(self, table: pa.Table, path: str, job_config: typing.Optional[LoadJobConfig] = None) -> LoadJob:
-        table_id = path.split("://", 1)[1].replace(":", ".")
-        client = bigquery.Client()
-        return client.load_table_from_dataframe(table.to_pandas(), table_id, job_config=job_config)
+        from flytekit.types.structured import PandasToBQPersistenceHandlers
+
+        return PandasToBQPersistenceHandlers().persist(table.to_pandas(), path, job_config)
 
 
 class ParquetToArrowRetrievalHandler(DatasetRetrievalHandler):
@@ -40,24 +37,10 @@ class ParquetToArrowRetrievalHandler(DatasetRetrievalHandler):
 
 class BQToArrowRetrievalHandler(DatasetRetrievalHandler):
     def retrieve(self, path: str, **kwargs) -> pa.Table:
-        # path will be like bq://photo-313016:flyte.new_table1
-        _, project_id, dataset_id, table_id = re.split("\\.|://|:", path)
-        client = bigquery_storage.BigQueryReadClient()
-        table = f"projects/{project_id}/datasets/{dataset_id}/tables/{table_id}"
-        parent = "projects/{}".format(project_id)
+        from flytekit.types.structured import BQToPandasRetrievalHandler
 
-        requested_session = types.ReadSession(
-            table=table,
-            data_format=types.DataFormat.ARROW,
-        )
-        read_session = client.create_read_session(parent=parent, read_session=requested_session)
-
-        stream = read_session.streams[0]
-        reader = client.read_rows(stream.name)
-        frames = []
-        for message in reader.rows().pages:
-            frames.append(message.to_dataframe())
-        return pa.Table.from_pandas(pd.concat(frames))
+        pd_dataframe = BQToPandasRetrievalHandler().retrieve(path)
+        return pa.Table.from_pandas(pd.concat(pd_dataframe))
 
 
 FLYTE_DATASET_TRANSFORMER.register_handler(DatasetFormat.PARQUET, pa.Table, ParquetToArrowRetrievalHandler())
