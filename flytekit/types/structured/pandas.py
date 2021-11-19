3304ec2254fc399aeefc89265905a3712be3cb68
Kevin Su <pingsutw@apache.org>
2021-11-19 14:02:49 +0800

    test
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/types/structured/pandas.py
+++ flytekit/types/structured/pandas.py
@@ -5,10 +5,12 @@ from typing import TypeVar
 
 import pandas
 import pandas as pd
+from fsspec.core import split_protocol
 from google.cloud import bigquery, bigquery_storage
 from google.cloud.bigquery import LoadJob, LoadJobConfig
 from google.cloud.bigquery_storage_v1 import types
 
+from flytekit.configuration import aws as _aws_config
 from flytekit.types.structured.structured_dataset import (
     FLYTE_DATASET_TRANSFORMER,
     DatasetFormat,
@@ -23,7 +25,7 @@ class PandasToParquetPersistenceHandlers(DatasetPersistenceHandler):
     def persist(
         self,
         df: pandas.DataFrame,
-        to_file: os.PathLike,
+        to_file: typing.Union[os.PathLike, str],
         coerce_timestamps: str = "us",
         allow_truncated_timestamps: bool = False,
     ):
@@ -37,10 +39,12 @@ class PandasToParquetPersistenceHandlers(DatasetPersistenceHandler):
         :param allow_truncated_timestamps: default False. Allow truncation when coercing timestamps to a coarser
             resolution.
         """
+
         df.to_parquet(
             to_file,
             coerce_timestamps=coerce_timestamps,
             allow_truncated_timestamps=allow_truncated_timestamps,
+            storage_options=_s3_setup_args(),
         )
 
 
@@ -53,7 +57,7 @@ class PandasToBQPersistenceHandlers(DatasetPersistenceHandler):
 
 class ParquetToPandasRetrievalHandler(DatasetRetrievalHandler):
     def retrieve(self, path) -> pd.DataFrame:
-        return pandas.read_parquet(path)
+        return pandas.read_parquet(path, storage_options=_s3_setup_args())
 
 
 class BQToPandasRetrievalHandler(DatasetRetrievalHandler):
@@ -76,6 +80,35 @@ class BQToPandasRetrievalHandler(DatasetRetrievalHandler):
         for message in reader.rows().pages:
             frames.append(message.to_dataframe())
         return pd.concat(frames)
+
+
+def _get_storage_optionals(path: str) -> dict:
+    protocol = "file"
+    if path:
+        protocol, _ = split_protocol(path)
+        if protocol is None and path.startswith("/"):
+            protocol = "file"
+    kwargs = {}
+    if protocol == "file":
+        kwargs = {"auto_mkdir": True}
+    if protocol == "s3":
+        kwargs = _s3_setup_args()
+    return kwargs
+
+
+def _s3_setup_args():
+    kwargs = {}
+    if _aws_config.S3_ACCESS_KEY_ID.get() is not None:
+        os.environ[_aws_config.S3_ACCESS_KEY_ID_ENV_NAME] = _aws_config.S3_ACCESS_KEY_ID.get()
+
+    if _aws_config.S3_SECRET_ACCESS_KEY.get() is not None:
+        os.environ[_aws_config.S3_SECRET_ACCESS_KEY_ENV_NAME] = _aws_config.S3_SECRET_ACCESS_KEY.get()
+
+    # S3fs takes this as a special arg
+    if _aws_config.S3_ENDPOINT.get() is not None:
+        kwargs["client_kwargs"] = {"endpoint_url": _aws_config.S3_ENDPOINT.get()}
+
+    return kwargs
 
 
 FLYTE_DATASET_TRANSFORMER.register_handler(pd.DataFrame, DatasetFormat.PARQUET, PandasToParquetPersistenceHandlers())
