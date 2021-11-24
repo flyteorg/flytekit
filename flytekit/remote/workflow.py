bcc8541bd6227b532f8462563fe8aac902242b21
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2021-11-24 10:01:25 -0800

    Fix subworkflow and launch plan FlyteRemote behavior (#751)
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- flytekit/remote/workflow.py
+++ flytekit/remote/workflow.py
@@ -1,16 +1,17 @@
+from __future__ import annotations
+
 from typing import Dict, List, Optional
 
 from flytekit.common import constants as _constants
-from flytekit.common.exceptions import system as _system_exceptions
 from flytekit.common.exceptions import user as _user_exceptions
 from flytekit.common.mixins import hash as _hash_mixin
 from flytekit.core.interface import Interface
 from flytekit.core.type_engine import TypeEngine
-from flytekit.models import launch_plan as _launch_plan_models
+from flytekit.models import launch_plan as launch_plan_models
 from flytekit.models import task as _task_models
-from flytekit.models.core import identifier as _identifier_model
+from flytekit.models.core import compiler as compiler_models
+from flytekit.models.core import identifier as id_models
 from flytekit.models.core import workflow as _workflow_models
-from flytekit.remote import identifier as _identifier
 from flytekit.remote import interface as _interfaces
 from flytekit.remote import nodes as _nodes
 
@@ -23,10 +24,15 @@ class FlyteWorkflow(_hash_mixin.HashOnReferenceMixin, _workflow_models.WorkflowT
         nodes: List[_nodes.FlyteNode],
         interface,
         output_bindings,
-        id,
+        id: id_models.Identifier,
         metadata,
         metadata_defaults,
+        subworkflows: Optional[Dict[id_models.Identifier, _workflow_models.WorkflowTemplate]] = None,
+        tasks: Optional[Dict[id_models.Identifier, _task_models.TaskSpec]] = None,
+        launch_plans: Optional[Dict[id_models.Identifier, launch_plan_models.LaunchPlanSpec]] = None,
+        compiled_closure: Optional[compiler_models.CompiledWorkflowClosure] = None,
     ):
+        # TODO: Remove check
         for node in nodes:
             for upstream in node.upstream_nodes:
                 if upstream.id is None:
@@ -46,9 +52,13 @@ class FlyteWorkflow(_hash_mixin.HashOnReferenceMixin, _workflow_models.WorkflowT
         self._flyte_nodes = nodes
         self._python_interface = None
 
-    @property
-    def upstream_entities(self):
-        return set(n.executable_flyte_object for n in self._flyte_nodes)
+        # Optional things that we save for ease of access when promoting from a model or CompiledWorkflowClosure
+        self._subworkflows = subworkflows
+        self._tasks = tasks
+        self._launch_plans = launch_plans
+        self._compiled_closure = compiled_closure
+
+        self._node_map = None
 
     @property
     def interface(self) -> _interfaces.TypedInterface:
@@ -60,7 +70,7 @@ class FlyteWorkflow(_hash_mixin.HashOnReferenceMixin, _workflow_models.WorkflowT
 
     @property
     def resource_type(self):
-        return _identifier_model.ResourceType.WORKFLOW
+        return id_models.ResourceType.WORKFLOW
 
     @property
     def flyte_nodes(self) -> List[_nodes.FlyteNode]:
@@ -76,37 +86,6 @@ class FlyteWorkflow(_hash_mixin.HashOnReferenceMixin, _workflow_models.WorkflowT
             return
         self._python_interface = value
 
-    def get_sub_workflows(self) -> List["FlyteWorkflow"]:
-        result = []
-        for node in self.flyte_nodes:
-            if node.workflow_node is not None and node.workflow_node.sub_workflow_ref is not None:
-                if node.flyte_entity is not None and node.flyte_entity.entity_type_text == "Workflow":
-                    result.append(node.flyte_entity)
-                    result.extend(node.flyte_entity.get_sub_workflows())
-                else:
-                    raise _system_exceptions.FlyteSystemException(
-                        "workflow node with subworkflow found but bad executable " "object {}".format(node.flyte_entity)
-                    )
-
-            # get subworkflows in conditional branches
-            if node.branch_node is not None:
-                if_else: _workflow_models.IfElseBlock = node.branch_node.if_else
-                leaf_nodes: List[_nodes.FlyteNode] = filter(
-                    None,
-                    [
-                        if_else.case.then_node,
-                        *([] if if_else.other is None else [x.then_node for x in if_else.other]),
-                        if_else.else_node,
-                    ],
-                )
-                for leaf_node in leaf_nodes:
-                    exec_flyte_obj = leaf_node.flyte_entity
-                    if exec_flyte_obj is not None and exec_flyte_obj.entity_type_text == "Workflow":
-                        result.append(exec_flyte_obj)
-                        result.extend(exec_flyte_obj.get_sub_workflows())
-
-        return result
-
     @classmethod
     def get_non_system_nodes(cls, nodes: List[_workflow_models.Node]) -> List[_workflow_models.Node]:
         return [n for n in nodes if n.id not in {_constants.START_NODE_ID, _constants.END_NODE_ID}]
@@ -115,10 +94,10 @@ class FlyteWorkflow(_hash_mixin.HashOnReferenceMixin, _workflow_models.WorkflowT
     def promote_from_model(
         cls,
         base_model: _workflow_models.WorkflowTemplate,
-        sub_workflows: Optional[Dict[_identifier.Identifier, _workflow_models.WorkflowTemplate]] = None,
-        node_launch_plans: Optional[Dict[_identifier.Identifier, _launch_plan_models.LaunchPlanSpec]] = None,
-        tasks: Optional[Dict[_identifier.Identifier, _task_models.TaskTemplate]] = None,
-    ) -> "FlyteWorkflow":
+        sub_workflows: Optional[Dict[id_models, _workflow_models.WorkflowTemplate]] = None,
+        node_launch_plans: Optional[Dict[id_models, launch_plan_models.LaunchPlanSpec]] = None,
+        tasks: Optional[Dict[id_models, _task_models.TaskTemplate]] = None,
+    ) -> FlyteWorkflow:
         base_model_non_system_nodes = cls.get_non_system_nodes(base_model.nodes)
         sub_workflows = sub_workflows or {}
         tasks = tasks or {}
@@ -137,11 +116,14 @@ class FlyteWorkflow(_hash_mixin.HashOnReferenceMixin, _workflow_models.WorkflowT
         # No inputs/outputs specified, see the constructor for more information on the overrides.
         wf = cls(
             nodes=list(node_map.values()),
-            id=_identifier.Identifier.promote_from_model(base_model.id),
+            id=base_model.id,
             metadata=base_model.metadata,
             metadata_defaults=base_model.metadata_defaults,
             interface=_interfaces.TypedInterface.promote_from_model(base_model.interface),
             output_bindings=base_model.outputs,
+            subworkflows=sub_workflows,
+            tasks=tasks,
+            launch_plans=node_launch_plans,
         )
 
         if wf.interface is not None:
@@ -149,8 +131,35 @@ class FlyteWorkflow(_hash_mixin.HashOnReferenceMixin, _workflow_models.WorkflowT
                 inputs=TypeEngine.guess_python_types(wf.interface.inputs),
                 outputs=TypeEngine.guess_python_types(wf.interface.outputs),
             )
+        wf._node_map = node_map
 
         return wf
+
+    @classmethod
+    def promote_from_closure(
+        cls,
+        closure: compiler_models.CompiledWorkflowClosure,
+        node_launch_plans: Optional[Dict[id_models, launch_plan_models.LaunchPlanSpec]] = None,
+    ):
+        """
+        Extracts out the relevant portions of a FlyteWorkflow from a closure from the control plane.
+
+        :param closure: This is the closure returned by Admin
+        :param node_launch_plans: The reason this exists is because the compiled closure doesn't have launch plans.
+          It only has subworkflows and tasks. Why this is is unclear. If supplied, this map of launch plans will be
+        :return:
+        """
+        sub_workflows = {sw.template.id: sw.template for sw in closure.sub_workflows}
+        tasks = {t.template.id: t.template for t in closure.tasks}
+
+        flyte_wf = FlyteWorkflow.promote_from_model(
+            base_model=closure.primary.template,
+            sub_workflows=sub_workflows,
+            node_launch_plans=node_launch_plans,
+            tasks=tasks,
+        )
+        flyte_wf._compiled_closure = closure
+        return flyte_wf
 
     def __call__(self, *args, **input_map):
         raise NotImplementedError
