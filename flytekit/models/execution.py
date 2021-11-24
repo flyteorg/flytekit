9b84040421fc25cc5dbfe371d742c14d0908a2d2
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2021-11-24 10:01:25 -0800

    Fix subworkflow and launch plan FlyteRemote behavior (#751)
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>
    Signed-off-by: maximsmol <maximsmol@gmail.com>

--- flytekit/models/execution.py
+++ flytekit/models/execution.py
@@ -1,3 +1,5 @@
+import typing
+
 import flyteidl.admin.execution_pb2 as _execution_pb2
 import flyteidl.admin.node_execution_pb2 as _node_execution_pb2
 import flyteidl.admin.task_execution_pb2 as _task_execution_pb2
@@ -7,6 +9,7 @@ from flytekit.models import common as _common_models
 from flytekit.models import literals as _literals_models
 from flytekit.models.core import execution as _core_execution
 from flytekit.models.core import identifier as _identifier
+from flytekit.models.node_execution import DynamicWorkflowNodeMetadata
 
 
 class ExecutionMetadata(_common_models.FlyteIdlEntity):
@@ -238,7 +241,6 @@ class Execution(_common_models.FlyteIdlEntity):
     def __init__(self, id, spec, closure):
         """
         :param flytekit.models.core.identifier.WorkflowExecutionIdentifier id:
-        :param Text id:
         :param ExecutionSpec spec:
         :param ExecutionClosure closure:
         """
@@ -403,8 +405,8 @@ class _CommonDataResponse(_common_models.FlyteIdlEntity):
         """
         :param _common_models.UrlBlob inputs:
         :param _common_models.UrlBlob outputs:
-        :param _literals_pb2.LiteralMap full_inputs:
-        :param _literals_pb2.LiteralMap full_outputs:
+        :param _literals_models.LiteralMap full_inputs:
+        :param _literals_models.LiteralMap full_outputs:
         """
         self._inputs = inputs
         self._outputs = outputs
@@ -428,14 +430,14 @@ class _CommonDataResponse(_common_models.FlyteIdlEntity):
     @property
     def full_inputs(self):
         """
-        :rtype: _literals_pb2.LiteralMap
+        :rtype: _literals_models.LiteralMap
         """
         return self._full_inputs
 
     @property
     def full_outputs(self):
         """
-        :rtype: _literals_pb2.LiteralMap
+        :rtype: _literals_models.LiteralMap
         """
         return self._full_outputs
 
@@ -493,6 +495,14 @@ class TaskExecutionGetDataResponse(_CommonDataResponse):
 
 
 class NodeExecutionGetDataResponse(_CommonDataResponse):
+    def __init__(self, *args, dynamic_workflow: typing.Optional[DynamicWorkflowNodeMetadata] = None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._dynamic_workflow = dynamic_workflow
+
+    @property
+    def dynamic_workflow(self) -> typing.Optional[DynamicWorkflowNodeMetadata]:
+        return self._dynamic_workflow
+
     @classmethod
     def from_flyte_idl(cls, pb2_object):
         """
@@ -504,6 +514,9 @@ class NodeExecutionGetDataResponse(_CommonDataResponse):
             outputs=_common_models.UrlBlob.from_flyte_idl(pb2_object.outputs),
             full_inputs=_literals_models.LiteralMap.from_flyte_idl(pb2_object.full_inputs),
             full_outputs=_literals_models.LiteralMap.from_flyte_idl(pb2_object.full_outputs),
+            dynamic_workflow=DynamicWorkflowNodeMetadata.from_flyte_idl(pb2_object.dynamic_workflow)
+            if pb2_object.HasField("dynamic_workflow")
+            else None,
         )
 
     def to_flyte_idl(self):
@@ -515,4 +528,5 @@ class NodeExecutionGetDataResponse(_CommonDataResponse):
             outputs=self.outputs.to_flyte_idl(),
             full_inputs=self.full_inputs.to_flyte_idl(),
             full_outputs=self.full_outputs.to_flyte_idl(),
+            dynamic_workflow=self.dynamic_workflow.to_flyte_idl() if self.dynamic_workflow else None,
         )
