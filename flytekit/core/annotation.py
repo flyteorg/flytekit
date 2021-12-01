1417294011c425fa4e981de06913da727e67795f
Kenny Workman <kennyworkman@sbcglobal.net>
2021-12-01 08:47:56 -0800

    feat: core  obj
    Signed-off-by: Kenny Workman <kennyworkman@sbcglobal.net>

---
+++ flytekit/core/annotation.py
@@ -0,0 +1,27 @@
+from typing import Any, Dict
+
+
+class FlyteAnnotation:
+    """A core object to add arbitrary annotations to flyte types.
+
+    This metadata is ingested as a python dictionary and will be serialized
+    into fields on the flyteidl type literals. This data is not accessible at
+    runtime but rather can be retrieved from flyteadmin for custom presentation
+    of typed parameters.
+
+    For a task definition:
+
+    .. code-block:: python
+
+        @task
+        def x(a: typing.Annotated[int, FlyteAnnotation({"foo": {"bar": 1}})]):
+            return
+
+    """
+
+    def __init__(self, data: Dict[str, Any]):
+        self._data = data
+
+    @property
+    def data(self):
+        return self._data
