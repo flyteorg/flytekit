258667197b3b98582bbd3d08bdd0068296bf964e
Kenny Workman <kennyworkman@sbcglobal.net>
2021-12-01 22:56:02 -0800

    feat: more complete docstring parsing

--- flytekit/core/docstring.py
+++ flytekit/core/docstring.py
@@ -1,27 +1,125 @@
-from typing import Callable, Dict, Optional
+import re
+from typing import Any, Dict, Union
 
-from docstring_parser import parse
+import yaml
+from docstring_parser import parse as _parse
+from docstring_parser.common import Docstring
 
 
-class Docstring(object):
-    def __init__(self, docstring: str = None, callable_: Callable = None):
-        if docstring is not None:
-            self._parsed_docstring = parse(docstring)
-        else:
-            self._parsed_docstring = parse(callable_.__doc__)
+def parse_docstring(x: Union[str, Any]) -> Docstring:
+    """ """
 
-    @property
-    def input_descriptions(self) -> Dict[str, str]:
-        return {p.arg_name: p.description for p in self._parsed_docstring.params}
+    if isinstance(x, str):
+        return _parse(x)
 
-    @property
-    def output_descriptions(self) -> Dict[str, str]:
-        return {p.return_name: p.description for p in self._parsed_docstring.many_returns}
+    if not hasattr(x, "__doc__"):
+        raise ValueError(f"No docstring found: '{x}'")
 
-    @property
-    def short_description(self) -> Optional[str]:
-        return self._parsed_docstring.short_description
+    return _parse(x.__doc__)
 
-    @property
-    def long_description(self) -> Optional[str]:
-        return self._parsed_docstring.long_description
+
+_yaml_metadata_regex = re.compile(r"(.*)__metadata__:(.*)", re.MULTILINE | re.DOTALL)
+
+
+def param_metadata_from_docstring(doc: Docstring) -> Dict[str, Dict[str, Any]]:
+    """Extracts structured metadata from docstrings.
+
+    The following pieces of information are extracted from the body:
+
+        * short descrption text
+        * long description text
+        * YAML serialized long description metadata
+
+    The following pieces of information are extracted from each parameter
+    described in the docstring
+
+        * name
+        * description
+        * index/order
+        * default value
+        * YAML serialized parameter description metadata
+
+    An example:
+
+    .. code-block:: python
+
+        @task
+        def foo():
+            ""<short description>
+
+            <long description>
+
+            __metadata__:
+                <YAML serialized long description metadata>
+
+            Args:
+                <param name>:
+                    <param description>
+
+                    __metadata__:
+                        <YAML serialized param description metadata>
+            ""
+    """
+
+    res = {}
+    for idx, x in enumerate(doc.params):
+        meta = _yaml_metadata_regex.match(x.description)
+
+        if meta is not None:
+            try:
+                res[x.arg_name] = {
+                    "idx": idx,
+                    "name": x.arg_name,
+                    "default": x.default,
+                    "description": meta.group(1),
+                    "meta": yaml.safe_load(meta.group(2)),
+                }
+            except:
+                pass
+
+        if x.arg_name not in res:
+            res[x.arg_name] = {"idx": idx, "name": x.arg_name, "default": x.default, "description": x.description}
+
+        if idx == 0:
+            if doc.long_description is not None:
+                meta = _yaml_metadata_regex.match(doc.long_description)
+                if meta is not None:
+                    try:
+                        res[x.arg_name]["__workflow_meta__"] = {
+                            "short_description": doc.short_description,
+                            "long_description": meta.group(1),
+                            "meta": yaml.safe_load(meta.group(2)),
+                        }
+                    except:
+                        pass
+
+            if "__workflow_meta__" not in res[x.arg_name]:
+                res[x.arg_name]["__workflow_meta__"] = {
+                    "short_description": doc.short_description,
+                    "long_description": doc.long_description,
+                }
+
+    return res
+
+
+def returns_metadata_from_docstring(doc: Docstring) -> Dict[str, Dict[str, Any]]:
+    res = {}
+    for idx, x in enumerate(doc.many_returns):
+        meta = _yaml_metadata_regex.match(x.description)
+        if meta is not None:
+            try:
+                tmp = {
+                    "idx": idx,
+                    "name": x.return_name,
+                    "description": meta.group(1),
+                    "meta": yaml.safe_load(meta.group(2)),
+                }
+                res[x.return_name] = tmp
+                continue
+            except Exception as e:
+                print(e)
+                pass
+
+        res[x.return_name] = {"idx": idx, "name": x.return_name, "description": x.description}
+
+    return res
