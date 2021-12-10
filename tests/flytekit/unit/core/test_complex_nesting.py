2ae8c5a078860c8add14ad2ef004691bc10f8c9a
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2021-12-10 12:05:01 -0800

    dynamic compile
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- tests/flytekit/unit/core/test_complex_nesting.py
+++ tests/flytekit/unit/core/test_complex_nesting.py
@@ -3,10 +3,11 @@ from typing import List
 
 from dataclasses_json import dataclass_json
 
+from flytekit.core.context_manager import ExecutionState, FlyteContextManager, Image, ImageConfig, SerializationSettings
+from flytekit.core.dynamic_workflow_task import dynamic
+from flytekit.core.type_engine import TypeEngine
 from flytekit.types.directory import FlyteDirectory
 from flytekit.types.file import FlyteFile
-from flytekit.core.type_engine import TypeEngine
-from flytekit.core.context_manager import FlyteContextManager
 
 
 @dataclass_json
@@ -44,20 +45,30 @@ class MyInput:
     proxy_params: MyProxyParameters
 
 
+level1b_product = FlyteFile("/tmp/complex/product")
+apriori = MyAprioriConfiguration(
+    static_data_dir=FlyteDirectory("/tmp/apriori/static_data"),
+    external_data_dir=FlyteDirectory("/tmp/apriori/external_data"),
+)
+proxy_c = MyProxyConfiguration(splat_data_dir="/tmp/proxy_splat", apriori_file="/opt/config/a_file")
+proxy_p = MyProxyParameters(id="pp_id", job_i_step=1)
+
+my_input = MyInput(
+    level1b_product=level1b_product,
+    apriori_config=apriori,
+    proxy_config=proxy_c,
+    proxy_params=proxy_p,
+)
+
+my_input2 = MyInput(
+    level1b_product=level1b_product,
+    apriori_config=apriori,
+    proxy_config=proxy_c,
+    proxy_params=proxy_p,
+)
+
+
 def test_dataclass_complex_transform():
-    level1b_product = FlyteFile("/tmp/complex/product")
-    apriori = MyAprioriConfiguration(static_data_dir=FlyteDirectory("/tmp/apriori/static_data"),
-                                     external_data_dir=FlyteDirectory("/tmp/apriori/external_data"))
-    proxy_c = MyProxyConfiguration(splat_data_dir="/tmp/proxy_splat", apriori_file="/opt/config/a_file")
-    proxy_p = MyProxyParameters(id="pp_id", job_i_step=1)
-
-    my_input = MyInput(
-        level1b_product=level1b_product,
-        apriori_config=apriori,
-        proxy_config=proxy_c,
-        proxy_params=proxy_p,
-    )
-
     ctx = FlyteContextManager.current_context()
     literal_type = TypeEngine.to_literal_type(MyInput)
     first_literal = TypeEngine.to_literal(ctx, my_input, MyInput, literal_type)
@@ -72,5 +83,45 @@ def test_dataclass_complex_transform():
     converted_back_2 = TypeEngine.to_python_value(ctx, second_literal, MyInput)
     print(converted_back_2)
 
+    input_list = [my_input, my_input2]
+    input_list_type = TypeEngine.to_literal_type(List[MyInput])
+    literal_list_1 = TypeEngine.to_literal(ctx, input_list, List[MyInput], input_list_type)
+    print(literal_list_1)
 
 
+def test_two():
+    @dynamic
+    def dt1(a: List[MyInput]) -> List[FlyteFile]:
+        x = []
+        for aa in a:
+            x.append(aa.level1b_product)
+
+        return x
+
+    with FlyteContextManager.with_context(
+        FlyteContextManager.current_context().with_serialization_settings(
+            SerializationSettings(
+                project="test_proj",
+                domain="test_domain",
+                version="abc",
+                image_config=ImageConfig(Image(name="name", fqn="image", tag="name")),
+                env={},
+            )
+        )
+    ) as ctx:
+        with FlyteContextManager.with_context(
+            ctx.with_execution_state(
+                ctx.execution_state.with_params(
+                    mode=ExecutionState.Mode.TASK_EXECUTION,
+                    additional_context={
+                        "dynamic_addl_distro": "s3://my-s3-bucket/fast/123",
+                        "dynamic_dest_dir": "/User/flyte/workflows",
+                    },
+                )
+            )
+        ) as ctx:
+            input_literal_map = TypeEngine.dict_to_literal_map(
+                ctx, d={"a": [my_input, my_input2]}, guessed_python_types={"a": List[MyInput]}
+            )
+            dynamic_job_spec = dt1.dispatch_execute(ctx, input_literal_map)
+            print(dynamic_job_spec)
