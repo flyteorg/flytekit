c2a0de7138887ded6f127d179919e02a9bcbd6ba
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2021-12-10 11:49:19 -0800

    add tiny test
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- tests/flytekit/unit/core/test_complex_nesting.py
+++ tests/flytekit/unit/core/test_complex_nesting.py
@@ -5,50 +5,72 @@ from dataclasses_json import dataclass_json
 
 from flytekit.types.directory import FlyteDirectory
 from flytekit.types.file import FlyteFile
+from flytekit.core.type_engine import TypeEngine
+from flytekit.core.context_manager import FlyteContextManager
 
 
 @dataclass_json
 @dataclass
-class Level2ProxyConfiguration:
+class MyProxyConfiguration:
     # File and directory paths kept as 'str' so Flyte doesn't manage these static resources
     splat_data_dir: str
-    atmosphere_apriori_file: str
-    isrf_lut_file: str  # ISRF lookup table required for methaneair runs
+    apriori_file: str
 
 
 @dataclass_json
 @dataclass
-class Level2ProxyParameters:
+class MyProxyParameters:
     id: str  # unique identifier for this collection
     job_i_step: int  # step size
-    job_j_step: int  # step size
-    control_template: str  # name of the control template to use from 'templates' folder
 
 
 @dataclass_json
 @dataclass
-class Level2AprioriConfiguration:
+class MyAprioriConfiguration:
     # Directory with static data directories.
     # TODO: Identify what this data is, possibly split into multiple values if necessary.
     static_data_dir: FlyteDirectory
     # Directory for external dynamic data.
     # TODO: Is this just GEOS data? If so, do we need all of it? Can we preprocess?
     external_data_dir: FlyteDirectory
-    # Define vertical grid
-    lmx: int
-    # Albedo Wavelength
-    albedo_wavelengths: List[float]
 
 
 @dataclass_json
 @dataclass
-class Level2Input:
+class MyInput:
     level1b_product: FlyteFile
-    apriori_config: Level2AprioriConfiguration
-    proxy_config: Level2ProxyConfiguration
-    proxy_params: Level2ProxyParameters
-    level2_output_uri: str
+    apriori_config: MyAprioriConfiguration
+    proxy_config: MyProxyConfiguration
+    proxy_params: MyProxyParameters
 
 
 def test_dataclass_complex_transform():
-    pass
+    level1b_product = FlyteFile("/tmp/complex/product")
+    apriori = MyAprioriConfiguration(static_data_dir=FlyteDirectory("/tmp/apriori/static_data"),
+                                     external_data_dir=FlyteDirectory("/tmp/apriori/external_data"))
+    proxy_c = MyProxyConfiguration(splat_data_dir="/tmp/proxy_splat", apriori_file="/opt/config/a_file")
+    proxy_p = MyProxyParameters(id="pp_id", job_i_step=1)
+
+    my_input = MyInput(
+        level1b_product=level1b_product,
+        apriori_config=apriori,
+        proxy_config=proxy_c,
+        proxy_params=proxy_p,
+    )
+
+    ctx = FlyteContextManager.current_context()
+    literal_type = TypeEngine.to_literal_type(MyInput)
+    first_literal = TypeEngine.to_literal(ctx, my_input, MyInput, literal_type)
+    print(first_literal)
+
+    converted_back_1 = TypeEngine.to_python_value(ctx, first_literal, MyInput)
+    print(converted_back_1)
+
+    second_literal = TypeEngine.to_literal(ctx, converted_back_1, MyInput, literal_type)
+    print(second_literal)
+
+    converted_back_2 = TypeEngine.to_python_value(ctx, second_literal, MyInput)
+    print(converted_back_2)
+
+
+
