24980ed34616c168b472ffdd16d9145fab1fec46
Kevin Su <pingsutw@apache.org>
2021-12-22 18:02:59 +0800

    Add bigquery plugin
    Signed-off-by: Kevin Su <pingsutw@apache.org>

---
+++ plugins/flytekit-bigquery/flytekitplugins/bigquery/task.py
@@ -0,0 +1,81 @@
+from dataclasses import dataclass
+from typing import Any, Dict, Optional, Type
+
+from google.cloud import bigquery
+from google.protobuf.struct_pb2 import Struct
+
+from flytekit.extend import SerializationSettings, SQLTask
+from flytekit.models import task as _task_model
+from flytekit.types.schema import FlyteSchema
+
+
+@dataclass
+class BigQueryConfig(object):
+    """
+    BigQueryConfig should be used to configure a BigQuery Task.
+    """
+
+    Location: Optional[str]
+    ProjectID: Optional[str]
+    QueryJobConfig: Optional[bigquery.QueryJobConfig] = None
+
+
+class BigQueryTask(SQLTask[BigQueryConfig]):
+    """
+    This is the simplest form of a BigQuery Task, that can be used even for tasks that do not produce any output.
+    """
+
+    # This task is executed using the BigQuery handler in the backend.
+    _TASK_TYPE = "bigquery_query_job_task"
+
+    def __init__(
+        self,
+        name: str,
+        query_template: str,
+        task_config: Optional[BigQueryConfig],
+        inputs: Optional[Dict[str, Type]] = None,
+        output_schema_type: Optional[Type[FlyteSchema]] = None,
+        **kwargs,
+    ):
+        """
+        To be used to query BigQuery Tables.
+
+        :param name: Name of this task, should be unique in the project
+        :param query_template: The actual query to run. We use Flyte's Golang templating format for Query templating.
+          Refer to the templating documentation
+        :param task_config: BigQueryConfig object
+        :param inputs: Name and type of inputs specified as an ordered dictionary
+        :param output_schema_type: If some data is produced by this query, then you can specify the output schema type
+        :param kwargs: All other args required by Parent type - SQLTask
+        """
+        outputs = None
+        if output_schema_type is not None:
+            outputs = {
+                "results": output_schema_type,
+            }
+        super().__init__(
+            name=name,
+            task_config=task_config,
+            query_template=query_template,
+            inputs=inputs,
+            outputs=outputs,
+            task_type=self._TASK_TYPE,
+            **kwargs,
+        )
+        self._output_schema_type = output_schema_type
+
+    def get_custom(self, settings: SerializationSettings) -> Dict[str, Any]:
+        config: dict = self.task_config.QueryJobConfig.to_api_repr()["query"]
+        config.update(
+            {
+                "Location": self.task_config.Location,
+                "ProjectID": self.task_config.ProjectID,
+            }
+        )
+        s = Struct()
+        s.update(config)
+        return s
+
+    def get_sql(self, settings: SerializationSettings) -> Optional[_task_model.Sql]:
+        sql = _task_model.Sql(statement=self.query_template, dialect=_task_model.Sql.Dialect.ANSI)
+        return sql
---
+++ plugins/flytekit-bigquery/tests/test_bigquery.py
@@ -0,0 +1,78 @@
+from collections import OrderedDict
+
+import pytest
+from flytekitplugins.bigquery import BigQueryConfig, BigQueryTask
+from google.cloud.bigquery import QueryJobConfig
+from google.protobuf.struct_pb2 import Struct
+
+from flytekit import kwtypes, workflow
+from flytekit.extend import Image, ImageConfig, SerializationSettings, get_serializable
+from flytekit.types.schema import FlyteSchema
+
+query_template = """
+            insert overwrite directory '{{ .rawOutputDataPrefix }}' stored as parquet
+            select *
+            from my_table
+            where ds = '{{ .Inputs.ds }}'
+        """
+
+
+def test_serialization():
+    bigquery_task = BigQueryTask(
+        name="flytekit.demo.bigquery_task.query",
+        inputs=kwtypes(ds=str),
+        task_config=BigQueryConfig(
+            ProjectID="Flyte", Location="Asia", QueryJobConfig=QueryJobConfig(allow_large_results=True)
+        ),
+        query_template=query_template,
+        # the schema literal's backend uri will be equal to the value of .raw_output_data
+        output_schema_type=FlyteSchema,
+    )
+
+    @workflow
+    def my_wf(ds: str) -> FlyteSchema:
+        return bigquery_task(ds=ds)
+
+    default_img = Image(name="default", fqn="test", tag="tag")
+    serialization_settings = SerializationSettings(
+        project="proj",
+        domain="dom",
+        version="123",
+        image_config=ImageConfig(default_image=default_img, images=[default_img]),
+        env={},
+    )
+
+    task_spec = get_serializable(OrderedDict(), serialization_settings, bigquery_task)
+
+    assert "{{ .rawOutputDataPrefix" in task_spec.template.sql.statement
+    assert "insert overwrite directory" in task_spec.template.sql.statement
+    assert task_spec.template.sql.dialect == task_spec.template.sql.Dialect.ANSI
+    s = Struct()
+    s.update({"ProjectID": "Flyte", "Location": "Asia", "allowLargeResults": True})
+    assert task_spec.template.custom == s
+    assert len(task_spec.template.interface.inputs) == 1
+    assert len(task_spec.template.interface.outputs) == 1
+
+    admin_workflow_spec = get_serializable(OrderedDict(), serialization_settings, my_wf)
+    assert admin_workflow_spec.template.interface.outputs["o0"].type.schema is not None
+    assert admin_workflow_spec.template.outputs[0].var == "o0"
+    assert admin_workflow_spec.template.outputs[0].binding.promise.node_id == "n0"
+    assert admin_workflow_spec.template.outputs[0].binding.promise.var == "results"
+
+
+def test_local_exec():
+    bigquery_task = BigQueryTask(
+        name="flytekit.demo.bigquery_task.query2",
+        inputs=kwtypes(ds=str),
+        query_template=query_template,
+        task_config=BigQueryConfig(ProjectID="Flyte", Location="Asia"),
+        # the schema literal's backend uri will be equal to the value of .raw_output_data
+        output_schema_type=FlyteSchema,
+    )
+
+    assert len(bigquery_task.interface.inputs) == 1
+    assert len(bigquery_task.interface.outputs) == 1
+
+    # will not run locally
+    with pytest.raises(Exception):
+        bigquery_task()
