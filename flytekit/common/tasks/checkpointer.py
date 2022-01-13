cf93b6390b9b996dd45921e8553754b0a68433ff
Ketan Umare <ketan.umare@gmail.com>
2022-01-13 14:37:35 -0800

    print
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

--- flytekit/common/tasks/checkpointer.py
+++ flytekit/common/tasks/checkpointer.py
@@ -4,8 +4,6 @@ import typing
 from abc import abstractmethod
 from pathlib import Path
 
-from flytekit import logger
-
 
 class Checkpoint(object):
     """
@@ -97,7 +95,7 @@ class SyncCheckpoint(Checkpoint):
         from flytekit.core.context_manager import FlyteContextManager
 
         if self._checkpoint_src is None or self._checkpoint_src == "":
-            logger.debug(f"Previous checkpoint not available.")
+            print(f"Previous checkpoint not available.")
             return None
 
         if self._prev_download_path:
