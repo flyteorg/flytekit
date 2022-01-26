57fa31e52ea4025fac7a8e03a25e8fa3987e63d1
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2022-01-26 14:58:03 -0800

    nit
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- plugins/flytekit-greatexpectations/tests/conftest.py
+++ plugins/flytekit-greatexpectations/tests/conftest.py
@@ -2,5 +2,5 @@ import os
 
 
 def pytest_sessionstart():
-    # Use Structured Datasets. This should get picked up automatically by the
+    # Use Structured Datasets. This should get picked up automatically by the loading of structured_dataset.py
     os.environ["FLYTE_SDK_USE_STRUCTURED_DATASET"] = "TRUE"
