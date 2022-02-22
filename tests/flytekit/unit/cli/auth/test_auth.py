c7e8b6d42e0fe42f64ea9b3c9f4347f94292f981
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2022-02-22 14:51:04 -0800

    fix test for ci, no keyring backend
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- tests/flytekit/unit/cli/auth/test_auth.py
+++ tests/flytekit/unit/cli/auth/test_auth.py
@@ -1,6 +1,8 @@
 import re
 from multiprocessing import Queue as _Queue
 
+from mock import patch
+
 from flytekit.clis.auth import auth as _auth
 
 try:  # Python 3
@@ -35,10 +37,10 @@ def test_oauth_http_server():
     assert test_auth_code == auth_code
 
 
-def test_clear():
+@patch("flytekit.clis.auth.auth._keyring.get_password")
+def test_clear(mock_get_password):
+    mock_get_password.return_value = "token"
     ac = _auth.AuthorizationClient()
-    ac._credentials = "hello"
-    ac._refresh_token = "world"
     ac.clear()
     assert ac.credentials is None
     assert not ac.can_refresh_token
