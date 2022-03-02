301bb7ab5ac80281bfc04dd934f9b0d2bb4b5dcf
Kevin Su <pingsutw@apache.org>
2022-03-03 04:31:38 +0800

    Updated logger
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/core/context_manager.py
+++ flytekit/core/context_manager.py
@@ -38,7 +38,7 @@ from flytekit.core.data_persistence import FileAccessProvider, default_local_fil
 from flytekit.core.node import Node
 from flytekit.interfaces.cli_identifiers import WorkflowExecutionIdentifier
 from flytekit.interfaces.stats import taggable
-from flytekit.loggers import user_space_logger
+from flytekit.loggers import logger, user_space_logger
 from flytekit.models.core import identifier as _identifier
 
 # TODO: resolve circular import from flytekit.core.python_auto_container import TaskResolverMixin
@@ -156,7 +156,7 @@ class ExecutionParameters(object):
     class Builder(object):
         stats: taggable.TaggableStats
         execution_date: datetime
-        logging: user_space_logger
+        logging: logger
         execution_id: str
         attrs: typing.Dict[str, typing.Any]
         working_dir: typing.Union[os.PathLike, utils.AutoDeletingTempDir]
@@ -245,7 +245,7 @@ class ExecutionParameters(object):
         return self._stats
 
     @property
-    def logging(self) -> user_space_logger:
+    def logging(self) -> logger:
         """
         A handle to a useful logging object.
         TODO: Usage examples
@@ -898,7 +898,7 @@ class FlyteContextManager(object):
         ctx.set_stackframe(f)
         FlyteContextManager._OBJS.append(ctx)
         t = "\t"
-        user_space_logger.debug(
+        logger.debug(
             f"{t * ctx.level}[{len(FlyteContextManager._OBJS)}] Pushing context - {'compile' if ctx.compilation_state else 'execute'}, branch[{ctx.in_a_condition}], {ctx.get_origin_stackframe_repr()}"
         )
         return ctx
@@ -907,7 +907,7 @@ class FlyteContextManager(object):
     def pop_context() -> FlyteContext:
         ctx = FlyteContextManager._OBJS.pop()
         t = "\t"
-        user_space_logger.debug(
+        logger.debug(
             f"{t * ctx.level}[{len(FlyteContextManager._OBJS) + 1}] Popping context - {'compile' if ctx.compilation_state else 'execute'}, branch[{ctx.in_a_condition}], {ctx.get_origin_stackframe_repr()}"
         )
         if len(FlyteContextManager._OBJS) == 0:
