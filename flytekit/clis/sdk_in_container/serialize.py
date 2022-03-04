bf6fb48ef345eae73bd9aa861676f7b3eb17172e
Ketan Umare <ketan.umare@gmail.com>
2022-03-04 11:00:17 -0800

    updated
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

--- flytekit/clis/sdk_in_container/serialize.py
+++ flytekit/clis/sdk_in_container/serialize.py
@@ -127,7 +127,7 @@ def serialize(ctx, image, local_source_root, in_container_config_path, in_contai
     ctx.obj[CTX_IMAGE] = image
     ctx.obj[CTX_LOCAL_SRC_ROOT] = local_source_root
     ctx.obj[CTX_FLYTEKIT_VIRTUALENV_ROOT] = in_container_virtualenv_root
-    ctx.obj[CTX_PYTHON_INTERPRETER] = ctx.obj[CTX_FLYTEKIT_VIRTUALENV_ROOT] + "/bin/python3"
+    ctx.obj[CTX_PYTHON_INTERPRETER] = os.path.join(in_container_virtualenv_root, "/bin/python3")
     click.echo("Serializing Flyte elements with image {}".format(image))
 
     if not in_container_virtualenv_root:
