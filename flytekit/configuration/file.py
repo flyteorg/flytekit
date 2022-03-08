4e5fad0c2e107bd1ad121443ea916d6525da46e1
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2022-03-07 16:14:42 -0800

    default
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- flytekit/configuration/file.py
+++ flytekit/configuration/file.py
@@ -5,8 +5,10 @@ import configparser as _configparser
 import os
 import typing
 from dataclasses import dataclass
+from pathlib import Path
 
 from flytekit.exceptions import user as _user_exceptions
+from flytekit.loggers import logger
 
 
 @dataclass
@@ -131,10 +133,22 @@ class ConfigFile(object):
 def get_config_file(c: typing.Union[str, ConfigFile, None]) -> typing.Optional[ConfigFile]:
     """
     Checks if the given argument is a file or a configFile and returns a loaded configFile else returns None
-
-    # TODO support automatic loading from Home dir .flyte/
     """
     if c is None:
+        # See if there's a config file in the current directory where Python is being run from
+        current_location_config = Path("flytekit.config")
+        if current_location_config.exists():
+            logger.debug(f"Using configuration from Python process root {current_location_config.absolute()}")
+            return ConfigFile(current_location_config.absolute())
+
+        # If not, see if there's a config in the user's home directory
+        home = Path.home()
+        home_dir_config = Path(home, ".flyte/config")  # _default_config_file_name in main.py
+        if home_dir_config.exists():
+            logger.debug(f"Using configuration from home directory {home_dir_config.absolute()}")
+            return ConfigFile(home_dir_config.absolute())
+
+        # If not, then return None and let caller handle
         return None
     if isinstance(c, str):
         return ConfigFile(c)
