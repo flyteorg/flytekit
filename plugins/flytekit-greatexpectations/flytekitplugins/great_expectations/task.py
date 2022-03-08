1aeddb6d67b8efc80046c6b6fc2f6bf71b531037
SmritiSatyanV <smriti@union.ai>
2022-03-08 17:54:39 +0530

    Updates based on comments
    Reverted NumPy to numpy
    Changed 'flytectl' to Flytectl
    Updated doc-requirements.txt to match the master
    Signed-off-by: SmritiSatyanV <smriti@union.ai>

--- plugins/flytekit-greatexpectations/flytekitplugins/great_expectations/task.py
+++ plugins/flytekit-greatexpectations/flytekitplugins/great_expectations/task.py
@@ -140,7 +140,7 @@ class GreatExpectationsTask(PythonInstanceTask[BatchRequestConfig]):
                 dataset.remote_path, self._local_file_path, is_multipart=True
             )
 
-        # DataFrame (pandas, Spark, etc.)
+        # DataFrame (Pandas, Spark, etc.)
         else:
             if not os.path.exists(self._local_file_path):
                 os.makedirs(self._local_file_path, exist_ok=True)
