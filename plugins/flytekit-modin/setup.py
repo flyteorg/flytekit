4abd55b916283b69d3c8e4026db2db3549540623
SmritiSatyanV <smriti@union.ai>
2022-03-15 15:28:19 +0530

    Fixed error
    Added whitespace to fix lint error
    Signed-off-by: SmritiSatyanV <smriti@union.ai>

--- plugins/flytekit-modin/setup.py
+++ plugins/flytekit-modin/setup.py
@@ -4,7 +4,7 @@ PLUGIN_NAME = "modin"
 
 microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
 
-plugin_requires = ["flytekit>=0.22.0,<1.0.0", "modin>=0.13.0","modin", "ray", "fsspec"]
+plugin_requires = ["flytekit>=0.22.0,<1.0.0", "modin>=0.13.0", "modin", "ray", "fsspec"]
 
 __version__ = "0.0.0+develop"
 
