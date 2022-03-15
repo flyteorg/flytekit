0405ef24121e741b38f73ef94f746706993cb565
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2022-03-14 17:11:59 -0700

    Turn on StructuredDataset (#885)
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- plugins/flytekit-spark/flytekitplugins/spark/__init__.py
+++ plugins/flytekit-spark/flytekitplugins/spark/__init__.py
@@ -1,7 +1,5 @@
 from flytekit.configuration import internal as _internal
 
 from .schema import SparkDataFrameSchemaReader, SparkDataFrameSchemaWriter, SparkDataFrameTransformer  # noqa
+from .sd_transformers import ParquetToSparkDecodingHandler, SparkToParquetEncodingHandler
 from .task import Spark, new_spark_session  # noqa
-
-if _internal.LocalSDK.USE_STRUCTURED_DATASET.read():
-    from .sd_transformers import ParquetToSparkDecodingHandler, SparkToParquetEncodingHandler
