8f298a209ec57e672f0d41f0f9a7ba8a0c3715cf
Tim Bauer <tim.bauer@wolt.com>
2022-03-24 17:35:18 +0100

    Remove old filter test
    Signed-off-by: Tim Bauer <tim.bauer@wolt.com>

--- tests/flytekit/unit/tools/test_fast_registration.py
+++ tests/flytekit/unit/tools/test_fast_registration.py
@@ -1,20 +1,6 @@
 import tarfile
 
-from flytekit.tools.fast_registration import filter_tar_file_fn, get_additional_distribution_loc
-
-
-def testfilter_tar_file_fn():
-    valid_tarinfo = tarfile.TarInfo(name="foo.py")
-    assert filter_tar_file_fn(valid_tarinfo) is not None
-
-    invalid_tarinfo = tarfile.TarInfo(name="foo.pyc")
-    assert not filter_tar_file_fn(invalid_tarinfo)
-
-    invalid_tarinfo = tarfile.TarInfo(name=".cache/foo")
-    assert not filter_tar_file_fn(invalid_tarinfo)
-
-    invalid_tarinfo = tarfile.TarInfo(name="__pycache__")
-    assert not filter_tar_file_fn(invalid_tarinfo)
+from flytekit.tools.fast_registration import get_additional_distribution_loc
 
 
 def test_get_additional_distribution_loc():
