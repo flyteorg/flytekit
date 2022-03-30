72144dc8da2ad4e12829f034d26e880bc8038a5b
Kevin Su <pingsutw@apache.org>
2022-03-30 22:40:28 +0800

    Fixed tests
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- tests/flytekit/unit/deck/test_deck.py
+++ tests/flytekit/unit/deck/test_deck.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 from flytekit import Deck, FlyteContextManager
 from flytekit.deck import TopFrameRenderer
-from flytekit.deck.deck import _output_deck, default_deck
+from flytekit.deck.deck import _output_deck
 
 
 def test_deck():
     df = pd.DataFrame({"Name": ["Tom", "Joseph"], "Age": [1, 22]})
     ctx = FlyteContextManager.current_context()
-    ctx.user_space_params._decks = [default_deck]
+    ctx.user_space_params._decks = [ctx.user_space_params.default_deck]
     renderer = TopFrameRenderer()
     deck_name = "test"
     deck = Deck(deck_name)
