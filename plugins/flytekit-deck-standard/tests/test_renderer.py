163aa393a1c4a263f5e1a0c4fedecb21abeb6bf6
Kevin Su <pingsutw@apache.org>
2022-04-01 05:47:31 +0800

    Fixed tests
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- plugins/flytekit-deck-standard/tests/test_renderer.py
+++ plugins/flytekit-deck-standard/tests/test_renderer.py
@@ -6,16 +6,16 @@ df = pd.DataFrame({"Name": ["Tom", "Joseph"], "Age": [1, 22]})
 
 
 def test_frame_profiling_renderer():
-    renderer = FrameProfilingRenderer(df)
-    assert "Profile Report Generated With The `Pandas-Profiling`" in renderer.to_html().title()
+    renderer = FrameProfilingRenderer()
+    assert "Profile Report Generated With The `Pandas-Profiling`" in renderer.to_html(df).title()
 
 
 def test_markdown_renderer():
     md_text = "#Hello Flyte\n##Hello Flyte\n###Hello Flyte"
-    renderer = MarkdownRenderer(md_text)
-    assert renderer.to_html() == markdown.markdown(md_text)
+    renderer = MarkdownRenderer()
+    assert renderer.to_html(md_text) == markdown.markdown(md_text)
 
 
 def test_box_renderer():
-    renderer = BoxRenderer(df, "Name")
-    assert "Plotlyconfig = {Mathjaxconfig: 'Local'}" in renderer.to_html().title()
+    renderer = BoxRenderer("Name")
+    assert "Plotlyconfig = {Mathjaxconfig: 'Local'}" in renderer.to_html(df).title()
