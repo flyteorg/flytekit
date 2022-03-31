78292811334c241245d7d803fcde2d476467745e
Mike Zhong <mzhong@embarkvet.com>
2022-03-31 13:42:57 -0400

    more comments
    Signed-off-by: Mike Zhong <mzhong@embarkvet.com>

--- flytekit/extras/tasks/shell.py
+++ flytekit/extras/tasks/shell.py
@@ -255,7 +255,9 @@ class ShellTask(PythonInstanceTask[T]):
     def post_execute(self, user_params: ExecutionParameters, rval: typing.Any) -> typing.Any:
         return self._config_task_instance.post_execute(user_params, rval)
 
-
+# The portable_shell_task is an instance of ShellTask which wraps a 'pure' shell script
+# This utility function allows for the specification of env variables, arguments, and the actual script within the
+# workflow definition rather than at `ShellTask` instantiation
 portable_shell_task = ShellTask(
     name="portable_shell_task_instance",
     debug=True,
