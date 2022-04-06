94eeaea197791f9a8772775a731bb8be3c166539
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2022-04-05 23:54:51 -0700

    Rename internal functions and add version to suffix in call to data proxy service
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- flytekit/clis/sdk_in_container/run.py
+++ flytekit/clis/sdk_in_container/run.py
@@ -81,7 +81,7 @@ def run(
     module = os.path.splitext(filename)[0]
 
     # Load code naively, i.e. without taking into account the
-    wf_entity = load_naive_entity(module, workflow_name)
+    wf_entity = _load_naive_entity(module, workflow_name)
 
     if help:
         # TODO Write a custom help message containing the types of inputs, if any, and an example of how to specify
@@ -91,11 +91,11 @@ def run(
 
     config_obj = PlatformConfig.auto()
     client = friendly.SynchronousFlyteClient(config_obj)
+    version = script_mode.hash_script_file(filename)
     # TODO: the data proxy creates a fixed path. We should add a notion of randomness in the suffix. Haytham is fixing it.
     upload_location: CreateUploadLocationResponse = client.create_upload_location(
-        project=project, domain=domain, suffix="scriptmode.tar.gz"
+        project=project, domain=domain, suffix=f"scriptmode-{version}.tar.gz"
     )
-    version = script_mode.hash_script_file(filename)
     serialization_settings = SerializationSettings(
         image_config=image_config,
         fast_serialization_settings=FastSerializationSettings(
@@ -112,15 +112,12 @@ def run(
     full_remote_path = upload_location.native_url
     script_mode.fast_register_single_script(version, wf_entity, full_remote_path)
 
-    inputs = parse_workflow_inputs(click_ctx, wf_entity)
+    inputs = _parse_workflow_inputs(click_ctx, wf_entity)
     execution = remote.execute(wf, inputs=inputs, project=project, domain=domain, wait=True)
-    dump_flyte_remote_snippet(execution, project, domain)
+    _dump_flyte_remote_snippet(execution, project, domain)
 
 
-def parse_workflow_inputs(click_ctx, wf_entity):
-    """
-    TODO: parse extraneous inputs from the click context and cast them to the appropriate types.
-    """
+def _parse_workflow_inputs(click_ctx, wf_entity):
     type_hints = {k: TypeEngine.guess_python_type(v.type) for k, v in wf_entity.interface.inputs.items()}
     args = {}
     for i in range(0, len(click_ctx.args), 2):
@@ -138,7 +135,7 @@ def parse_workflow_inputs(click_ctx, wf_entity):
     return args
 
 
-def load_naive_entity(module_name: str, workflow_name: str) -> WorkflowBase:
+def _load_naive_entity(module_name: str, workflow_name: str) -> WorkflowBase:
     """
     Load the workflow of a the script file.
     N.B.: it assumes that the file is self-contained, in other words, there are no relative imports.
@@ -152,7 +149,7 @@ def load_naive_entity(module_name: str, workflow_name: str) -> WorkflowBase:
     return module_loader.load_object_from_module(f"{module_name}.{workflow_name}")
 
 
-def dump_flyte_remote_snippet(execution: FlyteWorkflowExecution, project: str, domain: str):
+def _dump_flyte_remote_snippet(execution: FlyteWorkflowExecution, project: str, domain: str):
     click.secho(
         f"""
 In order to have programmatic access to the execution, use the following snippet:
