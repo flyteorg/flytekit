6d1c4c81b5ca5850032d67053d6d5350b553328d
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2022-04-07 13:54:07 -0700

    write back updates to singleton
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- flytekit/clients/raw.py
+++ flytekit/clients/raw.py
@@ -191,10 +191,6 @@ class RawSynchronousFlyteClient(object):
             # will have read them into its _credentials field, but it won't be in the RawSynchronousFlyteClient's
             # metadata field yet. Therefore, if there's a mismatch, copy it over.
             self.set_access_token(client.credentials.access_token, authorization_header_key)
-            # However, after copying over credentials from the AuthorizationClient, we have to clear it to avoid the
-            # scenario where the stored credentials in the keyring are expired. If that's the case, then we only try
-            # them once (because client here is a singleton), and the next time, we'll do one of the two other conditions
-            # below.
             return
         elif client.can_refresh_token:
             client.refresh_access_token()
