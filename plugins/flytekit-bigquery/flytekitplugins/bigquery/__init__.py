3d84207d278a9199db5504b0bd0e5cbf584f0d50
Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>
2022-04-11 22:23:49 +0530

    Big query plugin
    Signed-off-by: Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>

--- plugins/flytekit-bigquery/flytekitplugins/bigquery/__init__.py
+++ plugins/flytekit-bigquery/flytekitplugins/bigquery/__init__.py
@@ -1,1 +1,18 @@
+"""
+========================
+Big query plugin
+========================
+Big query plugin
+
+.. currentmodule:: flytekitplugins.bigquery
+
+.. autosummary::
+
+    BigQueryConfig
+    BigQueryTask
+"""
+
+__all__ = ["BigQueryConfig", "BigQueryTask"]
+
+
 from .task import BigQueryConfig, BigQueryTask
