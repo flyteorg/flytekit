76848f0310a729614b15280956534c507ba06066
Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>
2022-04-12 14:07:54 +0530

    Adding Modin and Spark
    Signed-off-by: Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>

--- plugins/flytekit-modin/flytekitplugins/modin/__init__.py
+++ plugins/flytekit-modin/flytekitplugins/modin/__init__.py
@@ -1,1 +1,16 @@
+"""
+====================
+Modin
+====================
+Modin plugin
+
+.. currentmodule:: flytekitplugins.modin
+
+.. autosummary::
+
+   ModinPandasDataFrameTransformer
+"""
+
 from .schema import ModinPandasDataFrameTransformer
+
+__all__ = ["ModinPandasDataFrameTransformer"]
--- plugins/flytekit-spark/flytekitplugins/spark/__init__.py
+++ plugins/flytekit-spark/flytekitplugins/spark/__init__.py
@@ -1,3 +1,37 @@
+"""
+====================
+Spark
+====================
+Spark plugin
+
+.. currentmodule:: flytekitplugins.spark
+
+.. autosummary::
+
+   Spark
+   PysparkFunctionTask
+   new_spark_session
+   SparkDataFrameSchemaReader
+   SparkDataFrameSchemaWriter
+   SparkDataFrameTransformer
+   ParquetToSparkDecodingHandler
+   SparkToParquetEncodingHandler
+
+"""
+
+
+__all__ = [
+    "Spark",
+    "PysparkFunctionTask",
+    "new_spark_session",
+    "SparkDataFrameSchemaReader",
+    "SparkDataFrameSchemaWriter",
+    "SparkDataFrameTransformer",
+    "SparkToParquetEncodingHandler",
+    "ParquetToSparkDecodingHandler",
+]
+
+
 from flytekit.configuration import internal as _internal
 
 from .schema import SparkDataFrameSchemaReader, SparkDataFrameSchemaWriter, SparkDataFrameTransformer  # noqa
