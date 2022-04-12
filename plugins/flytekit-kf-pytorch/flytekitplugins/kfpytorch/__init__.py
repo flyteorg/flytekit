398a3649a89dad44bc0bcdcac086b68cac7c420a
Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>
2022-04-12 12:02:51 +0530

    Adding Pytorch
    Signed-off-by: Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>

--- plugins/flytekit-kf-pytorch/flytekitplugins/kfpytorch/__init__.py
+++ plugins/flytekit-kf-pytorch/flytekitplugins/kfpytorch/__init__.py
@@ -1,1 +1,17 @@
-from .task import PyTorch
+"""
+====================
+Pytorch
+====================
+Pytorch plugin
+
+.. currentmodule:: flytekitplugins.pytorchjob
+
+.. autosummary::
+
+   PyTorch
+   PyTorchFunctionTask
+"""
+
+from .task import PyTorch, PyTorchFunctionTask
+
+__all__ = ["PyTorch", "PyTorchFunctionTask"]
