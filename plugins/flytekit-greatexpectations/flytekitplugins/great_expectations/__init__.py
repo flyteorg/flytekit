18b095228c3499abc58e7322c05cc4ec913e2d11
Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>
2022-04-12 12:19:06 +0530

    Adding great expectations
    Signed-off-by: Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>

--- plugins/flytekit-greatexpectations/flytekitplugins/great_expectations/__init__.py
+++ plugins/flytekit-greatexpectations/flytekitplugins/great_expectations/__init__.py
@@ -1,2 +1,18 @@
+"""
+====================
+Great Expectations
+====================
+Great Expectations plugin
+
+.. currentmodule:: flytekitplugins.great_expectations
+
+.. autosummary::
+    :template: custom.rst
+
+
+"""
+
 from .schema import GreatExpectationsFlyteConfig, GreatExpectationsType  # noqa: F401
-from .task import BatchRequestConfig, GreatExpectationsTask  # noqa: F401
+from .task import BatchRequestConfig, GreatExpectationsTask
+
+__all__ = ["GreatExpectationsFlyteConfig", "GreatExpectationsType", "BatchRequestConfig", "GreatExpectationsTask"]
