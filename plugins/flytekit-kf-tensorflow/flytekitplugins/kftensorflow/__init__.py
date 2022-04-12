398b40c1087d6037d8a1954ef4303580f55173c4
Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>
2022-04-12 11:50:22 +0530

    Adding tensor flow
    Signed-off-by: Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>

--- plugins/flytekit-kf-tensorflow/flytekitplugins/kftensorflow/__init__.py
+++ plugins/flytekit-kf-tensorflow/flytekitplugins/kftensorflow/__init__.py
@@ -1,1 +1,17 @@
-from .task import TfJob
+"""
+====================
+Tensorflow
+====================
+Tensorflow plugin
+
+.. currentmodule:: flytekitplugins.tensorflowjob
+
+.. autosummary::
+
+   TfJob
+   TensorflowFunctionTask
+"""
+
+from .task import TensorflowFunctionTask, TfJob
+
+__all__ = ["TfJob", "TensorflowFunctionTask"]
