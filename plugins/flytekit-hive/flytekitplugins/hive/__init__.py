00d298111df83bf791ba3d84188409ac909a9043
Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>
2022-04-12 12:24:47 +0530

    Adding Hive task
    Signed-off-by: Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>

--- plugins/flytekit-hive/flytekitplugins/hive/__init__.py
+++ plugins/flytekit-hive/flytekitplugins/hive/__init__.py
@@ -1,1 +1,18 @@
+"""
+====================
+Hive Task
+====================
+Hive Task plugin
+
+.. currentmodule:: flytekitplugins.hive.task
+
+.. autosummary::
+
+   HiveConfig
+   HiveSelectTask
+   HiveTask
+"""
+
 from .task import HiveConfig, HiveSelectTask, HiveTask
+
+__all__ = ["HiveConfig", "HiveSelectTask", "HiveTask"]
