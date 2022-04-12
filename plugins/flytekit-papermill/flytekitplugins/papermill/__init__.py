c2af7c8c5fc9fc249e2a0983b9a4f21d9136e763
Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>
2022-04-12 11:28:42 +0530

    Adding papermill
    Signed-off-by: Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>

--- plugins/flytekit-papermill/flytekitplugins/papermill/__init__.py
+++ plugins/flytekit-papermill/flytekitplugins/papermill/__init__.py
@@ -1,1 +1,18 @@
+"""
+====================
+Papermill
+====================
+Papermill plugin
+
+.. currentmodule:: flytekitplugins.papermill
+
+.. autosummary::
+    :template: custom.rst
+
+   NotebookTask
+   record_outputs
+"""
+
 from .task import NotebookTask, record_outputs
+
+__all__ = ["NotebookTask", "record_outputs"]
