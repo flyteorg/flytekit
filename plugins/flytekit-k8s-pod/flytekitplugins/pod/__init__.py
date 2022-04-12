989ed9eb3be46c40669df5ec4fd18b5e882b8c59
Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>
2022-04-12 12:29:38 +0530

    Adding pod task
    Signed-off-by: Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>

--- plugins/flytekit-k8s-pod/flytekitplugins/pod/__init__.py
+++ plugins/flytekit-k8s-pod/flytekitplugins/pod/__init__.py
@@ -1,1 +1,17 @@
-from .task import Pod
+"""
+====================
+Pod
+====================
+Pod plugin
+
+.. currentmodule:: flytekitplugins.pod.task
+
+.. autosummary::
+
+   Pod
+   PodFunctionTask
+"""
+
+from .task import Pod, PodFunctionTask
+
+__all__ = ["Pod", "PodFunctionTask"]
