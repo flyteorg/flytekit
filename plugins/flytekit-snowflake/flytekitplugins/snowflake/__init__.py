f69974cc4a2c4dbbe7d1970d05fbd0440fc4306a
Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>
2022-04-12 11:12:28 +0530

    Adding snowflake
    Signed-off-by: Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>

--- plugins/flytekit-snowflake/flytekitplugins/snowflake/__init__.py
+++ plugins/flytekit-snowflake/flytekitplugins/snowflake/__init__.py
@@ -1,1 +1,18 @@
+"""
+====================
+Snowflake
+====================
+Snowflake plugin
+
+.. currentmodule:: flytekitplugins.snowflake
+
+.. autosummary::
+    :template: custom.rst
+
+   SnowflakeConfig
+   SnowflakeTask
+"""
+
 from .task import SnowflakeConfig, SnowflakeTask
+
+__all__ = ["SnowflakeConfig", "SnowflakeTask"]
--- plugins/flytekit-sqlalchemy/flytekitplugins/sqlalchemy/__init__.py
+++ plugins/flytekit-sqlalchemy/flytekitplugins/sqlalchemy/__init__.py
@@ -1,1 +1,22 @@
+"""
+====================
+SQL Alchemy
+====================
+SQL Alchemy
+
+.. currentmodule:: flytekitplugins.sqlalchemy
+
+.. autosummary::
+    :template: custom.rst
+
+   SQLAlchemyConfig
+   SQLAlchemyTask
+   SQLAlchemyTaskExecutor
+"""
+
+__all__ = ["SQLAlchemyConfig", "SQLAlchemyTask", "SQLAlchemyTaskExecutor"]
+
+
+from flytekitplugins.sqlalchemy.task import SQLAlchemyTaskExecutor
+
 from .task import SQLAlchemyConfig, SQLAlchemyTask
