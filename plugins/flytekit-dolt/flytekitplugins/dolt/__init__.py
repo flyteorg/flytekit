c5394fcba79abce56668dba309817eb1ab161936
Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>
2022-04-12 14:38:18 +0530

    Adding dolt
    Signed-off-by: Alekhya Sai Punnamaraju <alekhyasaip@gmail.com>

--- plugins/flytekit-dolt/flytekitplugins/dolt/__init__.py
+++ plugins/flytekit-dolt/flytekitplugins/dolt/__init__.py
@@ -1,1 +1,19 @@
+"""
+====================
+Dolt
+====================
+Dolt plugin
+
+.. currentmodule:: flytekitplugins.dolt.schema
+
+.. autosummary::
+
+   DoltConfig
+   DoltTable
+   DoltTableNameTransformer
+"""
+
+
+__all__ = ["DoltConfig", "DoltTable", "DoltTableNameTransformer"]
+
 from .schema import DoltConfig, DoltTable, DoltTableNameTransformer
