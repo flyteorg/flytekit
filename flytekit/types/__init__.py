eb1c7cc964d4aeea7f6771b900aa34f54ab32131
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2022-04-21 15:59:48 -0700

    undo types init file
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- flytekit/types/__init__.py
+++
@@ -1,4 +0,0 @@
-from flytekit.types.directory import FlyteDirectory
-from flytekit.types.file import FlyteFile
-from flytekit.types.pickle import FlytePickle
-from flytekit.types.structured.structured_dataset import StructuredDataset, StructuredDatasetTransformerEngine
