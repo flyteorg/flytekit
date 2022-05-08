3940ef241ae63a3ba3abce1904133fce0f910e81
Haytham Abuelfutuh <haytham@afutuh.com>
2022-05-08 08:09:46 -0700

    more tests
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- tests/flytekit/unit/configuration/test_internal.py
+++ tests/flytekit/unit/configuration/test_internal.py
@@ -1,7 +1,7 @@
 import os
 
-from flytekit.configuration import get_config_file
-from flytekit.configuration.internal import Images, Credentials
+from flytekit.configuration import Config, get_config_file
+from flytekit.configuration.internal import Credentials, Images
 
 
 def test_load_images():
@@ -22,6 +22,14 @@ def test_client_secret_location():
     assert secret_location is None
 
     cfg = get_config_file(
-        os.path.join(os.path.dirname(os.path.realpath(__file__)), "configs/creds_secret_location.yaml"))
+        os.path.join(os.path.dirname(os.path.realpath(__file__)), "configs/creds_secret_location.yaml")
+    )
     secret_location = Credentials.CLIENT_CREDENTIALS_SECRET_LOCATION.read(cfg)
     assert secret_location == "/file/somewhere"
+
+
+def test_client_secret_parsing_from_location():
+    cfg = Config.auto(
+        config_file=os.path.join(os.path.dirname(os.path.realpath(__file__)), "configs/creds_secret_location.yaml")
+    )
+    assert cfg.platform.client_credentials_secret == "hello"
