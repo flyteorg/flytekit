eaa81dfc12ca27e649e6710a52130b84c9f49887
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2022-05-12 15:36:40 -0700

    Add default list transformer for legacy config (#998)
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- tests/flytekit/unit/configuration/test_internal.py
+++ tests/flytekit/unit/configuration/test_internal.py
@@ -1,7 +1,9 @@
 import os
 
+import mock
+
 from flytekit.configuration import get_config_file, read_file_if_exists
-from flytekit.configuration.internal import Credentials, Images
+from flytekit.configuration.internal import AWS, Credentials, Images
 
 
 def test_load_images():
@@ -41,3 +43,20 @@ def test_command():
     cfg = get_config_file(os.path.join(os.path.dirname(os.path.realpath(__file__)), "configs/good.config"))
     res = Credentials.COMMAND.read(cfg)
     assert res == ["aws", "sso", "get-token"]
+
+
+@mock.patch("flytekit.configuration.file.os")
+def test_command_2(mocked):
+    mocked.environ.get.return_value = "a,b,c"
+    cfg = get_config_file(os.path.join(os.path.dirname(os.path.realpath(__file__)), "configs/good.config"))
+    res = Credentials.COMMAND.read(cfg)
+    assert res == ["a", "b", "c"]
+
+
+@mock.patch("flytekit.configuration.file.os")
+def test_some_int(mocked):
+    mocked.environ.get.side_effect = "5"
+    cfg = get_config_file(os.path.join(os.path.dirname(os.path.realpath(__file__)), "configs/good.config"))
+    res = AWS.RETRIES.read(cfg)
+    assert type(res) is int
+    assert res == 5
