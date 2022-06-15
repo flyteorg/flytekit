8573d5b27f767de68faaa2fcfac2521344f4af32
Sonja Ericsson <sonjae@spotify.com>
2022-06-15 11:18:26 +0200

    fix
    Signed-off-by: Sonja Ericsson <sonjae@spotify.com>

--- flytekit/clients/raw.py
+++ flytekit/clients/raw.py
@@ -275,7 +275,7 @@ class RawSynchronousFlyteClient(object):
             cli_logger.error("Failed to generate token from command {}".format(command))
             raise _user_exceptions.FlyteAuthenticationException("Problems refreshing token with command: " + str(e))
 
-        authorization_header_key = self.public_client_config.authorization_metadata_key or Credentials.COMMAND.read()
+        authorization_header_key = self.public_client_config.authorization_metadata_key or Credentials.AUTHORIZATION_METADATA_KEY.read()
         if not authorization_header_key:
             self.set_access_token(output.stdout.strip())
         self.set_access_token(output.stdout.strip(), authorization_header_key)
