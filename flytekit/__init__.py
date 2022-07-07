2fe939c581dce18ab3bc77110e4a9bb122826676
Samhita Alla <aallasamhita@gmail.com>
2022-07-07 10:34:37 +0530

    TypeTransformers for PyTorch Tensor, Module, and Checkpoint (#1032)
    * TypeTransformers for PyTorch Tensor and Module
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * add torch to requirements
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * add module as a native type and PyTorchCheckpoint
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * update requirements
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * procedural to OOP approach
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * nit
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * verify device conversion
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * verify device conversion
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * hyperparameters can be None
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * device conversion
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * device conversion
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * checkpoint code cleanup
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * resolve merge conflict
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * fix pytorch api reference; resolve merge conflict
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * fix pytorch import
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    Signed-off-by: murilommen <murilommen@gmail.com>

--- flytekit/__init__.py
+++ flytekit/__init__.py
@@ -182,6 +182,7 @@ from flytekit.core.task import Secret, reference_task, task
 from flytekit.core.workflow import ImperativeWorkflow as Workflow
 from flytekit.core.workflow import WorkflowFailurePolicy, reference_workflow, workflow
 from flytekit.deck import Deck
+from flytekit.extras import pytorch
 from flytekit.extras.persistence import GCSPersistence, HttpPersistence, S3Persistence
 from flytekit.loggers import logger
 from flytekit.models.common import Annotations, AuthRole, Labels
