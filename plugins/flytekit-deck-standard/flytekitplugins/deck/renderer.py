b1a81087ce8a437cb4df02ec0dc0064785470430
murilommen <murilommen@gmail.com>
2022-07-13 15:38:03 -0300

    making renderers accept pandas dataframes
    Signed-off-by: murilommen <murilommen@gmail.com>

--- plugins/flytekit-deck-standard/flytekitplugins/deck/renderer.py
+++ plugins/flytekit-deck-standard/flytekitplugins/deck/renderer.py
@@ -1,7 +1,9 @@
+from typing import Optional
 import markdown
 import pandas
 import plotly.express as px
 from pandas_profiling import ProfileReport
+import whylogs as why
 from whylogs import DatasetProfileView
 from whylogs.viz import NotebookProfileVisualizer
 from whylogs.core.constraints import Constraints
@@ -54,8 +56,29 @@ class BoxRenderer:
 
 
 class WhylogsSummaryDriftRenderer:
+    """
+    Creates a whylogs' Summary Drift report from two pandas DataFrames. One of them
+    is the reference and the other one is the target data, meaning that this is what 
+    the report will compare it against.
+    """
     @staticmethod
-    def to_html(target_view: DatasetProfileView, reference_view: DatasetProfileView) -> str:
+    def to_html(
+        reference_data: pandas.DataFrame, 
+        target_data: pandas.DataFrame
+        ) -> str:
+        """
+        This static method will profile the input data and then generate an HTML report
+        with the Summary Drift calculations for all of the dataframe's columns
+        
+        :param reference_data: The DataFrame that will be the reference for the drift report
+        :type: pandas.DataFrame
+
+        :param target_data: The data to compare against and create the Summary Drift report
+        :type target_data: pandas.DataFrame        
+        """
+        
+        target_view = why.log(target_data).view()
+        reference_view = why.log(reference_data).view()
         viz = NotebookProfileVisualizer()
         viz.set_profiles(target_profile_view=target_view, reference_profile_view=reference_view)
         return viz.summary_drift_report().data
--- plugins/flytekit-whylogs/tests/test_schema.py
+++ plugins/flytekit-whylogs/tests/test_schema.py
@@ -1,14 +1,17 @@
-from typing import Optional, Tuple
+from typing import Optional
 import unittest
 
 import plotly.express as px
 import numpy as np
 import pandas as pd
+import pyspark
 import whylogs as why
 from whylogs.core import DatasetProfileView
 from whylogs.core.constraints import ConstraintsBuilder, MetricsSelector, MetricConstraint
+from whylogs.api.pyspark.experimental.profiler import collect_dataset_profile_view
 
 import flytekit
+from flytekitplugins.spark import Spark
 from flytekit import task, workflow
 from flytekitplugins.deck.renderer import WhylogsSummaryDriftRenderer, WhylogsConstraintsRenderer
 
@@ -30,32 +33,26 @@ def make_data(n_rows: int = 3) -> pd.DataFrame:
     }
     return pd.DataFrame(data)
 
-
 @task
-def profile_data(df: pd.DataFrame) -> DatasetProfileView:
-    result = why.log(df)
-    return result.view()
-
-
-@task
-def summary_drift_report(target_profile_view: DatasetProfileView, ref_profile_view: DatasetProfileView) -> DatasetProfileView:
+def summary_drift_report(target_data: pd.DataFrame, reference_data: pd.DataFrame) -> None:
     renderer = WhylogsSummaryDriftRenderer()
-    flytekit.Deck("summary drift", renderer.to_html(target_view=target_profile_view, reference_view=ref_profile_view))
-    return target_profile_view
-
+    flytekit.Deck("summary drift", renderer.to_html(target_data=target_data, reference_data=reference_data))
 
 @task
-def run_constraints(profile_view: DatasetProfileView,
+def run_constraints(df: pd.DataFrame,
                     min_value: Optional[float] = 0.0,
                     max_value: Optional[float] = 4.0
                     ) -> bool:
-    # This API constraints workflow is flexible but a bit cumbersome.
-    # It will be simplified in the future.
+    # This API constraints workflow is very flexible but a bit cumbersome.
+    # It will be simplified in the future, so for now we'll stick with injecting
+    # a Constraints object to the renderer.
+    profile_view = why.log(df).view()
     builder = ConstraintsBuilder(profile_view)
     num_constraint = MetricConstraint(
         name=f'numbers between {min_value} and {max_value} only',
         condition=lambda x: x.min > min_value and x.max < max_value,
         metric_selector=MetricsSelector(metric_name='distribution', column_name='sepal_length'))
+    
     builder.add_constraint(num_constraint)
     constraints = builder.build()
 
@@ -65,32 +62,76 @@ def run_constraints(profile_view: DatasetProfileView,
     return constraints.validate()
 
 
+
 @workflow
-def whylogs_workflow(min_value: float, max_value: float) -> Tuple[DatasetProfileView, bool]:
+def whylogs_workflow(min_value: float, max_value: float) -> bool:
     new_data = make_data(n_rows=10)
     reference_data = read_data()
 
-    new_profile = profile_data(df=new_data)
-    ref_profile = profile_data(df=reference_data)
+    summary_drift_report(target_data=new_data, reference_data=reference_data)
+    validated = run_constraints(df=new_data, min_value=min_value, max_value=max_value)
+    return validated
 
-    summary_drift_report(target_profile_view=new_profile, ref_profile_view=ref_profile)
-    validated = run_constraints(profile_view=new_profile, min_value=min_value, max_value=max_value)
-    return new_profile, validated
+
+@task(
+    task_config=Spark(
+        spark_conf={
+            "spark.driver.memory": "1000M",
+            "spark.executor.instances": "2",
+            "spark.driver.cores": "1",
+        }
+    ),
+    cache_version="1",
+    cache=True,
+)
+def make_spark_dataframe(n_rows: int = 10) -> pyspark.sql.DataFrame:
+    spark = flytekit.current_context().spark_session
+    data = {
+        'sepal_length': np.random.random_sample(n_rows),
+        'sepal_width': np.random.random_sample(n_rows),
+        'petal_length': np.random.random_sample(n_rows),
+        'petal_width': np.random.random_sample(n_rows),
+        'species': np.random.choice(['virginica', 'setosa', 'versicolor'], n_rows),
+        'species_id': np.random.choice([1, 2, 3], n_rows)
+    }
+    pandas_df = pd.DataFrame(data)
+    spark_df = spark.createDataFrame(pandas_df)
+    return spark_df
+
+@task(
+    task_config=Spark(
+        spark_conf={
+            "spark.driver.memory": "1000M",
+            "spark.executor.instances": "2",
+            "spark.driver.cores": "1",
+        }
+    ),
+    cache_version="1",
+    cache=True,
+)
+def profile_spark_df(df: pyspark.sql.DataFrame) -> DatasetProfileView:
+    profile_view = collect_dataset_profile_view(df)
+    return profile_view
+
+
+@workflow
+def spark_whylogs_wf() -> DatasetProfileView:
+    spark_df = make_spark_dataframe(n_rows=10)
+    profile_view = profile_spark_df(df=spark_df)
+    return profile_view
+
 
 
 class TestWhylogsWorkflow(unittest.TestCase):
     def test_workflow_with_whylogs(self):
-        profile_view, _ = whylogs_workflow(min_value=0.0, max_value=1.0)
-        keys = list(profile_view.get_columns().keys())
-        assert keys.sort() == [
-            'sepal_length',
-            'sepal_width',
-            'petal_length',
-            'petal_width',
-            'species',
-            'species_id'
-        ].sort()
+        validated = whylogs_workflow(min_value=0.0, max_value=1.0)
+        assert validated is True
 
     def test_constraints(self):
-        _, validated = whylogs_workflow(min_value=-1.0, max_value=0.0)
+        validated = whylogs_workflow(min_value=-1.0, max_value=0.0)
         assert validated is False
+
+    def test_pyspark_wf(self):
+        profile_view = spark_whylogs_wf()
+        assert profile_view is not None
+        assert isinstance(profile_view, DatasetProfileView)
