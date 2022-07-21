c6e7237331554fcf2a1b728b65e59b1e4f796dbf
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2022-07-21 11:45:20 -0700

    StructuredDatasetTransformerEngine should derive default protocol from raw output prefix (#1107)
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

---
+++ tests/flytekit/unit/core/test_dataclass.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from typing import List
+
+from dataclasses_json import dataclass_json
+
+from flytekit.core.task import task
+from flytekit.core.workflow import workflow
+
+
+def test_dataclass():
+    @dataclass_json
+    @dataclass
+    class AppParams(object):
+        snapshotDate: str
+        region: str
+        preprocess: bool
+        listKeys: List[str]
+
+    @task
+    def t1() -> AppParams:
+        ap = AppParams(snapshotDate="4/5/2063", region="us-west-3", preprocess=False, listKeys=["a", "b"])
+        return ap
+
+    @workflow
+    def wf() -> AppParams:
+        return t1()
+
+    res = wf()
+    assert res.region == "us-west-3"
