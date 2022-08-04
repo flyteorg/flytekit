12b50971706a97967affcde8e204de2fec532eb0
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2022-08-04 15:10:29 -0700

    nit
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- tests/flytekit/unit/core/test_local_cache.py
+++ tests/flytekit/unit/core/test_local_cache.py
@@ -1,7 +1,7 @@
 import datetime
 import typing
 from dataclasses import dataclass
-from typing import Dict, List
+from typing import List
 
 import pandas
 from dataclasses_json import dataclass_json
@@ -10,15 +10,12 @@ from typing_extensions import Annotated
 
 from flytekit.core.base_sql_task import SQLTask
 from flytekit.core.base_task import kwtypes
-from flytekit.core.context_manager import FlyteContextManager
 from flytekit.core.dynamic_workflow_task import dynamic
 from flytekit.core.hash import HashMethod
-from flytekit.core.local_cache import LocalTaskCache, _calculate_cache_key
+from flytekit.core.local_cache import LocalTaskCache
 from flytekit.core.task import TaskMetadata, task
 from flytekit.core.testing import task_mock
-from flytekit.core.type_engine import TypeEngine
 from flytekit.core.workflow import workflow
-from flytekit.models.literals import LiteralMap
 from flytekit.types.schema import FlyteSchema
 
 # Global counter used to validate number of calls to cache
