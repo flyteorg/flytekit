e55990e4dc487610e0ef76f7b2a54d5e94384d80
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2022-08-04 17:20:14 -0700

    cherry pick 97b454b1
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- tests/flytekit/unit/core/test_local_cache.py
+++ tests/flytekit/unit/core/test_local_cache.py
@@ -1,19 +1,24 @@
 import datetime
 import typing
 from dataclasses import dataclass
-from typing import List
+from typing import Dict, List
 
 import pandas
 from dataclasses_json import dataclass_json
 from pytest import fixture
 from typing_extensions import Annotated
 
-from flytekit import SQLTask, dynamic, kwtypes
+from flytekit.core.base_sql_task import SQLTask
+from flytekit.core.base_task import kwtypes
+from flytekit.core.context_manager import FlyteContextManager
+from flytekit.core.dynamic_workflow_task import dynamic
 from flytekit.core.hash import HashMethod
-from flytekit.core.local_cache import LocalTaskCache
+from flytekit.core.local_cache import LocalTaskCache, _calculate_cache_key
 from flytekit.core.task import TaskMetadata, task
 from flytekit.core.testing import task_mock
+from flytekit.core.type_engine import TypeEngine
 from flytekit.core.workflow import workflow
+from flytekit.models.literals import LiteralMap
 from flytekit.types.schema import FlyteSchema
 
 # Global counter used to validate number of calls to cache
@@ -383,3 +388,26 @@ def test_list_of_pandas_dataframe_hash():
     # Confirm that we see a cache hit in the case of annotated dataframes.
     my_workflow()
     assert n_cached_task_calls == 1
+
+
+def test_cache_key_repetition():
+    pt = Dict
+    lt = TypeEngine.to_literal_type(pt)
+    ctx = FlyteContextManager.current_context()
+    kwargs = {
+        "a": 0.41083513079747874,
+        "b": 0.7773927872515183,
+        "c": 17,
+    }
+    keys = set()
+    for i in range(0, 100):
+        lit = TypeEngine.to_literal(ctx, kwargs, Dict, lt)
+        lm = LiteralMap(
+            literals={
+                "d": lit,
+            }
+        )
+        key = _calculate_cache_key("t1", "007", lm)
+        keys.add(key)
+
+    assert len(keys) == 1
