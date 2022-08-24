c42638b2678920e5214b80e49d07725dda317ccd
Niels Bantilan <niels.bantilan@gmail.com>
2022-08-24 15:39:59 -0400

    isolate exception to torch import
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- flytekit/extras/pytorch/__init__.py
+++ flytekit/extras/pytorch/__init__.py
@@ -14,9 +14,18 @@ from flytekit.loggers import logger
 # TODO: abstract this out so that there's an established pattern for registering plugins
 # that have soft dependencies
 try:
+    # isolate the exception to the torch import
+    import torch
+
+    _torch_installed = True
+except (ImportError, OSError):
+    _torch_installed = False
+
+
+if _torch_installed:
     from .checkpoint import PyTorchCheckpoint, PyTorchCheckpointTransformer
     from .native import PyTorchModuleTransformer, PyTorchTensorTransformer
-except (ImportError, OSError):
+else:
     logger.info(
         "We won't register PyTorchCheckpointTransformer, PyTorchTensorTransformer, and PyTorchModuleTransformer because torch is not installed."
     )
