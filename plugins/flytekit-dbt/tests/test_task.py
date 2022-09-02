cc166acc8976b0c6c86a620464a4c94027f34bae
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2022-09-01 18:08:03 -0700

    Fix test_task.py tests
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- plugins/flytekit-dbt/tests/test_task.py
+++ plugins/flytekit-dbt/tests/test_task.py
@@ -10,14 +10,15 @@ from flytekitplugins.dbt.task import DBTRun, DBTTest
 from flytekit import workflow
 from flytekit.tools.subprocess import check_call
 
-# DBT_PROJECT_DIR = "tests/jaffle_shop"
-# DBT_PROFILES_DIR =
+DBT_PROJECT_DIR = str(pathlib.Path(os.path.dirname(os.path.realpath(__file__)), "testdata", "jaffle_shop"))
+DBT_PROFILES_DIR = str(pathlib.Path(os.path.dirname(os.path.realpath(__file__)), "testdata", "profiles"))
+DBT_PROFILE = "jaffle_shop"
 
 
 @pytest.fixture(scope="module", autouse=True)
 def prepare_db():
     # Ensure path to sqlite database file exists
-    dbs_path = pathlib.Path(os.path.dirname(os.path.realpath(__file__)), "jaffle_shop", "dbs")
+    dbs_path = pathlib.Path(DBT_PROJECT_DIR, "dbs")
     dbs_path.mkdir(exist_ok=True, parents=True)
     database_file = pathlib.Path(dbs_path, "database_name.db")
     touch.touch(database_file)
@@ -30,14 +31,13 @@ def prepare_db():
             "json",
             "seed",
             "--project-dir",
-            "tests/jaffle_shop",
+            DBT_PROJECT_DIR,
             "--profiles-dir",
-            "tests/jaffle_shop/profiles",
+            DBT_PROFILES_DIR,
             "--profile",
-            "jaffle_shop",
+            DBT_PROFILE,
         ]
     )
-    # subprocess.run(args=["dbt", "--log-format", "json", "seed", "--project-dir", "tests/jaffle_shop", "--profiles-dir", "tests/jaffle_shop/profiles", "--profile", "jaffle_shop"])
 
     yield
 
@@ -55,9 +55,9 @@ def prepare_db():
 #         # run all models
 #         return dbt_run_task(
 #             input=DBTRunInput(
-#                 project_dir="tests/jaffle_shop",
-#                 profiles_dir="tests/jaffle_shop/profiles",
-#                 profile="jaffle_shop",
+#                 project_dir=DBT_PROJECT_DIR,
+#                 profiles_dir=DBT_PROFILES_DIR,
+#                 profile=DBT_PROFILE,
 #             )
 #         )
 #     result = my_workflow()
@@ -75,7 +75,7 @@ class TestDBTRun:
             # run all models
             return dbt_run_task(
                 input=DBTRunInput(
-                    project_dir="tests/jaffle_shop",
+                    project_dir=DBT_PROJECT_DIR,
                     profiles_dir="tests/jaffle_shop/profiles",
                     profile="jaffle_shop",
                 )
@@ -93,7 +93,7 @@ class TestDBTRun:
             dbt_run_task(
                 input=DBTRunInput(
                     project_dir=".",
-                    profiles_dir="tests/jaffle_shop/profiles",
+                    profiles_dir=DBT_PROFILES_DIR,
                     profile="jaffle_shop",
                 )
             )
@@ -103,25 +103,21 @@ class TestDBTRun:
             name="test-task",
         )
 
-        project_dir = "tests/jaffle_shop"
-        profiles_dir = "tests/jaffle_shop/profiles"
-        profile = "jaffle_shop"
-
         output = dbt_run_task.execute(
-            input=DBTRunInput(project_dir=project_dir, profiles_dir=profiles_dir, profile=profile)
+            input=DBTRunInput(project_dir=DBT_PROJECT_DIR, profiles_dir=DBT_PROFILES_DIR, profile=DBT_PROFILE)
         )
 
         assert output.exit_code == 0
         assert (
             output.command
-            == f"dbt --log-format json run --project-dir {project_dir} --profiles-dir {profiles_dir} --profile {profile}"
+            == f"dbt --log-format json run --project-dir {DBT_PROJECT_DIR} --profiles-dir {DBT_PROFILES_DIR} --profile {DBT_PROFILE}"
         )
 
-        with open("tests/jaffle_shop/target/run_results.json", "r") as fp:
+        with open(f"{DBT_PROJECT_DIR}/target/run_results.json", "r") as fp:
             exp_run_result = fp.read()
         assert output.raw_run_result == exp_run_result
 
-        with open("tests/jaffle_shop/target/manifest.json", "r") as fp:
+        with open(f"{DBT_PROJECT_DIR}/target/manifest.json", "r") as fp:
             exp_manifest = fp.read()
         assert output.raw_manifest == exp_manifest
 
@@ -137,9 +133,9 @@ class TestDBTTest:
             # run all tests
             return dbt_test_task(
                 input=DBTTestInput(
-                    project_dir="tests/jaffle_shop",
-                    profiles_dir="tests/jaffle_shop/profiles",
-                    profile="jaffle_shop",
+                    project_dir=DBT_PROJECT_DIR,
+                    profiles_dir=DBT_PROFILES_DIR,
+                    profile=DBT_PROFILE,
                 )
             )
 
@@ -154,34 +150,30 @@ class TestDBTTest:
             dbt_test_task(
                 input=DBTTestInput(
                     project_dir=".",
-                    profiles_dir="tests/jaffle_shop/profiles",
-                    profile="jaffle_shop",
+                    profiles_dir=DBT_PROFILES_DIR,
+                    profile=DBT_PROFILE,
                 )
             )
 
-    def test_task_output(self):
+    def test_task_outputt(self):
         dbt_test_task = DBTTest(
             name="test-task",
         )
 
-        project_dir = "tests/jaffle_shop"
-        profiles_dir = "tests/jaffle_shop/profiles"
-        profile = "jaffle_shop"
-
         output = dbt_test_task.execute(
-            input=DBTTestInput(project_dir=project_dir, profiles_dir=profiles_dir, profile=profile)
+            input=DBTTestInput(project_dir=DBT_PROJECT_DIR, profiles_dir=DBT_PROFILES_DIR, profile=DBT_PROFILE)
         )
 
         assert output.exit_code == 0
         assert (
             output.command
-            == f"dbt --log-format json test --project-dir {project_dir} --profiles-dir {profiles_dir} --profile {profile}"
+            == f"dbt --log-format json test --project-dir {DBT_PROJECT_DIR} --profiles-dir {DBT_PROFILES_DIR} --profile {DBT_PROFILE}"
         )
 
-        with open("tests/jaffle_shop/target/run_results.json", "r") as fp:
+        with open(f"{DBT_PROJECT_DIR}/target/run_results.json", "r") as fp:
             exp_run_result = fp.read()
         assert output.raw_run_result == exp_run_result
 
-        with open("tests/jaffle_shop/target/manifest.json", "r") as fp:
+        with open(f"{DBT_PROJECT_DIR}/target/manifest.json", "r") as fp:
             exp_manifest = fp.read()
         assert output.raw_manifest == exp_manifest
