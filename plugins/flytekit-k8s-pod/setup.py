46a61292158de47609d1eb9723b6af5ed113d2db
Eduardo Apolinario <653394+eapolinario@users.noreply.github.com>
2022-09-08 14:28:55 -0700

    Set flytekit<2.0 in plugins (#1152)
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    Co-authored-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- plugins/flytekit-k8s-pod/setup.py
+++ plugins/flytekit-k8s-pod/setup.py
@@ -5,7 +5,7 @@ PLUGIN_NAME = "pod"
 microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
 
 plugin_requires = [
-    "flytekit>=1.1.0b0,<1.2.0",
+    "flytekit>=1.1.0b0,<2.0.0",
     "kubernetes>=12.0.1",
 ]
 
