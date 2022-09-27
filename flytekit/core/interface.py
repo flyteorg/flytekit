6499c427486c7fcb72db80dc040d5c6138f47b3b
Kevin Su <pingsutw@apache.org>
2022-09-27 23:38:16 +0800

    Fix tests
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/core/interface.py
+++ flytekit/core/interface.py
@@ -316,7 +316,7 @@ def transform_function_to_interface(fn: typing.Callable, docstring: Optional[Doc
 
 def transform_variable_map(
     variable_map: Dict[str, type],
-    descriptions: Dict[str, str],
+    descriptions: Dict[str, str] = {},
 ) -> Dict[str, _interface_models.Variable]:
     """
     Given a map of str (names of inputs for instance) to their Python native types, return a map of the name to a
