9b8f255b900041fc1323535398fdb8c7dcc62449
Samhita Alla <aallasamhita@gmail.com>
2022-10-08 02:46:53 +0530

    Adds support for modifying allow_pickle and mmap_mode in numpy array type (#1209)
    * Adds support for modifying allow_pickle and mmap_mode in numpy array type
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * ordered dict to dict
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * add test
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    * change metadata default
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>
    
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- flytekit/types/numpy/ndarray.py
+++ flytekit/types/numpy/ndarray.py
@@ -1,14 +1,27 @@
 import pathlib
 import typing
-from typing import Type
+from collections import OrderedDict
+from typing import Dict, Tuple, Type
 
 import numpy as np
+from typing_extensions import Annotated, get_args, get_origin
 
 from flytekit.core.context_manager import FlyteContext
 from flytekit.core.type_engine import TypeEngine, TypeTransformer, TypeTransformerFailedError
 from flytekit.models.core import types as _core_types
 from flytekit.models.literals import Blob, BlobMetadata, Literal, Scalar
 from flytekit.models.types import LiteralType
+
+
+def extract_metadata(t: Type[np.ndarray]) -> Tuple[Type[np.ndarray], Dict[str, bool]]:
+    metadata = {}
+    if get_origin(t) is Annotated:
+        base_type, metadata = get_args(t)
+        if isinstance(metadata, OrderedDict):
+            return base_type, metadata
+        else:
+            raise TypeTransformerFailedError(f"{t}'s metadata needs to be of type kwtypes.")
+    return t, metadata
 
 
 class NumpyArrayTransformer(TypeTransformer[np.ndarray]):
@@ -31,6 +44,8 @@ class NumpyArrayTransformer(TypeTransformer[np.ndarray]):
     def to_literal(
         self, ctx: FlyteContext, python_val: np.ndarray, python_type: Type[np.ndarray], expected: LiteralType
     ) -> Literal:
+        python_type, metadata = extract_metadata(python_type)
+
         meta = BlobMetadata(
             type=_core_types.BlobType(
                 format=self.NUMPY_ARRAY_FORMAT, dimensionality=_core_types.BlobType.BlobDimensionality.SINGLE
@@ -40,9 +55,8 @@ class NumpyArrayTransformer(TypeTransformer[np.ndarray]):
         local_path = ctx.file_access.get_random_local_path() + ".npy"
         pathlib.Path(local_path).parent.mkdir(parents=True, exist_ok=True)
 
-        # save numpy array to a file
-        # allow_pickle=False prevents numpy from trying to save object arrays (dtype=object) using pickle
-        np.save(file=local_path, arr=python_val, allow_pickle=False)
+        # save numpy array to file
+        np.save(file=local_path, arr=python_val, allow_pickle=metadata.get("allow_pickle", False))
 
         remote_path = ctx.file_access.get_random_remote_path(local_path)
         ctx.file_access.put_data(local_path, remote_path, is_multipart=False)
@@ -54,11 +68,17 @@ class NumpyArrayTransformer(TypeTransformer[np.ndarray]):
         except AttributeError:
             raise TypeTransformerFailedError(f"Cannot convert from {lv} to {expected_python_type}")
 
+        expected_python_type, metadata = extract_metadata(expected_python_type)
+
         local_path = ctx.file_access.get_random_local_path()
         ctx.file_access.get_data(uri, local_path, is_multipart=False)
 
         # load numpy array from a file
-        return np.load(file=local_path)
+        return np.load(
+            file=local_path,
+            allow_pickle=metadata.get("allow_pickle", False),
+            mmap_mode=metadata.get("mmap_mode"),
+        )
 
     def guess_python_type(self, literal_type: LiteralType) -> typing.Type[np.ndarray]:
         if (
