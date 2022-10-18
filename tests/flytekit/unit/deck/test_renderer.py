543b8bfa032f93626170fe8b3d895b3fdb4e829f
Eduardo Apolinario <653394+eapolinario@users.noreply.github.com>
2022-10-18 14:45:21 -0700

    Set reasonable defaults for pandas dataframe default renderer (#1238)
    * Set reasonable defaults for pandas dataframe default renderer
    
    * Remove erroneous use of Optional
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    Co-authored-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    Signed-off-by: Kilian <kilian.tep@go-jek.com>

--- tests/flytekit/unit/deck/test_renderer.py
+++ tests/flytekit/unit/deck/test_renderer.py
@@ -1,12 +1,33 @@
 import pandas as pd
 import pyarrow as pa
+import pytest
 
-from flytekit.deck.renderer import ArrowRenderer, TopFrameRenderer
+from flytekit.deck.renderer import DEFAULT_MAX_COLS, DEFAULT_MAX_ROWS, ArrowRenderer, TopFrameRenderer
 
 
-def test_renderer():
-    df = pd.DataFrame({"Name": ["Tom", "Joseph"], "Age": [1, 22]})
+@pytest.mark.parametrize(
+    "rows, cols, max_rows, expected_max_rows, max_cols, expected_max_cols",
+    [
+        (1, 1, None, DEFAULT_MAX_ROWS, None, DEFAULT_MAX_COLS),
+        (10, 1, None, DEFAULT_MAX_ROWS, None, DEFAULT_MAX_COLS),
+        (1, 10, None, DEFAULT_MAX_ROWS, None, DEFAULT_MAX_COLS),
+        (DEFAULT_MAX_ROWS + 1, 10, None, DEFAULT_MAX_ROWS, None, DEFAULT_MAX_COLS),
+        (1, DEFAULT_MAX_COLS + 1, None, DEFAULT_MAX_ROWS, None, DEFAULT_MAX_COLS),
+        (10, DEFAULT_MAX_COLS + 1, None, DEFAULT_MAX_ROWS, None, DEFAULT_MAX_COLS),
+        (DEFAULT_MAX_ROWS + 1, DEFAULT_MAX_COLS + 1, None, DEFAULT_MAX_ROWS, None, DEFAULT_MAX_COLS),
+        (100_000, 10, 123, 123, 5, 5),
+        (10_000, 1000, DEFAULT_MAX_ROWS, DEFAULT_MAX_ROWS, DEFAULT_MAX_COLS, DEFAULT_MAX_COLS),
+    ],
+)
+def test_renderer(rows, cols, max_rows, expected_max_rows, max_cols, expected_max_cols):
+    df = pd.DataFrame({f"abc-{k}": list(range(rows)) for k in range(cols)})
     pa_df = pa.Table.from_pandas(df)
 
-    assert TopFrameRenderer().to_html(df) == df.to_html()
+    kwargs = {}
+    if max_rows is not None:
+        kwargs["max_rows"] = max_rows
+    if max_cols is not None:
+        kwargs["max_cols"] = max_cols
+
+    assert TopFrameRenderer(**kwargs).to_html(df) == df.to_html(max_rows=expected_max_rows, max_cols=expected_max_cols)
     assert ArrowRenderer().to_html(pa_df) == pa_df.to_string()
