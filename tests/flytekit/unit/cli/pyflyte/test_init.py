e3e2989bf987e2b3cb4b7fbd257a65a1a1e4cf87
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2022-10-31 11:32:27 -0700

    Ignore PermissionError in test
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- tests/flytekit/unit/cli/pyflyte/test_init.py
+++ tests/flytekit/unit/cli/pyflyte/test_init.py
@@ -15,12 +15,19 @@ from flytekit.clis.sdk_in_container import pyflyte
     ],
 )
 def test_pyflyte_init(command, monkeypatch: pytest.MonkeyPatch):
-    with tempfile.TemporaryDirectory() as tdir:
-        monkeypatch.chdir(tdir)
-        runner = CliRunner()
-        result = runner.invoke(
-            pyflyte.init,
-            command,
-            catch_exceptions=True,
-        )
-        assert result.exit_code == 0
+    try:
+        with tempfile.TemporaryDirectory() as tdir:
+            monkeypatch.chdir(tdir)
+            runner = CliRunner()
+            result = runner.invoke(
+                pyflyte.init,
+                command,
+                catch_exceptions=True,
+            )
+            assert result.exit_code == 0
+    except PermissionError as e:
+        # Just ignore permission errors during cleanup.
+        # This is particularyly problematic on Windows (https://bugs.python.org/issue29982),
+        # so much so that starting on python>=3.10 the TemporaryDirectory constructor
+        # takes an extra paramater to allow for cleanup errors to be ignored.
+        print(f"Ignoring permission error = {e}")
