641d2ac99a2de8377c15612ac875e871517d0311
Bernhard Stadlbauer <b.stadlbauer@gmx.net>
2022-12-10 20:58:24 +0100

    Add tests to `dask` task
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>

--- plugins/flytekit-dask/flytekitplugins/dask/task.py
+++ plugins/flytekit-dask/flytekitplugins/dask/task.py
@@ -1,32 +1,109 @@
-import typing
 from dataclasses import dataclass
+from typing import Any, Callable, Dict, Optional
 
-from flytekit import PythonFunctionTask
+from flytekitplugins.dask import models
+from flytekitplugins.dask.resources import convert_resources_to_resource_model
+from google.protobuf.json_format import MessageToDict
+
+from flytekit import PythonFunctionTask, Resources
+from flytekit.configuration import SerializationSettings
 from flytekit.core.task import TaskPlugins
+
+
+@dataclass
+class JobPodSpec:
+    """
+    Configuration for the dask job runner pod
+
+    :param image: Custom image to use. If ``None``, will use the same image the task was registered with. Optional,
+        defaults to ``None``.
+    :param requests: Resources to request for the job runner pod. If ``None``, the requests passed into the task will be
+        used. Optional, defaults to ``None``
+    :param limits: Resource limits for the job runner pod. If ``None``, the limits passed into the task will be used.
+        Optional, defaults to ``None``
+    """
+
+    image: Optional[str] = None
+    requests: Optional[Resources] = None
+    limits: Optional[Resources] = None
+
+
+@dataclass
+class DaskCluster:
+    """
+    Configuration for the dask cluster pods
+
+    :param image: Custom image to use. If ``None``, will use the same image the task was registered with. Optional,
+        defaults to ``None``.
+    :param n_workers: Number of workers to use. Optional, defaults to 1.
+    :param requests: Resources to request for the scheduler pod as well as the worker pods. If ``None``, the requests
+        passed into the task will be used. Optional, defaults to ``None``
+    :param limits: Resource limits for the scheduler pod as well as the worker pods. If ``None``, the limits
+        passed into the task will be used. Optional, defaults to ``None``
+    """
+
+    image: Optional[str] = None
+    n_workers: Optional[int] = 1
+    requests: Optional[Resources] = None
+    limits: Optional[Resources] = None
 
 
 @dataclass
 class Dask:
     """
-    # FIXME: TODO
+    Configuration for the dask task
+
+    :param namespace: Custom namespace to run the dask pods in. If ``None`` will use the default namespace. Optional,
+        defaults to ``None``.
+    :param job_pod_spec: Configuration for the job runner pod. Optional, defaults to ``JobPodSpec()``
+    :param cluster: Configuration for the dask cluster pods (scheduler and workers). Optional, defaults to
+        ``DaskCluster()``
     """
-    pass
+
+    namespace: Optional[str] = None
+    job_pod_spec: JobPodSpec = JobPodSpec()
+    cluster: DaskCluster = DaskCluster()
 
 
 class DaskTask(PythonFunctionTask[Dask]):
     """
-    Actual Plugin that transforms the local python code for execution within a spark context
+    Actual Plugin that transforms the local python code for execution within a dask cluster
     """
 
     _DASK_TASK_TYPE = "dask"
 
-    def __init__(self, task_config: Dask, task_function: typing.Callable, **kwargs):
+    def __init__(self, task_config: Dask, task_function: Callable, **kwargs):
         super(DaskTask, self).__init__(
             task_config=task_config,
             task_type=self._DASK_TASK_TYPE,
             task_function=task_function,
             **kwargs,
         )
+
+    def get_custom(self, settings: SerializationSettings) -> Optional[Dict[str, Any]]:
+        """
+        Serialize the `dask` task config into a dict.
+
+        :param settings: Current serialization settings
+        :return: Dictionary representation of the dask task config.
+        """
+        job_pod_spec = models.JobPodSpec(
+            image=self.task_config.job_pod_spec.image,
+            resources=convert_resources_to_resource_model(
+                requests=self.task_config.job_pod_spec.requests,
+                limits=self.task_config.job_pod_spec.limits,
+            ),
+        )
+        dask_cluster = models.DaskCluster(
+            image=self.task_config.cluster.image,
+            n_workers=self.task_config.cluster.n_workers,
+            resources=convert_resources_to_resource_model(
+                requests=self.task_config.cluster.requests,
+                limits=self.task_config.cluster.limits,
+            ),
+        )
+        job = models.DaskJob(namespace=self.task_config.namespace, job_pod_spec=job_pod_spec, dask_cluster=dask_cluster)
+        return MessageToDict(job.to_flyte_idl())
 
 
 # Inject the `dask` plugin into flytekits dynamic plugin loading system
---
+++ plugins/flytekit-dask/tests/test_task.py
@@ -0,0 +1,89 @@
+import pytest
+from flytekitplugins.dask import Dask
+from flytekitplugins.dask.task import DaskCluster, JobPodSpec
+
+from flytekit import PythonFunctionTask, Resources, task
+from flytekit.configuration import Image, ImageConfig, SerializationSettings
+
+
+@pytest.fixture
+def serialization_settings() -> SerializationSettings:
+    default_img = Image(name="default", fqn="test", tag="tag")
+    settings = SerializationSettings(
+        project="project",
+        domain="domain",
+        version="version",
+        env={"FOO": "baz"},
+        image_config=ImageConfig(default_image=default_img, images=[default_img]),
+    )
+    return settings
+
+
+def test_dask_task_with_default_config(serialization_settings: SerializationSettings):
+    task_config = Dask()
+
+    @task(task_config=task_config)
+    def dask_task():
+        pass
+
+    # Helping type completion in PyCharm
+    dask_task: PythonFunctionTask[Dask]
+
+    assert dask_task.task_config == task_config
+    assert dask_task.task_type == "dask"
+
+    expected_dict = {
+        "jobPodSpec": {
+            "resources": {},
+        },
+        "cluster": {
+            "nWorkers": 1,
+            "resources": {},
+        },
+    }
+    assert dask_task.get_custom(serialization_settings) == expected_dict
+
+
+def test_dask_task_get_custom(serialization_settings: SerializationSettings):
+    task_config = Dask(
+        namespace="foobar",
+        job_pod_spec=JobPodSpec(
+            image="job_pod_spec:latest",
+            requests=Resources(cpu="1"),
+            limits=Resources(cpu="2"),
+        ),
+        cluster=DaskCluster(
+            image="dask_cluster:latest",
+            n_workers=123,
+            requests=Resources(cpu="3"),
+            limits=Resources(cpu="4"),
+        ),
+    )
+
+    @task(task_config=task_config)
+    def dask_task():
+        pass
+
+    # Helping type completion in PyCharm
+    dask_task: PythonFunctionTask[Dask]
+
+    expected_custom_dict = {
+        "namespace": "foobar",
+        "jobPodSpec": {
+            "image": "job_pod_spec:latest",
+            "resources": {
+                "requests": [{"name": "CPU", "value": "1"}],
+                "limits": [{"name": "CPU", "value": "2"}],
+            },
+        },
+        "cluster": {
+            "nWorkers": 123,
+            "image": "dask_cluster:latest",
+            "resources": {
+                "requests": [{"name": "CPU", "value": "3"}],
+                "limits": [{"name": "CPU", "value": "4"}],
+            },
+        },
+    }
+    custom_dict = dask_task.get_custom(serialization_settings)
+    assert custom_dict == expected_custom_dict
