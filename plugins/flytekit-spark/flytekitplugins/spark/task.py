faae0d5927941ae61517e1a46f37da5cbef78050
Kevin Su <pingsutw@apache.org>
2022-12-20 18:45:30 -0800

    nit
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- plugins/flytekit-spark/flytekitplugins/spark/task.py
+++ plugins/flytekit-spark/flytekitplugins/spark/task.py
@@ -151,4 +151,3 @@ class PysparkFunctionTask(PythonFunctionTask[Spark]):
 # Inject the Spark plugin into flytekits dynamic plugin loading system
 TaskPlugins.register_pythontask_plugin(Spark, PysparkFunctionTask)
 TaskPlugins.register_pythontask_plugin(Databricks, PysparkFunctionTask)
-
