3de0912e855647896ba95327b39f1cba79f814c8
Kevin Su <pingsutw@gmail.com>
2023-01-10 17:15:59 -0800

    AWS batch return error code once it fails
    Signed-off-by: Kevin Su <pingsutw@gmail.com>

--- flytekit/bin/entrypoint.py
+++ flytekit/bin/entrypoint.py
@@ -161,7 +161,8 @@ def _dispatch_execute(
     logger.info(f"Engine folder written successfully to the output prefix {output_prefix}")
     logger.debug("Finished _dispatch_execute")
 
-    if task_def.task_type == "aws-batch" and _constants.ERROR_FILE_NAME in output_file_dict:
+    if os.environ.get("FLYTE_FAIL_ON_ERROR", "").lower() == "true" and _constants.ERROR_FILE_NAME in output_file_dict:
+        # This env is set by the flytepropeller
         # AWS batch job get the status from the exit code, so once we catch the error,
         # we should return the error code here
         exit(1)
