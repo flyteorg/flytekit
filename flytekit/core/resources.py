689c4aeff5ff3808d8032443382ce567fae59d86
bstadlbauer <11799671+bstadlbauer@users.noreply.github.com>
2023-01-12 03:16:50 +0100

    Add dask plugin #patch (#1366)
    * Add dummy task type to test backend plugin
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Add docs page
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Add dask models
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Add function to convert resources
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Add tests to `dask` task
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Remove namespace
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Update setup.py
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Add dask to `plugin/README.md`
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Add README.md for `dask`
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Top level export of `JopPodSpec` and `DaskCluster`
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Update docs for images
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Update README.md
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Update models after `flyteidl` change
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Update task after `flyteidl` change
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Raise error when less than 1 worker
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Update flyteidl to >= 1.3.2
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Update doc requirements
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Update doc-requirements.txt
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Re-lock dependencies on linux
    
    Signed-off-by: Bernhard Stadlbauer <bernhard@pachama.com>
    
    * Update dask API docs
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Fix documentation links
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Default optional model constructor arguments to `None`
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Refactor `convert_resources_to_resource_model` to `core.resources`
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Use `convert_resources_to_resource_model` in `core.node`
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    
    * Incorporate review feedback
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    * Lint
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    
    Signed-off-by: Bernhard Stadlbauer <b.stadlbauer@gmx.net>
    Signed-off-by: Bernhard Stadlbauer <bernhard@pachama.com>
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>
    Co-authored-by: Eduardo Apolinario <653394+eapolinario@users.noreply.github.com>
    Co-authored-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- flytekit/core/resources.py
+++ flytekit/core/resources.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import List, Optional
+
+from flytekit.models import task as task_models
 
 
 @dataclass
@@ -35,3 +37,42 @@ class Resources(object):
 class ResourceSpec(object):
     requests: Optional[Resources] = None
     limits: Optional[Resources] = None
+
+
+_ResouceName = task_models.Resources.ResourceName
+_ResourceEntry = task_models.Resources.ResourceEntry
+
+
+def _convert_resources_to_resource_entries(resources: Resources) -> List[_ResourceEntry]:
+    resource_entries = []
+    if resources.cpu is not None:
+        resource_entries.append(_ResourceEntry(name=_ResouceName.CPU, value=resources.cpu))
+    if resources.mem is not None:
+        resource_entries.append(_ResourceEntry(name=_ResouceName.MEMORY, value=resources.mem))
+    if resources.gpu is not None:
+        resource_entries.append(_ResourceEntry(name=_ResouceName.GPU, value=resources.gpu))
+    if resources.storage is not None:
+        resource_entries.append(_ResourceEntry(name=_ResouceName.STORAGE, value=resources.storage))
+    if resources.ephemeral_storage is not None:
+        resource_entries.append(_ResourceEntry(name=_ResouceName.EPHEMERAL_STORAGE, value=resources.ephemeral_storage))
+    return resource_entries
+
+
+def convert_resources_to_resource_model(
+    requests: Optional[Resources] = None,
+    limits: Optional[Resources] = None,
+) -> task_models.Resources:
+    """
+    Convert flytekit ``Resources`` objects to a Resources model
+
+    :param requests: Resource requests. Optional, defaults to ``None``
+    :param limits: Resource limits. Optional, defaults to ``None``
+    :return: The given resources as requests and limits
+    """
+    request_entries = []
+    limit_entries = []
+    if requests is not None:
+        request_entries = _convert_resources_to_resource_entries(requests)
+    if limits is not None:
+        limit_entries = _convert_resources_to_resource_entries(limits)
+    return task_models.Resources(requests=request_entries, limits=limit_entries)
