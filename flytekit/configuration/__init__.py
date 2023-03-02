d715d5630e8d1e6f0cc0f63c1707e2771bfa0134
Niels Bantilan <niels.bantilan@gmail.com>
2023-03-02 16:26:38 -0500

    add config init command
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- flytekit/configuration/__init__.py
+++ flytekit/configuration/__init__.py
@@ -31,7 +31,8 @@ be ``FLYTE_PLATFORM_URL``.
 
 **YAML Format Configuration File**: A configuration file that contains settings for both
 `flytectl <https://docs.flyte.org/projects/flytectl/>`__ and ``flytekit``. This is the recommended configuration
-file format. Invoke ``flytectl --help`` to learn about all of the configuration yaml options.
+file format. Invoke the :ref:`flytectl config init <flytectl_config_init>` command to create a boilerplate
+``~/.flyte/config.yaml`` file, and  ``flytectl --help`` to learn about all of the configuration yaml options.
 
 .. dropdown:: See example ``config.yaml`` file
    :title: text-muted
