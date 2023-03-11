d23df46a68a30cc09210589831941bdc0e4d08de
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2023-03-10 16:12:19 -0800

    [v1.2 Backport] be24c52 through e90ee25 (#1549)
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- plugins/flytekit-data-fsspec/tests/test_persist.py
+++ plugins/flytekit-data-fsspec/tests/test_persist.py
@@ -2,6 +2,7 @@ import os
 import pathlib
 import tempfile
 
+import mock
 from flytekitplugins.fsspec.persist import FSSpecPersistence, s3_setup_args
 from fsspec.implementations.local import LocalFileSystem
 
@@ -17,6 +18,54 @@ def test_s3_setup_args():
 
     kwargs = s3_setup_args(S3Config(access_key_id="access"))
     assert kwargs == {"key": "access"}
+
+
+@mock.patch.dict(os.environ, {}, clear=True)
+def test_s3_setup_args_env_empty():
+    kwargs = s3_setup_args(S3Config.auto())
+    assert kwargs == {}
+
+
+@mock.patch.dict(
+    os.environ,
+    {
+        "AWS_ACCESS_KEY_ID": "ignore-user",
+        "AWS_SECRET_ACCESS_KEY": "ignore-secret",
+        "FLYTE_AWS_ACCESS_KEY_ID": "flyte",
+        "FLYTE_AWS_SECRET_ACCESS_KEY": "flyte-secret",
+    },
+    clear=True,
+)
+def test_s3_setup_args_env_both():
+    kwargs = s3_setup_args(S3Config.auto())
+    assert kwargs == {"key": "flyte", "secret": "flyte-secret"}
+
+
+@mock.patch.dict(
+    os.environ,
+    {
+        "FLYTE_AWS_ACCESS_KEY_ID": "flyte",
+        "FLYTE_AWS_SECRET_ACCESS_KEY": "flyte-secret",
+    },
+    clear=True,
+)
+def test_s3_setup_args_env_flyte():
+    kwargs = s3_setup_args(S3Config.auto())
+    assert kwargs == {"key": "flyte", "secret": "flyte-secret"}
+
+
+@mock.patch.dict(
+    os.environ,
+    {
+        "AWS_ACCESS_KEY_ID": "ignore-user",
+        "AWS_SECRET_ACCESS_KEY": "ignore-secret",
+    },
+    clear=True,
+)
+def test_s3_setup_args_env_aws():
+    kwargs = s3_setup_args(S3Config.auto())
+    # not explicitly in kwargs, since fsspec/boto3 will use these env vars by default
+    assert kwargs == {}
 
 
 def test_get_protocol():
