bbc258bd309be4bf8b6115e1724c259f08d0744f
Kevin Su <pingsutw@apache.org>
2023-03-14 12:25:46 -0700

    wip
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/extend/image_spec/base_image.py
+++ flytekit/extend/image_spec/base_image.py
@@ -1,8 +1,7 @@
 import pathlib
-from typing import List
+from typing import List, Optional
 from dataclasses import dataclass
 import docker
-from flytekit.core import context_manager
 import subprocess
 
 client = docker.from_env()
@@ -10,23 +9,30 @@ client = docker.from_env()
 
 @dataclass
 class ImageSpec:
+    """
+    Args:
+        packages: list of packages that will be installed in the image.
+        os: operating system. by default is ubuntu 20.04.
+        registry: docker registry. if it's specified, flytekit will push the image.
+        python_version: python version in the image.
+    """
+    packages: list[str]
     os: str = "ubuntu20.04"
-    python_version: str = "3.9"
+    registry: Optional[str] = None
+    python_version: Optional[str] = None,
 
 
-def create_envd_config(os: str, packages: List[str]) -> str:
+def create_envd_config(image_spec: ImageSpec) -> str:
     packages_list = ""
-    for pkg in packages:
+    for pkg in image_spec.packages:
         packages_list += f'"{pkg}", '
 
-    return f"""
+    envd_config = f"""
 def build():
-    base(os="{os}", language="python3")
+    base(os="{image_spec.os}", language="python3")
     install.python_packages(name = [{packages_list}])
 """
-
-
-def build_docker_image(envd_config: str):
+    from flytekit.core import context_manager
     ctx = context_manager.FlyteContextManager.current_context()
     cfg_path = ctx.file_access.get_random_local_path("build.envd")
     pathlib.Path(cfg_path).parent.mkdir(parents=True, exist_ok=True)
@@ -34,10 +40,16 @@ def build_docker_image(envd_config: str):
     with open(cfg_path, "x") as f:
         f.write(envd_config)
 
+    return cfg_path
+
+
+def build_docker_image(image_spec: ImageSpec):
+    cfg_path = create_envd_config(image_spec)
     p = subprocess.run(["envd", "build",
                         "--path", f"{pathlib.Path(cfg_path).parent}",
                         ],
                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
     print("=====stdout=====")
     print(p.stdout.decode())
     print("=====stderr=====")
@@ -45,5 +57,4 @@ def build_docker_image(envd_config: str):
 
 
 if __name__ == '__main__':
-    cfg = create_envd_config("ubuntu20.04", ["pandas"])
-    build_docker_image(cfg)
+    build_docker_image(ImageSpec(os="ubuntu20.04", packages=["pandas"]))
