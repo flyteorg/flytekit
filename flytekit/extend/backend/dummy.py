693b6cda87bca6eec6a32eef03a478f7a9bec524
Kevin Su <pingsutw@apache.org>
2023-03-22 04:58:42 -0700

    test
    Signed-off-by: Kevin Su <pingsutw@apache.org>

---
+++ flytekit/extend/backend/dummy.py
@@ -0,0 +1,29 @@
+import typing
+
+from flyteidl.service import plugin_system_pb2
+
+from flytekit.extend.backend.base_plugin import BackendPluginBase, BackendPluginRegistry
+from flytekit.models.literals import LiteralMap
+from flytekit.models.task import TaskTemplate
+
+
+class DummyPlugin(BackendPluginBase):
+    def __init__(self):
+        super().__init__(task_type="dummy")
+
+    def initialize(self):
+        pass
+
+    def create(
+        self, inputs: typing.Optional[LiteralMap], output_prefix: str, task_template: TaskTemplate
+    ) -> plugin_system_pb2.TaskCreateResponse:
+        return plugin_system_pb2.TaskCreateResponse(job_id="dummy_id")
+
+    def get(self, job_id: str) -> plugin_system_pb2.TaskGetResponse:
+        return plugin_system_pb2.TaskGetResponse(state=plugin_system_pb2.SUCCEEDED)
+
+    def delete(self, job_id) -> plugin_system_pb2.TaskDeleteResponse:
+        return plugin_system_pb2.TaskDeleteResponse()
+
+
+BackendPluginRegistry.register(DummyPlugin())
