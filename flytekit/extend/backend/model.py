a58e95277240225bdff43e3da8091321af51bcd3
Kevin Su <pingsutw@apache.org>
2023-03-23 15:39:46 -0700

    test
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/extend/backend/model.py
+++ flytekit/extend/backend/model.py
@@ -5,9 +5,10 @@ from flytekit.models.literals import LiteralMap
 
 
 class TaskCreateRequest(common.FlyteIdlEntity):
-    def __init__(self, inputs: LiteralMap, template: task.TaskTemplate):
+    def __init__(self, inputs: LiteralMap, template: task.TaskTemplate, output_prefix: str):
         self._inputs = inputs
         self._template = template
+        self._output_prefix = output_prefix
 
     @property
     def inputs(self):
@@ -17,9 +18,13 @@ class TaskCreateRequest(common.FlyteIdlEntity):
     def template(self):
         return self._template
 
+    @property
+    def output_prefix(self):
+        return self._output_prefix
+
     def to_flyte_idl(self):
         return plugin_system_pb2.TaskCreateRequest(
-            inputs=self.inputs.to_flyte_idl(), template=self.template.to_flyte_idl()
+            inputs=self.inputs.to_flyte_idl(), template=self.template.to_flyte_idl(), output_prefix=self.output_prefix
         )
 
     @classmethod
@@ -27,4 +32,5 @@ class TaskCreateRequest(common.FlyteIdlEntity):
         return cls(
             inputs=LiteralMap.from_flyte_idl(proto.inputs) if proto.inputs is not None else None,
             template=task.TaskTemplate.from_flyte_idl(proto.template),
+            output_prefix=proto.output_prefix,
         )
