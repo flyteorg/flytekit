28ce9396af2d28a06c45d889372645241afa021f
Ketan Umare <16888709+kumare3@users.noreply.github.com>
2023-03-27 22:45:38 -0700

    Device auth flow / Headless auth (#1552)
    * Device auth flow
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    * Device AuthFlow is now available in flytekit
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    * unit tests
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    * test added
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    * updated
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    * Fixed test
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    * Fixed unit test
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    * Fixed lint errors
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    ---------
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

---
+++ tests/flytekit/unit/clients/auth/test_token_client.py
@@ -0,0 +1,78 @@
+import json
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from flytekit.clients.auth.exceptions import AuthenticationError
+from flytekit.clients.auth.token_client import (
+    DeviceCodeResponse,
+    error_auth_pending,
+    get_basic_authorization_header,
+    get_device_code,
+    get_token,
+    poll_token_endpoint,
+)
+
+
+def test_get_basic_authorization_header():
+    header = get_basic_authorization_header("client_id", "abc")
+    assert header == "Basic Y2xpZW50X2lkOmFiYw=="
+
+
+@patch("flytekit.clients.auth.token_client.requests")
+def test_get_token(mock_requests):
+    response = MagicMock()
+    response.status_code = 200
+    response.json.return_value = json.loads("""{"access_token": "abc", "expires_in": 60}""")
+    mock_requests.post.return_value = response
+    access, expiration = get_token("https://corp.idp.net", client_id="abc123", scopes=["my_scope"])
+    assert access == "abc"
+    assert expiration == 60
+
+
+@patch("flytekit.clients.auth.token_client.requests")
+def test_get_device_code(mock_requests):
+    response = MagicMock()
+    response.ok = False
+    mock_requests.post.return_value = response
+    with pytest.raises(AuthenticationError):
+        get_device_code("test.com", "test")
+
+    response.ok = True
+    response.json.return_value = {
+        "device_code": "code",
+        "user_code": "BNDJJFXL",
+        "verification_uri": "url",
+        "verification_uri_complete": "url",
+        "expires_in": 600,
+        "interval": 5,
+    }
+    mock_requests.post.return_value = response
+    c = get_device_code("test.com", "test")
+    assert c
+    assert c.device_code == "code"
+
+
+@patch("flytekit.clients.auth.token_client.requests")
+def test_poll_token_endpoint(mock_requests):
+    response = MagicMock()
+    response.ok = False
+    response.json.return_value = {"error": error_auth_pending}
+    mock_requests.post.return_value = response
+
+    r = DeviceCodeResponse(
+        device_code="x", user_code="y", verification_uri="v", verification_uri_complete="v1", expires_in=1, interval=1
+    )
+    with pytest.raises(AuthenticationError):
+        poll_token_endpoint(r, "test.com", "test")
+
+    response = MagicMock()
+    response.ok = True
+    response.json.return_value = {"access_token": "abc", "expires_in": 60}
+    mock_requests.post.return_value = response
+    r = DeviceCodeResponse(
+        device_code="x", user_code="y", verification_uri="v", verification_uri_complete="v1", expires_in=1, interval=0
+    )
+    t, e = poll_token_endpoint(r, "test.com", "test")
+    assert t
+    assert e
