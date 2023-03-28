f7b59bc8a407f1b7818cd13c3c38cf49d47087cf
Josep Cugat <jcugat@gmail.com>
2023-03-28 20:42:21 +0200

    Less strict docker dependency versions (#1536) (#1564)
    Signed-off-by: Josep Cugat <josepc@spotify.com>
    Co-authored-by: Josep Cugat <josepc@spotify.com>
    Co-authored-by: Kevin Su <pingsutw@apache.org>

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         "click>=6.6,<9.0",
         "croniter>=0.3.20,<4.0.0",
         "deprecated>=1.0,<2.0",
-        "docker>=5.0.3,<7.0.0",
+        "docker>=4.0.0,<7.0.0",
         "python-dateutil>=2.1",
         # Restrict grpcio and grpcio-status.  The 1.49.1 version pulls in a version of protobuf that is not compatible.
         # More details in https://github.com/flyteorg/flyte/issues/3006
