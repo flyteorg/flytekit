23fd9332b12c313e80bdf79960a7d9a1c88864a6
Kevin Su <pingsutw@apache.org>
2023-03-31 00:06:43 -0700

    test
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- tests/flytekit/unit/cli/pyflyte/test_build.py
+++ tests/flytekit/unit/cli/pyflyte/test_build.py
@@ -1,27 +1,11 @@
-import os
-import sys
-
 from click.testing import CliRunner
 
 from flytekit.clis.sdk_in_container import pyflyte
-
-sample_file_contents = """
-from flytekit import task
-from flytekit.image_spec.image_spec import ImageSpec
-
-@task()
-def t1() -> str:
-    return "hello"
-"""
+from tests.flytekit.unit.cli.pyflyte.test_run import WORKFLOW_FILE
 
 
 def test_build():
     runner = CliRunner()
-    with runner.isolated_filesystem():
-        os.makedirs("core", exist_ok=True)
-        with open(os.path.join("core", "sample.py"), "w") as f:
-            f.write(sample_file_contents)
-        sys.path.append(os.path.join(os.getcwd(), "core"))
-        result = runner.invoke(pyflyte.main, ["build", "--file", "core/sample.py"])
-        assert result.output == ""
-        assert result.exit_code == 0
+    result = runner.invoke(pyflyte.main, ["build", "--file", WORKFLOW_FILE])
+    assert result.output == ""
+    assert result.exit_code == 0
