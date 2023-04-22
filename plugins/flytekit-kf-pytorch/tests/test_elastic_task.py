d45a23e6c559f760d9f82c580a22cea9bbb606c1
Fabio Graetz <fabiograetz@googlemail.com>
2023-04-22 17:29:33 +0000

    Add more tests
    Signed-off-by: Fabio Graetz <fabiograetz@googlemail.com>

--- plugins/flytekit-kf-pytorch/tests/test_elastic_task.py
+++ plugins/flytekit-kf-pytorch/tests/test_elastic_task.py
@@ -1,10 +1,13 @@
+import os
 from dataclasses import dataclass
 
 import torch
+import torch.distributed as dist
+import pytest
 from dataclasses_json import dataclass_json
-from flytekitplugins.torchelastic.task import Elastic
 
 from flytekit import task, workflow
+from flytekitplugins.kfpytorch.task import Elastic
 
 
 @dataclass_json
@@ -15,36 +18,56 @@ class Config:
     name: str = "foo"
 
 
-@task
-def init_model() -> torch.nn.Module:
-    model = torch.nn.Linear(11, 22)
+def dist_communicate() -> int:
+    """Communicate between distributed workers."""
+    rank = torch.distributed.get_rank()
+    world_size = dist.get_world_size()
+    tensor = torch.tensor([5], dtype=torch.int64) + 2 * rank + world_size
+    dist.all_reduce(tensor, op=dist.ReduceOp.SUM)
 
-    return model
+    return tensor.item()
 
 
-@task(task_config=Elastic())
-def train(config: Config, model: torch.nn.Module) -> tuple[str, Config, torch.nn.Module]:
-    import os
-
-    import torch
+def train(config: Config) -> tuple[str, Config, torch.nn.Module, int]:
+    """Mock training a model using torch-elastic for test purposes."""
+    dist.init_process_group(backend="gloo")
 
     local_rank = os.environ["LOCAL_RANK"]
 
     out_model = torch.nn.Linear(1000, int(local_rank) + 1)
-    
-    config.name = "modified"
-    return f"result from local rank {local_rank}", config, out_model
+    config.name = "elastic-test"
+
+    distributed_result = dist_communicate()
+
+    return f"result from local rank {local_rank}", config, out_model, distributed_result
 
 
-@workflow
-def wf(config: Config = Config()) -> tuple[str, Config, torch.nn.Module]:
-    model = init_model()
-    return train(config=config, model=model)
+@pytest.mark.parametrize("start_method", ["spawn", "fork"])
+def test_end_to_end(start_method: str) -> None:
+    """Test that the workflow with elastic task runs end to end."""
+    world_size = 2
 
+    train_task = task(train, task_config=Elastic(replicas=1, nproc_per_node=world_size, start_method=start_method))
 
-def test_end_to_end():
-    r, cfg, m = wf()
+    @workflow
+    def wf(config: Config = Config()) -> tuple[str, Config, torch.nn.Module, int]:
+        return train_task(config=config)
+
+    r, cfg, m, distributed_result = wf()
     assert "result from local rank 0" in r
-    assert cfg.name == "modified"
+    assert cfg.name == "elastic-test"
     assert m.in_features == 1000
     assert m.out_features == 1
+    """
+    The distributed result is calculated by the workers of the elastic train
+    task by performing a `dist.all_reduce` operation. The correct result can
+    only be obtained if the distributed process group is initialized correctly.
+    """
+    assert distributed_result == sum([5 + 2 * rank + world_size for rank in range(world_size)])
+
+
+def test_bad_replica_config() -> None:
+    """Test that bad replica config is caught."""
+    
+    with pytest.raises(ValueError):
+        task(train, task_config=Elastic(replicas=1, min_replicas=2))
