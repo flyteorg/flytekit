77fceb09f6acaecac133ee82f9856ba0506b04c5
Ketan Umare <ketan.umare@gmail.com>
2023-04-24 17:34:08 -0700

    removed dynamic execute
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

--- plugins/flytekit-kf-pytorch/flytekitplugins/kfpytorch/task.py
+++ plugins/flytekit-kf-pytorch/flytekitplugins/kfpytorch/task.py
@@ -13,8 +13,7 @@ from google.protobuf.json_format import MessageToDict
 import flytekit
 from flytekit import PythonFunctionTask
 from flytekit.configuration import SerializationSettings
-from flytekit.core.base_task import IgnoreOutputs
-from flytekit.extend import TaskPlugins
+from flytekit.extend import IgnoreOutputs, TaskPlugins
 
 
 @dataclass
@@ -141,7 +140,7 @@ class PytorchElasticFunctionTask(PythonFunctionTask[Elastic]):
         """
         self.rdzv_backend = "c10d"
 
-    def _execute(self, **kwargs) -> Any:
+    def execute(self, **kwargs) -> Any:
         """
         This helper method will be invoked to execute the task.
 
@@ -212,19 +211,6 @@ class PytorchElasticFunctionTask(PythonFunctionTask[Elastic]):
             return out[0]
         else:
             raise IgnoreOutputs()
-
-    def execute(self, **kwargs) -> Any:
-        """
-        This method will be invoked to execute the task.
-
-        Handles the exception scope for the `_execute` method.
-        """
-        from flytekit.exceptions import scopes as exception_scopes
-
-        if self.execution_mode == self.ExecutionBehavior.DEFAULT:
-            return exception_scopes.user_entry_point(self._execute)(**kwargs)
-        elif self.execution_mode == self.ExecutionBehavior.DYNAMIC:
-            return self.dynamic_execute(self._execute, **kwargs)
 
     def get_custom(self, settings: SerializationSettings) -> Optional[Dict[str, Any]]:
         if self.task_config.replicas == 1:
