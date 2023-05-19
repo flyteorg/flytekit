502b69f02a9a4c938d0d22ec31956c88c93d2b97
byhsu <byhsu@linkedin.com>
2023-05-18 21:48:20 -0700

    lint
    Signed-off-by: byhsu <byhsu@linkedin.com>

--- flytekit/remote/remote.py
+++ flytekit/remote/remote.py
@@ -747,7 +747,9 @@ class FlyteRemote(object):
                 upload_location.signed_url,
                 data=content,
                 headers={"Content-Length": str(content_length), "Content-MD5": encoded_md5},
-                verify=False if self._config.platform.insecure_skip_verify == True else self._config.platform.ca_cert_file_path,
+                verify=False
+                if self._config.platform.insecure_skip_verify is True
+                else self._config.platform.ca_cert_file_path,
             )
 
             if rsp.status_code != requests.codes["OK"]:
