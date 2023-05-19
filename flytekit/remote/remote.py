25a40123afd123f412bb9825daf284378fc7f40d
ByronHsu <byronhsu1230@gmail.com>
2023-05-19 10:43:47 -0700

    Improve flytekit register (#1643)
    * Fix pyflyte register
    
    Signed-off-by: byhsu <byhsu@linkedin.com>
    
    * revert
    
    Signed-off-by: byhsu <byhsu@linkedin.com>
    
    * lint
    
    Signed-off-by: byhsu <byhsu@linkedin.com>
    
    ---------
    
    Signed-off-by: byhsu <byhsu@linkedin.com>
    Co-authored-by: byhsu <byhsu@linkedin.com>

--- flytekit/remote/remote.py
+++ flytekit/remote/remote.py
@@ -788,6 +788,9 @@ class FlyteRemote(object):
                 upload_location.signed_url,
                 data=content,
                 headers={"Content-Length": str(content_length), "Content-MD5": encoded_md5},
+                verify=False
+                if self._config.platform.insecure_skip_verify is True
+                else self._config.platform.ca_cert_file_path,
             )
 
             if rsp.status_code != requests.codes["OK"]:
