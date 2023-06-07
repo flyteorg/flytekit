72612028c564f0f1950d9b1dfc026519cfd9ce0d
ChungYujoyce <joyce.bhps@gmail.com>
2023-06-07 16:17:07 -0700

    add unit tests
    Signed-off-by: ChungYujoyce <joyce.bhps@gmail.com>

--- tests/flytekit/unit/types/structured_dataset/test_structured_dataset_workflow.py
+++ tests/flytekit/unit/types/structured_dataset/test_structured_dataset_workflow.py
@@ -10,11 +10,13 @@ from typing_extensions import Annotated
 
 from flytekit import FlyteContext, FlyteContextManager, kwtypes, task, workflow
 from flytekit.models import literals
+from flytekit.types.structured.basic_dfs import PandasToCSVEncodingHandler, CSVToPandasDecodingHandler
 from flytekit.models.literals import StructuredDatasetMetadata
 from flytekit.models.types import StructuredDatasetType
 from flytekit.types.structured.structured_dataset import (
     DF,
     PARQUET,
+    CSV,
     StructuredDataset,
     StructuredDatasetDecoder,
     StructuredDatasetEncoder,
@@ -198,6 +200,21 @@ def t10(dataset: Annotated[StructuredDataset, my_cols]) -> np.ndarray:
     return np_array
 
 
+StructuredDatasetTransformerEngine.register(PandasToCSVEncodingHandler())
+StructuredDatasetTransformerEngine.register(CSVToPandasDecodingHandler())
+
+@task
+def t11(dataframe: pd.DataFrame) -> Annotated[StructuredDataset, CSV]:
+    # pandas -> csv
+    return StructuredDataset(dataframe=dataframe, uri=PANDAS_PATH)
+
+@task
+def t12(dataset: Annotated[StructuredDataset, my_cols]) -> pd.DataFrame:
+    # cav -> pandas
+    df = dataset.open(pd.DataFrame).all()
+    return df
+
+
 @task
 def generate_pandas() -> pd.DataFrame:
     return pd_df
@@ -231,6 +248,8 @@ def wf():
     t8a(dataframe=arrow_df)
     t9(dataframe=np_array)
     t10(dataset=StructuredDataset(uri=NUMPY_PATH))
+    t11(dataframe=df)
+    t12(dataset=StructuredDataset(uri=PANDAS_PATH))
 
 
 def test_structured_dataset_wf():
