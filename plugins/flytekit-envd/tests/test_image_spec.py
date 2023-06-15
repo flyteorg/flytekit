207d01986d744f8212a3e2e28826ce947efa96d9
Yini <131146298+yini7777@users.noreply.github.com>
2023-06-15 05:18:06 +0200

    Support configuring pip index url with the image spec (#1692)
    * Support configuring pip index url with the image spec
    
    Signed-off-by: yini.gao@schibsted.com <yini.gao@schibsted.com>
    
    * fix tests and replace dash to make tag valid
    
    Signed-off-by: yini.gao@schibsted.com <yini.gao@schibsted.com>
    
    * fix envd plugin test
    
    Signed-off-by: yini.gao@schibsted.com <yini.gao@schibsted.com>
    
    ---------
    
    Signed-off-by: yini.gao@schibsted.com <yini.gao@schibsted.com>
    Co-authored-by: yini.gao@schibsted.com <yini.gao@schibsted.com>

--- plugins/flytekit-envd/tests/test_image_spec.py
+++ plugins/flytekit-envd/tests/test_image_spec.py
@@ -12,6 +12,7 @@ def test_image_spec():
         python_version="3.8",
         registry="",
         base_image="cr.flyte.org/flyteorg/flytekit:py3.8-latest",
+        pip_index="https://private-pip-index/simple",
     )
 
     EnvdImageSpecBuilder().build_image(image_spec)
@@ -26,7 +27,8 @@ def build():
     base(image="cr.flyte.org/flyteorg/flytekit:py3.8-latest", dev=False)
     install.python_packages(name = ["pandas"])
     install.apt_packages(name = ["git"])
-    runtime.environ(env={'PYTHONPATH': '/root', '_F_IMG_ID': 'flytekit:OLFSrRjcG5_uXuRqd0TSdQ..'})
+    runtime.environ(env={'PYTHONPATH': '/root', '_F_IMG_ID': 'flytekit:46qVNvYHJxppEvVIYrthdA..'})
+    config.pip_index(url = "https://private-pip-index/simple")
     install.python(version="3.8")
 """
     )
--- tests/flytekit/unit/cli/pyflyte/test_run.py
+++ tests/flytekit/unit/cli/pyflyte/test_run.py
@@ -269,9 +269,9 @@ ic_result_3 = ImageConfig(
 )
 
 ic_result_4 = ImageConfig(
-    default_image=Image(name="default", fqn="flytekit", tag="mMxGzKCqxVk8msz0yV22-g.."),
+    default_image=Image(name="default", fqn="flytekit", tag="6y6c8ofS_Pwa2FImlcm3Qg.."),
     images=[
-        Image(name="default", fqn="flytekit", tag="mMxGzKCqxVk8msz0yV22-g.."),
+        Image(name="default", fqn="flytekit", tag="6y6c8ofS_Pwa2FImlcm3Qg.."),
         Image(name="xyz", fqn="docker.io/xyz", tag="latest"),
         Image(name="abc", fqn="docker.io/abc", tag=None),
     ],
