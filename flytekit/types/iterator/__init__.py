5ef1278918001904e3e8d71a1a8c5d17b546a4a8
Kevin Su <pingsutw@apache.org>
2023-06-21 14:00:02 -0700

    Add Flyte iterator
    Signed-off-by: Kevin Su <pingsutw@apache.org>

---
+++ flytekit/types/iterator/__init__.py
@@ -0,0 +1,12 @@
+"""
+Flytekit Iterator Type
+==========================================================
+.. currentmodule:: flytekit.types.iterator
+
+.. autosummary::
+   :toctree: generated/
+
+   FlyteIterator
+"""
+
+from .iterator import FlyteIterator
