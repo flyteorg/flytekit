540c704fcd2ba165a76d691a4f077e553c902273
Kevin Su <pingsutw@apache.org>
2023-06-20 22:12:45 -0700

    Add bigquery project and location to metadata
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- plugins/flytekit-bigquery/flytekitplugins/bigquery/agent.py
+++ plugins/flytekit-bigquery/flytekitplugins/bigquery/agent.py
@@ -4,7 +4,14 @@ from dataclasses import asdict, dataclass
 from typing import Dict, Optional
 
 import grpc
-from flyteidl.admin.agent_pb2 import SUCCEEDED, CreateTaskResponse, DeleteTaskResponse, GetTaskResponse, Resource
+from flyteidl.admin.agent_pb2 import (
+    PERMANENT_FAILURE,
+    SUCCEEDED,
+    CreateTaskResponse,
+    DeleteTaskResponse,
+    GetTaskResponse,
+    Resource,
+)
 from google.cloud import bigquery
 
 from flytekit import FlyteContextManager, StructuredDataset, logger
@@ -30,6 +37,8 @@ pythonTypeToBigQueryType: Dict[type, str] = {
 @dataclass
 class Metadata:
     job_id: str
+    project: str
+    location: str
 
 
 class BigQueryAgent(AgentBase):
@@ -50,7 +59,6 @@ class BigQueryAgent(AgentBase):
                 name: TypeEngine.guess_python_type(lt.type) for name, lt in task_template.interface.inputs.items()
             }
             native_inputs = TypeEngine.literal_map_to_kwargs(ctx, inputs, python_interface_inputs)
-
             logger.info(f"Create BigQuery job config with inputs: {native_inputs}")
             job_config = bigquery.QueryJobConfig(
                 query_parameters=[
@@ -60,40 +68,49 @@ class BigQueryAgent(AgentBase):
             )
 
         custom = task_template.custom
-        client = bigquery.Client(project=custom["ProjectID"], location=custom["Location"])
+        project = custom["ProjectID"]
+        location = custom["Location"]
+        client = bigquery.Client(project=project, location=location)
         query_job = client.query(task_template.sql.statement, job_config=job_config)
+        metadata = Metadata(job_id=str(query_job.job_id), location=location, project=project)
 
-        return CreateTaskResponse(
-            resource_meta=json.dumps(asdict(Metadata(job_id=str(query_job.job_id)))).encode("utf-8")
-        )
+        return CreateTaskResponse(resource_meta=json.dumps(asdict(metadata)).encode("utf-8"))
 
     def get(self, context: grpc.ServicerContext, resource_meta: bytes) -> GetTaskResponse:
         client = bigquery.Client()
         metadata = Metadata(**json.loads(resource_meta.decode("utf-8")))
-        job = client.get_job(metadata.job_id)
+        job = client.get_job(metadata.job_id, metadata.project, metadata.location)
+        if job.errors:
+            logger.error(job.errors.__str__())
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details(job.errors.__str__())
+            return GetTaskResponse(resource=Resource(state=PERMANENT_FAILURE))
         cur_state = convert_to_flyte_state(str(job.state))
         res = None
 
         if cur_state == SUCCEEDED:
             ctx = FlyteContextManager.current_context()
-            output_location = f"bq://{job.destination.project}:{job.destination.dataset_id}.{job.destination.table_id}"
-            res = literals.LiteralMap(
-                {
-                    "results": TypeEngine.to_literal(
-                        ctx,
-                        StructuredDataset(uri=output_location),
-                        StructuredDataset,
-                        LiteralType(structured_dataset_type=StructuredDatasetType(format="")),
-                    )
-                }
-            )
+            if job.destination:
+                output_location = (
+                    f"bq://{job.destination.project}:{job.destination.dataset_id}.{job.destination.table_id}"
+                )
+                res = literals.LiteralMap(
+                    {
+                        "results": TypeEngine.to_literal(
+                            ctx,
+                            StructuredDataset(uri=output_location),
+                            StructuredDataset,
+                            LiteralType(structured_dataset_type=StructuredDatasetType(format="")),
+                        )
+                    }
+                ).to_flyte_idl()
 
-        return GetTaskResponse(resource=Resource(state=cur_state, outputs=res.to_flyte_idl()))
+        return GetTaskResponse(resource=Resource(state=cur_state, outputs=res))
 
     def delete(self, context: grpc.ServicerContext, resource_meta: bytes) -> DeleteTaskResponse:
         client = bigquery.Client()
         metadata = Metadata(**json.loads(resource_meta.decode("utf-8")))
-        client.cancel_job(metadata.job_id)
+        client.cancel_job(metadata.job_id, metadata.project, metadata.location)
         return DeleteTaskResponse()
 
 
