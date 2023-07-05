337b296ae4765d3a7e583e7014a108f6e7c67189
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2023-07-05 15:06:09 -0700

    add testing examples
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

---
+++ tests/flytekit/unit/remote/test_mocked.py
@@ -0,0 +1,19 @@
+import mock
+
+from flytekit.core.task import task
+
+
+@mock.patch("flytekit.remote.remote.FlyteRemote")
+def test_mocking_remote(mock_remote) -> None:
+    @task
+    def t1() -> float:
+        return 6.62607015e-34
+
+    @task
+    def t2() -> bool:
+        return False
+
+    mock_remote.return_value.fetch_task.side_effect = [t1, t2]
+    from . import wf_with_remote
+    x = wf_with_remote.hello_wf(a=3)
+    assert x == (6.62607015e-34, False)
