9b399b9ede6acc0d97761c3fc3f8a67a1b507903
Daniel Rammer <daniel@union.ai>
2023-07-07 10:05:16 -0500

    using flyteidl main now that merged
    Signed-off-by: Daniel Rammer <daniel@union.ai>

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setup(
     install_requires=[
         "googleapis-common-protos>=1.57",
         # TODO: put this back
-        # "flyteidl>=1.3.16,<1.4.0",
-        "flyteidl @ git+https://github.com/flyteorg/flyteidl.git@feature/array-node#egg=flyteidl",
+        "flyteidl>=1.3.16,<1.4.0",
+        #"flyteidl @ git+https://github.com/flyteorg/flyteidl.git@feature/array-node#egg=flyteidl",
         "wheel>=0.30.0,<1.0.0",
         "pandas>=1.0.0,<2.0.0",
         "pyarrow>=4.0.0,<11.0.0",
