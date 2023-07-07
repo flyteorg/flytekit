f76b1f290898e0747ddb054252901721d5b699b5
Daniel Rammer <daniel@union.ai>
2023-07-07 10:16:10 -0500

    comma
    Signed-off-by: Daniel Rammer <daniel@union.ai>

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         # TODO: put this back
         #"flyteidl>=1.3.16,<1.4.0",
         #"flyteidl @ git+https://github.com/flyteorg/flyteidl.git@feature/array-node#egg=flyteidl",
-        "flyteidl>=1.5.12"
+        "flyteidl>=1.5.12",
         "wheel>=0.30.0,<1.0.0",
         "pandas>=1.0.0,<2.0.0",
         "pyarrow>=4.0.0,<11.0.0",
