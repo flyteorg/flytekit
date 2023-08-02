9b0de74202a30639505d219b4a08bd32372d189d
Kevin Su <pingsutw@apache.org>
2023-08-02 11:28:28 -0700

    add extract function
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/extend/backend/base_sensor.py
+++ flytekit/extend/backend/base_sensor.py
@@ -1,8 +1,8 @@
+import typing
 from typing import Optional
 
 import cloudpickle
 import grpc
-import typing
 from flyteidl.admin.agent_pb2 import (
     RUNNING,
     SUCCEEDED,
@@ -12,11 +12,9 @@ from flyteidl.admin.agent_pb2 import (
     Resource,
 )
 
-from flytekit.core.sensor_task import FileSensorConfig
 from flytekit.extend.backend.base_agent import AgentBase
 from flytekit.models.literals import LiteralMap
 from flytekit.models.task import TaskTemplate
-
 
 T = typing.TypeVar("T")
 
@@ -25,11 +23,18 @@ class SensorBase(AgentBase):
     def __init__(self, task_type: str):
         super().__init__(task_type=task_type, asynchronous=True)
 
-    async def poke(self, v: T) -> bool:
+    async def poke(self, metadata: T) -> bool:
         # Sensor will use this method to check if the condition is met
+        # For example:
         # - Check if the file exists
         # - Check if the file is modified
         # - Check if the workflow is completed or failed
+        raise NotImplementedError
+
+    async def extract(
+        self, context: grpc.ServicerContext, task_template: TaskTemplate, inputs: Optional[LiteralMap] = None
+    ) -> T:
+        # Sensor will use this method to extract the metadata from the task template
         raise NotImplementedError
 
     async def async_create(
@@ -39,12 +44,12 @@ class SensorBase(AgentBase):
         task_template: TaskTemplate,
         inputs: Optional[LiteralMap] = None,
     ) -> CreateTaskResponse:
-        return CreateTaskResponse(resource_meta=cloudpickle.dumps(task_template.custom))
+        metadata = await self.extract(context, task_template, inputs)
+        return CreateTaskResponse(resource_meta=cloudpickle.dumps(metadata))
 
     async def async_get(self, context: grpc.ServicerContext, resource_meta: bytes) -> GetTaskResponse:
         meta = cloudpickle.loads(resource_meta)
-        path = FileSensorConfig(**meta).path
-        cur_state = SUCCEEDED if await self.poke(path) else RUNNING
+        cur_state = SUCCEEDED if await self.poke(meta) else RUNNING
         return GetTaskResponse(resource=Resource(state=cur_state, outputs=None))
 
     async def async_delete(self, context: grpc.ServicerContext, resource_meta: bytes) -> DeleteTaskResponse:
--- flytekit/extend/backend/file_sensor.py
+++ flytekit/extend/backend/file_sensor.py
@@ -1,25 +1,36 @@
 import asyncio
+from typing import Optional
 
 import fsspec
+import grpc
 from fsspec.utils import get_protocol
 
 from flytekit.configuration import DataConfig
 from flytekit.core.data_persistence import s3_setup_args
+from flytekit.core.sensor_task import FileSensorConfig
 from flytekit.extend.backend.base_agent import AgentRegistry
 from flytekit.extend.backend.base_sensor import SensorBase
+from flytekit.models.literals import LiteralMap
+from flytekit.models.task import TaskTemplate
 
 
 class FileSensor(SensorBase):
     def __init__(self):
         super().__init__(task_type="file_sensor")
 
-    async def poke(self, path: str) -> bool:
+    async def poke(self, cfg: FileSensorConfig) -> bool:
+        path = cfg.path
         protocol = get_protocol(path)
         kwargs = {}
         if get_protocol(path):
             kwargs = s3_setup_args(DataConfig.auto().s3, anonymous=False)
         fs = fsspec.filesystem(protocol, **kwargs)
         return await asyncio.to_thread(fs.exists, path)
+
+    async def extract(
+        self, context: grpc.ServicerContext, task_template: TaskTemplate, inputs: Optional[LiteralMap] = None
+    ) -> FileSensorConfig:
+        return FileSensorConfig(**task_template.custom)
 
 
 AgentRegistry.register(FileSensor())
