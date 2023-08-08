0a4e336ec53ca8d45dfd4a07c3a2c7fa3b830bef
Kevin Su <pingsutw@apache.org>
2023-08-08 16:19:06 -0700

    update comment
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- plugins/flytekit-memverge/flytekitplugins/memverge/agent.py
+++ plugins/flytekit-memverge/flytekitplugins/memverge/agent.py
@@ -28,8 +28,17 @@ class MemvergeAgent(AgentBase):
     ) -> CreateTaskResponse:
         print("task template", task_template)
         print("container args", task_template.container.args)
+        # container args ['pyflyte-fast-execute', '--additional-distribution',
+        # 's3://my-s3-bucket/flytesnacks/development/XYP3YCR5RKERZXBH5ACD7E56PY======/script_mode.tar.gz',
+        # '--dest-dir', '/root', '--', 'pyflyte-execute', '--inputs',
+        # 's3://my-s3-bucket/metadata/propeller/flytesnacks-development-axq2rsxnmht57kq9sxpt/n0/data/inputs.pb',
+        # '--output-prefix', 's3://my-s3-bucket/metadata/propeller/flytesnacks-development-axq2rsxnmht57kq9sxpt/n0/data/0',
+        # '--raw-output-data-prefix', 's3://my-s3-bucket/test/vr/axq2rsxnmht57kq9sxpt-n0-0', '--checkpoint-path',
+        # 's3://my-s3-bucket/test/vr/axq2rsxnmht57kq9sxpt-n0-0/_flytecheckpoints', '--prev-checkpoint', '""',
+        # '--resolver', 'flytekit.core.python_auto_container.default_task_resolver', '--', 'task-module',
+        # 'memverge_wf', 'task-name', 't1']
         print("container image", task_template.container.image)
-
+        # container image pingsutw/flytekit:GAC0mevQ3yV7P0Q8uH6JwQ..
         job_id = "memverge_job_id"
         return CreateTaskResponse(resource_meta=cloudpickle.dumps(job_id))
 
