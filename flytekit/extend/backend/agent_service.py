19c19ee073c28cddd14f8dd14df60a5b06409d93
Kevin Su <pingsutw@apache.org>
2023-08-12 16:11:57 -0700

    Async Agent (#1712)
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/extend/backend/agent_service.py
+++ flytekit/extend/backend/agent_service.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import grpc
 from flyteidl.admin.agent_pb2 import (
     PERMANENT_FAILURE,
@@ -17,37 +19,74 @@ from flytekit.models.literals import LiteralMap
 from flytekit.models.task import TaskTemplate
 
 
-class AgentService(AsyncAgentServiceServicer):
-    def CreateTask(self, request: CreateTaskRequest, context: grpc.ServicerContext) -> CreateTaskResponse:
+class AsyncAgentService(AsyncAgentServiceServicer):
+    async def CreateTask(self, request: CreateTaskRequest, context: grpc.ServicerContext) -> CreateTaskResponse:
         try:
             tmp = TaskTemplate.from_flyte_idl(request.template)
             inputs = LiteralMap.from_flyte_idl(request.inputs) if request.inputs else None
             agent = AgentRegistry.get_agent(context, tmp.type)
+            logger.info(f"{tmp.type} agent start creating the job")
             if agent is None:
                 return CreateTaskResponse()
-            return agent.create(context=context, inputs=inputs, output_prefix=request.output_prefix, task_template=tmp)
+            if agent.asynchronous:
+                try:
+                    return await agent.async_create(
+                        context=context, inputs=inputs, output_prefix=request.output_prefix, task_template=tmp
+                    )
+                except Exception as e:
+                    logger.error(f"failed to run async create with error {e}")
+                    raise e
+            try:
+                return await asyncio.to_thread(
+                    agent.create, context=context, inputs=inputs, output_prefix=request.output_prefix, task_template=tmp
+                )
+            except Exception as e:
+                logger.error(f"failed to run sync create with error {e}")
+                raise
         except Exception as e:
             logger.error(f"failed to create task with error {e}")
             context.set_code(grpc.StatusCode.INTERNAL)
             context.set_details(f"failed to create task with error {e}")
 
-    def GetTask(self, request: GetTaskRequest, context: grpc.ServicerContext) -> GetTaskResponse:
+    async def GetTask(self, request: GetTaskRequest, context: grpc.ServicerContext) -> GetTaskResponse:
         try:
             agent = AgentRegistry.get_agent(context, request.task_type)
+            logger.info(f"{agent.task_type} agent start checking the status of the job")
             if agent is None:
                 return GetTaskResponse(resource=Resource(state=PERMANENT_FAILURE))
-            return agent.get(context=context, resource_meta=request.resource_meta)
+            if agent.asynchronous:
+                try:
+                    return await agent.async_get(context=context, resource_meta=request.resource_meta)
+                except Exception as e:
+                    logger.error(f"failed to run async get with error {e}")
+                    raise
+            try:
+                return await asyncio.to_thread(agent.get, context=context, resource_meta=request.resource_meta)
+            except Exception as e:
+                logger.error(f"failed to run sync get with error {e}")
+                raise
         except Exception as e:
             logger.error(f"failed to get task with error {e}")
             context.set_code(grpc.StatusCode.INTERNAL)
             context.set_details(f"failed to get task with error {e}")
 
-    def DeleteTask(self, request: DeleteTaskRequest, context: grpc.ServicerContext) -> DeleteTaskResponse:
+    async def DeleteTask(self, request: DeleteTaskRequest, context: grpc.ServicerContext) -> DeleteTaskResponse:
         try:
             agent = AgentRegistry.get_agent(context, request.task_type)
+            logger.info(f"{agent.task_type} agent start deleting the job")
             if agent is None:
                 return DeleteTaskResponse()
-            return agent.delete(context=context, resource_meta=request.resource_meta)
+            if agent.asynchronous:
+                try:
+                    return await agent.async_delete(context=context, resource_meta=request.resource_meta)
+                except Exception as e:
+                    logger.error(f"failed to run async delete with error {e}")
+                    raise
+            try:
+                return asyncio.to_thread(agent.delete, context=context, resource_meta=request.resource_meta)
+            except Exception as e:
+                logger.error(f"failed to run sync delete with error {e}")
+                raise
         except Exception as e:
             logger.error(f"failed to delete task with error {e}")
             context.set_code(grpc.StatusCode.INTERNAL)
