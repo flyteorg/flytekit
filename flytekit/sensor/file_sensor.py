f96670be08e3bb8083a4de0429caf55c732803ca
Kevin Su <pingsutw@apache.org>
2023-08-14 15:11:41 -0700

    Fix local execution
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/sensor/file_sensor.py
+++ flytekit/sensor/file_sensor.py
@@ -11,5 +11,8 @@ class FileSensor(BaseSensor):
         super().__init__(name=name, sensor_config=config, **kwargs)
 
     async def poke(self, path: str) -> bool:
-        fs = FlyteContextManager.current_context().file_access.get_filesystem_for_path(path, asynchronous=True)
-        return await fs._exists(path)
+        file_access = FlyteContextManager.current_context().file_access
+        fs = file_access.get_filesystem_for_path(path, asynchronous=True)
+        if file_access.is_remote(path):
+            return await fs._exists(path)
+        return fs.exists(path)
