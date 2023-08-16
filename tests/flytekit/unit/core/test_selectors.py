dfdb38e9442e6d0c63441711ee04daea9216a038
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2023-08-16 11:16:38 -0700

    add asserts
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>
    Signed-off-by: Jeev B <jeevb@users.noreply.github.com>

--- tests/flytekit/unit/core/test_selectors.py
+++ tests/flytekit/unit/core/test_selectors.py
@@ -1,7 +1,9 @@
 from collections import OrderedDict
 
-import flytekit
+from flyteidl.core.tasks_pb2 import Selector
+
 from flytekit.configuration import Image, ImageConfig, SerializationSettings
+from flytekit.core.context_manager import FlyteContextManager
 from flytekit.core.data_persistence import flyte_tmp_dir
 from flytekit.core.selectors import GPUDeviceA100, GPUPartition2G10GB
 from flytekit.core.task import task
@@ -19,9 +21,14 @@ serialization_settings = SerializationSettings(
 def test_selectors():
     @task(selectors=[GPUDeviceA100, GPUPartition2G10GB])
     def needs_a100(a: int):
-        wf_params = flytekit.current_context()
+        ctx = FlyteContextManager.current_context()
+        wf_params = ctx.user_space_params
         assert str(wf_params.execution_id) == "ex:local:local:local"
         assert flyte_tmp_dir in wf_params.raw_output_prefix
 
     ts = get_serializable(OrderedDict(), serialization_settings, needs_a100)
     assert len(ts.template.metadata.selectors) == 2
+    s = ts.template.metadata.selectors[0]
+    assert isinstance(s, Selector)
+    assert s.HasField("gpu_device")
+    assert s.gpu_device == "nvidia-tesla-a100"
