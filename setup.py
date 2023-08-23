42e3e2303d2df84e1b9216120d91debba4cfc88e
tnam <tommy.nam@aiq-index.com>
2023-08-23 18:41:05 +0800

    build(chore): Upgrade to v1.8.0
    Signed-off-by: tnam <tommy.nam@aiq-index.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup  # noqa
 
 extras_require = {}
 
-__version__ = "1.6.0+add-token-caching"
+__version__ = "1.8.0+image-spec"
 
 setup(
     name="flytekit",
