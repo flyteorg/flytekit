a30c25e6ae2f469cf27b6e7837d977fb441e30b5
Kevin Su <pingsutw@apache.org>
2023-08-30 11:33:03 -0700

    Update counter
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/extend/backend/agent_service.py
+++ flytekit/extend/backend/agent_service.py
@@ -13,14 +13,16 @@ from flyteidl.admin.agent_pb2 import (
 )
 from flyteidl.service.agent_pb2_grpc import AsyncAgentServiceServicer
 from prometheus_async.aio import time
-from prometheus_client import Gauge, Histogram, Summary
+from prometheus_client import Counter, Summary
 
 from flytekit import logger
 from flytekit.extend.backend.base_agent import AgentRegistry
 from flytekit.models.literals import LiteralMap
 from flytekit.models.task import TaskTemplate
 
-request_count = Gauge("request_count", "Total number of requests")
+request_count = Counter("request_count", "Total number of requests")
+request_success_count = Counter("request_success_count", "Total number of successful requests")
+request_failure_count = Counter("request_failure_count", "Total number of failed requests")
 create_req_process_time = Summary("create_request_processing_seconds", "Time spent processing agent create request")
 get_req_process_time = Summary("get_request_processing_seconds", "Time spent processing agent get request")
 delete_req_process_time = Summary("delete_request_processing_seconds", "Time spent processing agent delete request")
@@ -41,7 +43,7 @@ class AsyncAgentService(AsyncAgentServiceServicer):
                     res = await agent.async_create(
                         context=context, inputs=inputs, output_prefix=request.output_prefix, task_template=tmp
                     )
-                    request_count.dec()
+                    request_success_count.inc()
                     return res
                 except Exception as e:
                     logger.error(f"failed to run async create with error {e}")
@@ -50,7 +52,7 @@ class AsyncAgentService(AsyncAgentServiceServicer):
                 res = await asyncio.to_thread(
                     agent.create, context=context, inputs=inputs, output_prefix=request.output_prefix, task_template=tmp
                 )
-                request_count.dec()
+                request_success_count.inc()
                 return res
             except Exception as e:
                 logger.error(f"failed to run sync create with error {e}")
@@ -59,7 +61,7 @@ class AsyncAgentService(AsyncAgentServiceServicer):
             logger.error(f"failed to create task with error {e}")
             context.set_code(grpc.StatusCode.INTERNAL)
             context.set_details(f"failed to create task with error {e}")
-            request_count.dec()
+            request_failure_count.inc()
 
     @time(get_req_process_time)
     async def GetTask(self, request: GetTaskRequest, context: grpc.ServicerContext) -> GetTaskResponse:
@@ -72,14 +74,14 @@ class AsyncAgentService(AsyncAgentServiceServicer):
             if agent.asynchronous:
                 try:
                     res = await agent.async_get(context=context, resource_meta=request.resource_meta)
-                    request_count.dec()
+                    request_success_count.inc()
                     return res
                 except Exception as e:
                     logger.error(f"failed to run async get with error {e}")
                     raise
             try:
                 res = await asyncio.to_thread(agent.get, context=context, resource_meta=request.resource_meta)
-                request_count.dec()
+                request_success_count.inc()
                 return res
             except Exception as e:
                 logger.error(f"failed to run sync get with error {e}")
@@ -88,7 +90,7 @@ class AsyncAgentService(AsyncAgentServiceServicer):
             logger.error(f"failed to get task with error {e}")
             context.set_code(grpc.StatusCode.INTERNAL)
             context.set_details(f"failed to get task with error {e}")
-            request_count.dec()
+            request_failure_count.inc()
 
     async def DeleteTask(self, request: DeleteTaskRequest, context: grpc.ServicerContext) -> DeleteTaskResponse:
         try:
@@ -100,14 +102,14 @@ class AsyncAgentService(AsyncAgentServiceServicer):
             if agent.asynchronous:
                 try:
                     res = await agent.async_delete(context=context, resource_meta=request.resource_meta)
-                    request_count.dec()
+                    request_success_count.inc()
                     return res
                 except Exception as e:
                     logger.error(f"failed to run async delete with error {e}")
                     raise
             try:
                 res = asyncio.to_thread(agent.delete, context=context, resource_meta=request.resource_meta)
-                request_count.dec()
+                request_success_count.inc()
                 return res
             except Exception as e:
                 logger.error(f"failed to run sync delete with error {e}")
@@ -116,4 +118,4 @@ class AsyncAgentService(AsyncAgentServiceServicer):
             logger.error(f"failed to delete task with error {e}")
             context.set_code(grpc.StatusCode.INTERNAL)
             context.set_details(f"failed to delete task with error {e}")
-            request_count.dec()
+            request_failure_count.inc()
