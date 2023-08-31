3789dfa12408efedea3dc72df9b4822c37c0b240
HH <hhcs9527@gmail.com>
2023-08-31 12:21:37 +0800

    add unit-test for snowflake structure dataset encoder/decoder
    Signed-off-by: HH <hhcs9527@gmail.com>

---
+++ tests/flytekit/unit/types/structured_dataset/test_snowflake.py
@@ -0,0 +1,48 @@
+import mock
+import pytest
+import pandas as pd
+from typing_extensions import Annotated
+
+from flytekit import StructuredDataset, kwtypes, task, workflow
+
+pd_df = pd.DataFrame({"Name": ["Tom", "Joseph"], "Age": [20, 22]})
+my_cols = kwtypes(Name=str, Age=int)
+
+
+@task
+def gen_df() -> Annotated[pd.DataFrame, my_cols, "parquet"]:
+    return pd_df
+
+
+@task
+def t1(df: pd.DataFrame) -> Annotated[StructuredDataset, my_cols]:
+    return StructuredDataset(dataframe=df, uri="snowflake://dummy_user:dummy_account/dummy_database/dummy_schema/dummy_warehouse/dummy_table")
+
+
+@task
+def t2(sd: Annotated[StructuredDataset, my_cols]) -> pd.DataFrame:
+    return sd.open(pd.DataFrame).all()
+
+
+@workflow
+def wf() -> pd.DataFrame:
+    df = gen_df()
+    sd = t1(df=df)
+    return t2(sd=sd)
+
+
+@mock.patch("snowflake.connector.connect")
+@pytest.mark.asyncio
+async def test_sf_wf(mock_connect):
+    class mock_pages:
+        def to_dataframe(self):
+            return pd.DataFrame({"Name": ["Tom", "Joseph"], "Age": [20, 22]})
+
+    class mock_rows:
+        pages = [mock_pages()]
+
+    mock_connect_instance = mock_connect.return_value
+    mock_coursor_instance = mock_connect.cursor.return_value
+    mock_coursor_instance.fetch_pandas_all.return_value = mock_rows
+
+    assert wf().equals(pd_df)
