18be31a6935cf5c1594c68ac5030238396e804a4
Jeev B <jeevb@users.noreply.github.com>
2023-09-01 15:08:54 -0700

    unpartitioned selector
    Signed-off-by: Jeev B <jeevb@users.noreply.github.com>

--- flytekit/core/selectors.py
+++ flytekit/core/selectors.py
@@ -1,34 +1,40 @@
 import abc
 from dataclasses import dataclass
-from typing import Optional
 
 from flyteidl.core import tasks_pb2
 
 
-@dataclass
 class BaseSelector(abc.ABC):
-    value: Optional[str]
-
     @abc.abstractmethod
     def to_flyte_idl(self) -> tasks_pb2.Selector:
         pass
 
 
+@dataclass
 class GPUDeviceSelector(BaseSelector):
+    value: str
+
     def to_flyte_idl(self) -> tasks_pb2.Selector:
         return tasks_pb2.Selector(gpu_device=self.value)
 
 
+class GPUUnpartitionedSelector(BaseSelector):
+    def to_flyte_idl(self) -> tasks_pb2.Selector:
+        return tasks_pb2.Selector(gpu_unpartitioned=True)
+
+
+@dataclass
 class GPUPartitionSizeSelector(BaseSelector):
+    value: str
+
     def to_flyte_idl(self) -> tasks_pb2.Selector:
         return tasks_pb2.Selector(gpu_partition_size=self.value)
 
 
 GPUDeviceA100 = GPUDeviceSelector(value="nvidia-tesla-a100")
-
+GPUUnpartitioned = GPUUnpartitionedSelector()
 GPUPartition1G5GB = GPUPartitionSizeSelector(value="1g.5gb")
 GPUPartition2G10GB = GPUPartitionSizeSelector(value="2g.10gb")
 GPUPartition3G20GB = GPUPartitionSizeSelector(value="3g.20gb")
 GPUPartition4G20GB = GPUPartitionSizeSelector(value="4g.20gb")
 GPUPartition7G40GB = GPUPartitionSizeSelector(value="7g.40gb")
-GPUUnpartitioned = GPUPartitionSizeSelector(value="")
--- tests/flytekit/unit/core/test_selectors.py
+++ tests/flytekit/unit/core/test_selectors.py
@@ -5,7 +5,7 @@ from flyteidl.core.tasks_pb2 import Selector
 from flytekit.configuration import Image, ImageConfig, SerializationSettings
 from flytekit.core.context_manager import FlyteContextManager
 from flytekit.core.data_persistence import flyte_tmp_dir
-from flytekit.core.selectors import GPUDeviceA100, GPUPartition2G10GB
+from flytekit.core.selectors import GPUDeviceA100, GPUPartition2G10GB, GPUUnpartitioned
 from flytekit.core.task import task
 from flytekit.tools.translator import get_serializable
 
@@ -18,17 +18,52 @@ serialization_settings = SerializationSettings(
 )
 
 
-def test_selectors():
-    @task(selectors=[GPUDeviceA100, GPUPartition2G10GB])
-    def needs_a100(a: int):
-        ctx = FlyteContextManager.current_context()
-        wf_params = ctx.user_space_params
-        assert str(wf_params.execution_id) == "ex:local:local:local"
-        assert flyte_tmp_dir in wf_params.raw_output_prefix
+class TestSelectors:
+    def test_gpu_device_selector(self):
+        @task(selectors=[GPUDeviceA100])
+        def needs_a100(a: int):
+            ctx = FlyteContextManager.current_context()
+            wf_params = ctx.user_space_params
+            assert str(wf_params.execution_id) == "ex:local:local:local"
+            assert flyte_tmp_dir in wf_params.raw_output_prefix
 
-    ts = get_serializable(OrderedDict(), serialization_settings, needs_a100)
-    assert len(ts.template.metadata.selectors) == 2
-    s = ts.template.metadata.selectors[0]
-    assert isinstance(s, Selector)
-    assert s.HasField("gpu_device")
-    assert s.gpu_device == "nvidia-tesla-a100"
+        ts = get_serializable(OrderedDict(), serialization_settings, needs_a100)
+        assert len(ts.template.metadata.selectors) == 1
+        s = ts.template.metadata.selectors[0]
+        assert isinstance(s, Selector)
+        assert s.HasField("gpu_device")
+        assert s.gpu_device == "nvidia-tesla-a100"
+
+    def test_gpu_unpartitioned(self):
+        @task(selectors=[GPUDeviceA100, GPUUnpartitioned])
+        def needs_unpartitioned_a100(a: int):
+            ctx = FlyteContextManager.current_context()
+            wf_params = ctx.user_space_params
+            assert str(wf_params.execution_id) == "ex:local:local:local"
+            assert flyte_tmp_dir in wf_params.raw_output_prefix
+
+        ts = get_serializable(OrderedDict(), serialization_settings, needs_unpartitioned_a100)
+        assert len(ts.template.metadata.selectors) == 2
+        s1 = ts.template.metadata.selectors[0]
+        assert s1.HasField("gpu_device")
+        assert s1.gpu_device == "nvidia-tesla-a100"
+        s2 = ts.template.metadata.selectors[1]
+        assert s2.HasField("gpu_unpartitioned")
+        assert s2.gpu_unpartitioned
+
+    def test_gpu_partitioned(self):
+        @task(selectors=[GPUDeviceA100, GPUPartition2G10GB])
+        def needs_partitioned_a100(a: int):
+            ctx = FlyteContextManager.current_context()
+            wf_params = ctx.user_space_params
+            assert str(wf_params.execution_id) == "ex:local:local:local"
+            assert flyte_tmp_dir in wf_params.raw_output_prefix
+
+        ts = get_serializable(OrderedDict(), serialization_settings, needs_partitioned_a100)
+        assert len(ts.template.metadata.selectors) == 2
+        s1 = ts.template.metadata.selectors[0]
+        assert s1.HasField("gpu_device")
+        assert s1.gpu_device == "nvidia-tesla-a100"
+        s2 = ts.template.metadata.selectors[1]
+        assert s2.HasField("gpu_partition_size")
+        assert s2.gpu_partition_size == "2g.10gb"
