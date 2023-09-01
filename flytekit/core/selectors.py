caac8fde97652e6b932b384acfc360c030a70da2
Jeev B <jeevb@users.noreply.github.com>
2023-09-01 11:28:55 -0700

    switch to abc and add support for gpu unpartitioned

--- flytekit/core/selectors.py
+++ flytekit/core/selectors.py
@@ -1,25 +1,27 @@
+import abc
 from dataclasses import dataclass
+from typing import Optional
 
 from flyteidl.core import tasks_pb2
 
 
 @dataclass
-class BaseSelector(object):
-    value: str
-    only_preferred: bool = False
+class BaseSelector(abc.ABC):
+    value: Optional[str]
 
+    @abc.abstractmethod
     def to_flyte_idl(self) -> tasks_pb2.Selector:
-        raise NotImplementedError("Please use one of the specific selector classes")
+        pass
 
 
 class GPUDeviceSelector(BaseSelector):
     def to_flyte_idl(self) -> tasks_pb2.Selector:
-        return tasks_pb2.Selector(gpu_device=self.value, only_preferred=self.only_preferred)
+        return tasks_pb2.Selector(gpu_device=self.value)
 
 
 class GPUPartitionSizeSelector(BaseSelector):
     def to_flyte_idl(self) -> tasks_pb2.Selector:
-        return tasks_pb2.Selector(gpu_partition_size=self.value, only_preferred=self.only_preferred)
+        return tasks_pb2.Selector(gpu_partition_size=self.value)
 
 
 GPUDeviceA100 = GPUDeviceSelector(value="nvidia-tesla-a100")
@@ -29,3 +31,4 @@ GPUPartition2G10GB = GPUPartitionSizeSelector(value="2g.10gb")
 GPUPartition3G20GB = GPUPartitionSizeSelector(value="3g.20gb")
 GPUPartition4G20GB = GPUPartitionSizeSelector(value="4g.20gb")
 GPUPartition7G40GB = GPUPartitionSizeSelector(value="7g.40gb")
+GPUUnpartitioned = GPUPartitionSizeSelector(value=None)
