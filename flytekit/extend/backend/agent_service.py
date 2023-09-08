edb951a24967e7484b1b76e15aaeb73bb3435d0c
Kevin Su <pingsutw@apache.org>
2023-09-08 12:30:21 -0700

    update metric
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/extend/backend/agent_service.py
+++ flytekit/extend/backend/agent_service.py
@@ -19,27 +19,18 @@ from flytekit.extend.backend.base_agent import AgentRegistry
 from flytekit.models.literals import LiteralMap
 from flytekit.models.task import TaskTemplate
 
-request_count = Counter("request_count", "Total number of requests", ["task_type"])
-request_success_count = Counter("request_success_count", "Total number of successful requests", ["task_type"])
-request_failure_count = Counter("request_failure_count", "Total number of failed requests", ["task_type"])
+request_count = Counter("flyte_agent_req_count", "Total number of requests", ["task_type"])
+request_success_count = Counter("flyte_agent_req_success_count", "Total number of successful requests", ["task_type"])
+request_failure_count = Counter("flyte_agent_req_failure_count", "Total number of failed requests", ["task_type"])
 
-create_req_process_time = Summary(
-    "create_request_processing_seconds", "Time spent processing agent create request", ["task_type"]
-)
-get_req_process_time = Summary(
-    "get_request_processing_seconds", "Time spent processing agent get request", ["task_type"]
-)
-delete_req_process_time = Summary(
-    "delete_request_processing_seconds", "Time spent processing agent delete request", ["task_type"]
-)
-
-input_literal_size = Summary("input_literal_size", "Size of input literal", ["task_type"])
+request_latency = Summary("flyte_agent_req_latency", "Time spent processing agent request", ["task_type", "operation"])
+input_literal_size = Summary("flyte_agent_input_literal_size", "Size of input literal", ["task_type"])
 
 
 class AsyncAgentService(AsyncAgentServiceServicer):
     async def CreateTask(self, request: CreateTaskRequest, context: grpc.ServicerContext) -> CreateTaskResponse:
         try:
-            with create_req_process_time.labels(task_type=request.template.type).time():
+            with request_latency.labels(task_type=request.template.type, operation="create").time():
                 tmp = TaskTemplate.from_flyte_idl(request.template)
                 inputs = LiteralMap.from_flyte_idl(request.inputs) if request.inputs else None
 
@@ -81,7 +72,7 @@ class AsyncAgentService(AsyncAgentServiceServicer):
 
     async def GetTask(self, request: GetTaskRequest, context: grpc.ServicerContext) -> GetTaskResponse:
         try:
-            with get_req_process_time.labels(task_type=request.task_type).time():
+            with request_latency.labels(task_type=request.task_type, operation="get").time():
                 request_count.labels(task_type=request.task_type).inc()
                 agent = AgentRegistry.get_agent(context, request.task_type)
                 logger.info(f"{agent.task_type} agent start checking the status of the job")
@@ -109,7 +100,7 @@ class AsyncAgentService(AsyncAgentServiceServicer):
 
     async def DeleteTask(self, request: DeleteTaskRequest, context: grpc.ServicerContext) -> DeleteTaskResponse:
         try:
-            with delete_req_process_time.labels(task_type=request.task_type).time():
+            with request_latency.labels(task_type=request.task_type, operation="delete").time():
                 request_count.labels(task_type=request.task_type).inc()
                 agent = AgentRegistry.get_agent(context, request.task_type)
                 logger.info(f"{agent.task_type} agent start deleting the job")
