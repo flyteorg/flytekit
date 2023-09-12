78aae5db6da35dc80f5257db8964a5110c3d0016
Jeev B <jeevb@users.noreply.github.com>
2023-09-12 08:38:53 -0700

    add test for unspecified mig

--- tests/flytekit/unit/core/test_accelerators.py
+++ tests/flytekit/unit/core/test_accelerators.py
@@ -27,7 +27,19 @@ class TestAccelerators:
         assert not gpu_accelerator.HasField("unpartitioned")
         assert not gpu_accelerator.HasField("partition_size")
 
-    def test_gpu_accelerator_unpartitioned(self):
+    def test_mig(self):
+        @task(accelerator=NvidiaTeslaA100)
+        def needs_a100(a: int):
+            pass
+
+        ts = get_serializable(OrderedDict(), serialization_settings, needs_a100).to_flyte_idl()
+        gpu_accelerator = ts.template.metadata.resource_metadata.gpu_accelerator
+        assert gpu_accelerator is not None
+        assert gpu_accelerator.device == "nvidia-tesla-a100"
+        assert not gpu_accelerator.HasField("unpartitioned")
+        assert not gpu_accelerator.HasField("partition_size")
+
+    def test_mig_unpartitioned(self):
         @task(accelerator=NvidiaTeslaA100.with_partition_size(None))
         def needs_unpartitioned_a100(a: int):
             pass
@@ -39,7 +51,7 @@ class TestAccelerators:
         assert gpu_accelerator.unpartitioned
         assert not gpu_accelerator.HasField("partition_size")
 
-    def test_gpu_accelerator_partitioned(self):
+    def test_mig_partitioned(self):
         @task(accelerator=NvidiaTeslaA100.with_partition_size(NvidiaTeslaA100.partition_sizes.PARTITION_1G_5GB))
         def needs_partitioned_a100(a: int):
             pass
