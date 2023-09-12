2f4833f1b70d26aee2e29673766dbeb5aae75fb4
Kevin Su <pingsutw@apache.org>
2023-09-12 13:03:25 -0700

    nit
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/clis/sdk_in_container/serve.py
+++ flytekit/clis/sdk_in_container/serve.py
@@ -3,7 +3,6 @@ from concurrent import futures
 import click
 from flyteidl.service.agent_pb2_grpc import add_AsyncAgentServiceServicer_to_server
 from grpc import aio
-from prometheus_client import start_http_server
 
 from flytekit.extend.backend.agent_service import AsyncAgentService
 
@@ -45,7 +44,12 @@ def serve(_: click.Context, port, worker, timeout):
 
 async def _start_grpc_server(port: int, worker: int, timeout: int):
     click.secho("Starting up the server to expose the prometheus metrics...", fg="blue")
-    start_http_server(9090)
+    try:
+        from prometheus_client import start_http_server
+
+        start_http_server(9090)
+    except ImportError as e:
+        click.secho(f"Failed to start the prometheus server with error {e}", fg="red")
     click.secho("Starting the agent service...", fg="blue")
     server = aio.server(futures.ThreadPoolExecutor(max_workers=worker))
     add_AsyncAgentServiceServicer_to_server(AsyncAgentService(), server)
