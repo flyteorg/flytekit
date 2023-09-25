7ffca4093f5fcb2a339915b3f86a1c762e37dcdc
Thomas Newton <thomas.w.newton@gmail.com>
2023-09-25 08:51:18 +0100

    Autoformat
    Signed-off-by: Thomas Newton <thomas.w.newton@gmail.com>

--- tests/flytekit/unit/core/test_data_persistence.py
+++ tests/flytekit/unit/core/test_data_persistence.py
@@ -2,6 +2,7 @@ import os
 
 from azure.identity import ClientSecretCredential, DefaultAzureCredential
 from mock import patch
+
 from flytekit.core.data_persistence import FileAccessProvider
 
 
@@ -21,22 +22,33 @@ def test_is_remote():
 
 
 def test_initialise_azure_file_provider_with_account_key():
-    with patch.dict(os.environ, {"FLYTE_AZURE_ACCOUNT_NAME": "accountname", "FLYTE_AZURE_ACCOUNT_KEY": "accountkey"}, clear=True):
+    with patch.dict(
+        os.environ, {"FLYTE_AZURE_ACCOUNT_NAME": "accountname", "FLYTE_AZURE_ACCOUNT_KEY": "accountkey"}, clear=True
+    ):
         fp = FileAccessProvider("/tmp", "abfs://container/path/within/container")
         assert fp.get_filesystem().account_name == "accountname"
         assert fp.get_filesystem().account_key == "accountkey"
         assert fp.get_filesystem().sync_credential is None
 
+
 def test_initialise_azure_file_provider_with_service_principal():
-    with patch.dict(os.environ, {"FLYTE_AZURE_ACCOUNT_NAME": "accountname", "FLYTE_AZURE_CLIENT_SECRET": "clientsecret", "FLYTE_AZURE_CLIENT_ID": "clientid", 
-                                 "FLYTE_AZURE_TENANT_ID": "tenantid"
-                                 }, clear=True):
+    with patch.dict(
+        os.environ,
+        {
+            "FLYTE_AZURE_ACCOUNT_NAME": "accountname",
+            "FLYTE_AZURE_CLIENT_SECRET": "clientsecret",
+            "FLYTE_AZURE_CLIENT_ID": "clientid",
+            "FLYTE_AZURE_TENANT_ID": "tenantid",
+        },
+        clear=True,
+    ):
         fp = FileAccessProvider("/tmp", "abfs://container/path/within/container")
         assert fp.get_filesystem().account_name == "accountname"
         assert isinstance(fp.get_filesystem().sync_credential, ClientSecretCredential)
         assert fp.get_filesystem().client_secret == "clientsecret"
         assert fp.get_filesystem().client_id == "clientid"
         assert fp.get_filesystem().tenant_id == "tenantid"
+
 
 def test_initialise_azure_file_provider_with_default_credential():
     with patch.dict(os.environ, {"FLYTE_AZURE_ACCOUNT_NAME": "accountname"}, clear=True):
