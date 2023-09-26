dfe6b3e9b28be7af5ab66dbdc632af4e4cd0e809
Ketan Umare <ketan.umare@gmail.com>
2023-09-25 22:13:46 -0700

    Improved gate node handling
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

--- flytekit/interaction/parse_stdin.py
+++ flytekit/interaction/parse_stdin.py
@@ -6,31 +6,21 @@ import click
 
 from flytekit.core.context_manager import FlyteContext
 from flytekit.core.type_engine import TypeEngine
-from flytekit.loggers import logger
 from flytekit.models.literals import Literal
 
 
 # TODO: Move the improved click parsing here. https://github.com/flyteorg/flyte/issues/3124
-def parse_stdin_to_literal(ctx: FlyteContext, t: typing.Type, message_prefix: typing.Optional[str]) -> Literal:
+def parse_stdin_to_literal(ctx: FlyteContext, t: typing.Type, message: typing.Optional[str]) -> Literal:
+    from flytekit.interaction.click_types import FlyteLiteralConverter
 
-    message = message_prefix or ""
-    message += f"Please enter value for type {t} to continue"
-    if issubclass(t, bool):
-        user_input = click.prompt(message, type=bool)
-        l = TypeEngine.to_literal(ctx, user_input, bool, TypeEngine.to_literal_type(bool))  # noqa
-    elif issubclass(t, int):
-        user_input = click.prompt(message, type=int)
-        l = TypeEngine.to_literal(ctx, user_input, int, TypeEngine.to_literal_type(int))  # noqa
-    elif issubclass(t, float):
-        user_input = click.prompt(message, type=float)
-        l = TypeEngine.to_literal(ctx, user_input, float, TypeEngine.to_literal_type(float))  # noqa
-    elif issubclass(t, str):
-        user_input = click.prompt(message, type=str)
-        l = TypeEngine.to_literal(ctx, user_input, str, TypeEngine.to_literal_type(str))  # noqa
-    else:
-        # Todo: We should implement the rest by way of importing the code in pyflyte run
-        #   that parses text from the command line
-        raise Exception("Only bool, int/float, or strings are accepted for now.")
-
-    logger.debug(f"Parsed literal {l} from user input {user_input}")
-    return l
+    literal_type = TypeEngine.to_literal_type(t)
+    literal_converter = FlyteLiteralConverter(
+        ctx,
+        literal_type=literal_type,
+        python_type=t,
+        get_upload_url_fn=lambda: None,
+        is_remote=False,
+        remote_instance_accessor=None,
+    )
+    user_input = click.prompt(message, type=literal_converter.click_type)
+    return literal_converter.convert_to_literal(click.Context(click.Command(None)), None, user_input)
