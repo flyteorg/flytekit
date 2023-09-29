f2ceffaf56065db95b81f91a5b693c3cb203747a
HH <hhcs9527@gmail.com>
2023-09-29 22:51:53 +0800

    change uri and add private_key.pem to let kubectl readable
    Signed-off-by: HH <hhcs9527@gmail.com>

--- plugins/flytekit-snowflake/flytekitplugins/snowflake/agent.py
+++ plugins/flytekit-snowflake/flytekitplugins/snowflake/agent.py
@@ -46,7 +46,7 @@ class SnowflakeAgent(AgentBase):
 
         import flytekit
 
-        pk_string = flytekit.current_context().secrets.get(TASK_TYPE, "private_key", encode_mode="rb")
+        pk_string = flytekit.current_context().secrets.get(TASK_TYPE, "private_key.pem", encode_mode="rb")
         p_key = serialization.load_pem_private_key(pk_string, password=None, backend=default_backend())
 
         pkb = p_key.private_bytes(
@@ -125,7 +125,7 @@ class SnowflakeAgent(AgentBase):
 
         if cur_state == SUCCEEDED:
             ctx = FlyteContextManager.current_context()
-            output_metadata = f"snowflake://{metadata.user}:{metadata.account}/{metadata.database}/{metadata.schema}/{metadata.warehouse}/{metadata.table}"
+            output_metadata = f"snowflake://{metadata.user}:{metadata.account}/{metadata.warehouse}/{metadata.database}/{metadata.schema}/{metadata.table}"
             res = literals.LiteralMap(
                 {
                     "results": TypeEngine.to_literal(
