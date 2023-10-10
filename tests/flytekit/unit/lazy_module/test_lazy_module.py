c2921d07ab6563c5a81cf1f5e3d8167941e2bc31
Kevin Su <pingsutw@apache.org>
2023-10-10 13:53:30 -0700

    lint
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- tests/flytekit/unit/lazy_module/test_lazy_module.py
+++ tests/flytekit/unit/lazy_module/test_lazy_module.py
@@ -9,4 +9,4 @@ def test_lazy_module():
     mod = lazy_module("fake_module")
     assert isinstance(mod, DummyModule)
     with pytest.raises(ImportError, match="Module fake_module is not yet installed."):
-        a = mod.attr
+        print(mod.attr)
