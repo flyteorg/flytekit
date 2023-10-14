b8adb8a1afe390df71e48080062debccbc6e2800
Kevin Su <pingsutw@apache.org>
2023-10-14 02:16:30 -0700

    Add a test for test_sensor_engine
    Signed-off-by: Kevin Su <pingsutw@apache.org>

---
+++ tests/flytekit/unit/sensor/test_sensor_engine.py
@@ -0,0 +1,54 @@
+import tempfile
+from unittest.mock import MagicMock
+
+import grpc
+import pytest
+import cloudpickle
+from flyteidl.admin.agent_pb2 import (
+    SUCCEEDED,
+    DeleteTaskResponse,
+)
+
+import flytekit.models.interface as interface_models
+from flytekit.extend.backend.base_agent import (
+    AgentRegistry,
+)
+from flytekit.models import literals, types
+from flytekit.sensor.base_sensor import SENSOR_MODULE, SENSOR_NAME
+from tests.flytekit.unit.extend.test_agent import get_task_template
+
+
+@pytest.mark.asyncio
+async def test_sensor_engine():
+    interfaces = interface_models.TypedInterface(
+        {
+            "path": interface_models.Variable(types.LiteralType(types.SimpleType.STRING), "description1"),
+        },
+        {},
+    )
+    tmp = get_task_template("sensor")
+    tmp._custom = {
+        SENSOR_MODULE: "flytekit.sensor.file_sensor",
+        SENSOR_NAME: "FileSensor",
+    }
+    file = tempfile.NamedTemporaryFile()
+
+    tmp._interface = interfaces
+
+    task_inputs = literals.LiteralMap(
+        {
+            "path": literals.Literal(scalar=literals.Scalar(primitive=literals.Primitive(string_value=file.name))),
+        },
+    )
+    ctx = MagicMock(spec=grpc.ServicerContext)
+    agent = AgentRegistry.get_agent("sensor")
+
+    res = await agent.async_create(ctx, "/tmp", tmp, task_inputs)
+
+    metadata_bytes = cloudpickle.dumps(tmp.custom)
+    assert res.resource_meta == metadata_bytes
+    res = await agent.async_get(ctx, metadata_bytes)
+    print(res)
+    assert res.resource.state == SUCCEEDED
+    res = await agent.async_delete(ctx, metadata_bytes)
+    assert res == DeleteTaskResponse()
