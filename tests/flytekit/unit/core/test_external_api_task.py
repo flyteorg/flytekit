713db18d0dec693d680b2f5b3bd6b8bd6f5daa54
Future Outlier <eric901201@gmai.com>
2023-10-16 16:41:47 +0800

    add for flyteidl version remove for building image
    Signed-off-by: Future Outlier <eric901201@gmai.com>

--- tests/flytekit/unit/core/test_external_api_task.py
+++ tests/flytekit/unit/core/test_external_api_task.py
@@ -1,31 +1,30 @@
 import json
 import collections
-from flytekit.core.external_api_task import ExternalApiTask, TASK_MODULE, TASK_NAME, TASK_CONFIG_PKL  # replace "your_module" with the actual module name where ExternalApiTask is defined
+from flytekit.core.external_api_task import ExternalApiTask, TASK_MODULE, TASK_NAME, TASK_CONFIG_PKL
 
 
-# Mocking ExternalApiTask to make it instantiable
 class MockExternalApiTask(ExternalApiTask):
 
-    async def do(self, **kwargs):
-        pass
+    async def do(self, test_int_input : int, **kwargs) -> int:
+        return test_int_input
 
-
-# Test for the __init__ method
 def test_init():
-    task = MockExternalApiTask(name="test_task", return_type=str)
+    task = MockExternalApiTask(name="test_task", return_type=int)
     assert task.name == "test_task"
-    assert task.interface.inputs == collections.OrderedDict()
-    assert task.interface.outputs == collections.OrderedDict({"o0": str})
+    assert task.interface.inputs == collections.OrderedDict({"test_int_input": int})
+    assert task.interface.outputs == collections.OrderedDict({"o0": int})
 
-# Test for the get_custom method
+# use asyncio
+def test_do():
+    task = MockExternalApiTask(name="test_task", return_type=str)
+    assert task.interface.inputs == collections.OrderedDict({"test_int_input": int})
+
 def test_get_custom():
     task = MockExternalApiTask(name="test_task", config={"key": "value"})
     custom = task.get_custom()
 
-    expected_config = json.loads('{"key": "value"}')  # replace with the expected serialized config
+    expected_config = json.loads('{"key": "value"}')
     assert custom[TASK_MODULE] == MockExternalApiTask.__module__
     assert custom[TASK_NAME] == MockExternalApiTask.__name__
-    assert json.loads(custom[TASK_CONFIG_PKL]) == expected_config  # you might need to adjust this depending on how you expect the config to be serialized
+    assert json.loads(custom[TASK_CONFIG_PKL]) == expected_config
 
-
-# Run this with `pytest test_external_api_task.py`
