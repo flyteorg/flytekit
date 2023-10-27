75b71488d27bfec7b359c2fc50a82e825f383c19
Kevin Su <pingsutw@apache.org>
2023-10-27 03:19:06 -0700

    init
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- plugins/flytekit-airflow/flytekitplugins/airflow/agent.py
+++ plugins/flytekit-airflow/flytekitplugins/airflow/agent.py
@@ -1,15 +1,12 @@
 import importlib
-from dataclasses import dataclass
+import typing
+from dataclasses import dataclass, field
 from typing import Optional
 
 import cloudpickle
 import grpc
 import jsonpickle
-from airflow.providers.google.cloud.operators.dataproc import (
-    DataprocDeleteClusterOperator,
-    DataprocJobBaseOperator,
-    JobStatus,
-)
+from airflow.models import BaseOperator
 from airflow.sensors.base import BaseSensorOperator
 from airflow.utils.context import Context
 from flyteidl.admin.agent_pb2 import (
@@ -19,8 +16,10 @@ from flyteidl.admin.agent_pb2 import (
     CreateTaskResponse,
     DeleteTaskResponse,
     GetTaskResponse,
-    Resource,
+    Resource, State,
 )
+
+from flytekit.exceptions.user import FlyteUserException
 from flytekitplugins.airflow.task import AirflowConfig
 from google.cloud.exceptions import NotFound
 
@@ -32,11 +31,17 @@ from flytekit.models.task import TaskTemplate
 
 @dataclass
 class ResourceMetadata:
-    job_id: str
     airflow_config: AirflowConfig
+    job_id: typing.Optional[str] = field(default=None)
+    operation: typing.Optional[Future] = field(default=None)
+
+    def with_job_id(self, job_id: str):
+        return ResourceMetadata(airflow_config=self.airflow_config if self.airflow_config else None,
+                                job_id=job_id)
 
 
-def _get_airflow_task(ctx: FlyteContext, airflow_config: AirflowConfig):
+def _get_airflow_task(airflow_config: AirflowConfig):
+    ctx = FlyteContextManager.current_context()
     task_module = importlib.import_module(name=airflow_config.task_module)
     task_def = getattr(task_module, airflow_config.task_name)
     task_config = airflow_config.task_config
@@ -44,9 +49,53 @@ def _get_airflow_task(ctx: FlyteContext, airflow_config: AirflowConfig):
     # Set the GET_ORIGINAL_TASK attribute to True so that task_def will return the original
     # airflow task instead of the Flyte task.
     ctx.user_space_params.builder().add_attr("GET_ORIGINAL_TASK", True).build()
-    if issubclass(task_def, DataprocJobBaseOperator):
-        return task_def(**task_config, asynchronous=True)
     return task_def(**task_config)
+
+
+def _execute_airflow_operator(airflow_operator: BaseOperator, airflow_config: AirflowConfig):
+    """
+    It's
+    """
+    try:
+        from airflow.providers.google.cloud.operators.dataproc import DataprocJobBaseOperator, DataprocDeleteClusterOperator, DataprocHook
+        ctx = FlyteContextManager.current_context()
+        if isinstance(airflow_operator, DataprocJobBaseOperator):
+            airflow_operator.asynchronous = True
+            airflow_operator.execute(context=Context())
+            job_id = ctx.user_space_params.xcom_data["value"]["resource"]
+            return ResourceMetadata(job_id=job_id, airflow_config=airflow_config)
+        elif isinstance(airflow_operator, DataprocDeleteClusterOperator):
+            hook = DataprocHook(gcp_conn_id=airflow_operator.gcp_conn_id, impersonation_chain=airflow_operator.impersonation_chain)
+            operation = hook.delete_cluster()
+            operation.done()
+            return ResourceMetadata(airflow_config=airflow_config)
+    except ImportError:
+        logger.debug("apache-airflow[google] is not installed")
+
+    return ResourceMetadata(airflow_config=airflow_config)
+
+
+def _get_status_from_operator(airflow_operator: BaseOperator, meta: ResourceMetadata) -> State:
+    job_id = meta.job_id
+    airflow_config = meta.airflow_config
+    try:
+        from airflow.providers.google.cloud.operators.dataproc import DataprocJobBaseOperator, JobStatus, DataprocDeleteClusterOperator
+        if isinstance(airflow_operator, DataprocJobBaseOperator):
+            job = airflow_operator.hook.get_job(
+                job_id=job_id,
+                region=airflow_config.task_config["region"],
+                project_id=airflow_config.task_config["project_id"],
+            )
+            if job.status.state == JobStatus.State.DONE:
+                return SUCCEEDED
+            elif job.status.state in (JobStatus.State.ERROR, JobStatus.State.CANCELLED):
+                return PERMANENT_FAILURE
+        elif isinstance(airflow_operator, DataprocDeleteClusterOperator):
+            try:
+                airflow_operator.execute(context=Context())
+            except NotFound:
+                logger.info("Dataproc cluster already deleted.")
+            return SUCCEEDED
 
 
 class AirflowAgent(AgentBase):
@@ -61,45 +110,30 @@ class AirflowAgent(AgentBase):
         inputs: Optional[LiteralMap] = None,
     ) -> CreateTaskResponse:
         airflow_config = jsonpickle.decode(task_template.custom.get("task_config_pkl"))
-        resource_meta = ResourceMetadata(job_id="", airflow_config=airflow_config)
 
-        ctx = FlyteContextManager.current_context()
-        airflow_task = _get_airflow_task(ctx, airflow_config)
-        if isinstance(airflow_task, DataprocJobBaseOperator):
-            airflow_task.execute(context=Context())
-            resource_meta.job_id = ctx.user_space_params.xcom_data["value"]["resource"]
+        airflow_task = _get_airflow_task(airflow_config)
+        if isinstance(airflow_task, BaseSensorOperator):
+            resource_meta = ResourceMetadata(airflow_config=airflow_config)
+        elif isinstance(airflow_task, BaseOperator):
+            resource_meta = _execute_airflow_operator(airflow_task, airflow_config)
+        else:
+            raise FlyteUserException("Airflow task must be a BaseOperator or BaseSensorOperator")
 
         return CreateTaskResponse(resource_meta=cloudpickle.dumps(resource_meta))
 
     def get(self, context: grpc.ServicerContext, resource_meta: bytes) -> GetTaskResponse:
         meta = cloudpickle.loads(resource_meta)
-        airflow_config = meta.airflow_config
-        job_id = meta.job_id
-        task = _get_airflow_task(FlyteContextManager.current_context(), meta.airflow_config)
+        airflow_task = _get_airflow_task(meta.airflow_config)
         cur_state = RUNNING
 
-        if issubclass(type(task), BaseSensorOperator):
-            if task.poke(context=Context()):
-                cur_state = SUCCEEDED
-        elif issubclass(type(task), DataprocJobBaseOperator):
-            job = task.hook.get_job(
-                job_id=job_id,
-                region=airflow_config.task_config["region"],
-                project_id=airflow_config.task_config["project_id"],
-            )
-            if job.status.state == JobStatus.State.DONE:
-                cur_state = SUCCEEDED
-            elif job.status.state in (JobStatus.State.ERROR, JobStatus.State.CANCELLED):
-                cur_state = PERMANENT_FAILURE
-        elif isinstance(task, DataprocDeleteClusterOperator):
-            try:
-                task.execute(context=Context())
-            except NotFound:
-                logger.info("Cluster already deleted.")
-            cur_state = SUCCEEDED
+        if isinstance(airflow_task, BaseSensorOperator):
+            ok = airflow_task.poke(context=Context())
+            cur_state = SUCCEEDED if ok else RUNNING
+        elif isinstance(airflow_task, BaseSensorOperator):
+            meta = cloudpickle.loads(resource_meta)
+            _get_status_from_operator()
         else:
-            task.execute(context=Context())
-            cur_state = SUCCEEDED
+            ...
         return GetTaskResponse(resource=Resource(state=cur_state, outputs=None))
 
     def delete(self, context: grpc.ServicerContext, resource_meta: bytes) -> DeleteTaskResponse:
