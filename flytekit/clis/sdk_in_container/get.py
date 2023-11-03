614f8cd38d2bf1071264430ea2b5fca72836419c
Ketan Umare <ketan.umare@gmail.com>
2023-11-02 22:39:49 -0700

    updated single lp
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

--- flytekit/clis/sdk_in_container/get.py
+++ flytekit/clis/sdk_in_container/get.py
@@ -1,11 +1,15 @@
 import rich_click as click
+from google.protobuf.json_format import MessageToJson
+from rich import print
 from rich.console import Console
 from rich.table import Table
 
 from flytekit.clis.sdk_in_container.helpers import get_and_save_remote_with_click_context
-from flytekit.clis.sdk_in_container.utils import project_option_dec, domain_option_dec
+from flytekit.clis.sdk_in_container.utils import domain_option_dec, project_option_dec
+from flytekit.interfaces.cli_identifiers import Identifier
 from flytekit.models.admin.common import Sort
 from flytekit.models.common import NamedEntityIdentifier
+from flytekit.models.core.identifier import ResourceType
 from flytekit.models.launch_plan import LaunchPlanState
 from flytekit.remote import FlyteRemote
 
@@ -27,12 +31,30 @@ def get(ctx: click.Context):
 @click.argument("launchplan-name", type=str, required=False, metavar="LAUNCHPLAN-NAME")
 @click.argument("version", type=str, required=False, metavar="LAUNCHPLAN-VERSION")
 @click.pass_context
-def launchplan(ctx: click.Context, project: str, domain: str, limit: int, active_only: bool, launchplan_name: str,
-               version: str):
+def launchplan(
+    ctx: click.Context, project: str, domain: str, limit: int, active_only: bool, launchplan_name: str, version: str
+):
     """
     Interact with launchplans.
     """
     remote: FlyteRemote = get_and_save_remote_with_click_context(ctx, project="flytesnacks", domain="development")
+
+    console = Console()
+    if launchplan_name:
+        if not version:
+            lps, _ = remote.client.list_launch_plans_paginated(
+                NamedEntityIdentifier(project, domain, name=launchplan_name),
+                limit=1,
+                sort_by=Sort(key="updated_at", direction=Sort.Direction.DESCENDING),
+            )
+            if len(lps) > 0:
+                version = lps[0].id.version
+        lp = remote.client.get_launch_plan(
+            Identifier(ResourceType.LAUNCH_PLAN, project, domain, launchplan_name, version)
+        )
+        j = MessageToJson(lp.to_flyte_idl())
+        print(j)
+        return
 
     title = f"LaunchPlans for {project}/{domain}"
     if active_only:
@@ -40,8 +62,10 @@ def launchplan(ctx: click.Context, project: str, domain: str, limit: int, active
         lps, _ = remote.client.list_active_launch_plans_paginated(project, domain, limit=limit)
     else:
         lps, _ = remote.client.list_launch_plans_paginated(
-            NamedEntityIdentifier(project, domain), limit=limit,
-            sort_by=Sort(key="updated_at", direction=Sort.Direction.DESCENDING))
+            NamedEntityIdentifier(project, domain),
+            limit=limit,
+            sort_by=Sort(key="updated_at", direction=Sort.Direction.DESCENDING),
+        )
 
     table = Table(title=title)
 
@@ -60,5 +84,4 @@ def launchplan(ctx: click.Context, project: str, domain: str, limit: int, active
                 schedule = f"{lp.spec.entity_metadata.schedule.rate.value} {lp.spec.entity_metadata.schedule.rate.unit}"
         table.add_row(lp.id.name, lp.id.version, s, schedule)
 
-    console = Console()
     console.print(table)
