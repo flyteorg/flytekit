ab57b9263d09474b5103afba4cfa37587216e431
troychiu <y.troychiu@gmail.com>
2023-11-05 14:40:12 -0500

    rename
    Signed-off-by: troychiu <y.troychiu@gmail.com>

--- plugins/flytekit-data-asyncS3FS/flytekitplugins/asyncS3FS/__init__.py
+++ plugins/flytekit-data-asyncS3FS/flytekitplugins/asyncS3FS/__init__.py
@@ -7,7 +7,7 @@ This package contains things that are useful when extending Flytekit.
    :template: custom.rst
    :toctree: generated/
 
-   
+   AsyncS3FileSystem
 """
 
 from .asyncS3FS import AsyncS3FileSystem
