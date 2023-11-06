55632d73afa40f982dadd991d0a71325d8224c68
Ketan Umare <16888709+kumare3@users.noreply.github.com>
2023-11-06 09:18:34 -0800

    Auto download for fetch (#1918)
    * Auto download for fetch
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    * Adds new fetch with download command
    
    ` pyflyte fetch --download-to /tmp/. flyte://v1/flytesnacks/development/f5dc81c5a8c6441d4a0a/rotaterotateimage/o/o0`
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    * Auto download and improved some bugs
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    * Updated fetch and hitl
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>
    
    ---------
    
    Signed-off-by: Ketan Umare <ketan.umare@gmail.com>

--- flytekit/core/data_persistence.py
+++ flytekit/core/data_persistence.py
@@ -247,6 +247,7 @@ class FileAccessProvider(object):
                 return shutil.copytree(
                     self.strip_file_header(from_path), self.strip_file_header(to_path), dirs_exist_ok=True
                 )
+            print(f"Getting {from_path} to {to_path}")
             return file_system.get(from_path, to_path, recursive=recursive, **kwargs)
         except OSError as oe:
             logger.debug(f"Error in getting {from_path} to {to_path} rec {recursive} {oe}")
@@ -427,31 +428,31 @@ class FileAccessProvider(object):
             self.get_random_string(),
         )
 
-    def download_directory(self, remote_path: str, local_path: str):
+    def download_directory(self, remote_path: str, local_path: str, **kwargs):
         """
         Downloads directory from given remote to local path
         """
         return self.get_data(remote_path, local_path, is_multipart=True)
 
-    def download(self, remote_path: str, local_path: str):
+    def download(self, remote_path: str, local_path: str, **kwargs):
         """
         Downloads from remote to local
         """
-        return self.get_data(remote_path, local_path)
+        return self.get_data(remote_path, local_path, **kwargs)
 
-    def upload(self, file_path: str, to_path: str):
+    def upload(self, file_path: str, to_path: str, **kwargs):
         """
         :param Text file_path:
         :param Text to_path:
         """
-        return self.put_data(file_path, to_path)
+        return self.put_data(file_path, to_path, **kwargs)
 
-    def upload_directory(self, local_path: str, remote_path: str):
+    def upload_directory(self, local_path: str, remote_path: str, **kwargs):
         """
         :param Text local_path:
         :param Text remote_path:
         """
-        return self.put_data(local_path, remote_path, is_multipart=True)
+        return self.put_data(local_path, remote_path, is_multipart=True, **kwargs)
 
     def get_data(self, remote_path: str, local_path: str, is_multipart: bool = False, **kwargs):
         """
