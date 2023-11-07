1bffa88ba0519d08e93f1ba3519b39130c2e31a6
Mark Elliot <123787712+mark-thm@users.noreply.github.com>
2023-11-07 10:37:29 -0500

    fix lint
    Signed-off-by: Mark Elliot <123787712+mark-thm@users.noreply.github.com>

--- tests/flytekit/unit/types/structured_dataset/test_basic_dfs.py
+++ tests/flytekit/unit/types/structured_dataset/test_basic_dfs.py
@@ -1,7 +1,8 @@
 import pathlib
+
 from flytekit.types.structured.basic_dfs import _path_from_uri
 
 
-def test_path_from_uri():
+def test_path_from_uri() -> None:
     assert _path_from_uri("file:///test") == pathlib.Path("/test")
     assert _path_from_uri("/test") == pathlib.Path("/test")
