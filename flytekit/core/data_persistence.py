4516c029dbdfc7993f4cf3881050ad107dd04092
Kevin Su <pingsutw@apache.org>
2023-11-14 13:00:45 -0800

    Fix remote file system (get/put) (#1955)
    * test
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    
    * test
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    
    * test
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    
    * lint
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    
    * update-get
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    
    * add unit test
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    
    ---------
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    Signed-off-by: Rafael Raposo <rafaelraposo@spotify.com>

--- flytekit/core/data_persistence.py
+++ flytekit/core/data_persistence.py
@@ -248,7 +248,10 @@ class FileAccessProvider(object):
                     self.strip_file_header(from_path), self.strip_file_header(to_path), dirs_exist_ok=True
                 )
             print(f"Getting {from_path} to {to_path}")
-            return file_system.get(from_path, to_path, recursive=recursive, **kwargs)
+            dst = file_system.get(from_path, to_path, recursive=recursive, **kwargs)
+            if isinstance(dst, (str, pathlib.Path)):
+                return dst
+            return to_path
         except OSError as oe:
             logger.debug(f"Error in getting {from_path} to {to_path} rec {recursive} {oe}")
             file_system = self.get_filesystem(get_protocol(from_path), anonymous=True)
@@ -271,7 +274,11 @@ class FileAccessProvider(object):
                     self.strip_file_header(from_path), self.strip_file_header(to_path), dirs_exist_ok=True
                 )
             from_path, to_path = self.recursive_paths(from_path, to_path)
-        return file_system.put(from_path, to_path, recursive=recursive, **kwargs)
+        dst = file_system.put(from_path, to_path, recursive=recursive, **kwargs)
+        if isinstance(dst, (str, pathlib.Path)):
+            return dst
+        else:
+            return to_path
 
     def put_raw_data(
         self,
