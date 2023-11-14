2ce790571a9c3b98c8cf7c1434c2788f04be8d39
Kevin Su <pingsutw@apache.org>
2023-11-14 15:32:00 -0800

    nit
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- plugins/setup.py
+++ plugins/setup.py
@@ -11,7 +11,7 @@ __version__ = "0.0.0+develop"
 
 # Please maintain an alphabetical order in the following list
 SOURCES = {
-    "flytekitplugins-async_fsspec": "flytekit-async_fsspec",
+    "flytekitplugins-async-fsspec": "flytekit-async-fsspec",
     "flytekitplugins-athena": "flytekit-aws-athena",
     "flytekitplugins-awsbatch": "flytekit-aws-batch",
     "flytekitplugins-awssagemaker": "flytekit-aws-sagemaker",
