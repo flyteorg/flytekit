016f6a564910b4a7007077d7f388c7cecf12d449
zeryx <1892175+zeryx@users.noreply.github.com>
2023-11-20 13:56:05 -0500

    initial NeuronX commit, added basic inf2 and trn1 instances
    Signed-off-by: zeryx <1892175+zeryx@users.noreply.github.com>

--- flytekit/extras/accelerators.py
+++ flytekit/extras/accelerators.py
@@ -21,6 +21,18 @@ class GPUAccelerator(BaseAccelerator):
     def to_flyte_idl(self) -> tasks_pb2.GPUAccelerator:
         return tasks_pb2.GPUAccelerator(device=self._device)
 
+class NeuronxAccelerator(BaseAccelerator):
+    def __init__(self, neurondevice: str = None, neuron = None, neuroncore = None) -> None:
+        if neuron:
+            self._neuron = neuron
+        if neuroncore:
+            self._neuroncore = neuroncore
+        if neurondevice:
+            self._neurondevice = neurondevice
+    def to_flyte_idl(self) -> tasks_pb2.NeuronxAccelerator:
+        return tasks_pb2.NeuronxAccelerator(neurondevice=self._neurondevice, neuron=self._neuron, neuroncore=self._neuroncore)
+
+
 
 A10G = GPUAccelerator("nvidia-a10g")
 L4 = GPUAccelerator("nvidia-l4-vws")
@@ -30,6 +42,8 @@ P4 = GPUAccelerator("nvidia-tesla-p4")
 P100 = GPUAccelerator("nvidia-tesla-p100")
 T4 = GPUAccelerator("nvidia-tesla-t4")
 V100 = GPUAccelerator("nvidia-tesla-v100")
+Infv2 = NeuronxAccelerator("Inferentia-v2")
+Trn1 = NeuronxAccelerator("Trainium-v1")
 
 
 class MultiInstanceGPUAccelerator(BaseAccelerator):
