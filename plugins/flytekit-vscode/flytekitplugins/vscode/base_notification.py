f7bf415730e6f97db43bf64f3a4bf6873adfe7b7
Future Outlier <eric901201@gmai.com>
2023-11-20 14:07:41 +0800

    base notification template
    Signed-off-by: Future Outlier <eric901201@gmai.com>

---
+++ plugins/flytekit-vscode/flytekitplugins/vscode/base_notification.py
@@ -0,0 +1,15 @@
+"""
+
+routing mechanism
+1. route to the notification class
+ex: sendgrid, slack. pageduty, etc.
+
+methods:
+1. send_notification
+2. check_if_config_is_valid
+3. get notification secret from flytekit
+
+error handling:
+1. use try... except... to catch the error
+
+"""
