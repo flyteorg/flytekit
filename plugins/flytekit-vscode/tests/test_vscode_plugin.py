744c167aeae58a0fd48a678218487af42c546956
Jason Zhu <jasonchu13@outlook.com>
2023-11-20 22:52:14 -0800

    New Features in VSCode Plugin: Auto server termination after max idle time and default installation of python + jupyter notebook extensions (#1977)
    ---------
    
    Signed-off-by: JasonZhu1313 <jasonchu13@outlook.com>
    Signed-off-by: byhsu <byhsu@linkedin.com>
    Co-authored-by: byhsu <byhsu@linkedin.com>

--- plugins/flytekit-vscode/tests/test_vscode_plugin.py
+++ plugins/flytekit-vscode/tests/test_vscode_plugin.py
@@ -4,11 +4,10 @@ from flytekitplugins.vscode import vscode
 from flytekit import task, workflow
 
 
-@mock.patch("sys.exit")
-@mock.patch("time.sleep")
 @mock.patch("multiprocessing.Process")
+@mock.patch("flytekitplugins.vscode.decorator.exit_handler")
 @mock.patch("flytekitplugins.vscode.decorator.download_vscode")
-def test_vscode_plugin(mock_download_vscode, mock_process, mock_sleep, mock_exit):
+def test_vscode_plugin(mock_download_vscode, mock_exit_handler, mock_process):
     @task
     @vscode
     def t():
@@ -21,5 +20,4 @@ def test_vscode_plugin(mock_download_vscode, mock_process, mock_sleep, mock_exit
     wf()
     mock_download_vscode.assert_called_once()
     mock_process.assert_called_once()
-    mock_sleep.assert_called_once()
-    mock_exit.assert_called_once()
+    mock_exit_handler.assert_called_once()
