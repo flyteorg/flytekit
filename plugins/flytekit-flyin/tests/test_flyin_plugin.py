2faf165d9380efd3dc403ed0b40ccf48285f27d8
Future Outlier <eric901201@gmai.com>
2023-11-26 23:26:04 +0800

    add tests
    Signed-off-by: Future Outlier <eric901201@gmai.com>

--- plugins/flytekit-flyin/tests/test_flyin_plugin.py
+++ plugins/flytekit-flyin/tests/test_flyin_plugin.py
@@ -1,5 +1,5 @@
 import mock
-from flytekitplugins.flyin import vscode
+from flytekitplugins.flyin import vscode, VscodeConfig, COPILOT_EXTENSION, VIM_EXTENSION, CODE_TOGETHER_EXTENSION
 
 from flytekit import task, workflow
 
@@ -21,3 +21,14 @@ def test_vscode(mock_download_vscode, mock_exit_handler, mock_process):
     mock_download_vscode.assert_called_once()
     mock_process.assert_called_once()
     mock_exit_handler.assert_called_once()
+
+def test_vscode_config_add_extensions():
+    additional_extensions = [COPILOT_EXTENSION, VIM_EXTENSION, CODE_TOGETHER_EXTENSION]
+
+    config = VscodeConfig(
+        additional_extensions=additional_extensions,
+    )
+    config.add_extensions()
+
+    for extension in additional_extensions:
+        assert extension in config.extension_remote_paths
