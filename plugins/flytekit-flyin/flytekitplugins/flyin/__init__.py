cde5098d603c421fd4f9d97d59c7bf2534c46fe4
Future Outlier <eric901201@gmai.com>
2023-11-28 12:42:07 +0800

    Add init doc
    Signed-off-by: Future Outlier <eric901201@gmai.com>

--- plugins/flytekit-flyin/flytekitplugins/flyin/__init__.py
+++ plugins/flytekit-flyin/flytekitplugins/flyin/__init__.py
@@ -15,21 +15,17 @@ This package contains flyin plugin for Flytekit.
    COPILOT_EXTENSION
    VIM_EXTENSION
    CODE_TOGETHER_EXTENSION
+   VIM_CONFIG
+   COPILOT_CONFIG
+   CODE_TOGETHER_CONFIG
+   jupyter
 """
 
-from .vscode_lib.decorator import vscode
-from .vscode_lib.constants import (
-    DEFAULT_CODE_SERVER_DIR_NAME,
-    DEFAULT_CODE_SERVER_REMOTE_PATH,
-    DEFAULT_CODE_SERVER_EXTENSIONS,
-)
-from .vscode_lib.config import (
-    VscodeConfig,
-    COPILOT_EXTENSION,
-    VIM_EXTENSION,
-    CODE_TOGETHER_EXTENSION,
-    VIM_CONFIG,
-    COPILOT_CONFIG,
-    CODE_TOGETHER_CONFIG,
-)
 from .jupyter_lib.decorator import jupyter
+from .vscode_lib.config import (CODE_TOGETHER_CONFIG, CODE_TOGETHER_EXTENSION,
+                                COPILOT_CONFIG, COPILOT_EXTENSION, VIM_CONFIG,
+                                VIM_EXTENSION, VscodeConfig)
+from .vscode_lib.constants import (DEFAULT_CODE_SERVER_DIR_NAME,
+                                   DEFAULT_CODE_SERVER_EXTENSIONS,
+                                   DEFAULT_CODE_SERVER_REMOTE_PATH)
+from .vscode_lib.decorator import vscode
