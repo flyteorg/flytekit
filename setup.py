d905723b2841012b96e215b0c0c3b691e6d57692
Thomas J. Fan <thomasjpfan@gmail.com>
2023-11-29 16:50:06 -0800

    Remove wheel dependency
    Signed-off-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- setup.py
+++ setup.py
@@ -68,7 +68,6 @@ setup(
         "statsd>=3.0.0,<4.0.0",
         "typing_extensions",
         "urllib3>=1.22,<2.0.0",
-        "wheel>=0.30.0,<1.0.0",
     ],
     extras_require=extras_require,
     scripts=[
