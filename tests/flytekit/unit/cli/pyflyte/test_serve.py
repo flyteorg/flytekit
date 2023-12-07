ea21b9e5c25f0ec9f575a01007977b5e7d2e146e
Chao-Heng Lee <chaohengstudent@gmail.com>
2023-12-07 18:27:35 +0800

    Separate agent from pyflyte serve (#2019)
    * Separate agent from pyflyte serve.
    
    Signed-off-by: Chao-Heng Lee <chaohengstudent@gmail.com>
    
    * removed unused serve help message.
    
    Signed-off-by: Chao-Heng Lee <chaohengstudent@gmail.com>
    
    * fix typo.
    
    Signed-off-by: Chao-Heng Lee <chaohengstudent@gmail.com>
    
    * update dockerfile of agent.
    
    Signed-off-by: Chao-Heng Lee <chaohengstudent@gmail.com>
    
    * remove agent dockerfile apt cache
    
    Signed-off-by: Future Outlier <eric901201@gmai.com>
    
    ---------
    
    Signed-off-by: Chao-Heng Lee <chaohengstudent@gmail.com>
    Signed-off-by: Future Outlier <eric901201@gmai.com>
    Co-authored-by: Future Outlier <eric901201@gmai.com>
    Signed-off-by: Rafael Raposo <rafaelraposo@spotify.com>

--- tests/flytekit/unit/cli/pyflyte/test_serve.py
+++ tests/flytekit/unit/cli/pyflyte/test_serve.py
@@ -5,5 +5,5 @@ from flytekit.clis.sdk_in_container import pyflyte
 
 def test_pyflyte_serve():
     runner = CliRunner()
-    result = runner.invoke(pyflyte.main, ["serve", "--port", "0", "--timeout", "1"], catch_exceptions=False)
+    result = runner.invoke(pyflyte.main, ["serve", "agent", "--port", "0", "--timeout", "1"], catch_exceptions=False)
     assert result.exit_code == 0
