aa1cabcf779da192a08b5f3519ff8312d1cdbd62
Yue Shang <s.yue3074@gmail.com>
2023-12-07 10:19:13 -0800

    add overwrite cache flag for creating launch plan
    Signed-off-by: Yue Shang <s.yue3074@gmail.com>

--- flytekit/core/launch_plan.py
+++ flytekit/core/launch_plan.py
@@ -117,6 +117,7 @@ class LaunchPlan(object):
         max_parallelism: Optional[int] = None,
         security_context: Optional[security.SecurityContext] = None,
         auth_role: Optional[_common_models.AuthRole] = None,
+        overwrite_cache: Optional[bool] = None,
     ) -> LaunchPlan:
         ctx = FlyteContextManager.current_context()
         default_inputs = default_inputs or {}
@@ -167,6 +168,7 @@ class LaunchPlan(object):
             raw_output_data_config=raw_output_data_config,
             max_parallelism=max_parallelism,
             security_context=security_context,
+            overwrite_cache=overwrite_cache,
         )
 
         # This is just a convenience - we'll need the fixed inputs LiteralMap for when serializing the Launch Plan out
@@ -195,6 +197,7 @@ class LaunchPlan(object):
         max_parallelism: Optional[int] = None,
         security_context: Optional[security.SecurityContext] = None,
         auth_role: Optional[_common_models.AuthRole] = None,
+        overwrite_cache: Optional[bool] = None,
     ) -> LaunchPlan:
         """
         This function offers a friendlier interface for creating launch plans. If the name for the launch plan is not
@@ -232,6 +235,7 @@ class LaunchPlan(object):
             or auth_role is not None
             or max_parallelism is not None
             or security_context is not None
+            or overwrite_cache is not None
         ):
             raise ValueError(
                 "Only named launchplans can be created that have other properties. Drop the name if you want to create a default launchplan. Default launchplans cannot have any other associations"
@@ -263,6 +267,7 @@ class LaunchPlan(object):
                 or raw_output_data_config != cached_outputs["_raw_output_data_config"]
                 or max_parallelism != cached_outputs["_max_parallelism"]
                 or security_context != cached_outputs["_security_context"]
+                or overwrite_cache != cached_outputs['_overwrite_cache']
             ):
                 raise AssertionError("The cached values aren't the same as the current call arguments")
 
@@ -288,6 +293,7 @@ class LaunchPlan(object):
                 max_parallelism,
                 auth_role=auth_role,
                 security_context=security_context,
+                overwrite_cache=overwrite_cache
             )
         LaunchPlan.CACHE[name or workflow.name] = lp
         return lp
@@ -305,6 +311,7 @@ class LaunchPlan(object):
         raw_output_data_config: Optional[_common_models.RawOutputDataConfig] = None,
         max_parallelism: Optional[int] = None,
         security_context: Optional[security.SecurityContext] = None,
+        overwrite_cache: Optional[bool] = None,
     ):
         self._name = name
         self._workflow = workflow
@@ -322,6 +329,7 @@ class LaunchPlan(object):
         self._raw_output_data_config = raw_output_data_config
         self._max_parallelism = max_parallelism
         self._security_context = security_context
+        self._overwrite_cache = overwrite_cache
 
         FlyteEntities.entities.append(self)
 
@@ -337,6 +345,7 @@ class LaunchPlan(object):
         raw_output_data_config: Optional[_common_models.RawOutputDataConfig] = None,
         max_parallelism: Optional[int] = None,
         security_context: Optional[security.SecurityContext] = None,
+        overwrite_cache: Optional[bool] = None,
     ) -> LaunchPlan:
         return LaunchPlan(
             name=name,
@@ -350,7 +359,12 @@ class LaunchPlan(object):
             raw_output_data_config=raw_output_data_config or self.raw_output_data_config,
             max_parallelism=max_parallelism or self.max_parallelism,
             security_context=security_context or self.security_context,
+            overwrite_cache=overwrite_cache or self.overwrite_cache,
         )
+
+    @property
+    def overwrite_cache(self) -> Optional[bool]:
+        return self._overwrite_cache
 
     @property
     def python_interface(self) -> Interface:
