68ad44f6ef3420593ddcee605ac9efbd12b1f745
Future Outlier <eric901201@gmai.com>
2023-12-10 20:18:51 +0800

    remove print
    Signed-off-by: Future Outlier <eric901201@gmai.com>

--- flytekit/clis/sdk_in_container/serve.py
+++ flytekit/clis/sdk_in_container/serve.py
@@ -52,13 +52,12 @@ def agent(_: click.Context, port, worker, timeout):
 
 async def _start_grpc_server(port: int, worker: int, timeout: int):
     click.secho("Starting up the server to expose the prometheus metrics...", fg="blue")
-    print("port:", port)
     from flytekit.extend.backend.agent_service import AsyncAgentService, AgentMetadataService
 
     try:
         from prometheus_client import start_http_server
 
-        start_http_server(10000 + port)
+        start_http_server(9090)
     except ImportError as e:
         click.secho(f"Failed to start the prometheus server with error {e}", fg="red")
     click.secho("Starting the agent service...", fg="blue")
