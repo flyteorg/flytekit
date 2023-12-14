9cf1c62a637519db03793bad0497fc3e23d5548a
Thomas J. Fan <thomasjpfan@gmail.com>
2023-12-14 08:48:23 -0500

    Use a better name
    Signed-off-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- tests/flytekit/conftest.py
+++ tests/flytekit/conftest.py
@@ -8,4 +8,4 @@ from flytekit.configuration.plugin import FlytekitPlugin
 def configure_plugin():
     """If a plugin is installed then the global plugin refers to an external plugin.
     For testing, we want to test against flytekit's own plugin, so we override the state."""
-    flytekit.configuration.plugin._GLOBAL_PLUGIN_STATE["plugin"] = FlytekitPlugin
+    flytekit.configuration.plugin._GLOBAL_CONFIG["plugin"] = FlytekitPlugin
