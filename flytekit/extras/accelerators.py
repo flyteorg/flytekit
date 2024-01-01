706966bba5817a521093ffb739294dccdca1d965
Ketan Umare <kumare3@users.noreply.github.com>
2024-01-01 15:18:33 -0800

    Updated docs
    Signed-off-by: Ketan Umare <kumare3@users.noreply.github.com>

--- flytekit/extras/accelerators.py
+++ flytekit/extras/accelerators.py
@@ -9,12 +9,22 @@ MIG = TypeVar("MIG", bound="MultiInstanceGPUAccelerator")
 
 
 class BaseAccelerator(abc.ABC, Generic[T]):
+    """
+    Base class for all accelerator types. This class is not meant to be instantiated directly.
+    """
+
     @abc.abstractmethod
     def to_flyte_idl(self) -> T:
         ...
 
 
 class GPUAccelerator(BaseAccelerator):
+    """
+    Class that represents a GPU accelerator. The class can be instantiated with any valid GPU device name, but
+    it is recommended to use one of the pre-defined constants below, as name has to match the name of the device
+    configured on the cluster.
+    """
+
     def __init__(self, device: str) -> None:
         self._device = device
 
@@ -22,13 +32,36 @@ class GPUAccelerator(BaseAccelerator):
         return tasks_pb2.GPUAccelerator(device=self._device)
 
 
+#: use this constant to specify that the task should run on an
+#: `NVIDIA A10 Tensor Core GPU<https://www.nvidia.com/en-us/data-center/a10-tensor-core-gpu/>`_
 A10G = GPUAccelerator("nvidia-a10g")
+
+#: use this constant to specify that the task should run on an
+#: `NVIDIA L4 Tensor Core GPU<https://www.nvidia.com/en-us/data-center/l4/>`_
 L4 = GPUAccelerator("nvidia-l4-vws")
+
+#: use this constant to specify that the task should run on an
+#: `NVIDIA Tesla K80 GPU<https://www.nvidia.com/en-gb/data-center/tesla-k80/>`_
 K80 = GPUAccelerator("nvidia-tesla-k80")
+
+#: use this constant to specify that the task should run on an
+#: `NVIDIA Tesla M60 GPU<https://www.nvidia.com/en-us/data-center/tesla-m60/>`_
 M60 = GPUAccelerator("nvidia-tesla-m60")
+
+#: use this constant to specify that the task should run on an
+#: `NVIDIA Tesla P4 GPU<https://www.nvidia.com/en-us/data-center/tesla-p4/>`_
 P4 = GPUAccelerator("nvidia-tesla-p4")
+
+#: use this constant to specify that the task should run on an
+#: `NVIDIA Tesla P100 GPU<https://www.nvidia.com/en-us/data-center/tesla-p100/>`_
 P100 = GPUAccelerator("nvidia-tesla-p100")
+
+#: use this constant to specify that the task should run on an
+#: `NVIDIA T4 Tensor Core GPU<https://www.nvidia.com/en-us/data-center/t4/>`_
 T4 = GPUAccelerator("nvidia-tesla-t4")
+
+#: use this constant to specify that the task should run on an
+#: `NVIDIA Tesla V100 GPU<https://www.nvidia.com/en-us/data-center/tesla-v100/>`_
 V100 = GPUAccelerator("nvidia-tesla-v100")
 
 
@@ -65,6 +98,12 @@ class _A100_Base(MultiInstanceGPUAccelerator):
 
 
 class _A100(_A100_Base):
+    """
+    Class that represents an `NVIDIA A100 GPU<https://www.nvidia.com/en-us/data-center/a100/>`_. It is possible
+    to specify a partition of an A100 GPU by using the provided paritions on the class. For example, to specify a
+    10GB partition, use ``A100.partition_2g_10gb``.
+    """
+
     partition_1g_5gb = _A100_Base.partitioned("1g.5gb")
     partition_2g_10gb = _A100_Base.partitioned("2g.10gb")
     partition_3g_20gb = _A100_Base.partitioned("3g.20gb")
@@ -72,6 +111,10 @@ class _A100(_A100_Base):
     partition_7g_40gb = _A100_Base.partitioned("7g.40gb")
 
 
+#: use this constant to specify that the task should run on an entire
+#: `NVIDIA A100 GPU<https://www.nvidia.com/en-us/data-center/a100/>`_.
+#: It is also possible to specify a partition of an A100 GPU by using the provided paritions on the class.
+#: For example, to specify a 10GB partition, use ``A100.partition_2g_10gb``.
 A100 = _A100()
 
 
