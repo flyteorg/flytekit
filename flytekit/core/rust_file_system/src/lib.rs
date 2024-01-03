61fe0e0df9e908b50e92c761dec343d1c267af97
troychiu <y.troychiu@gmail.com>
2024-01-03 17:05:24 +0800

    can work
    Signed-off-by: troychiu <y.troychiu@gmail.com>

--- flytekit/core/rust_file_system/src/lib.rs
+++ flytekit/core/rust_file_system/src/lib.rs
@@ -11,8 +11,10 @@ pub struct FileSystem {
 }
 
 fn get_s3_store() -> Arc<dyn ObjectStore> {
+
     let s3 = AmazonS3Builder::new()
         .with_endpoint("http://localhost:30002")
+        .with_allow_http(true)
         .with_region("us-east-1") // Dummy region for local testing
         .with_bucket_name("troy-flyte")
         .with_access_key_id("minio")
@@ -20,6 +22,7 @@ fn get_s3_store() -> Arc<dyn ObjectStore> {
         .build()
         .expect("error creating s3");
     Arc::new(s3)
+    
 }
 
 #[pymethods]
@@ -44,14 +47,12 @@ impl FileSystem {
         let path = Path::from(key);
         let rt = tokio::runtime::Runtime::new().unwrap();
         rt.block_on(async {
-            let downloaded_bytes = self.store.get(&path).await.unwrap();
-            let downloaded_bytes = downloaded_bytes.bytes().await.unwrap();
+            let downloaded_bytes = self.store.get(&path).await.unwrap().bytes().await.unwrap();
             let mut file = tokio::fs::File::create(file_path).await.unwrap();
             file.write_all(&downloaded_bytes).await.unwrap();
         });
         Ok(())
     }
-    
 }
 
 /// A Python module implemented in Rust.
