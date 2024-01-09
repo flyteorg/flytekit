37708ae56403677db6e1d12e474464056c3cca4a
Niels Bantilan <niels.bantilan@gmail.com>
2024-01-09 18:44:45 -0500

    add monodocs redirects to the flytekit readthedocs project
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,6 +57,7 @@ extensions = [
     "sphinx-prompt",
     "sphinx_copybutton",
     "sphinx_panels",
+    "sphinx_reredirects",
     "sphinxcontrib.youtube",
     "sphinx_tags",
     "sphinx_click",
@@ -239,3 +240,26 @@ autoclass_content = "both"
 tags_create_tags = True
 tags_page_title = "Tag"
 tags_overview_title = "All Tags"
+
+
+# Sphinx redirects to the monodocs
+page_pattern = "https://docs.flyte.org/en/latest/api/flytekit/$source.html"
+redirects = {
+    "index.html": "https://docs.flyte.org/en/latest/reference_flytekit.html",
+    "generated/*": page_pattern,
+    "design/*": page_pattern,
+    "plugins/*": page_pattern,
+    "flytekit.html": "https://docs.flyte.org/en/latest/api/flytekit/flytekit.html",
+    "configuration.html": "https://docs.flyte.org/en/latest/api/flytekit/configuration.html",
+    "remote.html": "https://docs.flyte.org/en/latest/api/flytekit/remote.html",
+    "clients.html": "https://docs.flyte.org/en/latest/api/flytekit/clients.html",
+    "testing.html": "https://docs.flyte.org/en/latest/api/flytekit/testing.html",
+    "extend.html": "https://docs.flyte.org/en/latest/api/flytekit/extend.html",
+    "deck.html": "https://docs.flyte.org/en/latest/api/flytekit/deck.html",
+    "tasks*": page_pattern,
+    "types*": page_pattern,
+    "extras*": page_pattern,
+    "experimental.html": "https://docs.flyte.org/en/latest/api/flytekit/experimental.html",
+    "pyflyte.html": "https://docs.flyte.org/en/latest/api/flytekit/pyflyte.html",
+    "contributing.html": "https://docs.flyte.org/en/latest/api/flytekit/contributing.html",
+}
