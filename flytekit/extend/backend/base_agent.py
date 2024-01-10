1f8e377f695883362f664938fc31e40d6ceed091
Kevin Su <pingsutw@apache.org>
2024-01-10 02:09:33 -0800

    nit
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/extend/backend/base_agent.py
+++ flytekit/extend/backend/base_agent.py
@@ -241,7 +241,7 @@ class AsyncAgentExecutorMixin:
                 else:
                     res = self._agent.get(grpc_ctx, resource_meta)
                 state = res.resource.state
-            logger.info(f"Task state: {State.Name(state)}, State message: {res.resource.message}")
+            progress.print(f"Task state: {State.Name(state)}, State message: {res.resource.message}")
             if hasattr(res.resource, "log_links"):
                 for link in res.resource.log_links:
                     progress.print(f"{link.name}: {link.uri}")
