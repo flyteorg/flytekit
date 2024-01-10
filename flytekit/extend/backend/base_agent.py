dc35e7f91dc22b7a124b1984fb134d5beec196ff
Kevin Su <pingsutw@apache.org>
2024-01-10 15:00:38 -0800

    Print log links in local execution (#2097)
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/extend/backend/base_agent.py
+++ flytekit/extend/backend/base_agent.py
@@ -241,7 +241,10 @@ class AsyncAgentExecutorMixin:
                 else:
                     res = self._agent.get(grpc_ctx, resource_meta)
                 state = res.resource.state
-                logger.info(f"Task state: {state}, State message: {res.resource.message}")
+            progress.print(f"Task state: {State.Name(state)}, State message: {res.resource.message}")
+            if hasattr(res.resource, "log_links"):
+                for link in res.resource.log_links:
+                    progress.print(f"{link.name}: {link.uri}")
         return res
 
     def signal_handler(self, resource_meta: bytes, signum: int, frame: FrameType) -> typing.Any:
