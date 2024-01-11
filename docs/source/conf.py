d451ee2171e6ab9bef53b65fbdfac1e54feb00ac
Niels Bantilan <niels.bantilan@gmail.com>
2024-01-11 12:38:49 -0500

    add monodocs index page in flytekit (#2100)
    * add monodocs index page in flytekit
    
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>
    
    * updates
    
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>
    
    ---------
    
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,7 +96,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = []
+exclude_patterns = ["docs_index.rst"]
 
 # -- Options for HTML output -------------------------------------------------
 
