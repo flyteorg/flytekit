892b4741d0c38bd61b9cdaf1defb002d729acba2
Thomas J. Fan <thomasjpfan@gmail.com>
2024-01-13 03:18:49 -0500

    Adds conda support to envd plugin (#2020)
    Signed-off-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- plugins/flytekit-envd/tests/test_image_spec.py
+++ plugins/flytekit-envd/tests/test_image_spec.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from textwrap import dedent
 
 from flytekitplugins.envd.image_builder import EnvdImageSpecBuilder, create_envd_config
 
@@ -35,3 +36,37 @@ def build():
     install.python(version="3.8")
 """
     )
+
+
+def test_image_spec_conda():
+    image_spec = ImageSpec(
+        base_image="ubuntu:20.04",
+        python_version="3.11",
+        packages=["flytekit"],
+        conda_packages=["pytorch", "cpuonly"],
+        conda_channels=["pytorch"],
+    )
+
+    EnvdImageSpecBuilder().build_image(image_spec)
+    config_path = create_envd_config(image_spec)
+    assert image_spec.platform == "linux/amd64"
+    image_name = image_spec.image_name()
+    contents = Path(config_path).read_text()
+    expected_contents = dedent(
+        f"""\
+    # syntax=v1
+
+    def build():
+        base(image="ubuntu:20.04", dev=False)
+        run(commands=[])
+        install.python_packages(name=["flytekit"])
+        install.apt_packages(name=[])
+        runtime.environ(env={{'PYTHONPATH': '/root', '_F_IMG_ID': '{image_name}'}}, extra_path=['/root'])
+        config.pip_index(url="https://pypi.org/simple")
+        install.conda(use_mamba=True)
+        install.conda_packages(name=["pytorch", "cpuonly"], channel=["pytorch"])
+        install.python(version="3.11")
+    """
+    )
+
+    assert contents == expected_contents
