4ec34b3703ef79f5c406efadc15045edb10610c3
Karl Nordstrom <karl.n@muonspace.com>
2024-01-19 17:58:46 +0000

    Making it possible to pass in environment variable when running register_script
    Signed-off-by: Karl Nordstrom <karl.n@muonspace.com>

--- flytekit/remote/remote.py
+++ flytekit/remote/remote.py
@@ -892,6 +892,7 @@ class FlyteRemote(object):
         options: typing.Optional[Options] = None,
         source_path: typing.Optional[str] = None,
         module_name: typing.Optional[str] = None,
+        envs: typing.Optional[typing.Dict[str, str]] = None,
     ) -> typing.Union[FlyteWorkflow, FlyteTask]:
         """
         Use this method to register a workflow via script mode.
@@ -926,6 +927,7 @@ class FlyteRemote(object):
             domain=domain,
             image_config=image_config,
             git_repo=_get_git_repo_url(source_path),
+            env=envs,
             fast_serialization_settings=FastSerializationSettings(
                 enabled=True,
                 destination_dir=destination_dir,
