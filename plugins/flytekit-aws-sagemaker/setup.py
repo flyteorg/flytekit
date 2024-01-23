1fbdf512f9050d42dc033961babdfa7d07822f34
Samhita Alla <aallasamhita@gmail.com>
2024-01-23 11:31:31 +0530

    dockerfile and setup changes
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- plugins/flytekit-aws-sagemaker/setup.py
+++ plugins/flytekit-aws-sagemaker/setup.py
@@ -4,7 +4,7 @@ PLUGIN_NAME = "awssagemaker"
 
 microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
 
-plugin_requires = ["flytekit>=1.3.0b2,<2.0.0", "sagemaker-training>=3.6.2,<4.0.0", "retry2==0.9.5"]
+plugin_requires = ["flytekit>=1.10.0", "flyteidl>=1.10.7b0", "aioboto3"]
 
 __version__ = "0.0.0+develop"
 
@@ -14,9 +14,9 @@ setup(
     version=__version__,
     author="flyteorg",
     author_email="admin@flyte.org",
-    description="AWS Plugins for flytekit",
+    description="Flytekit AWS Sagemaker plugin",
     namespace_packages=["flytekitplugins"],
-    packages=[f"flytekitplugins.{PLUGIN_NAME}", f"flytekitplugins.{PLUGIN_NAME}.models"],
+    packages=[f"flytekitplugins.{PLUGIN_NAME}"],
     install_requires=plugin_requires,
     license="apache2",
     python_requires=">=3.8",
@@ -27,11 +27,12 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    scripts=["scripts/flytekit_sagemaker_runner.py"],
+    entry_points={"flytekit.plugins": [f"{PLUGIN_NAME}=flytekitplugins.{PLUGIN_NAME}"]},
 )
