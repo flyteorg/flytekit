e64d53d392dbe4f538aa3948700df87e11207021
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2024-02-07 16:01:20 -0800

    Move test that touches the local cache to test_local_cache
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- tests/flytekit/unit/core/test_checkpoint.py
+++ tests/flytekit/unit/core/test_checkpoint.py
@@ -154,23 +154,5 @@ def t1(n: int) -> int:
     return n + 1
 
 
-@flytekit.task(cache=True, cache_version="v0")
-def t2(n: int) -> int:
-    ctx = flytekit.current_context()
-    cp = ctx.checkpoint
-    cp.write(bytes(n + 1))
-    return n + 1
-
-
-@pytest.fixture(scope="function", autouse=True)
-def setup():
-    LocalTaskCache.initialize()
-    LocalTaskCache.clear()
-
-
 def test_checkpoint_task():
     assert t1(n=5) == 6
-
-
-def test_checkpoint_cached_task():
-    assert t2(n=5) == 6
