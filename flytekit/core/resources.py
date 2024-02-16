c52055a9eda6b567402411662aa79ca43c96b185
Ryan Lo <wenchih@apache.org>
2024-02-16 22:44:14 +0800

    description
    Signed-off-by: Ryan Lo <wenchih@apache.org>

--- flytekit/core/resources.py
+++ flytekit/core/resources.py
@@ -13,6 +13,7 @@ class Resources(object):
 
         Resources(cpu="1", mem="2048")  # This is 1 CPU and 2 KB of memory
         Resources(cpu="100m", mem="2Gi")  # This is 1/10th of a CPU and 2 gigabytes of memory
+        Resources(cpu=2, mem=1024) # This is 2 CPU and 1 KB of memory
 
         # For Kubernetes-based tasks, pods use ephemeral local storage for scratch space, caching, and for logs.
         # This allocates 1Gi of such local storage.
