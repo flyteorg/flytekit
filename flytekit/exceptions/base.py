77bc4592db29a636d7a3bb5a5e12cb35bd09dbff
noahjax <noah.jackson@dominodatalab.com>
2024-02-20 15:11:04 -0800

    define a __str__ method for FlyteException
    Signed-off-by: noahjax <noah.jackson@dominodatalab.com>

--- flytekit/exceptions/base.py
+++ flytekit/exceptions/base.py
@@ -7,6 +7,9 @@ class _FlyteCodedExceptionMetaclass(type):
 class FlyteException(Exception, metaclass=_FlyteCodedExceptionMetaclass):
     _ERROR_CODE = "UnknownFlyteException"
 
+    def __str__(self):
+        return f"{self.__class__.__name__}({self.__cause__})"
+
 
 class FlyteRecoverableException(FlyteException):
     _ERROR_CODE = "RecoverableFlyteException"
