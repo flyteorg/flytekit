330e62618953f5fe51b372e2c0f6aacfa85136c8
Yue Shang <s.yue3074@gmail.com>
2024-02-19 21:03:02 -0800

    merge conflict
    Signed-off-by: Yue Shang <s.yue3074@gmail.com>

--- tests/flytekit/unit/remote/test_remote.py
+++ tests/flytekit/unit/remote/test_remote.py
@@ -31,8 +31,8 @@ from flytekit.models.core.compiler import CompiledWorkflowClosure
 from flytekit.models.core.identifier import Identifier, ResourceType, WorkflowExecutionIdentifier
 from flytekit.models.execution import Execution
 from flytekit.models.task import Task
-from flytekit.remote.entities import FlyteWorkflow
 from flytekit.remote import FlyteTask
+from flytekit.remote.entities import FlyteWorkflow
 from flytekit.remote.lazy_entity import LazyEntity
 from flytekit.remote.remote import FlyteRemote, _get_git_repo_url
 from flytekit.tools.translator import Options, get_serializable, get_serializable_launch_plan
