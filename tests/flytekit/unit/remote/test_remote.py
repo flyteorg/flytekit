cb4b3efe0bd1d75e89d18e1eace3dfbaf84f37bc
Yue Shang <s.yue3074@gmail.com>
2024-02-20 22:42:27 -0800

    fix test
    Signed-off-by: Yue Shang <s.yue3074@gmail.com>

--- tests/flytekit/unit/remote/test_remote.py
+++ tests/flytekit/unit/remote/test_remote.py
@@ -7,7 +7,6 @@ import typing
 import uuid
 from collections import OrderedDict
 from datetime import datetime, timedelta
-from unittest.mock import Mock
 
 import mock
 import pytest
@@ -32,7 +31,6 @@ from flytekit.models.core.identifier import Identifier, ResourceType, WorkflowEx
 from flytekit.models.execution import Execution
 from flytekit.models.task import Task
 from flytekit.remote import FlyteTask
-from flytekit.remote.entities import FlyteWorkflow
 from flytekit.remote.lazy_entity import LazyEntity
 from flytekit.remote.remote import FlyteRemote, _get_git_repo_url
 from flytekit.tools.translator import Options, get_serializable, get_serializable_launch_plan
@@ -55,6 +53,52 @@ ENTITY_TYPE_TEXT = {
     ResourceType.TASK: "Task",
     ResourceType.LAUNCH_PLAN: "Launch Plan",
 }
+
+
+obj = _workflow.Node(
+    id="some:node:id",
+    metadata="1",
+    inputs=[],
+    upstream_node_ids=[],
+    output_aliases=[],
+    workflow_node=_workflow.WorkflowNode(launchplan_ref="LAUNCH_PLAN"),
+)
+node1 = _workflow.Node(
+    id="some:node:id",
+    metadata="1",
+    inputs=[],
+    upstream_node_ids=[],
+    output_aliases=[],
+    branch_node=_workflow.BranchNode(
+        _workflow.IfElseBlock(
+            case=_workflow.IfBlock(
+                condition=_condition.BooleanExpression(),
+                then_node=obj,
+            )
+        )
+    ),
+)
+nodes = [node1]
+
+obj2 = _workflow.Node(id="some:node:id", metadata="1", inputs=[], upstream_node_ids=[], output_aliases=[])
+
+node2 = node1 = _workflow.Node(
+    id="some:node:id",
+    metadata="1",
+    inputs=[],
+    upstream_node_ids=[],
+    output_aliases=[],
+    branch_node=_workflow.BranchNode(
+        _workflow.IfElseBlock(
+            case=_workflow.IfBlock(
+                condition=_condition.BooleanExpression(),
+                then_node=obj2,
+            ),
+            else_node=node1,
+        )
+    ),
+)
+nodes2 = [node2]
 
 
 @pytest.fixture
@@ -391,38 +435,15 @@ def test_launch_backfill(remote):
     assert wf.workflow_metadata.on_failure == WorkflowFailurePolicy.FAIL_IMMEDIATELY
 
 
+@patch("flytekit.remote.entities.FlyteWorkflow.get_non_system_nodes", return_value=nodes)
 @patch("flytekit.remote.entities.FlyteWorkflow.promote_from_closure")
-def test_fetch_workflow_with_branch(mock_promote, remote):
+def test_fetch_workflow_with_branch(mock_promote, mock_workflow, remote):
     mock_client = remote._client
     mock_client.get_workflow.return_value = Workflow(
         id=Identifier(ResourceType.TASK, "p", "d", "n", "v"),
         closure=WorkflowClosure(compiled_workflow=MagicMock()),
     )
-    obj = _workflow.Node(
-        id="some:node:id",
-        metadata="1",
-        inputs=[],
-        upstream_node_ids=[],
-        output_aliases=[],
-        workflow_node=_workflow.WorkflowNode(launchplan_ref="LAUNCH_PLAN"),
-    )
-    node1 = _workflow.Node(
-        id="some:node:id",
-        metadata="1",
-        inputs=[],
-        upstream_node_ids=[],
-        output_aliases=[],
-        branch_node=_workflow.BranchNode(
-            _workflow.IfElseBlock(
-                case=_workflow.IfBlock(
-                    condition=_condition.BooleanExpression(),
-                    then_node=obj,
-                )
-            )
-        ),
-    )
-    nodes = [node1]
-    FlyteWorkflow.get_non_system_nodes = Mock(return_value=nodes)
+
     admin_launch_plan = MagicMock()
     admin_launch_plan.spec = {"workflow_id": 123}
     mock_client.get_launch_plan.return_value = admin_launch_plan
@@ -432,55 +453,14 @@ def test_fetch_workflow_with_branch(mock_promote, remote):
     mock_promote.assert_called_with(ANY, node_launch_plans)
 
 
+@patch("flytekit.remote.entities.FlyteWorkflow.get_non_system_nodes", return_value=nodes2)
 @patch("flytekit.remote.entities.FlyteWorkflow.promote_from_closure")
-def test_fetch_workflow_with_nested_branch(mock_promote, remote):
+def test_fetch_workflow_with_nested_branch(mock_promote, mock_workflow, remote):
     mock_client = remote._client
     mock_client.get_workflow.return_value = Workflow(
         id=Identifier(ResourceType.TASK, "p", "d", "n", "v"),
         closure=WorkflowClosure(compiled_workflow=MagicMock()),
     )
-    obj = _workflow.Node(
-        id="some:node:id",
-        metadata="1",
-        inputs=[],
-        upstream_node_ids=[],
-        output_aliases=[],
-        workflow_node=_workflow.WorkflowNode(launchplan_ref="LAUNCH_PLAN"),
-    )
-    node1 = _workflow.Node(
-        id="some:node:id",
-        metadata="1",
-        inputs=[],
-        upstream_node_ids=[],
-        output_aliases=[],
-        branch_node=_workflow.BranchNode(
-            _workflow.IfElseBlock(
-                case=_workflow.IfBlock(
-                    condition=_condition.BooleanExpression(),
-                    then_node=obj,
-                )
-            )
-        ),
-    )
-    obj2 = _workflow.Node(id="some:node:id", metadata="1", inputs=[], upstream_node_ids=[], output_aliases=[])
-    node2 = node1 = _workflow.Node(
-        id="some:node:id",
-        metadata="1",
-        inputs=[],
-        upstream_node_ids=[],
-        output_aliases=[],
-        branch_node=_workflow.BranchNode(
-            _workflow.IfElseBlock(
-                case=_workflow.IfBlock(
-                    condition=_condition.BooleanExpression(),
-                    then_node=obj2,
-                ),
-                else_node=node1,
-            )
-        ),
-    )
-    nodes = [node2]
-    FlyteWorkflow.get_non_system_nodes = Mock(return_value=nodes)
     admin_launch_plan = MagicMock()
     admin_launch_plan.spec = {"workflow_id": 123}
     mock_client.get_launch_plan.return_value = admin_launch_plan
