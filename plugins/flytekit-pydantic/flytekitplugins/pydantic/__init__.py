31fa7c06e0fad4156197305562f1601647a78510
Future-Outlier <eric901201@gmail.com>
2024-02-27 23:00:14 +0800

    warning messages
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- plugins/flytekit-pydantic/flytekitplugins/pydantic/__init__.py
+++ plugins/flytekit-pydantic/flytekitplugins/pydantic/__init__.py
@@ -2,3 +2,9 @@ from .basemodel_transformer import BaseModelTransformer
 from .deserialization import set_validators_on_supported_flyte_types as _set_validators_on_supported_flyte_types
 
 _set_validators_on_supported_flyte_types()  # enables you to use flytekit.types in pydantic model
+import warnings
+
+warnings.warn(
+    "If you are using Pydantic version 2.0 or later, please import BaseModel using `from pydantic.v1 import BaseModel`.",
+    FutureWarning,
+)
