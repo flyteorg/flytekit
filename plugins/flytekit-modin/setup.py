3b4970bb511421c12efcae09402fc19ffa7d793d
Kevin Su <pingsutw@gmail.com>
2024-02-29 11:18:07 -0800

    test
    Signed-off-by: Kevin Su <pingsutw@gmail.com>

--- plugins/flytekit-modin/setup.py
+++ plugins/flytekit-modin/setup.py
@@ -5,7 +5,7 @@ PLUGIN_NAME = "modin"
 microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
 
 plugin_requires = [
-    "flytekit<1.3.0b2,<2.0.0",
+    "flytekit<1.3.0b2",
     "modin[ray]>=0.13.0",
     "fsspec",
 ]
