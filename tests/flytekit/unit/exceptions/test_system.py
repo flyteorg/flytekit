33053a8a9eb4e1119f976828f380a5de278fe1ee
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2024-03-04 10:44:49 -0800

    Fix lint errors
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- tests/flytekit/unit/exceptions/test_system.py
+++ tests/flytekit/unit/exceptions/test_system.py
@@ -1,5 +1,6 @@
 from flytekit.exceptions import base, system
 
+
 def test_flyte_system_exception():
     try:
         base_exn = Exception("system exploded somewhere else")
@@ -16,7 +17,10 @@ def test_flyte_not_implemented_exception():
         base_exn = Exception("somewhere else didn't implement this")
         raise system.FlyteNotImplementedException("I'm lazy so I didn't implement this.") from base_exn
     except Exception as e:
-        assert str(e) == "SYSTEM:NotImplemented: error=I'm lazy so I didn't implement this., cause=somewhere else didn't implement this"
+        assert (
+            str(e)
+            == "SYSTEM:NotImplemented: error=I'm lazy so I didn't implement this., cause=somewhere else didn't implement this"
+        )
         assert isinstance(e, NotImplementedError)
         assert type(e).error_code == "SYSTEM:NotImplemented"
         assert isinstance(e, system.FlyteSystemException)
@@ -47,10 +51,13 @@ def test_flyte_entrypoint_not_loadable_exception():
 
     try:
         base_exn = Exception("somewhere else couldn't load this")
-        raise system.FlyteEntrypointNotLoadable("fake.module", additional_msg="Shouldn't have used a fake module!") from base_exn
+        raise system.FlyteEntrypointNotLoadable(
+            "fake.module", additional_msg="Shouldn't have used a fake module!"
+        ) from base_exn
     except Exception as e:
         assert (
-            str(e) == "SYSTEM:UnloadableCode: error=Entrypoint is not loadable!  Could not load the module: 'fake.module' "
+            str(e)
+            == "SYSTEM:UnloadableCode: error=Entrypoint is not loadable!  Could not load the module: 'fake.module' "
             "due to error: Shouldn't have used a fake module!, cause=somewhere else couldn't load this"
         )
         assert type(e).error_code == "SYSTEM:UnloadableCode"
@@ -64,7 +71,8 @@ def test_flyte_entrypoint_not_loadable_exception():
         )
     except Exception as e:
         assert (
-            str(e) == "SYSTEM:UnloadableCode: error=Entrypoint is not loadable!  Could not find the task: 'secret_task' in 'fake.module' "
+            str(e)
+            == "SYSTEM:UnloadableCode: error=Entrypoint is not loadable!  Could not find the task: 'secret_task' in 'fake.module' "
             "due to error: Shouldn't have used a fake module!"
         )
         assert type(e).error_code == "SYSTEM:UnloadableCode"
@@ -76,7 +84,10 @@ def test_flyte_system_assertion():
         base_exn = Exception("somewhere else asserts this is wrong")
         raise system.FlyteSystemAssertion("I assert that the system messed up.") from base_exn
     except Exception as e:
-        assert str(e) == "SYSTEM:AssertionError: error=I assert that the system messed up., cause=somewhere else asserts this is wrong"
+        assert (
+            str(e)
+            == "SYSTEM:AssertionError: error=I assert that the system messed up., cause=somewhere else asserts this is wrong"
+        )
         assert type(e).error_code == "SYSTEM:AssertionError"
         assert isinstance(e, system.FlyteSystemException)
         assert isinstance(e, AssertionError)
--- tests/flytekit/unit/exceptions/test_user.py
+++ tests/flytekit/unit/exceptions/test_user.py
@@ -15,7 +15,9 @@ def test_flyte_user_exception():
 def test_flyte_type_exception():
     try:
         base_exn = Exception("Types need to be right!!!")
-        raise user.FlyteTypeException("int", "float", received_value=1, additional_msg="That was a bad idea!") from base_exn
+        raise user.FlyteTypeException(
+            "int", "float", received_value=1, additional_msg="That was a bad idea!"
+        ) from base_exn
     except Exception as e:
         assert (
             str(e) == "USER:TypeError: error=Type error!  Received: int with value: 1, Expected: float. "
@@ -35,7 +37,8 @@ def test_flyte_type_exception():
         ) from base_exn
     except Exception as e:
         assert (
-            str(e) == "USER:TypeError: error=Type error!  Received: int with value: 1, Expected one of: ('list', 'set'). That was a "
+            str(e)
+            == "USER:TypeError: error=Type error!  Received: int with value: 1, Expected one of: ('list', 'set'). That was a "
             "bad idea!, cause=everyone is upset with you"
         )
         assert isinstance(e, TypeError)
@@ -46,7 +49,10 @@ def test_flyte_type_exception():
         base_exn = Exception("int != float")
         raise user.FlyteTypeException("int", "float", additional_msg="That was a bad idea!") from base_exn
     except Exception as e:
-        assert str(e) == "USER:TypeError: error=Type error!  Received: int, Expected: float. That was a bad idea!, cause=int != float"
+        assert (
+            str(e)
+            == "USER:TypeError: error=Type error!  Received: int, Expected: float. That was a bad idea!, cause=int != float"
+        )
         assert isinstance(e, TypeError)
         assert type(e).error_code == "USER:TypeError"
         assert isinstance(e, user.FlyteUserException)
@@ -54,7 +60,10 @@ def test_flyte_type_exception():
     try:
         raise user.FlyteTypeException("int", ("list", "set"), additional_msg="That was a bad idea!")
     except Exception as e:
-        assert str(e) == "USER:TypeError: error=Type error!  Received: int, Expected one of: ('list', 'set'). That was a " "bad idea!"
+        assert (
+            str(e) == "USER:TypeError: error=Type error!  Received: int, Expected one of: ('list', 'set'). That was a "
+            "bad idea!"
+        )
         assert isinstance(e, TypeError)
         assert type(e).error_code == "USER:TypeError"
         assert isinstance(e, user.FlyteUserException)
@@ -65,7 +74,10 @@ def test_flyte_value_exception():
         base_exn = Exception("live up to expectations")
         raise user.FlyteValueException(-1, "Expected a value > 0") from base_exn
     except user.FlyteValueException as e:
-        assert str(e) == "USER:ValueError: error=Value error!  Received: -1. Expected a value > 0, cause=live up to expectations"
+        assert (
+            str(e)
+            == "USER:ValueError: error=Value error!  Received: -1. Expected a value > 0, cause=live up to expectations"
+        )
         assert isinstance(e, ValueError)
         assert type(e).error_code == "USER:ValueError"
         assert isinstance(e, user.FlyteUserException)
@@ -87,7 +99,10 @@ def test_flyte_validation_error():
         base_exn = Exception("somewhere else said this isn't valid")
         raise user.FlyteValidationException("I validated that your stuff was wrong.") from base_exn
     except user.FlyteValidationException as e:
-        assert str(e) == "USER:ValidationError: error=I validated that your stuff was wrong., cause=somewhere else said this isn't valid"
+        assert (
+            str(e)
+            == "USER:ValidationError: error=I validated that your stuff was wrong., cause=somewhere else said this isn't valid"
+        )
         assert isinstance(e, AssertionError)
         assert type(e).error_code == "USER:ValidationError"
         assert isinstance(e, user.FlyteUserException)
