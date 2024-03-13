b8600378f1ea487908d59823b47a519cc1a14af3
Future-Outlier <eric901201@gmail.com>
2024-03-13 18:34:01 +0800

    v1
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- flytekit/core/container_task.py
+++ flytekit/core/container_task.py
@@ -1,20 +1,24 @@
 import typing
 from enum import Enum
-from typing import Any, Dict, List, Optional, OrderedDict, Type
+from typing import Coroutine, Dict, List, Optional, OrderedDict, Tuple, Type, Union
 
 from flytekit.configuration import SerializationSettings
 from flytekit.core.base_task import PythonTask, TaskMetadata
 from flytekit.core.context_manager import FlyteContext
 from flytekit.core.interface import Interface
 from flytekit.core.pod_template import PodTemplate
+from flytekit.core.promise import Promise, VoidPromise, create_task_output
 from flytekit.core.python_auto_container import get_registerable_container_image
 from flytekit.core.resources import Resources, ResourceSpec
 from flytekit.core.utils import _get_container_definition, _serialize_pod_spec
 from flytekit.image_spec.image_spec import ImageSpec
+from flytekit.loggers import logger
+from flytekit.models import literals as _literal_models
 from flytekit.models import task as _task_model
 from flytekit.models.security import Secret, SecurityContext
 
 _PRIMARY_CONTAINER_NAME_FIELD = "primary_container_name"
+DOCKER_IMPORT_ERROR_MESSAGE = "Docker is not installed. Please install Docker by running `pip install docker`."
 
 
 class ContainerTask(PythonTask):
@@ -82,6 +86,7 @@ class ContainerTask(PythonTask):
         self._args = arguments
         self._input_data_dir = input_data_dir
         self._output_data_dir = output_data_dir
+        self._outputs = outputs
         self._md_format = metadata_format
         self._io_strategy = io_strategy
         self._resources = ResourceSpec(
@@ -93,12 +98,90 @@ class ContainerTask(PythonTask):
     def resources(self) -> ResourceSpec:
         return self._resources
 
-    def local_execute(self, ctx: FlyteContext, **kwargs) -> Any:
-        """
-        1. literals and promise generation. This can be easily done in local_execute.
-        2. use docker client api
-        """
-        raise RuntimeError("ContainerTask is not supported in local executions.")
+    def local_execute(
+        self, ctx: FlyteContext, **kwargs
+    ) -> Union[Tuple[Promise], Promise, VoidPromise, Coroutine, None]:
+        try:
+            import docker
+        except ImportError:
+            raise ImportError(DOCKER_IMPORT_ERROR_MESSAGE)
+        import os
+
+        from flytekit.core.promise import translate_inputs_to_literals
+        from flytekit.core.type_engine import TypeEngine, TypeTransformerFailedError
+
+        try:
+            kwargs = translate_inputs_to_literals(
+                ctx,
+                incoming_values=kwargs,
+                flyte_interface_types=self.interface.inputs,
+                native_types=self.get_input_types(),  # type: ignore
+            )
+        except TypeTransformerFailedError as exc:
+            msg = f"Failed to convert inputs of task '{self.name}':\n  {exc}"
+            logger.error(msg)
+            raise TypeError(msg) from exc
+        input_literal_map = _literal_models.LiteralMap(literals=kwargs)
+        try:
+            native_inputs = self._literal_map_to_python_input(input_literal_map, ctx)
+        except Exception as exc:
+            msg = f"Failed to convert inputs of task '{self.name}':\n  {exc}"
+            logger.error(msg)
+            raise type(exc)(msg) from exc
+
+        container_output_dir = "/flyte/raw-container-task/output"
+        output_directory = ctx.file_access.get_random_local_directory()
+        volume_bindings = {
+            output_directory: {
+                "bind": container_output_dir,
+                "mode": "rw",
+            },
+        }
+
+        commands = ""
+        if self._cmd:
+            for cmd in self._cmd:
+                if cmd.startswith("{{.inputs.") and cmd.endswith("}}"):
+                    v = cmd[len("{{.inputs.") : -len("}}")]
+                    commands += str(native_inputs[v]) + " "
+                elif cmd == self._output_data_dir:
+                    commands += container_output_dir + " "
+                else:
+                    commands += cmd + " "
+        if self._args:
+            for arg in self._args:
+                cmd += arg + " "
+
+        client = docker.from_env()
+        container = client.containers.run(
+            self._image,
+            command=[
+                "sh",
+                "-c",
+                commands,
+            ],
+            volumes=volume_bindings,
+            detach=True,
+        )
+        # Wait for the container to finish the task
+        container.wait()
+        container.stop()
+        container.remove()
+
+        output_dict = {}
+        if self._outputs:
+            for k, output_type in self._outputs.items():
+                with open(os.path.join(output_directory, k), "r") as f:
+                    output_val = f.read()
+                output_dict[k] = output_type(output_val)
+        outputs_literal_map = TypeEngine.dict_to_literal_map(ctx, output_dict)
+        outputs_literals = outputs_literal_map.literals
+        output_names = list(self.interface.outputs.keys())
+        # Tasks that don't return anything still return a VoidPromise
+        if len(output_names) == 0:
+            return VoidPromise(self.name)
+        vals = [Promise(var, outputs_literals[var]) for var in output_names]
+        return create_task_output(vals, self.python_interface)
 
     def get_container(self, settings: SerializationSettings) -> _task_model.Container:
         # if pod_template is specified, return None here but in get_k8s_pod, return pod_template merged with container
