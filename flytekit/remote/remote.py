e4f722012e29e65e135e426cc84f623c3469c963
Thomas J. Fan <thomasjpfan@gmail.com>
2024-03-15 11:08:46 -0400

    Migrate over to using datetime.now(timezone.utc)
    Signed-off-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- flytekit/remote/remote.py
+++ flytekit/remote/remote.py
@@ -18,7 +18,7 @@ import uuid
 from base64 import b64encode
 from collections import OrderedDict
 from dataclasses import asdict, dataclass
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import Dict
 
 import click
@@ -1661,9 +1661,9 @@ class FlyteRemote(object):
         :param sync_nodes: passed along to the sync call for the workflow execution
         """
         poll_interval = poll_interval or timedelta(seconds=30)
-        time_to_give_up = datetime.max if timeout is None else datetime.utcnow() + timeout
+        time_to_give_up = datetime.max if timeout is None else datetime.now(timezone.utc) + timeout
 
-        while datetime.utcnow() < time_to_give_up:
+        while datetime.now(timezone.utc) < time_to_give_up:
             execution = self.sync_execution(execution, sync_nodes=sync_nodes)
             if execution.is_done:
                 return execution
