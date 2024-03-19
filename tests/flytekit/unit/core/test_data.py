610e739ccebe9c263d8d17e534ce06805dc865f0
Future-Outlier <eric901201@gmail.com>
2024-03-19 11:56:42 +0800

    revert
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- tests/flytekit/unit/core/test_data.py
+++ tests/flytekit/unit/core/test_data.py
@@ -344,6 +344,7 @@ def test_crawl_local_non_nt(source_folder):
     res = fd.crawl()
     split = [(x, y) for x, y in res]
     files = [os.path.join(x, y) for x, y in split]
+    assert set(split) == {(source_folder, "original.txt"), (source_folder, os.path.join("nested", "more.txt"))}
     expected = {os.path.join(source_folder, "original.txt"), os.path.join(source_folder, "nested", "more.txt")}
     assert set(files) == expected
 
