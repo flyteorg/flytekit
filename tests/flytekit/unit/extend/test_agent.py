92a580c0156879317c4456f0597cacb8de2d0501
noahjax <noah.jackson@dominodatalab.com>
2024-03-18 19:25:59 -0700

    add additional field to agent create with task execution metadata
    Signed-off-by: noahjax <noah.jackson@dominodatalab.com>

--- tests/flytekit/unit/extend/test_agent.py
+++ tests/flytekit/unit/extend/test_agent.py
@@ -16,6 +16,7 @@ from flyteidl.admin.agent_pb2 import (
     ListAgentsResponse,
     TaskCategory,
 )
+from flyteidl.core.identifier_pb2 import ResourceType
 from flyteidl.core.execution_pb2 import TaskExecution, TaskLog
 
 from flytekit import PythonFunctionTask, task
@@ -37,8 +38,9 @@ from flytekit.extend.backend.base_agent import (
 )
 from flytekit.extend.backend.utils import convert_to_flyte_phase, get_agent_secret
 from flytekit.models import literals
+from flytekit.models.core.identifier import Identifier, NodeExecutionIdentifier, TaskExecutionIdentifier, WorkflowExecutionIdentifier
 from flytekit.models.literals import LiteralMap
-from flytekit.models.task import TaskTemplate
+from flytekit.models.task import TaskExecutionMetadata, TaskTemplate
 from flytekit.tools.translator import get_serializable
 
 dummy_id = "dummy_id"
@@ -48,6 +50,7 @@ dummy_id = "dummy_id"
 class DummyMetadata(ResourceMeta):
     job_id: str
     output_path: typing.Optional[str] = None
+    task_name: typing.Optional[str] = None
 
 
 class DummyAgent(AsyncAgentBase):
@@ -77,10 +80,12 @@ class AsyncDummyAgent(AsyncAgentBase):
         task_template: TaskTemplate,
         inputs: typing.Optional[LiteralMap] = None,
         output_prefix: typing.Optional[str] = None,
+        task_execution_metadata: typing.Optional[TaskExecutionMetadata] = None,
         **kwargs,
     ) -> DummyMetadata:
         output_path = f"{output_prefix}/{dummy_id}" if output_prefix else None
-        return DummyMetadata(job_id=dummy_id, output_path=output_path)
+        task_name = task_execution_metadata.task_execution_id.task_id.name if task_execution_metadata else "default"
+        return DummyMetadata(job_id=dummy_id, output_path=output_path, task_name=task_name)
 
     async def get(self, resource_meta: DummyMetadata, **kwargs) -> Resource:
         return Resource(phase=TaskExecution.SUCCEEDED, log_links=[TaskLog(name="console", uri="localhost:3000")])
@@ -136,6 +141,19 @@ task_inputs = literals.LiteralMap(
     },
 )
 
+task_execution_metadata = TaskExecutionMetadata(
+    task_execution_id=TaskExecutionIdentifier(
+        task_id=Identifier(ResourceType.TASK, "project", "domain", "name", "version"),
+        node_execution_id=NodeExecutionIdentifier("node_id", WorkflowExecutionIdentifier("project", "domain", "name")),
+        retry_attempt=1,
+    ),
+    namespace="namespace",
+    labels={"label_key": "label_val"},
+    annotations={"annotation_key": "annotation_val"},
+    k8s_service_account="k8s service account",
+    environment_variables={"env_var_key": "env_var_val"},
+)
+
 
 def test_dummy_agent():
     AgentRegistry.register(DummyAgent(), override=True)
@@ -170,11 +188,20 @@ async def test_async_agent_service(agent):
 
     inputs_proto = task_inputs.to_flyte_idl()
     output_prefix = "/tmp"
-    metadata_bytes = DummyMetadata(job_id=dummy_id, output_path=f"{output_prefix/{dummy_id}}").encode()
+    metadata_bytes = DummyMetadata(
+        job_id=dummy_id,
+        output_path=f"{output_prefix}/{dummy_id}",
+        task_name=task_execution_metadata.task_execution_id.task_id.name,
+    ).encode()
 
     tmp = get_task_template(agent.task_category.name).to_flyte_idl()
     task_category = TaskCategory(name=agent.task_category.name, version=0)
-    req = CreateTaskRequest(inputs=inputs_proto, output_prefix=output_prefix, template=tmp)
+    req = CreateTaskRequest(
+        inputs=inputs_proto,
+        template=tmp,
+        output_prefix=output_prefix,
+        task_execution_metadata=task_execution_metadata.to_flyte_idl(),
+    )
 
     res = await service.CreateTask(req, ctx)
     assert res.resource_meta == metadata_bytes
