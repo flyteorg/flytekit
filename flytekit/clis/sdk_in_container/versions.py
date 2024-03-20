a8a6edeebcbcab5080eaf6aa4e036bce27067080
novahow <b08902047@ntu.edu.tw>
2024-03-21 05:16:34 +0800

    remove unused params and add test
    Signed-off-by: novahow <b08902047@ntu.edu.tw>

--- flytekit/clis/sdk_in_container/versions.py
+++ flytekit/clis/sdk_in_container/versions.py
@@ -1,31 +1,47 @@
 import typing
+from dataclasses import dataclass
 
 import rich_click as click
 from click import Context, Parameter
 
-from flytekit.clis.sdk_in_container.run import DynamicEntityLaunchCommand, RemoteEntityGroup, RunCommand, RunLevelParams
+from flytekit.clis.sdk_in_container.run import DynamicEntityLaunchCommand, RemoteEntityGroup, RunBaseParams, RunCommand
 from flytekit.models.admin.common import Sort
 from flytekit.models.common import NamedEntityIdentifier
-from flytekit.remote import FlyteLaunchPlan, FlyteRemote, FlyteTask, FlyteWorkflow
+from flytekit.remote import FlyteLaunchPlan, FlyteRemote, FlyteTask
+
+
+@dataclass
+class VersionLevelParams(RunBaseParams):
+    """
+    This class is used to store the parameters for the version command.
+    """
+
+    pass
 
 
 class InstanceDisplayCommand(click.RichCommand):
+    """
+    Dummy command that displays the version of the entity.
+    """
+
     def __init__(self, name, h, **kwargs):
         super().__init__(name=name, help=h, **kwargs)
 
 
 class DynamicEntityVersionCommand(click.RichGroup, DynamicEntityLaunchCommand):
+    """
+    Command that retrieves the versions of a remote entity.
+    """
+
     def __init__(self, name: str, h: str, entity_name: str, launcher: str, **kwargs):
-        super(click.RichGroup, self).__init__(name, h, entity_name, launcher, **kwargs)
+        DynamicEntityLaunchCommand.__init__(self, name, h, entity_name, launcher, **kwargs)
 
     def get_params(self, ctx: Context) -> typing.List[Parameter]:
-        """
-        returns empty list to avoid parent adding task/workflow/launchplan params
-        """
-        return []
+        # we don't use super.get_params here, because DynamicEntityLaunchCommand.get_params adds the options of the entity
+        return click.RichGroup.get_params(self, ctx)
 
     def list_commands(self, ctx: click.Context):
-        run_params: RunLevelParams = ctx.obj
+        run_params: VersionLevelParams = ctx.obj
         named_entity = NamedEntityIdentifier(run_params.project, run_params.domain, ctx.info_name)
         _remote_instance: FlyteRemote = run_params.remote_instance()
         entity = self._fetch_entity(ctx)
@@ -37,36 +53,33 @@ class DynamicEntityVersionCommand(click.RichGroup, DynamicEntityLaunchCommand):
             sorted_entities, _ = _remote_instance.client.list_launch_plans_paginated(
                 named_entity, sort_by=Sort("created_at", Sort.Direction.DESCENDING)
             )
-        elif isinstance(entity, FlyteWorkflow):
-            sorted_entities, _ = _remote_instance.client.list_workflows_paginated(
-                named_entity, sort_by=Sort("created_at", Sort.Direction.DESCENDING)
-            )
         else:
             raise ValueError(f"Unknown entity type {type(entity)}")
 
-        self._entity_dict = {
-            _entity.id.version: _entity.closure.created_at.strftime("%Y-%m-%d %H:%M:%S") for _entity in sorted_entities
-        }
+        parse_creation_time = (
+            lambda x: x.closure.created_at.strftime("%Y-%m-%d %H:%M:%S")
+            if x.closure.created_at is not None
+            else "Unknown Time"
+        )
+        self._entity_dict = {_entity.id.version: parse_creation_time(_entity) for _entity in sorted_entities}
         return self._entity_dict.keys()
 
     def get_command(self, ctx, version):
-        """
-        returns version as command and created_at as help
-        """
         if ctx.obj is None:
             ctx.obj = {}
         return InstanceDisplayCommand(name=version, h=f"Created At {self._entity_dict[version]}")
 
+    def invoke(self, ctx: Context) -> typing.Any:
+        pass
+
 
 class RemoteEntityVersionGroup(RemoteEntityGroup):
     """
-    click multicommand that retrieves launchplans from a remote flyte instance and display version of them.
+    click multicommand that retrieves launchplans/tasks from a remote flyte instance and display version of them.
     """
 
-    def __init__(self, command_name: str):
-        super().__init__(
-            command_name,
-        )
+    def __init__(self, command_name: str, h: str):
+        super().__init__(command_name, h)
 
     def get_command(self, ctx: click.Context, name: str):
         if self._command_name in [self.LAUNCHPLAN_COMMAND, self.WORKFLOW_COMMAND]:
@@ -85,37 +98,40 @@ class RemoteEntityVersionGroup(RemoteEntityGroup):
 
 
 class VersionCommand(RunCommand):
-    _run_params: typing.Type[RunLevelParams] = RunLevelParams
+    _run_params: typing.Type[RunBaseParams] = VersionLevelParams
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self._files = []
 
-    def list_commands(self, ctx: click.Context, add_remote: bool = True):
-        self._files = sorted(self._files)
-        if add_remote:
-            self._files = self._files + [
-                RemoteEntityGroup.LAUNCHPLAN_COMMAND,
-                RemoteEntityGroup.WORKFLOW_COMMAND,
-                RemoteEntityGroup.TASK_COMMAND,
-            ]
+    def list_commands(self, ctx: click.Context):
+        self._files = self._files + [
+            RemoteEntityGroup.LAUNCHPLAN_COMMAND,
+            RemoteEntityGroup.WORKFLOW_COMMAND,
+            RemoteEntityGroup.TASK_COMMAND,
+        ]
         return self._files
 
     def get_command(self, ctx: click.Context, filename: str):
+        # call parent get_command to setup run_params
         super().get_command(ctx, filename)
+        entity_version_help = f"Show the versions of the specified {filename}."
         if filename == RemoteEntityGroup.LAUNCHPLAN_COMMAND:
-            return RemoteEntityVersionGroup(RemoteEntityGroup.LAUNCHPLAN_COMMAND)
+            return RemoteEntityVersionGroup(RemoteEntityGroup.LAUNCHPLAN_COMMAND, entity_version_help)
         elif filename == RemoteEntityGroup.WORKFLOW_COMMAND:
-            return RemoteEntityVersionGroup(RemoteEntityGroup.WORKFLOW_COMMAND)
+            return RemoteEntityVersionGroup(RemoteEntityGroup.WORKFLOW_COMMAND, entity_version_help)
         elif filename == RemoteEntityGroup.TASK_COMMAND:
-            return RemoteEntityVersionGroup(RemoteEntityGroup.TASK_COMMAND)
+            return RemoteEntityVersionGroup(RemoteEntityGroup.TASK_COMMAND, entity_version_help)
         else:
             raise NotImplementedError(f"File {filename} not found")
 
 
-_run_help = """
-Show the versions of the entity.
+_version_help = """
+Show the versions of the specified ``remote-task``, ``remote-launchplan``, or ``remote-workflow``.
+Usage resembles the ``pyflyte run`` command, but instead of running the task, launchplan, or workflow,
+it will display the versions of the remote entities and the time they were created.
 """
 version = VersionCommand(
     name="show-versions",
-    help=_run_help,
+    help=_version_help,
 )
