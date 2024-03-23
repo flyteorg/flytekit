c986886e4558c3b3718dd7fd43f5bc108ea4e857
novahow <b08902047@ntu.edu.tw>
2024-03-23 17:31:14 +0800

    add relaunch support
    Signed-off-by: novahow <b08902047@ntu.edu.tw>

--- flytekit/clis/sdk_in_container/executions.py
+++ flytekit/clis/sdk_in_container/executions.py
@@ -7,8 +7,27 @@ from flytekit.clis.sdk_in_container.utils import (
     project_option_dec,
 )
 from flytekit.interfaces import cli_identifiers
+from flytekit.remote import FlyteRemote
 
 EXECUTION_ID = "execution_id"
+
+
+@click.command("relaunch", help="Relaunch a failed execution")
+@click.pass_context
+def relaunch(ctx: click.Context):
+    """
+    Relaunches an execution.
+    """
+    project = ctx.obj[CTX_PROJECT]
+    domain = ctx.obj[CTX_DOMAIN]
+    remote: FlyteRemote = ctx.obj[FLYTE_REMOTE_INSTANCE_KEY]
+    execution_identifier = cli_identifiers.WorkflowExecutionIdentifier(
+        project=project, domain=domain, name=ctx.obj[EXECUTION_ID]
+    )
+    execution_identifier_resp = remote.client.relaunch_execution(id=execution_identifier)
+    execution_identifier = cli_identifiers.WorkflowExecutionIdentifier.promote_from_model(execution_identifier_resp)
+    click.secho("Launched execution: {}".format(execution_identifier), fg="blue")
+    click.echo("")
 
 
 @click.command("recover", help="Recover a failed execution")
@@ -19,7 +38,7 @@ def recover(ctx: click.Context):
     """
     project = ctx.obj[CTX_PROJECT]
     domain = ctx.obj[CTX_DOMAIN]
-    remote = ctx.obj[FLYTE_REMOTE_INSTANCE_KEY]
+    remote: FlyteRemote = ctx.obj[FLYTE_REMOTE_INSTANCE_KEY]
     execution_identifier = cli_identifiers.WorkflowExecutionIdentifier(
         project=project, domain=domain, name=ctx.obj[EXECUTION_ID]
     )
@@ -31,7 +50,7 @@ def recover(ctx: click.Context):
 
 execution_help = """
 The execution command allows you to interact with Flyte's execution system,
-such as recovering a failed execution.
+such as recovering/relaunching a failed execution.
 """
 
 
@@ -61,3 +80,4 @@ def execute(
 
 
 execute.add_command(recover)
+execute.add_command(relaunch)
--- tests/flytekit/unit/cli/pyflyte/test_executions.py
+++ tests/flytekit/unit/cli/pyflyte/test_executions.py
@@ -17,3 +17,17 @@ def test_execution_recover(remote):
     assert "Launched execution" in result.stdout
     assert result.exit_code == 0
     remote.return_value.client.recover_execution.assert_called_once_with(id=id)
+
+
+@mock.patch("flytekit.configuration.plugin.FlyteRemote")
+def test_execution_relaunch(remote):
+    runner = CliRunner()
+    id = cli_identifiers.WorkflowExecutionIdentifier(project="p1", domain="d1", name="test-execution-id")
+
+    result = runner.invoke(
+        pyflyte.main,
+        ["execution", "--execution-id", "test-execution-id", "--project", "p1", "--domain", "d1", "relaunch"],
+    )
+    assert "Launched execution" in result.stdout
+    assert result.exit_code == 0
+    remote.return_value.client.relaunch_execution.assert_called_once_with(id=id)
