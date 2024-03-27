eb294c971bd4bda9635dabf71229f2b3d74f01a2
Greg Gydush <35151789+ggydush@users.noreply.github.com>
2024-03-27 16:37:42 -0700

    feat: Support pep604 union operator (#2298)
    * feat: Support pep604 union operator
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: check union early
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * refactor: Change name to 604
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix lint
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: Fix duplicated code
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: Remove code for testing
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * test: Add simple tests
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * Add more tests
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: Fix names
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: Lint
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: Fix again
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: Fix default
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * test: Add test for parameter and defaults
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: Fix code coverage by ignoring
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * refactor: Use is_union_type
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix import sort
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: cleanup
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: fix
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * refactor: Clean it up
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: Fix lint
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: Fix pydantic plugin test failure
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * Update flytekit/core/type_engine.py
    
    Co-authored-by: Kevin Su <pingsutw@apache.org>
    
    * Address comment
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: Use UnionTransformer
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: Fix lint
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * Skip tests with | syntax on < 3.10
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: Fix test
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: More review comments
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    * fix: Fix lint
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    
    ---------
    
    Signed-off-by: ggydush <greggydush@gmail.com>
    Co-authored-by: Kevin Su <pingsutw@apache.org>
    Signed-off-by: Jan Fiedler <jan@union.ai>

--- flytekit/core/interface.py
+++ flytekit/core/interface.py
@@ -8,13 +8,13 @@ from collections import OrderedDict
 from typing import Any, Dict, Generator, List, Optional, Tuple, Type, TypeVar, Union, cast
 
 from flyteidl.core import artifact_id_pb2 as art_id
-from typing_extensions import get_args, get_origin, get_type_hints
+from typing_extensions import get_args, get_type_hints
 
 from flytekit.core import context_manager
 from flytekit.core.artifact import Artifact, ArtifactIDSpecification, ArtifactQuery
 from flytekit.core.docstring import Docstring
 from flytekit.core.sentinel import DYNAMIC_INPUT_BINDING
-from flytekit.core.type_engine import TypeEngine
+from flytekit.core.type_engine import TypeEngine, UnionTransformer
 from flytekit.exceptions.user import FlyteValidationException
 from flytekit.loggers import logger
 from flytekit.models import interface as _interface_models
@@ -218,7 +218,7 @@ def transform_inputs_to_parameters(
     inputs_with_def = interface.inputs_with_defaults
     for k, v in inputs_vars.items():
         val, _default = inputs_with_def[k]
-        if _default is None and get_origin(val) is typing.Union and type(None) in get_args(val):
+        if _default is None and UnionTransformer.is_optional_type(val):
             literal = Literal(scalar=Scalar(none_type=Void()))
             params[k] = _interface_models.Parameter(var=v, default=literal, required=False)
         else:
