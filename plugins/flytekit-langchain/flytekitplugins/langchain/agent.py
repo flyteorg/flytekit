615cb2d4423620ddc42be556775f4bbdc145e762
Future-Outlier <eric901201@gmail.com>
2024-03-27 20:41:13 +0800

    langchain input output success, function call error
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- plugins/flytekit-langchain/flytekitplugins/langchain/agent.py
+++ plugins/flytekit-langchain/flytekitplugins/langchain/agent.py
@@ -1,7 +1,7 @@
 import asyncio
 import typing
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import Optional, Any
 
 import cloudpickle
 import grpc
@@ -10,36 +10,19 @@ from flyteidl.admin.agent_pb2 import (
     CreateTaskResponse,
     DeleteTaskResponse,
     GetTaskResponse,
-    Resource,
 )
-from flyteidl.core.execution_pb2 import TaskExecution
-# from flytekitplugins.airflow.task import AirflowObj, _get_airflow_instance
+from flytekit.core.type_engine import TypeEngine
 
-from airflow.exceptions import AirflowException, TaskDeferred
-from airflow.models import BaseOperator
-from airflow.sensors.base import BaseSensorOperator
-from airflow.triggers.base import TriggerEvent
-from airflow.utils.context import Context
+from flyteidl.core.execution_pb2 import TaskExecution
+from flytekitplugins.langchain.task import _get_langchain_instance, LangChainTask
+from flytekit import FlyteContextManager, FlyteContext
 from flytekit import logger
 from flytekit.exceptions.user import FlyteUserException
-from flytekit.extend.backend.base_agent import AgentBase, AgentRegistry
+from flytekit.extend.backend.base_agent import AgentRegistry, Resource, SyncAgentBase
 from flytekit.models.literals import LiteralMap
 from flytekit.models.task import TaskTemplate
 
-
-@dataclass
-class ResourceMetadata:
-    """
-    This class is used to store the Airflow task configuration. It is serialized and returned to FlytePropeller.
-    """
-
-    # airflow_operator: AirflowObj
-    # airflow_trigger: AirflowObj = field(default=None)
-    # airflow_trigger_callback: str = field(default=None)
-    job_id: typing.Optional[str] = field(default=None)
-
-
-class LangChainAgent(AgentBase):
+class LangChainAgent(SyncAgentBase):
     """
     It is used to run Airflow tasks. It is registered as an agent in the AgentRegistry.
     There are three kinds of Airflow tasks: AirflowOperator, AirflowSensor, and AirflowHook.
@@ -63,86 +46,50 @@ class LangChainAgent(AgentBase):
     name = "LangChain Agent"
 
     def __init__(self):
-        super().__init__(task_type_name="langchain", asynchronous=True)
+        super().__init__(task_type_name="langchain")
 
-    async def async_create(
+    async def do(
         self,
-        context: grpc.ServicerContext,
-        output_prefix: str,
         task_template: TaskTemplate,
         inputs: Optional[LiteralMap] = None,
-    ) -> CreateTaskResponse:
-        return CreateTaskResponse(resource_meta=cloudpickle.dumps(ResourceMetadata()))
-        airflow_obj = jsonpickle.decode(task_template.custom["task_config_pkl"])
-        airflow_instance = _get_airflow_instance(airflow_obj)
-        resource_meta = ResourceMetadata(airflow_operator=airflow_obj)
+    ) -> Resource:
+        # print(task_template.interface.inputs)
+        # print(task_template.interface.outputs)
+        langchain_obj = jsonpickle.decode(task_template.custom["task_config_pkl"])
+        # langchain_openai.chat_models.base.ChatOpenAI
+        langchain_instance = _get_langchain_instance(langchain_obj)
+        print("v1 langchain_instance:", langchain_instance)
+        model = langchain_instance(**langchain_obj.parameters)
+        print("v2 langchain_instance:", model)
+        # print("langchain_instance.invoke:", model.invoke("hi"))
+        # print("langchain_obj:", langchain_obj)
+        # print("langchain_instance:", langchain_instance)
+        # print("inputs:", inputs)
+        # ctx = FlyteContextManager.current_context()
+        # input_python_value = TypeEngine.literal_map_to_kwargs(ctx, inputs, {"input": Any})
+        # print("input_python_value:", type(input_python_value))
+        # print("input_python_value:", input_python_value)
+        # message = input_python_value.get("input")
+        # print("message:", message)
+        # message = langchain_obj.invoke(message)
+        # outputs = {"output": message}
+        # custom = task_template.custom
+        # custom["chatgpt_config"]["messages"] = [{"role": "user", "content": message}]
+        # client = openai.AsyncOpenAI(
+        #     organization=custom["openai_organization"],
+        #     api_key=get_agent_secret(secret_key=OPENAI_API_KEY),
+        # )
 
-        if isinstance(airflow_instance, BaseOperator) and not isinstance(airflow_instance, BaseSensorOperator):
-            try:
-                resource_meta = ResourceMetadata(airflow_operator=airflow_obj)
-                airflow_instance.execute(context=Context())
-            except TaskDeferred as td:
-                parameters = td.trigger.__dict__.copy()
-                # Remove parameters that are in the base class
-                parameters.pop("task_instance", None)
-                parameters.pop("trigger_id", None)
+        # logger = logging.getLogger("httpx")
+        # logger.setLevel(logging.WARNING)
 
-                resource_meta.airflow_trigger = AirflowObj(
-                    module=td.trigger.__module__, name=td.trigger.__class__.__name__, parameters=parameters
-                )
-                resource_meta.airflow_trigger_callback = td.method_name
+        # completion = await asyncio.wait_for(client.chat.completions.create(**custom["chatgpt_config"]), TIMEOUT_SECONDS)
+        # message = completion.choices[0].message.content
+        # outputs = {"o0": message}
 
-        return CreateTaskResponse(resource_meta=cloudpickle.dumps(resource_meta))
 
-    async def async_get(self, context: grpc.ServicerContext, resource_meta: bytes) -> GetTaskResponse:
-        return GetTaskResponse(resource=Resource(phase=TaskExecution.SUCCEEDED))
-        meta = cloudpickle.loads(resource_meta)
-        airflow_operator_instance = _get_airflow_instance(meta.airflow_operator)
-        airflow_trigger_instance = _get_airflow_instance(meta.airflow_trigger) if meta.airflow_trigger else None
-        airflow_ctx = Context()
-        message = None
-        cur_phase = TaskExecution.RUNNING
-
-        if isinstance(airflow_operator_instance, BaseSensorOperator):
-            ok = airflow_operator_instance.poke(context=airflow_ctx)
-            cur_phase = TaskExecution.SUCCEEDED if ok else TaskExecution.RUNNING
-        elif isinstance(airflow_operator_instance, BaseOperator):
-            if airflow_trigger_instance:
-                try:
-                    # Airflow trigger returns immediately when
-                    # 1. Failed to get the task status
-                    # 2. Task succeeded or failed
-                    # succeeded or failed: returns a TriggerEvent with payload
-                    # running: runs forever, so set a default timeout (2 seconds) here.
-                    # failed to get the status: raises AirflowException
-                    event = await asyncio.wait_for(airflow_trigger_instance.run().__anext__(), 2)
-                    try:
-                        # Trigger callback will check the status of the task in the payload, and raise AirflowException if failed.
-                        trigger_callback = getattr(airflow_operator_instance, meta.airflow_trigger_callback)
-                        trigger_callback(context=airflow_ctx, event=typing.cast(TriggerEvent, event).payload)
-                        cur_phase = TaskExecution.SUCCEEDED
-                    except AirflowException as e:
-                        cur_phase = TaskExecution.FAILED
-                        message = e.__str__()
-                except asyncio.TimeoutError:
-                    logger.debug("No event received from airflow trigger")
-                except AirflowException as e:
-                    cur_phase = TaskExecution.FAILED
-                    message = e.__str__()
-            else:
-                # If there is no trigger, it means the operator is not deferrable. In this case, this operator will be
-                # executed in the creation step. Therefore, we can directly return SUCCEEDED here.
-                # For instance, SlackWebhookOperator is not deferrable. It sends a message to Slack in the creation step.
-                # If the message is sent successfully, agent will return SUCCEEDED here. Otherwise, it will raise an exception at creation step.
-                cur_phase = TaskExecution.SUCCEEDED
-
-        else:
-            raise FlyteUserException("Only sensor and operator are supported.")
-
-        return GetTaskResponse(resource=Resource(phase=cur_phase, message=message))
-
-    async def async_delete(self, context: grpc.ServicerContext, resource_meta: bytes) -> DeleteTaskResponse:
-        return DeleteTaskResponse()
+        # return Resource(phase=TaskExecution.SUCCEEDED,)
+        return Resource(phase=TaskExecution.SUCCEEDED, outputs={"o0": model})
 
 
 AgentRegistry.register(LangChainAgent())
--- plugins/flytekit-langchain/flytekitplugins/langchain/task.py
+++ plugins/flytekit-langchain/flytekitplugins/langchain/task.py
@@ -7,10 +7,11 @@ from typing import Any, Dict, Optional, Type
 import jsonpickle
 from langchain_core.runnables import Runnable
 
-from flytekit import FlyteContextManager
+from flytekit import FlyteContextManager, logger
 from flytekit.configuration import SerializationSettings
 from flytekit.core.base_task import PythonTask
 from flytekit.core.interface import Interface
+from flytekit.extend.backend.base_agent import SyncAgentExecutorMixin
 
 
 @dataclass
@@ -36,8 +37,7 @@ class LangChainObj(object):
     parameters: typing.Dict[str, Any]
 
 
-# class LangChainTaskResolver(AsyncAgentExecutorMixin, PythonTask):
-class LangChainTask(PythonTask[LangChainObj]):
+class LangChainTask(SyncAgentExecutorMixin, PythonTask[LangChainObj]):
     """
     This python task is used to wrap an LangChain task. It is used to run an LangChain task in Flyte agent.
     The langchain task module, name and parameters are stored in the task config. We run the LangChain task in the agent.
@@ -48,45 +48,34 @@ class LangChainTask(PythonTask[LangChainObj]):
     def __init__(
         self,
         name: str,
-        task_config: Optional[LangChainObj],
+        task_config: LangChainObj,
         inputs: Optional[Dict[str, Type]] = None,
         **kwargs,
     ):
-        inputs = collections.OrderedDict({"input": Any})
-        outputs = collections.OrderedDict({"output": Any})
+        inputs = {"input": Any}
+        outputs = {"o0": Any}
         super().__init__(
             name=name,
             task_config=task_config,
-            interface=Interface(inputs=inputs or {}, outputs=outputs or {}),
+            interface=Interface(inputs=inputs, outputs=outputs),
             task_type=self._TASK_TYPE,
             **kwargs,
         )
 
     def get_custom(self, settings: SerializationSettings) -> Dict[str, Any]:
-        # Use jsonpickle to serialize the Airflow task config since the return value should be json serializable.
+        # Use jsonpickle to serialize the LangChain task config since the return value should be json serializable.
         return {"task_config_pkl": jsonpickle.encode(self.task_config)}
-
-    def execute(self, **kwargs) -> Any:
-        print("name:", self.name)
-        print("task_config:", self.task_config)
-        print("kwargs:", kwargs)
-        print("hello world")
-        return 
-        # print("python task execute:", kwargs)
-        return super().execute(**kwargs)
 
 
 def _get_langchain_instance(
     langchain_obj: LangChainObj,
 ) -> Any:
-    # Set the GET_ORIGINAL_TASK attribute to True so that obj_def will return the original
-    # langchain task instead of the Flyte task.
-    ctx = FlyteContextManager.current_context()
-    ctx.user_space_params.builder().add_attr("GET_ORIGINAL_TASK", True).build()
-
+    print("langchain_obj:", langchain_obj)
     obj_module = importlib.import_module(name=langchain_obj.module)
     obj_def = getattr(obj_module, langchain_obj.name)
-
+    print("obj_def:", obj_def)
+    return obj_def
+    print("obj_def:", obj_def)
     return obj_def(**langchain_obj.parameters)
 
 
@@ -98,70 +87,16 @@ def _flyte_runnable(
     """
     This function is called by the Flyte task to create a new LangChain task.
     """
-    print(type(args), type(kwargs))
-    print("args:", args)
-    print("args len:", len(args))
-    print("kwargs:", kwargs)
-    print("kwargs len:", len(kwargs))
+    # print(type(args), type(kwargs))
+    # print("args:", args)
+    # print("args len:", len(args))
+    # print("kwargs:", kwargs)
+    # print("kwargs len:", len(kwargs))
 
     cls = args[0]
     task_id = kwargs.get("task_id", cls.__name__)
     config = LangChainObj(module=cls.__module__, name=cls.__name__, parameters=kwargs)
-    # print("task_id:", task_id)
-    # print("config", config)
     return LangChainTask(name=task_id, task_config=config)
 
 
 Runnable.__new__ = _flyte_runnable
-
-
-# def _flyte_operator(*args, **kwargs):
-#     """
-#     This function is called by the Airflow operator to create a new task. We intercept this call and return a Flyte
-#     task instead.
-#     """
-#     cls = args[0]
-#     try:
-#         if FlyteContextManager.current_context().user_space_params.get_original_task:
-#             # Return original task when running in the agent.
-#             return object.__new__(cls)
-#     except AssertionError:
-#         # This happens when the task is created in the dynamic workflow.
-#         # We don't need to return the original task in this case.
-#         logging.debug("failed to get the attribute GET_ORIGINAL_TASK from user space params")
-
-#     container_image = kwargs.pop("container_image", None)
-#     task_id = kwargs["task_id"] or cls.__name__
-#     config = LangChainObj(module=cls.__module__, name=cls.__name__, parameters=kwargs)
-
-#     return AirflowTask(name=task_id, task_config=config)()
-
-
-# def _flyte_xcom_push(*args, **kwargs):
-#     """
-#     This function is called by the Airflow operator to push data to XCom. We intercept this call and store the data
-#     in the Flyte context.
-#     """
-#     if len(args) < 2:
-#         return
-#     # Store the XCom data in the Flyte context.
-#     # args[0] is the operator instance.
-#     # args[1:] are the XCom data.
-#     # For example,
-#     # op.xcom_push(Context(), "key", "value")
-#     # args[0] is op, args[1:] is [Context(), "key", "value"]
-#     FlyteContextManager.current_context().user_space_params.xcom_data = args[1:]
-
-
-# params = FlyteContextManager.current_context().user_space_params
-# params.builder().add_attr("GET_ORIGINAL_TASK", False).add_attr("XCOM_DATA", {}).build()
-
-# We need this to monkey patch the LangChain Task
-
-# Monkey patch the Airflow operator. Instead of creating an airflow task, it returns a Flyte task.
-# airflow_models.BaseOperator.__new__ = _flyte_operator
-# airflow_models.BaseOperator.xcom_push = _flyte_xcom_push
-# # Monkey patch the xcom_push method to store the data in the Flyte context.
-# # Create a dummy DAG to avoid Airflow errors. This DAG is not used.
-# # TODO: Add support using Airflow DAG in Flyte workflow. We can probably convert the Airflow DAG to a Flyte subworkflow.
-# airflow_sensors.BaseSensorOperator.dag = airflow.DAG(dag_id="flyte_dag")
