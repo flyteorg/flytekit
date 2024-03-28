59ac75539082401331766bade67d4b6714798081
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2024-03-28 12:25:38 -0700

    Partition limit (#2301)
    Add a partition limit.
    
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>
    Signed-off-by: Jan Fiedler <jan@union.ai>

--- flytekit/core/artifact.py
+++ flytekit/core/artifact.py
@@ -15,6 +15,7 @@ from flytekit.core.sentinel import DYNAMIC_INPUT_BINDING
 from flytekit.loggers import logger
 
 TIME_PARTITION_KWARG = "time_partition"
+MAX_PARTITIONS = 10
 
 
 class InputsBase(object):
@@ -336,6 +337,9 @@ class Artifact(object):
             p = {k: Partition(None, name=k) for k in partition_keys}
             self._partitions = Partitions(p)
             self._partitions.set_reference_artifact(self)
+
+        if self.partition_keys and len(self.partition_keys) > MAX_PARTITIONS:
+            raise ValueError("There is a hard limit of 10 partition keys per artifact currently.")
 
     def __call__(self, *args, **kwargs) -> ArtifactIDSpecification:
         """
