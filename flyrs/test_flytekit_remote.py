3f8b882d7a85fdd7c585fd060335c6591f9db11e
Austin Liu <austin362667@gmail.com>
2024-04-05 00:58:39 +0800

    [wip]refactor: add regression test chart
    Signed-off-by: Austin Liu <austin362667@gmail.com>

--- flyrs/test_flytekit_remote.py
+++ flyrs/test_flytekit_remote.py
@@ -1,4 +1,5 @@
 import timeit
+import matplotlib.pyplot as plt
 
 from flytekit.configuration import Config
 from flytekit.remote import FlyteRemote
@@ -32,7 +33,20 @@ remote_rs = FlyteRemote(Config.auto(), enable_rs=True, default_project=PROJECT, 
 
 fetch_task_in_py = """task_py = remote_py.fetch_task(project=PROJECT, domain=DOMAIN, name="workflows_.say_1", version="WhIAnhpyjrAdaRvrQ9Cjpw")"""
 fetch_task_in_rs = """task_rs = remote_rs.fetch_task(project=PROJECT, domain=DOMAIN, name="workflows_.say_1", version="WhIAnhpyjrAdaRvrQ9Cjpw")"""
-# Python gRPC
-print(sum(timeit.repeat(fetch_task_in_py, setup=setup, repeat=10, number=100)))
-# Rust gRPC
-print(sum(timeit.repeat(fetch_task_in_rs, setup=setup, repeat=10, number=100)))
+
+r = 10
+Xs = [1, 10, 100, 1000]
+py_elpased, rs_elpased = [], []
+for x in Xs:
+    # Python gRPC
+    py_elpased.append(sum(timeit.repeat(fetch_task_in_py, setup=setup, repeat=r, number=x))/r)
+    print()
+    # Rust gRPC
+    rs_elpased.append(sum(timeit.repeat(fetch_task_in_rs, setup=setup, repeat=r, number=x))/r)
+    print()
+plt.xlabel('# of fetched tasks')
+plt.ylabel('average elapsed time (s)')
+plt.plot(Xs, py_elpased,'r-',label='Python gRPC')
+plt.plot(Xs, rs_elpased,'b-',label='Rust gRPC')
+plt.legend()
+plt.savefig("perf.png")
