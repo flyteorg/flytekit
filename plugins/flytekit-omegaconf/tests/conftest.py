0785c83a93a8931fcaf612adf28ffbf45c58325e
mg515 <miha.garafolj@gmail.com>
2024-04-09 11:06:51 +0200

    use default_factory instead of mutable default value
    Signed-off-by: mg515 <miha.garafolj@gmail.com>

--- plugins/flytekit-omegaconf/tests/conftest.py
+++ plugins/flytekit-omegaconf/tests/conftest.py
@@ -13,7 +13,7 @@ class ExampleConfig:
     union_key: t.Union[int, str] = 1337
     any_key: t.Any = "1337"
     optional_key: t.Optional[int] = 1337
-    dictconfig_key: ExampleNestedConfig = ExampleNestedConfig()
+    dictconfig_key: ExampleNestedConfig = field(default_factory=ExampleNestedConfig)
     optional_dictconfig_key: t.Optional[ExampleNestedConfig] = None
     listconfig_key: t.List[int] = field(default_factory=lambda: (1, 2, 3))
     tuple_int_key: t.Tuple[int, int] = field(default_factory=lambda: (1, 2))
@@ -22,4 +22,4 @@ class ExampleConfig:
 @dataclass
 class ExampleConfigWithNonAnnotatedSubtree:
     unnanotated_key = 1
-    annotated_key: ExampleNestedConfig = ExampleNestedConfig()
+    annotated_key: ExampleNestedConfig = field(default_factory=ExampleNestedConfig)
