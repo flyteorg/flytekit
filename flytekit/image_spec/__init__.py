1003620777b99ffa7930ef87756e943450ef3433
Nikki Everett <neverett@users.noreply.github.com>
2024-04-12 15:19:05 -0500

    add imagespec reference docs to flyte docs (#2347)
    Signed-off-by: nikki everett <nikki@union.ai>
    Signed-off-by: Jan Fiedler <jan@union.ai>

--- flytekit/image_spec/__init__.py
+++ flytekit/image_spec/__init__.py
@@ -1,1 +1,16 @@
+"""
+==========
+ImageSpec
+==========
+
+.. currentmodule:: flytekit.image_spec
+
+This module contains the ImageSpec class parameters and methods.
+
+.. autosummary::
+   :toctree: generated/
+
+   ImageSpec
+"""
+
 from .image_spec import ImageSpec
