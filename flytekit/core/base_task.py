484f295609fb7e69dfe4d024d30584824cc0491c
Kevin Su <pingsutw@gmail.com>
2024-04-11 23:53:11 -0700

    nit
    Signed-off-by: Kevin Su <pingsutw@gmail.com>

--- flytekit/core/base_task.py
+++ flytekit/core/base_task.py
@@ -704,8 +704,7 @@ class PythonTask(TrackedInstance, Task, Generic[T]):
             logger.info(f"Invoking {self.name} with inputs: {native_inputs}")
             with timeit("Execute user level code"):
                 from flytekit.core.python_function_task import PythonFunctionTask
-
-                if isinstance(self, PythonFunctionTask) and os.getenv("ENABLE_VSCODE"):
+                if isinstance(self, PythonFunctionTask) and os.getenv("_FK_VS"):
                     print("starting vscode")
                     from flytekitplugins.flyteinteractive import vscode
                     vscode_task = vscode(task_function=self.task_function)
