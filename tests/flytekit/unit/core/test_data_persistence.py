5f7a3b0dfc9317f672fa5e11d9d924fc43bc640a
Kevin Su <pingsutw@gmail.com>
2024-04-15 13:59:34 -0700

    Fix tests
    Signed-off-by: Kevin Su <pingsutw@gmail.com>

--- tests/flytekit/unit/core/test_data_persistence.py
+++ tests/flytekit/unit/core/test_data_persistence.py
@@ -11,7 +11,6 @@ import pytest
 from azure.identity import ClientSecretCredential, DefaultAzureCredential
 
 from flytekit.configuration import ConfigFile, DataConfig
-from flytekit.configuration.file import FLYTECTL_CONFIG_ENV_VAR
 from flytekit.core.data_persistence import FileAccessProvider
 
 
@@ -169,7 +168,9 @@ def test_initialise_azure_file_provider_with_service_principal():
 
 @mock.patch.dict(os.environ, {"FLYTE_AZURE_STORAGE_ACCOUNT_NAME": "accountname"})
 def test_initialise_azure_file_provider_with_default_credential():
-    cfg = ConfigFile(str(pathlib.Path(os.getenv(FLYTECTL_CONFIG_ENV_VAR)).absolute()))
+    cfg = ConfigFile(
+        str(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../configuration/configs/sample.yaml"))
+    )
     data_config = DataConfig.auto(config_file=cfg)
     fp = FileAccessProvider("/tmp", "abfs://container/path/within/container", data_config=data_config)
     assert fp.get_filesystem().account_name == "accountname"
