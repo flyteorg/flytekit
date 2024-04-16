a7df2f199e7575ec73515da63f6e56ac78d65848
Thomas J. Fan <thomasjpfan@gmail.com>
2024-04-16 08:10:17 -0700

    TST Adds test for no groups
    Signed-off-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- tests/flytekit/unit/models/core/test_security.py
+++ tests/flytekit/unit/models/core/test_security.py
@@ -1,5 +1,7 @@
 from unittest.mock import Mock
 
+import pytest
+
 import flytekit.configuration.plugin
 from flytekit.models.security import Secret
 
@@ -14,6 +16,17 @@ def test_secret():
     obj2 = Secret.from_flyte_idl(obj.to_flyte_idl())
     assert obj2.key is None
     assert obj2.group_version == "v1"
+
+
+def test_secret_error(monkeypatch):
+    # Mock configuration to require groups for this test
+    plugin_mock = Mock()
+    plugin_mock.secret_requires_group.return_value = True
+    mock_global_plugin = {"plugin": plugin_mock}
+    monkeypatch.setattr(flytekit.configuration.plugin, "_GLOBAL_CONFIG", mock_global_plugin)
+
+    with pytest.raises(ValueError, match="Group is a required parameter"):
+        Secret(key="my_key")
 
 
 def test_secret_no_group(monkeypatch):
