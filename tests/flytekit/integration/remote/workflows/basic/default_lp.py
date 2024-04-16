72b979eb302e8b72fd0a04123703e621c5400af0
Kevin Su <pingsutw@apache.com>
2024-04-16 14:47:45 -0700

    Add tests
    Signed-off-by: Kevin Su <pingsutw@apache.com>

---
+++ tests/flytekit/integration/remote/workflows/basic/default_lp.py
@@ -0,0 +1,16 @@
+from flytekit import task, workflow
+import datetime
+
+
+@task
+def print_datetime(time: datetime.datetime):
+    print(time)
+
+
+@workflow
+def my_wf(time: datetime.datetime = datetime.datetime.now()):
+    print_datetime(time=time)
+
+
+if __name__ == "__main__":
+    print(f"Running my_wf() {my_wf()}")
