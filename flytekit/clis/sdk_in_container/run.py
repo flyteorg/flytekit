f70b60bdeed2af5ce25d036a83d8df675b65298f
Chi-Sheng Liu <chishengliu@chishengliu.com>
2024-04-17 06:45:50 +0800

    feat(pyflyte): Remove the limit that pyflyte can only run files in current and nested directories (#2295)
    Signed-off-by: Chi-Sheng Liu <chishengliu@chishengliu.com>
    Signed-off-by: Jan Fiedler <jan@union.ai>

--- flytekit/clis/sdk_in_container/run.py
+++ flytekit/clis/sdk_in_container/run.py
@@ -330,9 +330,14 @@ def get_entities_in_file(filename: pathlib.Path, should_delete: bool) -> Entitie
     Returns a list of flyte workflow names and list of Flyte tasks in a file.
     """
     flyte_ctx = context_manager.FlyteContextManager.current_context().new_builder()
-    module_name = os.path.splitext(os.path.relpath(filename))[0].replace(os.path.sep, ".")
+    if filename.is_relative_to("."):
+        module_name = str(filename.relative_to(".").with_suffix("")).replace(os.path.sep, ".")
+        additional_path = str(pathlib.Path.cwd())
+    else:
+        module_name = str(filename.stem)
+        additional_path = str(filename.parent)
     with context_manager.FlyteContextManager.with_context(flyte_ctx):
-        with module_loader.add_sys_path(os.getcwd()):
+        with module_loader.add_sys_path(additional_path):
             importlib.import_module(module_name)
 
     workflows = []
@@ -810,11 +815,6 @@ class WorkflowCommand(click.RichGroup):
             is_workflow = exe_entity in self._entities.workflows
         if not os.path.exists(self._filename):
             raise ValueError(f"File {self._filename} does not exist")
-        rel_path = os.path.relpath(self._filename)
-        if rel_path.startswith(".."):
-            raise ValueError(
-                f"You must call pyflyte from the same or parent dir, {self._filename} not under {os.getcwd()}"
-            )
 
         project_root = _find_project_root(self._filename)
 
