ae2ab326f9f728a48f85f84fff7dfe2f8def3d0c
Samhita Alla <aallasamhita@gmail.com>
2024-04-25 12:48:18 +0530

    modify return type
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- flytekit/types/iterator/json_iterator.py
+++ flytekit/types/iterator/json_iterator.py
@@ -93,15 +93,7 @@ class JSONIteratorTransformer(TypeTransformer[Iterator[JSON]]):
             and literal_type.blob.dimensionality == _core_types.BlobType.BlobDimensionality.SINGLE
             and literal_type.blob.format == self.JSON_ITERATOR_FORMAT
         ):
-            return Union[
-                Type[Dict[str, "JSON"]],
-                Type[List["JSON"]],
-                Type[str],
-                Type[int],
-                Type[float],
-                Type[bool],
-                Type[None],
-            ]
+            return Union[Dict[str, JSON], List[JSON], str, int, float, bool, None]
 
         raise ValueError(f"Transformer {self} cannot reverse {literal_type}")
 
--- plugins/flytekit-openai/setup.py
+++ plugins/flytekit-openai/setup.py
@@ -27,6 +27,8 @@ setup(
         "Intended Audience :: Science/Research",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
