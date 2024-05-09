44dd1c70f43f9339ca4e69f59f853ea0d43e7ea1
Chi-Sheng Liu <chishengliu@chishengliu.com>
2024-05-09 19:05:39 +0800

    fix(import): Fix relative import path error (#2397)
    Signed-off-by: Chi-Sheng Liu <chishengliu@chishengliu.com>
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    Co-authored-by: Kevin Su <pingsutw@apache.org>
    Signed-off-by: Jan Fiedler <jan@union.ai>

--- flytekit/clis/sdk_in_container/run.py
+++ flytekit/clis/sdk_in_container/run.py
@@ -330,12 +330,11 @@ def get_entities_in_file(filename: pathlib.Path, should_delete: bool) -> Entitie
     Returns a list of flyte workflow names and list of Flyte tasks in a file.
     """
     flyte_ctx = context_manager.FlyteContextManager.current_context().new_builder()
-    if filename.is_relative_to("."):
-        module_name = str(filename.relative_to(".").with_suffix("")).replace(os.path.sep, ".")
+    if filename.is_relative_to(pathlib.Path.cwd()):
         additional_path = str(pathlib.Path.cwd())
     else:
-        module_name = str(filename.stem)
-        additional_path = str(filename.parent)
+        additional_path = _find_project_root(filename)
+    module_name = str(filename.relative_to(additional_path).with_suffix("")).replace(os.path.sep, ".")
     with context_manager.FlyteContextManager.with_context(flyte_ctx):
         with module_loader.add_sys_path(additional_path):
             importlib.import_module(module_name)
