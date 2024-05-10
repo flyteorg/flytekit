06752b334e6b5883251654e4b98e5c723b4202d7
Ketan Umare <kumare3@users.noreply.github.com>
2024-05-09 22:49:11 -0700

    Fixed merge conflict
    Signed-off-by: Ketan Umare <kumare3@users.noreply.github.com>

--- rust/flyrs/src/main.rs
+++ rust/flyrs/src/main.rs
@@ -1,16 +1,9 @@
 use anyhow::{bail, Result};
 use clap::Parser;
-<<<<<<< Updated upstream
 use tracing::info;
 use tokio;
 use tokio::runtime::Builder;
 use tracing_subscriber;
-=======
-use env_logger::Env;
-use log::info;
-use tokio;
-use tokio::runtime::Builder;
->>>>>>> Stashed changes
 
 mod executor;
 mod distribution;
