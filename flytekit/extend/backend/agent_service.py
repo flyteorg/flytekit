6f3489dafd44a9d322f9fcec47cd2e4f38b32432
ddl-ebrown <ethan.brown@dominodatalab.com>
2024-05-16 13:25:47 -0700

    Propagate custom_info Dict through agent Resource
     - The agent defines a Resource return type with values:
    
       * outputs
       * message
       * log_links
       * phase
    
       These are all a part of the underlying protobuf contract defined in
       flyteidl.
    
       However, the message field custom_info from the protobuf is not here
    
       google.protobuf.Struct custom_info
    
       https://github.com/flyteorg/flyte/blob/519080b6e4e53fc0e216b5715ad9b5b5270f35c0/flyteidl/protos/flyteidl/admin/agent.proto#L140
    
       This field was added in https://github.com/flyteorg/flyte/pull/4874
       but never made it into the corresponding flytekit PR
       https://github.com/flyteorg/flytekit/pull/2146
    
     - It's useful for agents to return additional metadata about the job,
       and it looks like custom_info is the intended location
    
     - Make a minor refactor to how the agent responds to requests that
       return Resource by implementing to_flyte_idl / from_flyte_idl
       directly
    
    Signed-off-by: ddl-ebrown <ethan.brown@dominodatalab.com>
    Signed-off-by: ddl-rliu <richard.liu@dominodatalab.com>

--- flytekit/extend/backend/agent_service.py
+++ flytekit/extend/backend/agent_service.py
@@ -16,7 +16,6 @@ from flyteidl.admin.agent_pb2 import (
     GetTaskResponse,
     ListAgentsRequest,
     ListAgentsResponse,
-    Resource,
 )
 from flyteidl.service.agent_pb2_grpc import (
     AgentMetadataServiceServicer,
@@ -25,8 +24,7 @@ from flyteidl.service.agent_pb2_grpc import (
 )
 from prometheus_client import Counter, Summary
 
-from flytekit import FlyteContext, logger
-from flytekit.core.type_engine import TypeEngine
+from flytekit import logger
 from flytekit.exceptions.system import FlyteAgentNotFound
 from flytekit.extend.backend.base_agent import AgentRegistry, SyncAgentBase, mirror_async_methods
 from flytekit.models.literals import LiteralMap
@@ -136,16 +134,7 @@ class AsyncAgentService(AsyncAgentServiceServicer):
         logger.info(f"{agent.name} start checking the status of the job")
         res = await mirror_async_methods(agent.get, resource_meta=agent.metadata_type.decode(request.resource_meta))
 
-        if res.outputs is None:
-            outputs = None
-        elif isinstance(res.outputs, LiteralMap):
-            outputs = res.outputs.to_flyte_idl()
-        else:
-            ctx = FlyteContext.current_context()
-            outputs = TypeEngine.dict_to_literal_map_pb(ctx, res.outputs)
-        return GetTaskResponse(
-            resource=Resource(phase=res.phase, log_links=res.log_links, message=res.message, outputs=outputs)
-        )
+        return GetTaskResponse(resource=res.to_flyte_idl())
 
     @record_agent_metrics
     async def DeleteTask(self, request: DeleteTaskRequest, context: grpc.ServicerContext) -> DeleteTaskResponse:
@@ -178,17 +167,7 @@ class SyncAgentService(SyncAgentServiceServicer):
                     agent.do, task_template=template, inputs=literal_map, output_prefix=output_prefix
                 )
 
-                if res.outputs is None:
-                    outputs = None
-                elif isinstance(res.outputs, LiteralMap):
-                    outputs = res.outputs.to_flyte_idl()
-                else:
-                    ctx = FlyteContext.current_context()
-                    outputs = TypeEngine.dict_to_literal_map_pb(ctx, res.outputs)
-
-                header = ExecuteTaskSyncResponseHeader(
-                    resource=Resource(phase=res.phase, log_links=res.log_links, message=res.message, outputs=outputs)
-                )
+                header = ExecuteTaskSyncResponseHeader(resource=res.to_flyte_idl())
                 yield ExecuteTaskSyncResponse(header=header)
             request_success_count.labels(task_type=task_type, operation=do_operation).inc()
         except Exception as e:
