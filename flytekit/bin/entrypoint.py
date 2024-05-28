05e91cdc1b72169be85c7760ac2ff4599a26d113
Kevin Su <pingsutw@apache.org>
2024-05-28 12:36:37 -0700

    test
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/bin/entrypoint.py
+++ flytekit/bin/entrypoint.py
@@ -374,9 +374,6 @@ def _execute_task(
         dynamic_addl_distro,
         dynamic_dest_dir,
     ) as ctx:
-        subprocess.run(["ls", "-l", "."])
-        print("===============")
-        subprocess.run(["ls", "-l", "/root"])
         resolver_obj = load_object_from_module(resolver)
         # Use the resolver to load the actual task object
         _task_def = resolver_obj.load_task(loader_args=resolver_args)
