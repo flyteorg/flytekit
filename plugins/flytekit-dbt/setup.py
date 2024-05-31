14595b310b67358edcec2f0217769fc1fcf26e48
Future-Outlier <eric901201@gmail.com>
2024-05-31 11:27:06 +0800

    fix requirements by dev-requirements.in to setup.py
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- plugins/flytekit-dbt/setup.py
+++ plugins/flytekit-dbt/setup.py
@@ -6,7 +6,7 @@ microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
 
 plugin_requires = [
     "flytekit>=1.3.0b2,<2.0.0",
-    "dbt-core>=1.0.0,!=1.8.1",
+    "dbt-core>=1.0.0,<1.4.6",
 ]
 
 __version__ = "0.0.0+develop"
