d3fefba84fa06a11b987a77ff3989652bbf9bc49
Future-Outlier <eric901201@gmail.com>
2024-06-01 11:53:10 +0800

    try mashumaro[msgpack]==3.13
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- plugins/flytekit-dbt/setup.py
+++ plugins/flytekit-dbt/setup.py
@@ -5,8 +5,8 @@ PLUGIN_NAME = "dbt"
 microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
 
 plugin_requires = [
-    "flytekit>=1.3.0b2,<=1.10.7",
-    "dbt-core>=1.0.0,<1.4.6",
+    "flytekit>=1.3.0b2,<2.0.0",
+    "dbt-core>=1.8.1",
 ]
 
 __version__ = "0.0.0+develop"
