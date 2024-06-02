fa04788f9bddd5fdf863fd82b764b94aa33342a6
Future-Outlier <eric901201@gmail.com>
2024-06-02 09:42:34 +0800

    remove dev-requirements.txt by pingsu's advice
    Signed-off-by: Future-Outlier <eric901201@gmail.com>
    Co-authored-by: pingsutw  <pingsutw@apache.org>

--- plugins/flytekit-dbt/setup.py
+++ plugins/flytekit-dbt/setup.py
@@ -6,8 +6,8 @@ microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
 
 plugin_requires = [
     "flytekit>=1.3.0b2,<2.0.0",
-    "dbt-core==1.4.5",
-    "dbt-sqlite==1.4.0",
+    "dbt-core<=1.4.5",
+    "dbt-sqlite<=1.4.0",
     "dbt-semantic-interfaces<0.5.0",
 ]
 
