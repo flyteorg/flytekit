585f211917c68b61e998287e6efbf6225b290afc
Kevin Su <pingsutw@apache.org>
2024-06-02 20:04:15 -0700

    test
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- plugins/flytekit-dbt/setup.py
+++ plugins/flytekit-dbt/setup.py
@@ -5,7 +5,7 @@ PLUGIN_NAME = "dbt"
 microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
 
 plugin_requires = [
-    "flytekit>=1.3.0b2,<2.0.0",
+    "flytekit>=1.3.0b2",
     "dbt-core<1.8.0",
 ]
 
@@ -33,6 +33,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Software Development",
