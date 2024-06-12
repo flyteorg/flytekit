d79ad1faf859dc6ab6ec7a379ae030af6a953a13
Kevin Su <pingsutw@apache.org>
2024-06-12 12:53:50 -0700

    test
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/image_spec/image_spec.py
+++ flytekit/image_spec/image_spec.py
@@ -120,7 +120,7 @@ class ImageSpec:
             return True
         except APIError as e:
             if e.response.status_code == 404:
-                return False
+                return True
             return True
         except ImageNotFound:
             return True
