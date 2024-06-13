47bd9e7e38b55b0d2cbe9e34fab141bd1ad4e639
ggydush <greggydush@gmail.com>
2024-06-13 15:55:53 -0700

    fix: Fix to still handle uploads
    Signed-off-by: ggydush <greggydush@gmail.com>

--- flytekit/types/file/file.py
+++ flytekit/types/file/file.py
@@ -435,11 +435,12 @@ class FlyteFilePathTransformer(TypeTransformer[FlyteFile]):
                     f" specified. Since a simpler type was specified, we'll skip uploading!"
                 )
                 should_upload = False
-            if ctx.execution_state.is_local_execution and python_val.remote_path is None:
-                should_upload = False
 
             # Set the remote destination if one was given instead of triggering a random one below
             remote_path = python_val.remote_path or None
+
+            if ctx.execution_state.is_local_execution and python_val.remote_path is None:
+                should_upload = False
 
         elif isinstance(python_val, pathlib.Path) or isinstance(python_val, str):
             source_path = str(python_val)
