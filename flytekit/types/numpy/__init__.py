c65baba40bfc4537622ac26f1949a5425fb32225
Niels Bantilan <niels.bantilan@gmail.com>
2024-06-17 12:31:24 -0400

    remove numpy dep, conditionally register numpy type transformer
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- flytekit/types/numpy/__init__.py
+++ flytekit/types/numpy/__init__.py
@@ -1,1 +1,17 @@
+from flytekit.loggers import logger
+
 from .ndarray import NumpyArrayTransformer
+
+try:
+    # isolate the exception to the numpy import
+    import numpy
+
+    _numpy_installed = True
+except ImportError:
+    _numpy_installed = False
+
+
+if _numpy_installed:
+    from .ndarray import NumpyArrayTransformer
+else:
+    logger.info("We won't register NumpyArrayTransformer because numpy is not installed.")
