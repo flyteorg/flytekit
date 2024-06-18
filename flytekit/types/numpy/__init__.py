d9705e6fcea1ebad2932bfbc62e3df31bf3507a6
Niels Bantilan <niels.bantilan@gmail.com>
2024-06-18 09:31:18 -0400

    numpy version pin on sqlalchemy
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- flytekit/types/numpy/__init__.py
+++ flytekit/types/numpy/__init__.py
@@ -1,1 +1,16 @@
-from .ndarray import NumpyArrayTransformer
+from flytekit.loggers import logger
+
+
+try:
+    # isolate the exception to the numpy import
+    import numpy
+
+    _numpy_installed = True
+except ImportError:
+    _numpy_installed = False
+
+
+if _numpy_installed:
+    from .ndarray import NumpyArrayTransformer
+else:
+    logger.info("We won't register NumpyArrayTransformer because numpy is not installed.")
