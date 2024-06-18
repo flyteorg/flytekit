8c00681df0a9c976876d4d4ad45b50b96dd7aa25
Niels Bantilan <niels.bantilan@gmail.com>
2024-06-18 08:52:54 -0400

    debug
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- flytekit/types/numpy/__init__.py
+++ flytekit/types/numpy/__init__.py
@@ -1,15 +1,1 @@
-from flytekit.loggers import logger
-
-try:
-    # isolate the exception to the numpy import
-    import numpy
-
-    _numpy_installed = True
-except ImportError:
-    _numpy_installed = False
-
-
-if _numpy_installed:
-    from .ndarray import NumpyArrayTransformer
-else:
-    logger.info("We won't register NumpyArrayTransformer because numpy is not installed.")
+from .ndarray import NumpyArrayTransformer
