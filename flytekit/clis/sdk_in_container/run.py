e1b8a64b3634fd568dad6a381adc1eed14ecc527
Ketan Umare <kumare3@users.noreply.github.com>
2024-06-23 18:01:31 -0700

    Run flytekit tasks on remote with local default values passed correctly
    ```python
    @task
    def default_inputs(i: int = 0, f: float = 10.0, e: Color = Color.RED, b: bool = True, j: typing.Optional[int] = None):
      print(i, f, e, b, j)
    ```
    
    Running this on remote will now work correctly
    
    ```bash
    pyflyte run --remote exhaustive.py default_inputs
    ```
    
    Signed-off-by: Ketan Umare <kumare3@users.noreply.github.com>

--- flytekit/clis/sdk_in_container/run.py
+++ flytekit/clis/sdk_in_container/run.py
@@ -509,6 +509,13 @@ def _update_flyte_context(params: RunLevelParams) -> FlyteContext.Builder:
         return ctx_builder.with_file_access(file_access)
 
 
+def is_optional(_type):
+    """
+    Checks if the given type is Optional Type
+    """
+    return typing.get_origin(_type) is typing.Union and type(None) in typing.get_args(_type)
+
+
 def run_command(ctx: click.Context, entity: typing.Union[PythonFunctionWorkflow, PythonTask]):
     """
     Returns a function that is used to implement WorkflowCommand and execute a flyte workflow.
@@ -526,8 +533,13 @@ def run_command(ctx: click.Context, entity: typing.Union[PythonFunctionWorkflow,
         click.secho(f"Running Execution on {'Remote' if run_level_params.is_remote else 'local'}.", fg="cyan")
         try:
             inputs = {}
-            for input_name, _ in entity.python_interface.inputs.items():
+            for input_name, v in entity.python_interface.inputs_with_defaults.items():
                 processed_click_value = kwargs.get(input_name)
+                optional_v = False
+                if processed_click_value is None and isinstance(v, typing.Tuple):
+                    optional_v = is_optional(v[0])
+                    if len(v) == 2:
+                        processed_click_value = v[1]
                 if isinstance(processed_click_value, ArtifactQuery):
                     if run_level_params.is_remote:
                         click.secho(
@@ -542,7 +554,7 @@ def run_command(ctx: click.Context, entity: typing.Union[PythonFunctionWorkflow,
                         raise click.UsageError(
                             f"Default for '{input_name}' is a query, which must be specified when running locally."
                         )
-                if processed_click_value is not None:
+                if processed_click_value is not None or optional_v:
                     inputs[input_name] = processed_click_value
 
             if not run_level_params.is_remote:
@@ -780,7 +792,7 @@ class WorkflowCommand(click.RichGroup):
         ctx: click.Context,
         entity_name: str,
         run_level_params: RunLevelParams,
-        loaded_entity: typing.Any,
+        loaded_entity: [PythonTask, WorkflowBase],
         is_workflow: bool,
     ):
         """
