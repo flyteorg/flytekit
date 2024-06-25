45f8df064e4994f8ea44cfed5d683545981c4302
Kevin Su <pingsutw@apache.org>
2024-06-26 01:03:31 +0800

    Fix flaky test_initialise_azure_file_provider_with_default_credential (#2530)
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    Signed-off-by: bugra.gedik <bugra.gedik@predera.ai>

--- tests/flytekit/unit/core/test_data_persistence.py
+++ tests/flytekit/unit/core/test_data_persistence.py
@@ -165,8 +165,14 @@ def test_initialise_azure_file_provider_with_service_principal():
         assert fp.get_filesystem().tenant_id == "tenantid"
 
 
-@mock.patch.dict(os.environ, {"FLYTE_AZURE_STORAGE_ACCOUNT_NAME": "accountname", "AZURE_STORAGE_ANON": "false"})
 def test_initialise_azure_file_provider_with_default_credential():
-    fp = FileAccessProvider("/tmp", "abfs://container/path/within/container")
-    assert fp.get_filesystem().account_name == "accountname"
-    assert isinstance(fp.get_filesystem().sync_credential, DefaultAzureCredential)
+    with mock.patch.dict(
+            os.environ,
+            {
+                "FLYTE_AZURE_STORAGE_ACCOUNT_NAME": "accountname",
+                "AZURE_STORAGE_ANON": "false",
+            },
+    ):
+        fp = FileAccessProvider("/tmp", "abfs://container/path/within/container")
+        assert fp.get_filesystem().account_name == "accountname"
+        assert isinstance(fp.get_filesystem().sync_credential, DefaultAzureCredential)
