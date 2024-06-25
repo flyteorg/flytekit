2e13ad235767c352ec1f0662624c477f2c8d2456
Kevin Su <pingsutw@apache.org>
2024-06-24 17:51:13 -0700

    update
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/core/python_auto_container.py
+++ flytekit/core/python_auto_container.py
@@ -87,6 +87,19 @@ class PythonAutoContainerTask(PythonTask[T], ABC, metaclass=FlyteTrackedABC):
         kwargs["metadata"] = kwargs["metadata"] if "metadata" in kwargs else TaskMetadata()
         kwargs["metadata"].pod_template_name = pod_template_name
 
+        self._container_image = container_image
+        # TODO(katrogan): Implement resource overrides
+        self._resources = ResourceSpec(
+            requests=requests if requests else Resources(), limits=limits if limits else Resources()
+        )
+
+        # Call super().__init__ after setting _container_image because PythonAutoContainerTask
+        # is added to the FlyteEntities in super().__init__. The translator will iterate over
+        # FlyteEntities and call container_image.
+        # Therefore, we need to ensure the _container_image attribute is set
+        # before appending the task to FlyteEntities.
+        # https://github.com/flyteorg/flytekit/blob/876877abd8d6f4f54dec2738a0ca07a12e9115b1/flytekit/tools/translator.py#L181
+
         super().__init__(
             task_type=task_type,
             name=name,
@@ -94,12 +107,6 @@ class PythonAutoContainerTask(PythonTask[T], ABC, metaclass=FlyteTrackedABC):
             security_ctx=sec_ctx,
             environment=environment,
             **kwargs,
-        )
-
-        self._container_image = container_image
-        # TODO(katrogan): Implement resource overrides
-        self._resources = ResourceSpec(
-            requests=requests if requests else Resources(), limits=limits if limits else Resources()
         )
 
         compilation_state = FlyteContextManager.current_context().compilation_state
