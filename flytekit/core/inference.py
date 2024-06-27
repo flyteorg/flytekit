56d53f7b042a725767cb112d12c0d6ea22d284b4
Samhita Alla <aallasamhita@gmail.com>
2024-06-27 14:33:21 +0530

    update secrets interface
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- flytekit/core/inference.py
+++ flytekit/core/inference.py
@@ -15,9 +15,27 @@ This module includes inference subclasses that extend the `ModelInferenceTemplat
    NIM
 """
 
+from dataclasses import dataclass
 from typing import Optional
 
 from .utils import ModelInferenceTemplate
+
+
+@dataclass
+class NIMSecrets:
+    """
+    :param ngc_image_secret: The name of the Kubernetes secret containing the NGC image pull credentials.
+    :param ngc_secret_group: The group name for the NGC API key.
+    :param ngc_secret_key: The key name for the NGC API key.
+    :param hf_token_group: The group name for the HuggingFace token.
+    :param hf_token_key: The key name for the HuggingFace token.
+    """
+
+    ngc_image_secret: str  # kubernetes secret
+    ngc_secret_key: str
+    ngc_secret_group: Optional[str] = None
+    hf_token_group: Optional[str] = None
+    hf_token_key: Optional[str] = None
 
 
 class NIM(ModelInferenceTemplate):
@@ -30,14 +48,10 @@ class NIM(ModelInferenceTemplate):
         gpu: int = 1,
         mem: str = "20Gi",
         shm_size: str = "16Gi",
-        # kubernetes secrets
-        ngc_image_secret: Optional[str] = None,
-        ngc_secret_key: Optional[str] = None,
-        ####################
         env: Optional[dict[str, str]] = None,
         hf_repo_ids: Optional[list[str]] = None,
-        hf_token_key: Optional[str] = None,
         lora_adapter_mem: Optional[str] = None,
+        secrets: Optional[NIMSecrets] = None,
     ):
         """
         Initialize NIM class for managing a Kubernetes pod template.
@@ -49,24 +63,20 @@ class NIM(ModelInferenceTemplate):
         :param gpu: The number of GPU cores requested for the model server container. Default is 1.
         :param mem: The amount of memory requested for the model server container. Default is "20Gi".
         :param shm_size: The size of the shared memory volume. Default is "16Gi".
-        :param ngc_image_secret: The name of the Kubernetes secret containing the NGC image pull credentials.
-        :param ngc_secret_key: The key name for the NGC API key.
         :param env: A dictionary of environment variables to be set in the model server container.
         :param hf_repo_ids: A list of Hugging Face repository IDs for LoRA adapters to be downloaded.
-        :param hf_token_key: The key name for the HuggingFace token.
         :param lora_adapter_mem: The amount of memory requested for the init container that downloads LoRA adapters.
+        :param secrets: Instance of NIMSecrets for managing secrets.
         """
-        if ngc_image_secret is None:
-            raise ValueError("NGC image pull credentials must be provided.")
-        if ngc_secret_key is None:
+        if secrets.ngc_image_secret is None:
+            raise ValueError("NGC image pull secret must be provided.")
+        if secrets.ngc_secret_key is None:
             raise ValueError("NGC secret key must be provided.")
 
         self._shm_size = shm_size
-        self._ngc_image_secret = ngc_image_secret
-        self._ngc_secret_key = ngc_secret_key
         self._hf_repo_ids = hf_repo_ids
-        self._hf_token_key = hf_token_key
         self._lora_adapter_mem = lora_adapter_mem
+        self._secrets = secrets
 
         super().__init__(
             image=image,
@@ -78,9 +88,9 @@ class NIM(ModelInferenceTemplate):
             env=env,
         )
 
-        self.nim_pod_template()
+        self.setup_nim_pod_template()
 
-    def nim_pod_template(self):
+    def setup_nim_pod_template(self):
         from kubernetes.client.models import (
             V1Container,
             V1EmptyDirVolumeSource,
@@ -98,24 +108,21 @@ class NIM(ModelInferenceTemplate):
                 empty_dir=V1EmptyDirVolumeSource(medium="Memory", size_limit=self._shm_size),
             )
         ]
-        self.pod_template.pod_spec.image_pull_secrets = [V1LocalObjectReference(name=self._ngc_image_secret)]
+        self.pod_template.pod_spec.image_pull_secrets = [V1LocalObjectReference(name=self._secrets.ngc_image_secret)]
 
         model_server_container = self.pod_template.pod_spec.init_containers[0]
 
-        if model_server_container.env:
-            model_server_container.env.append(
-                V1EnvVar(
-                    name="NGC_API_KEY",
-                    value=f"$(_UNION_{self._ngc_secret_key.upper()})",
-                )
+        if self._secrets.ngc_secret_group:
+            ngc_api_key = (
+                f"$($(FLYTE_SECRETS_ENV_PREFIX){self._secrets.ngc_secret_group}_{self._secrets.ngc_secret_key})".upper()
             )
         else:
-            model_server_container.env = [
-                V1EnvVar(
-                    name="NGC_API_KEY",
-                    value=f"$(_UNION_{self._ngc_secret_key.upper()})",
-                )
-            ]
+            ngc_api_key = f"$($(FLYTE_SECRETS_ENV_PREFIX){self._secrets.ngc_secret_key})".upper()
+
+        if model_server_container.env:
+            model_server_container.env.append(V1EnvVar(name="NGC_API_KEY", value=ngc_api_key))
+        else:
+            model_server_container.env = [V1EnvVar(name="NGC_API_KEY", value=ngc_api_key)]
 
         model_server_container.volume_mounts = [V1VolumeMount(name="dshm", mount_path="/dev/shm")]
         model_server_container.security_context = V1SecurityContext(run_as_user=1000)
@@ -125,8 +132,12 @@ class NIM(ModelInferenceTemplate):
             if not self._lora_adapter_mem:
                 raise ValueError("Memory to allocate to download LoRA adapters must be set.")
 
-            if not self._hf_token_key:
-                self._hf_token_key = ""
+            if self._secrets.hf_token_group:
+                hf_key = f"{self._secrets.hf_token_group}_{self._secrets.hf_token_key}".upper()
+            elif self._secrets.hf_token_key:
+                hf_key = self._secrets.hf_token_key.upper()
+            else:
+                hf_key = ""
 
             local_peft_dir_env = next(
                 (env for env in model_server_container.env if env.name == "NIM_PEFT_SOURCE"),
@@ -154,9 +165,12 @@ class NIM(ModelInferenceTemplate):
             export LOCAL_PEFT_DIRECTORY={mount_path}
             mkdir -p $LOCAL_PEFT_DIRECTORY
 
+            PREFIX=$(printenv FLYTE_SECRETS_ENV_PREFIX)
+            TOKEN_VAR_NAME="${{PREFIX}}{hf_key}"
+
             # Check if HF token is provided and login if so
-            if [ -n "$_UNION_{self._hf_token_key.upper()}" ]; then
-                huggingface-cli login --token "$_UNION_{self._hf_token_key.upper()}"
+            if [ -n "$(printenv $TOKEN_VAR_NAME)" ]; then
+                huggingface-cli login --token "$(printenv $TOKEN_VAR_NAME)"
             fi
 
             # Download LoRAs from Huggingface Hub
