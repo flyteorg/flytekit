845e5c926ea6c637f8c51b9e4d0ebe2323681d93
Future-Outlier <eric901201@gmail.com>
2024-07-01 15:11:10 -0700

    fix bug
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- flytekit/types/file/file.py
+++ flytekit/types/file/file.py
@@ -440,7 +440,7 @@ class FlyteFilePathTransformer(TypeTransformer[FlyteFile]):
             # Set the remote destination if one was given instead of triggering a random one below
             remote_path = python_val.remote_path or None
 
-            if ctx.execution_state.is_local_execution and python_val.remote_path is None:
+            if ctx.execution_state.is_local_execution() and python_val.remote_path is None:
                 should_upload = False
 
         elif isinstance(python_val, pathlib.Path) or isinstance(python_val, str):
@@ -458,7 +458,7 @@ class FlyteFilePathTransformer(TypeTransformer[FlyteFile]):
                         p = pathlib.Path(python_val)
                         if not p.is_file():
                             raise TypeTransformerFailedError(f"Error converting {python_val} because it's not a file.")
-                        if ctx.execution_state.is_local_execution:
+                        if ctx.execution_state.is_local_execution():
                             should_upload = False
             # python_type must be os.PathLike - see check at beginning of function
             else:
