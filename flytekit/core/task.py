6e890f155d4569805930b6f4f496ec7999664615
novahow <58504997+novahow@users.noreply.github.com>
2024-07-02 10:00:10 +0800

    Core/enable deck (#2314)
    * add additional_decks support
    
    Signed-off-by: novahow <b08902047@ntu.edu.tw>
    
    	modified:   flytekit/core/base_task.py
    	modified:   flytekit/core/python_function_task.py
    	modified:   flytekit/core/task.py
    	modified:   flytekit/deck/deck.py
    	modified:   tests/flytekit/unit/core/test_flyte_file.py
    
    * add tests and remove confusing fields
    
    Signed-off-by: novahow <b08902047@ntu.edu.tw>
    
    	modified:   flytekit/core/base_task.py
    	modified:   flytekit/deck/deck.py
    	modified:   tests/flytekit/unit/deck/test_deck.py
    
    * add deckselector
    
    Signed-off-by: novahow <b08902047@ntu.edu.tw>
    
    	modified:   flytekit/core/base_task.py
    	modified:   flytekit/core/context_manager.py
    	modified:   flytekit/core/task.py
    	modified:   flytekit/deck/deck.py
    	modified:   tests/flytekit/unit/deck/test_deck.py
    
    * make deck_selector to tuple
    
    Signed-off-by: novahow <b08902047@ntu.edu.tw>
    
    * fix remote deck bug
    
    Signed-off-by: novahow <b08902047@ntu.edu.tw>
    
    * fix timelinedeck and remove rendered_deck param
    
    Signed-off-by: novahow <b08902047@ntu.edu.tw>
    
    * fix UI
    
    Signed-off-by: novahow <b08902047@ntu.edu.tw>
    
    * fix timelinedeck test multiple time_info
    
    Signed-off-by: novahow <b08902047@ntu.edu.tw>
    
    * nit
    
    Signed-off-by: novahow <b08902047@ntu.edu.tw>
    
    * nit with enum
    
    Signed-off-by: novahow <b08902047@ntu.edu.tw>
    
    * nit deck_fields
    
    Signed-off-by: novahow <b08902047@ntu.edu.tw>
    
    * enable all decks, remove plotly dep
    
    Signed-off-by: novahow <b08902047@ntu.edu.tw>
    
    * kevin's update
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    
    * nit
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    
    * remove chart
    
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    
    ---------
    
    Signed-off-by: novahow <b08902047@ntu.edu.tw>
    Signed-off-by: Kevin Su <pingsutw@apache.org>
    Co-authored-by: Kevin Su <pingsutw@apache.org>
    Signed-off-by: Jan Fiedler <jan@union.ai>

--- flytekit/core/task.py
+++ flytekit/core/task.py
@@ -12,6 +12,7 @@ from flytekit.core.pod_template import PodTemplate
 from flytekit.core.python_function_task import PythonFunctionTask
 from flytekit.core.reference_entity import ReferenceEntity, TaskReference
 from flytekit.core.resources import Resources
+from flytekit.deck import DeckField
 from flytekit.extras.accelerators import BaseAccelerator
 from flytekit.image_spec.image_spec import ImageSpec
 from flytekit.models.documentation import Documentation
@@ -114,6 +115,7 @@ def task(
     docs: Optional[Documentation] = ...,
     disable_deck: Optional[bool] = ...,
     enable_deck: Optional[bool] = ...,
+    deck_fields: Optional[Tuple[DeckField, ...]] = ...,
     pod_template: Optional["PodTemplate"] = ...,
     pod_template_name: Optional[str] = ...,
     accelerator: Optional[BaseAccelerator] = ...,
@@ -151,6 +153,7 @@ def task(
     docs: Optional[Documentation] = ...,
     disable_deck: Optional[bool] = ...,
     enable_deck: Optional[bool] = ...,
+    deck_fields: Optional[Tuple[DeckField, ...]] = ...,
     pod_template: Optional["PodTemplate"] = ...,
     pod_template_name: Optional[str] = ...,
     accelerator: Optional[BaseAccelerator] = ...,
@@ -187,6 +190,13 @@ def task(
     docs: Optional[Documentation] = None,
     disable_deck: Optional[bool] = None,
     enable_deck: Optional[bool] = None,
+    deck_fields: Optional[Tuple[DeckField, ...]] = (
+        DeckField.SOURCE_CODE,
+        DeckField.DEPENDENCIES,
+        DeckField.TIMELINE,
+        DeckField.INPUT,
+        DeckField.OUTPUT,
+    ),
     pod_template: Optional["PodTemplate"] = None,
     pod_template_name: Optional[str] = None,
     accelerator: Optional[BaseAccelerator] = None,
@@ -307,6 +317,7 @@ def task(
     :param task_resolver: Provide a custom task resolver.
     :param disable_deck: (deprecated) If true, this task will not output deck html file
     :param enable_deck: If true, this task will output deck html file
+    :param deck_fields: If specified and enble_deck is True, this task will output deck html file with the fields specified in the tuple
     :param docs: Documentation about this task
     :param pod_template: Custom PodTemplate for this task.
     :param pod_template_name: The name of the existing PodTemplate resource which will be used in this task.
@@ -339,6 +350,7 @@ def task(
             task_resolver=task_resolver,
             disable_deck=disable_deck,
             enable_deck=enable_deck,
+            deck_fields=deck_fields,
             docs=docs,
             pod_template=pod_template,
             pod_template_name=pod_template_name,
