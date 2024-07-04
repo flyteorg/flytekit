2427a132df810e3a4229411b4e8bb8f2c51c9f97
Mecoli1219 <michaellai901026@gmail.com>
2024-07-04 12:29:33 +0800

    Update plugins/flytekit-polars/setup.py
    Signed-off-by: Mecoli1219 <michaellai901026@gmail.com>

--- plugins/flytekit-polars/setup.py
+++ plugins/flytekit-polars/setup.py
@@ -4,7 +4,7 @@ PLUGIN_NAME = "polars"
 
 microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
 
-plugin_requires = ["flytekit>=1.3.0b2", "polars>=0.8.27", "pandas"]
+plugin_requires = ["flytekit>=1.3.0b2,<2.0.0", "polars>=0.8.27", "pandas"]
 
 __version__ = "0.0.0+develop"
 
