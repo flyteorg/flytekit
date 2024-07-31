40f6bc1fcef642f42584a27600801c96825c508f
pryce-turner <31577879+pryce-turner@users.noreply.github.com>
2024-07-31 14:08:28 -0700

    Added alt prefix head to FlyteFile.new_remote (#2601)
    * Added alt prefix head to FlyteFile.new_remote
    
    Signed-off-by: pryce-turner <pryce.turner@gmail.com>
    
    * Added get_new_path method to FileAccessProvider, fixed new_remote method of FlyteFile
    
    Signed-off-by: pryce-turner <pryce.turner@gmail.com>
    
    * Updated tests and added new path creator to FlyteFile/Dir new_remote methods
    
    Signed-off-by: pryce-turner <pryce.turner@gmail.com>
    
    * Improved docstrings, fixed minor path sep bug, more descriptive naming, better test
    
    Signed-off-by: pryce-turner <pryce.turner@gmail.com>
    
    * Formatting
    
    Signed-off-by: pryce-turner <pryce.turner@gmail.com>
    
    ---------
    
    Signed-off-by: pryce-turner <pryce.turner@gmail.com>

---
+++ tests/flytekit/unit/types/file/test_types.py
@@ -0,0 +1,7 @@
+from flytekit.types.file import FlyteFile
+from flytekit import FlyteContextManager
+
+def test_new_remote_alt():
+    ff = FlyteFile.new_remote_file(alt="my-alt-prefix", name="my-file.txt")
+    assert "my-alt-prefix" in ff.path
+    assert "my-file.txt" in ff.path
