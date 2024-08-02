a2f943bf2ea6f82ca3ce5d09a28b65e69ecfce22
Kevin Su <pingsutw@apache.org>
2024-08-02 13:25:07 -0700

    openai env
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- plugins/flytekit-ray/flytekitplugins/ray/agent.py
+++ plugins/flytekit-ray/flytekitplugins/ray/agent.py
@@ -34,6 +34,7 @@ class AnyscaleAgent(AsyncAgentBase):
                 "AWS_ACCESS_KEY_ID": os.getenv("AWS_ACCESS_KEY_ID"),
                 "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_SECRET_ACCESS_KEY"),
                 "AWS_SESSION_TOKEN": os.getenv("AWS_SESSION_TOKEN"),
+                "OPEN_API_KEY": os.getenv("OPEN_API_KEY"),
                 "PYTHONPATH": "/root:."
             },
             entrypoint=" ".join(container.args),
