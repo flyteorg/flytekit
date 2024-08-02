4bdce3486133655e2a715de30fbdaa73acc1b57e
Mecoli1219 <michaellai901026@gmail.com>
2024-08-02 12:50:32 +0800

    Fix intergration test
    Signed-off-by: Mecoli1219 <michaellai901026@gmail.com>

--- tests/flytekit/integration/remote/test_jupyter_remote.py
+++ tests/flytekit/integration/remote/test_jupyter_remote.py
@@ -44,7 +44,9 @@ def register():
 @mock.patch("flytekit.tools.interactive.ipython_check")
 def test_jupyter_remote_workflow(mock_ipython_check):
     mock_ipython_check.return_value = True
-    init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN)
+    init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN, interactive_mode_enabled=True)
+    with pytest.raises(AssertionError):
+        init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN, interactive_mode_enabled=True)
     from .workflows.basic.child_workflow import parent_wf, double
 
     # child_workflow.parent_wf asynchronously register a parent wf1 with child lp from another wf2.
@@ -69,7 +71,7 @@ def test_execute_jupyter_python_task(register):
     """Test execution of a @task-decorated python function that is already registered."""
     with patch("flytekit.tools.interactive.ipython_check") as mock_ipython_check:
         mock_ipython_check.return_value = True
-        init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN)
+        # init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN, interactive_mode_enabled=True)
         from .workflows.basic.basic_workflow import t1
 
         future = t1.remote(a=10, version=VERSION)
@@ -84,7 +86,7 @@ def test_execute_jupyter_python_workflow(register):
     """Test execution of a @workflow-decorated python function."""
     with patch("flytekit.tools.interactive.ipython_check") as mock_ipython_check:
         mock_ipython_check.return_value = True
-        init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN)
+        # init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN, interactive_mode_enabled=True)
         from .workflows.basic.basic_workflow import my_basic_wf
 
         future = my_basic_wf.remote(a=10, b="xyz", version=VERSION)
@@ -96,7 +98,7 @@ def test_execute_jupyter_python_workflow(register):
 @mock.patch("flytekit.tools.interactive.ipython_check")
 def test_fetch_execute_task_list_of_floats(mock_ipython_check):
     mock_ipython_check.return_value = True
-    init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN)
+    # init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN, interactive_mode_enabled=True)
     from .workflows.basic.list_float_wf import concat_list
 
     xs: typing.List[float] = [0.1, 0.2, 0.3, 0.4, -99999.7]
@@ -108,7 +110,7 @@ def test_fetch_execute_task_list_of_floats(mock_ipython_check):
 @mock.patch("flytekit.tools.interactive.ipython_check")
 def test_fetch_execute_task_convert_dict(mock_ipython_check):
     mock_ipython_check.return_value = True
-    init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN)
+    # init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN, interactive_mode_enabled=True)
     from .workflows.basic.dict_str_wf import convert_to_string
 
     d: typing.Dict[str, str] = {"key1": "value1", "key2": "value2"}
@@ -121,7 +123,7 @@ def test_fetch_execute_task_convert_dict(mock_ipython_check):
 def test_execute_jupyter_python_workflow_dict_of_string_to_string(mock_ipython_check):
     """Test execution of a @workflow-decorated python function."""
     mock_ipython_check.return_value = True
-    init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN)
+    # init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN, interactive_mode_enabled=True)
     from .workflows.basic.dict_str_wf import my_dict_str_wf
 
     d: typing.Dict[str, str] = {"k1": "v1", "k2": "v2"}
@@ -134,7 +136,7 @@ def test_execute_jupyter_python_workflow_dict_of_string_to_string(mock_ipython_c
 def test_execute_jupyter_python_workflow_list_of_floats(mock_ipython_check):
     """Test execution of a @workflow-decorated python function."""
     mock_ipython_check.return_value = True
-    init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN)
+    # init_remote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN, interactive_mode_enabled=True)
     from .workflows.basic.list_float_wf import my_list_float_wf
 
     xs: typing.List[float] = [42.24, 999.1, 0.0001]
