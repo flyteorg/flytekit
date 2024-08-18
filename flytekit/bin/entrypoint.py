c3c4bab131d4b96e40a0536ff5e16976ee39aa1d
Nelson Chen <asd3431090@gmail.com>
2024-08-19 07:27:25 +0800

    rollback changes back to previous
    Signed-off-by: Nelson Chen <asd3431090@gmail.com>

--- flytekit/bin/entrypoint.py
+++ flytekit/bin/entrypoint.py
@@ -378,8 +378,9 @@ def _execute_task(
     ) as ctx:
         import sys
 
-        if all(os.path.realpath(path) != dynamic_dest_dir for path in sys.path):
-            sys.path.append(dynamic_dest_dir)
+        working_dir=os.getcwd()
+        if all(os.path.realpath(path) != working_dir for path in sys.path):
+            sys.path.append(working_dir)
         resolver_obj = load_object_from_module(resolver)
         # Use the resolver to load the actual task object
         _task_def = resolver_obj.load_task(loader_args=resolver_args)
