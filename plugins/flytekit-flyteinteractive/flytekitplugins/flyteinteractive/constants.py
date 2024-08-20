0cfbdf17e48daf2961e62c5b8f3800060c3db1f6
Kevin Su <pingsutw@apache.org>
2024-08-19 21:11:49 -0700

    remove code from vscode plugin
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- plugins/flytekit-flyteinteractive/flytekitplugins/flyteinteractive/constants.py
+++ plugins/flytekit-flyteinteractive/flytekitplugins/flyteinteractive/constants.py
@@ -1,6 +1,1 @@
-# Default max idle seconds to terminate the flyteinteractive server
-HOURS_TO_SECONDS = 60 * 60
-MAX_IDLE_SECONDS = 10 * HOURS_TO_SECONDS  # 10 hours
-
-# Subprocess constants
-EXIT_CODE_SUCCESS = 0
+from flytekit.interactive.constants import *
