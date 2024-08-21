e50744f8558cdba3dc4bd1ece2a9436deb6bdf27
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2024-08-21 13:37:58 -0700

    Update plugins/flytekit-k8s-pod/flytekitplugins/pod/__init__.py
    Co-authored-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- plugins/flytekit-k8s-pod/flytekitplugins/pod/__init__.py
+++ plugins/flytekit-k8s-pod/flytekitplugins/pod/__init__.py
@@ -17,5 +17,5 @@ This package contains things that are useful when extending Flytekit.
 warnings.warn(
     "This pod plugin is no longer necessary, please use the pod_template and pod_template_name options to @task as described "
     "in https://docs.flyte.org/en/latest/deployment/configuration/general.html#configuring-task-pods-with-k8s-podtemplates",
-    DeprecationWarning,
+    FutureWarning,
 )
