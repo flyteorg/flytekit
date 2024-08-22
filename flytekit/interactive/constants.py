c0172003f5093c8905bcbb8e99cd299465fd5fed
Kevin Su <pingsutw@apache.org>
2024-08-22 15:44:14 -0700

    update key
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/interactive/constants.py
+++ flytekit/interactive/constants.py
@@ -6,4 +6,4 @@ MAX_IDLE_SECONDS = 10 * HOURS_TO_SECONDS  # 10 hours
 EXIT_CODE_SUCCESS = 0
 
 # Set it to True to run vscode server when the task
-FLYTE_ENABLE_VSCODE_KEY = "FLYTE_ENABLE_VSCODE"
+FLYTE_ENABLE_VSCODE_KEY = "_F_E_VS"
