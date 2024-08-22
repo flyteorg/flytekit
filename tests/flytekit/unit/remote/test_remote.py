27b14aafe3b68a35dd6d004eae4214bf9bde246a
wayner0628 <a901639@gmail.com>
2024-08-22 18:29:39 +0800

    fix call args tuple index
    Signed-off-by: wayner0628 <a901639@gmail.com>

--- tests/flytekit/unit/remote/test_remote.py
+++ tests/flytekit/unit/remote/test_remote.py
@@ -600,7 +600,7 @@ def test_execution_name(mock_client, mock_uuid):
             mock.call(ANY, ANY, None, ANY, ANY),
         ]
     )
-    assert mock_client.create_execution.call_args_list[1][1][2] == "execution-test-" + test_uuid.hex[:19]
+    assert mock_client.create_execution.call_args_list[1][0][2] == "execution-test-" + test_uuid.hex[:19]
     with pytest.raises(
         ValueError, match="Only one of execution_name and execution_name_prefix can be set, but got both set"
     ):
