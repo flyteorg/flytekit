5ad4af77b037716809cbb10256dee8fdf330ff81
Kevin Su <pingsutw@apache.org>
2024-09-04 17:01:40 -0700

    more tests
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- tests/flytekit/unit/exceptions/test_scopes.py
+++ tests/flytekit/unit/exceptions/test_scopes.py
@@ -1,6 +1,10 @@
 import pytest
 
+from flytekit.core import context_manager
 from flytekit.exceptions import scopes, system, user
+from flytekit.exceptions.scopes import system_error_handler, user_error_handler
+from flytekit.exceptions.system import FlyteNonRecoverableSystemException
+from flytekit.exceptions.user import FlyteUserRuntimeException
 from flytekit.models.core import errors as _error_models
 
 
@@ -116,3 +120,42 @@ def test_intercepted_scope_flyte_system_exception():
     assert "FlyteSystemAssertion:" in e.verbose_message
     assert e.error_code == "SYSTEM:AssertionError"
     assert e.kind == _error_models.ContainerError.Kind.RECOVERABLE
+
+
+def test_system_error_handler():
+    def t1():
+        raise ValueError("Bad value")
+
+    ctx = context_manager.FlyteContext.current_context()
+
+    with pytest.raises(FlyteNonRecoverableSystemException) as e:
+        system_error_handler(t1)()
+
+    assert isinstance(e.value.value, ValueError)
+
+    with pytest.raises(ValueError):
+        with context_manager.FlyteContextManager.with_context(
+                ctx.with_execution_state(
+                    ctx.execution_state.with_params(mode=context_manager.ExecutionState.Mode.LOCAL_TASK_EXECUTION)
+                )
+        ):
+            system_error_handler(t1)()
+
+
+def test_user_error_handler():
+    def t1():
+        raise ValueError("Bad value")
+
+    with pytest.raises(FlyteUserRuntimeException) as e:
+        user_error_handler(t1)()
+
+    assert isinstance(e.value.value, ValueError)
+
+    ctx = context_manager.FlyteContext.current_context()
+    with pytest.raises(ValueError):
+        with context_manager.FlyteContextManager.with_context(
+                ctx.with_execution_state(
+                    ctx.execution_state.with_params(mode=context_manager.ExecutionState.Mode.LOCAL_TASK_EXECUTION)
+                )
+        ):
+            system_error_handler(t1)()
