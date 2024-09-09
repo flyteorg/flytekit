26cec32b59c13789ae12a607778df4adfb507200
Niels Bantilan <niels.bantilan@gmail.com>
2024-09-09 12:29:32 -0400

    revert to asyncio.get_event_loop().run_until_complete(outputs)
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- flytekit/bin/entrypoint.py
+++ flytekit/bin/entrypoint.py
@@ -107,8 +107,7 @@ def _dispatch_execute(
         if inspect.iscoroutine(outputs):
             # Handle eager-mode (async) tasks
             logger.info("Output is a coroutine")
-            with asyncio.Runner() as runner:
-                outputs = runner.run(outputs)
+            outputs = asyncio.get_event_loop().run_until_complete(outputs)
 
         # Step3a
         if isinstance(outputs, VoidPromise):
