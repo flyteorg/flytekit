da4b67fbac84971f85519c90fa7a9606456dbe28
Niels Bantilan <niels.bantilan@gmail.com>
2024-09-10 12:56:33 -0400

    use asyncio.get_running_loop()
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- flytekit/bin/entrypoint.py
+++ flytekit/bin/entrypoint.py
@@ -107,7 +107,7 @@ def _dispatch_execute(
         if inspect.iscoroutine(outputs):
             # Handle eager-mode (async) tasks
             logger.info("Output is a coroutine")
-            outputs = asyncio.get_event_loop().run_until_complete(outputs)
+            outputs = asyncio.get_running_loop().run_until_complete(outputs)
 
         # Step3a
         if isinstance(outputs, VoidPromise):
