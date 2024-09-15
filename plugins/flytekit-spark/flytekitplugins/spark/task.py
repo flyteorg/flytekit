4066051d398d2737e72f95320f8ea694c0d0e552
Kevin Su <pingsutw@apache.org>
2024-09-15 01:02:23 -0700

    test
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- plugins/flytekit-spark/flytekitplugins/spark/task.py
+++ plugins/flytekit-spark/flytekitplugins/spark/task.py
@@ -206,7 +206,8 @@ class PysparkFunctionTask(AsyncAgentExecutorMixin, PythonFunctionTask[Spark]):
         for f in files:
             print(f)
         self.sess = sess_builder.getOrCreate()
-        self.sess.addArtifacts("fast_spark.py", file=True)
+        # self.sess.addArtifacts("fast_spark.py", file=True)
+        self.sess.sparkContext.addFile("fast_spark.py")
         return user_params.builder().add_attr("SPARK_SESSION", self.sess).build()
 
     def execute(self, **kwargs) -> Any:
