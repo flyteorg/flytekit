ddbdf730115895349feec0e6a973774af058793e
Future-Outlier <eric901201@gmail.com>
2024-09-24 00:17:45 +0800

    Test Backward Compatible
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- tests/flytekit/unit/core/test_type_engine_binary_idl.py
+++ tests/flytekit/unit/core/test_type_engine_binary_idl.py
@@ -5,14 +5,17 @@ from enum import Enum
 from typing import Dict, List
 from datetime import datetime, date, timedelta
 import pytest
-
+import json
+from google.protobuf import json_format as _json_format
+from google.protobuf import struct_pb2 as _struct
 import msgpack
+from mashumaro.codecs.json import JSONEncoder
 from mashumaro.codecs.msgpack import MessagePackEncoder
 
 from flytekit import workflow, task
 from flytekit.models.literals import Binary, Literal, Scalar
 from flytekit.core.context_manager import FlyteContextManager
-from flytekit.core.type_engine import TypeEngine
+from flytekit.core.type_engine import TypeEngine, DataclassTransformer
 from flytekit.types.file import FlyteFile
 from flytekit.types.directory import FlyteDirectory
 
@@ -521,3 +524,174 @@ def test_all_types_in_dataclass_wf(local_dummy_file, local_dummy_directory):
                               m=dc.inner_dc.m, n=dc.inner_dc.n, o=dc.inner_dc.o, enum_status=dc.inner_dc.enum_status)
 
     wf(dc=DC())
+
+def test_backward_compatible_with_dataclass_in_protobuf_struct(local_dummy_file, local_dummy_directory):
+    class Status(Enum):
+        PENDING = "pending"
+        APPROVED = "approved"
+        REJECTED = "rejected"
+
+    @dataclass
+    class InnerDC:
+        a: int = -1
+        b: float = 2.1
+        c: str = "Hello, Flyte"
+        d: bool = False
+        e: List[int] = field(default_factory=lambda: [0, 1, 2, -1, -2])
+        f: List[FlyteFile] = field(default_factory=lambda: [FlyteFile(local_dummy_file)])
+        g: List[List[int]] = field(default_factory=lambda: [[0], [1], [-1]])
+        h: List[Dict[int, bool]] = field(default_factory=lambda: [{0: False}, {1: True}, {-1: True}])
+        i: Dict[int, bool] = field(default_factory=lambda: {0: False, 1: True, -1: False})
+        j: Dict[int, FlyteFile] = field(default_factory=lambda: {0: FlyteFile(local_dummy_file),
+                                                                 1: FlyteFile(local_dummy_file),
+                                                                 -1: FlyteFile(local_dummy_file)})
+        k: Dict[int, List[int]] = field(default_factory=lambda: {0: [0, 1, -1]})
+        l: Dict[int, Dict[int, int]] = field(default_factory=lambda: {1: {-1: 0}})
+        m: dict = field(default_factory=lambda: {"key": "value"})
+        n: FlyteFile = field(default_factory=lambda: FlyteFile(local_dummy_file))
+        o: FlyteDirectory = field(default_factory=lambda: FlyteDirectory(local_dummy_directory))
+        enum_status: Status = field(default=Status.PENDING)
+
+    @dataclass
+    class DC:
+        a: int = -1
+        b: float = 2.1
+        c: str = "Hello, Flyte"
+        d: bool = False
+        e: List[int] = field(default_factory=lambda: [0, 1, 2, -1, -2])
+        f: List[FlyteFile] = field(default_factory=lambda: [FlyteFile(local_dummy_file), ])
+        g: List[List[int]] = field(default_factory=lambda: [[0], [1], [-1]])
+        h: List[Dict[int, bool]] = field(default_factory=lambda: [{0: False}, {1: True}, {-1: True}])
+        i: Dict[int, bool] = field(default_factory=lambda: {0: False, 1: True, -1: False})
+        j: Dict[int, FlyteFile] = field(default_factory=lambda: {0: FlyteFile(local_dummy_file),
+                                                                 1: FlyteFile(local_dummy_file),
+                                                                 -1: FlyteFile(local_dummy_file)})
+        k: Dict[int, List[int]] = field(default_factory=lambda: {0: [0, 1, -1]})
+        l: Dict[int, Dict[int, int]] = field(default_factory=lambda: {1: {-1: 0}})
+        m: dict = field(default_factory=lambda: {"key": "value"})
+        n: FlyteFile = field(default_factory=lambda: FlyteFile(local_dummy_file))
+        o: FlyteDirectory = field(default_factory=lambda: FlyteDirectory(local_dummy_directory))
+        inner_dc: InnerDC = field(default_factory=lambda: InnerDC())
+        enum_status: Status = field(default=Status.PENDING)
+
+    def t_inner(inner_dc: InnerDC):
+        assert (type(inner_dc), InnerDC) # type: ignore
+
+        # f: List[FlyteFile]
+        for ff in inner_dc.f:
+            assert (type(ff), FlyteFile) # type: ignore
+            with open(ff, "r") as f:
+                assert f.read() == "Hello FlyteFile"
+        # j: Dict[int, FlyteFile]
+        for _, ff in inner_dc.j.items():
+            assert (type(ff), FlyteFile) # type: ignore
+            with open(ff, "r") as f:
+                assert f.read() == "Hello FlyteFile"
+        # n: FlyteFile
+        assert (type(inner_dc.n), FlyteFile) # type: ignore
+        with open(inner_dc.n, "r") as f:
+            assert f.read() == "Hello FlyteFile"
+        # o: FlyteDirectory
+        assert (type(inner_dc.o), FlyteDirectory) # type: ignore
+        assert not inner_dc.o.downloaded
+        with open(os.path.join(inner_dc.o, "file"), "r") as fh:
+            assert fh.read() == "Hello FlyteDirectory"
+        assert inner_dc.o.downloaded
+        print("Test InnerDC Successfully Passed")
+        # enum: Status
+        assert inner_dc.enum_status == Status.PENDING
+
+    def t_test_all_attributes(a: int, b: float, c: str, d: bool, e: List[int], f: List[FlyteFile], g: List[List[int]],
+                              h: List[Dict[int, bool]], i: Dict[int, bool], j: Dict[int, FlyteFile],
+                              k: Dict[int, List[int]], l: Dict[int, Dict[int, int]], m: dict,
+                              n: FlyteFile, o: FlyteDirectory, enum_status: Status):
+        # Strict type checks for simple types
+        assert isinstance(a, int), f"a is not int, it's {type(a)}"
+        assert a == -1
+        assert isinstance(b, float), f"b is not float, it's {type(b)}"
+        assert isinstance(c, str), f"c is not str, it's {type(c)}"
+        assert isinstance(d, bool), f"d is not bool, it's {type(d)}"
+
+        # Strict type checks for List[int]
+        assert isinstance(e, list) and all(isinstance(i, int) for i in e), "e is not List[int]"
+
+        # Strict type checks for List[FlyteFile]
+        assert isinstance(f, list) and all(isinstance(i, FlyteFile) for i in f), "f is not List[FlyteFile]"
+
+        # Strict type checks for List[List[int]]
+        assert isinstance(g, list) and all(
+            isinstance(i, list) and all(isinstance(j, int) for j in i) for i in g), "g is not List[List[int]]"
+
+        # Strict type checks for List[Dict[int, bool]]
+        assert isinstance(h, list) and all(
+            isinstance(i, dict) and all(isinstance(k, int) and isinstance(v, bool) for k, v in i.items()) for i in h
+        ), "h is not List[Dict[int, bool]]"
+
+        # Strict type checks for Dict[int, bool]
+        assert isinstance(i, dict) and all(
+            isinstance(k, int) and isinstance(v, bool) for k, v in i.items()), "i is not Dict[int, bool]"
+
+        # Strict type checks for Dict[int, FlyteFile]
+        assert isinstance(j, dict) and all(
+            isinstance(k, int) and isinstance(v, FlyteFile) for k, v in j.items()), "j is not Dict[int, FlyteFile]"
+
+        # Strict type checks for Dict[int, List[int]]
+        assert isinstance(k, dict) and all(
+            isinstance(k, int) and isinstance(v, list) and all(isinstance(i, int) for i in v) for k, v in
+            k.items()), "k is not Dict[int, List[int]]"
+
+        # Strict type checks for Dict[int, Dict[int, int]]
+        assert isinstance(l, dict) and all(
+            isinstance(k, int) and isinstance(v, dict) and all(
+                isinstance(sub_k, int) and isinstance(sub_v, int) for sub_k, sub_v in v.items())
+            for k, v in l.items()), "l is not Dict[int, Dict[int, int]]"
+
+        # Strict type check for a generic dict
+        assert isinstance(m, dict), "m is not dict"
+
+        # Strict type check for FlyteFile
+        assert isinstance(n, FlyteFile), "n is not FlyteFile"
+
+        # Strict type check for FlyteDirectory
+        assert isinstance(o, FlyteDirectory), "o is not FlyteDirectory"
+
+        # Strict type check for Enum
+        assert isinstance(enum_status, Status), "enum_status is not Status"
+
+        print("All attributes passed strict type checks.")
+
+    # This is the old dataclass serialization behavior.
+    # https://github.com/flyteorg/flytekit/blob/94786cfd4a5c2c3b23ac29dcd6f04d0553fa1beb/flytekit/core/type_engine.py#L702-L728
+    dc = DC()
+    DataclassTransformer()._make_dataclass_serializable(python_val=dc, python_type=DC)
+    json_str = JSONEncoder(DC).encode(dc)
+    upstream_output = Literal(scalar=Scalar(generic=_json_format.Parse(json_str, _struct.Struct())))
+
+    downstream_input = TypeEngine.to_python_value(FlyteContextManager.current_context(), upstream_output, DC)
+    t_inner(downstream_input.inner_dc)
+    t_test_all_attributes(a=downstream_input.a, b=downstream_input.b, c=downstream_input.c,
+                          d=downstream_input.d, e=downstream_input.e, f=downstream_input.f,
+                          g=downstream_input.g, h=downstream_input.h, i=downstream_input.i,
+                          j=downstream_input.j, k=downstream_input.k, l=downstream_input.l,
+                          m=downstream_input.m, n=downstream_input.n, o=downstream_input.o,
+                          enum_status=downstream_input.enum_status)
+    t_test_all_attributes(a=downstream_input.inner_dc.a, b=downstream_input.inner_dc.b, c=downstream_input.inner_dc.c,
+                          d=downstream_input.inner_dc.d, e=downstream_input.inner_dc.e, f=downstream_input.inner_dc.f,
+                          g=downstream_input.inner_dc.g, h=downstream_input.inner_dc.h, i=downstream_input.inner_dc.i,
+                          j=downstream_input.inner_dc.j, k=downstream_input.inner_dc.k, l=downstream_input.inner_dc.l,
+                          m=downstream_input.inner_dc.m, n=downstream_input.inner_dc.n, o=downstream_input.inner_dc.o,
+                          enum_status=downstream_input.inner_dc.enum_status)
+
+def test_backward_compatible_with_untyped_dict_in_protobuf_struct():
+    # This is the old dataclass serialization behavior.
+    # https://github.com/flyteorg/flytekit/blob/94786cfd4a5c2c3b23ac29dcd6f04d0553fa1beb/flytekit/core/type_engine.py#L1699-L1720
+    dict_input = {"a" : 1.0, "b": "str",
+                  "c": False, "d": True,
+                  "e": [1.0, 2.0, -1.0, 0.0],
+                  "f": {"a": {"b": [1.0, -1.0]}}}
+
+    upstream_output = Literal(scalar=Scalar(generic=_json_format.Parse(json.dumps(dict_input), _struct.Struct())),
+                              metadata={"format": "json"})
+
+    downstream_input = TypeEngine.to_python_value(FlyteContextManager.current_context(), upstream_output, dict)
+    assert dict_input == downstream_input
