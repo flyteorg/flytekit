06da3df4a18fdf8a4208dea2d043504c8f137b2b
Future-Outlier <eric901201@gmail.com>
2024-10-02 14:24:43 +0800

    return html
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- flytekit/deck/deck.py
+++ flytekit/deck/deck.py
@@ -129,6 +129,9 @@ class DummyDeck(Deck):
         html = DUMMY_DECK_HTML
         super().__init__(name, html)
 
+    @property
+    def html(self) -> str:
+        return self.html
 
 class TimeLineDeck(Deck):
     """
