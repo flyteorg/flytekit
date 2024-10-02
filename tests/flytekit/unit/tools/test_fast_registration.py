b5648f7aa27b7a71789eca0538ee608d2e160044
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2024-10-02 11:23:39 -0400

    Add fast_package pigz test
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- tests/flytekit/unit/tools/test_fast_registration.py
+++ tests/flytekit/unit/tools/test_fast_registration.py
@@ -1,9 +1,13 @@
 import os
 import subprocess
 import tarfile
+import time
+from hashlib import md5
+from pathlib import Path
 
 import pytest
 
+from flytekit.constants import CopyFileDetection
 from flytekit.tools.fast_registration import (
     FAST_FILEENDING,
     FAST_PREFIX,
@@ -171,3 +175,30 @@ def test_skip_invalid_symlink_in_compute_digest(tmp_path):
 
     # Confirm that you can compute the digest without error
     assert compute_digest(tmp_path) is not None
+
+
+# Skip test if `pigz` is not installed
+@pytest.mark.skipif(
+    subprocess.run(["which", "pigz"], stdout=subprocess.PIPE).returncode != 0,
+    reason="pigz is not installed",
+)
+def test_package_with_pigz(flyte_project, tmp_path):
+    # Call fast_package twice and compare the md5 of the resulting tarballs
+
+    options = FastPackageOptions(ignores=[], copy_style=CopyFileDetection.ALL)
+
+    Path(tmp_path / "dir1").mkdir()
+    archive_fname_1 = fast_package(source=flyte_project, output_dir=tmp_path / "dir1", options=options)
+    # Copy the tarball bytes and remove the file to ensure it is not included in the next invocation of fast_package
+    archive_1_bytes = Path(archive_fname_1).read_bytes()
+    Path(archive_fname_1).unlink()
+
+    # Wait a second to ensure the next tarball has a different timestamp, which consequently tests if there is an impact
+    # to the metadata of the resulting tarball
+    time.sleep(1)
+
+    Path(tmp_path / "dir2").mkdir()
+    archive_fname_2 = fast_package(source=flyte_project, output_dir=tmp_path / "dir2", options=options)
+
+    # Compare the md5sum of the two tarballs
+    assert md5(archive_1_bytes).hexdigest() == md5(Path(archive_fname_2).read_bytes()).hexdigest()
