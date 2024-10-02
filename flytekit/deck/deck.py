99522d9313204cec543b4df8a109d394e848914d
Future-Outlier <eric901201@gmail.com>
2024-10-02 14:12:06 +0800

    update
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- flytekit/deck/deck.py
+++ flytekit/deck/deck.py
@@ -200,9 +200,9 @@ def _get_deck(
 
     # if len(nav_htmls) == 0 and len(body_htmls) == 0:
     #     body_htmls.append(DUMMY_DECK)
-    nav_htmls = []
-    body_htmls = []
-    body_htmls.append(DUMMY_DECK)
+    nav_htmls = [DUMMY_DECK]
+    body_htmls = [DUMMY_DECK]
+    # body_htmls.append(DUMMY_DECK)
     raw_html = get_deck_template().substitute(NAV_HTML="".join(nav_htmls), BODY_HTML="".join(body_htmls))
     if not ignore_jupyter and ipython_check():
         try:
