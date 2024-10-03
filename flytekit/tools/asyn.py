d1e37d5cee5159dd69156fc3c299b4d5ba39487a
Thomas J. Fan <thomasjpfan@gmail.com>
2024-10-03 12:38:32 -0400

    Catch asyncio.TimeError and use TimeoutError all the time
    Signed-off-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- flytekit/tools/asyn.py
+++ flytekit/tools/asyn.py
@@ -95,7 +95,9 @@ class _AsyncLoopManager:
                     raise TimeoutError
 
         return_result = result[0]
-        if isinstance(return_result, BaseException):
+        if isinstance(return_result, asyncio.TimeoutError):
+            raise TimeoutError from return_result
+        elif isinstance(return_result, BaseException):
             raise return_result
         else:
             return return_result
