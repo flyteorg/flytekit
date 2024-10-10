096679c08b1ae0205250af7a36a1e1829d594b42
Daniel Sola <daniel.sola@union.ai>
2024-10-10 16:48:01 -0700

    change version string message
    Signed-off-by: Daniel Sola <daniel.sola@union.ai>

--- flytekit/tools/repo.py
+++ flytekit/tools/repo.py
@@ -211,7 +211,7 @@ def secho(i: Identifier, state: str = "success", reason: str = None, op: str = "
         nl = True
         if not reason:
             url_string = click.style(console_url, fg="cyan")
-            reason = f"successful: {url_string}" if console_url else f"version: {i.version}"
+            reason = f"successful: {url_string}" if console_url else f"successful. Version: {i.version}"
     elif state == "failed":
         state_ind = "\r[x]"
         fg = "red"
