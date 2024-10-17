97811458801d186ed8573b7003c3caf0e8c6eeb5
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2024-10-17 12:40:22 -0700

    remove more async put data
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- flytekit/extras/pytorch/checkpoint.py
+++ flytekit/extras/pytorch/checkpoint.py
@@ -98,7 +98,7 @@ class PyTorchCheckpointTransformer(TypeTransformer[PyTorchCheckpoint]):
         # save checkpoint to a file
         torch.save(to_save, local_path)
 
-        remote_path = ctx.file_access.async_put_raw_data(local_path)
+        remote_path = ctx.file_access.put_raw_data(local_path)
         return Literal(scalar=Scalar(blob=Blob(metadata=meta, uri=remote_path)))
 
     def to_python_value(
