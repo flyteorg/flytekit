622c5e883ed7c7dfb3f3f48a041c510e1198fa0c
Future-Outlier <eric901201@gmail.com>
2024-10-22 23:34:00 +0800

    test
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- flytekit/core/type_engine.py
+++ flytekit/core/type_engine.py
@@ -1157,9 +1157,9 @@ class TypeEngine(typing.Generic[T]):
                 from flytekit.extras import pytorch  # noqa: F401
             if is_imported("sklearn"):
                 from flytekit.extras import sklearn  # noqa: F401
-            if is_imported("pydantic"):
-                print("@@@@ pydantic is imported")
-                from flytekit.extras import pydantic  # noqa: F401
+            # if is_imported("pydantic"):
+            #     print("@@@@ pydantic is imported")
+            #     from flytekit.extras import pydantic  # noqa: F401
             if is_imported("pandas"):
                 try:
                     from flytekit.types.schema.types_pandas import PandasSchemaReader, PandasSchemaWriter  # noqa: F401
