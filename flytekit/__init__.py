ff2d4a010bb5013d60cd54ea0d76ec2ee854169a
Future-Outlier <eric901201@gmail.com>
2024-10-22 13:22:32 +0800

    update
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- flytekit/__init__.py
+++ flytekit/__init__.py
@@ -216,6 +216,7 @@ if sys.version_info < (3, 10):
 else:
     from importlib.metadata import entry_points
 
+
 from flytekit._version import __version__
 from flytekit.configuration import Config
 from flytekit.core.array_node_map_task import map_task
@@ -262,6 +263,8 @@ from flytekit.types.structured.structured_dataset import (
     StructuredDatasetTransformerEngine,
     StructuredDatasetType,
 )
+
+from flytekit.core import type_utils  # noqa: I001
 
 
 def current_context() -> ExecutionParameters:
--- flytekit/extras/pydantic/__init__.py
+++ flytekit/extras/pydantic/__init__.py
@@ -1,4 +1,3 @@
-from flytekit.extras.pydantic import custom
 from flytekit.loggers import logger
 
 # TODO: abstract this out so that there's an established pattern for registering plugins
@@ -8,12 +7,7 @@ try:
     # model_validator and model_serializer are only available in pydantic > 2
     from pydantic import model_serializer, model_validator
 
-    _pydantic_installed = True
-except (ImportError, OSError):
-    _pydantic_installed = False
-
-
-if _pydantic_installed:
-    from . import custom, transformer
-else:
+    from . import transformer
+except (ImportError, OSError) as e:
     logger.info("Flytekit only support pydantic version > 2.")
+    print("error: ", e)
