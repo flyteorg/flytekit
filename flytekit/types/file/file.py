8017ae825b7c4cc3913971fab5308b172370d335
Grantham Taylor <54340816+granthamtaylor@users.noreply.github.com>
2024-10-24 15:29:01 -0400

    add hash method to flytefile to fix bug in interactive shell (#2853)

--- flytekit/types/file/file.py
+++ flytekit/types/file/file.py
@@ -372,6 +372,9 @@ class FlyteFile(SerializableType, os.PathLike, typing.Generic[T], DataClassJSONM
     def __str__(self):
         return self.path
 
+    def __hash__(self):
+        return hash(str(self.path))
+
 
 class FlyteFilePathTransformer(AsyncTypeTransformer[FlyteFile]):
     def __init__(self):
--- tests/flytekit/unit/types/file/test_types.py
+++ tests/flytekit/unit/types/file/test_types.py
@@ -10,3 +10,12 @@ def test_new_auto_file():
     ff = FlyteFile.new("my-file.txt")
     cwd = FlyteContextManager.current_context().user_space_params.working_directory
     assert cwd in ff.path
+
+def test_file_is_hashable():
+
+    ff1 = FlyteFile.new("file1.txt")
+    ff2 = FlyteFile.new("file2.txt")
+
+    assert isinstance({ff1, ff2}, set)
+
+    assert isinstance(hash(ff1), int)
