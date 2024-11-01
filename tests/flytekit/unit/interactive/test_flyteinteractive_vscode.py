5f3f45f278040d9c988bcf45a1ccf761b4d87e65
Kevin Su <pingsutw@apache.org>
2024-11-01 13:46:35 -0700

    update task
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- tests/flytekit/unit/interactive/test_flyteinteractive_vscode.py
+++ tests/flytekit/unit/interactive/test_flyteinteractive_vscode.py
@@ -91,7 +91,6 @@ def test_vscode_remote_execution(vscode_patches, mock_remote_execution):
     mock_process.assert_called_once()
     mock_exit_handler.assert_called_once()
     mock_prepare_interactive_python.assert_called_once()
-    assert mock_signal.call_count >=1
     mock_prepare_resume_task_python.assert_called_once()
     mock_prepare_launch_json.assert_called_once()
 
