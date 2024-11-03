c783fda8f304ecd4534b52a6b924c9569fac3d30
wayner0628 <a901639@gmail.com>
2024-11-02 18:12:37 -0700

    fix pytest raise
    Signed-off-by: wayner0628 <a901639@gmail.com>

--- tests/flytekit/unit/core/test_local_raw_container.py
+++ tests/flytekit/unit/core/test_local_raw_container.py
@@ -70,23 +70,38 @@ def test_flytefile_wrong_syntax():
     dockerfile_name = "Dockerfile.raw_container"
     client.images.build(path=path_to_dockerfile, dockerfile=dockerfile_name, tag="flytekit:rawcontainer")
 
+    flyte_file_io = ContainerTask(
+        name="flyte_file_io",
+        input_data_dir="/var/inputs",
+        output_data_dir="/var/outputs",
+        inputs=kwtypes(inputs=FlyteFile),
+        outputs=kwtypes(out=FlyteFile),
+        image="flytekit:rawcontainer",
+        command=[
+            "python",
+            "write_flytefile.py",
+            "{{.inputs.inputs}}",
+            "/var/outputs/out",
+        ],
+    )
+
+    @task
+    def flyte_file_task() -> FlyteFile:
+        working_dir = flytekit.current_context().working_directory
+        write_file = os.path.join(working_dir, "flyte_file.txt")
+        with open(write_file, "w") as file:
+            file.write("This is flyte_file.txt file.")
+        return FlyteFile(path=write_file)
+
+    @workflow
+    def flyte_file_io_wf() -> FlyteFile:
+        ff = flyte_file_task()
+        return flyte_file_io(inputs=ff)
+
     with pytest.raises(
         AssertionError, match="FlyteFile and FlyteDirectory commands should not use the syntax: {{.inputs.infile}}"
     ):
-        ContainerTask(
-            name="flyte_file_io",
-            input_data_dir="/var/inputs",
-            output_data_dir="/var/outputs",
-            inputs=kwtypes(inputs=FlyteFile),
-            outputs=kwtypes(out=FlyteFile),
-            image="flytekit:rawcontainer",
-            command=[
-                "python",
-                "write_flytefile.py",
-                "{{.inputs.inputs}}",
-                "/var/outputs/out",
-            ],
-        )
+        flyte_file_io_wf()
 
 
 @pytest.mark.skipif(
@@ -149,23 +164,41 @@ def test_flytedir_wrong_syntax():
     dockerfile_name = "Dockerfile.raw_container"
     client.images.build(path=path_to_dockerfile, dockerfile=dockerfile_name, tag="flytekit:rawcontainer")
 
+    flyte_dir_io = ContainerTask(
+        name="flyte_dir_io",
+        input_data_dir="/var/inputs",
+        output_data_dir="/var/outputs",
+        inputs=kwtypes(inputs=FlyteDirectory),
+        outputs=kwtypes(out=FlyteDirectory),
+        image="flytekit:rawcontainer",
+        command=[
+            "python",
+            "write_flytedir.py",
+            "{{.inputs.inputs}}",
+            "/var/outputs/out",
+        ],
+    )
+
+    @task
+    def flyte_dir_task() -> FlyteDirectory:
+        working_dir = flytekit.current_context().working_directory
+        local_dir = Path(os.path.join(working_dir, "csv_files"))
+        local_dir.mkdir(exist_ok=True)
+        write_file = os.path.join(local_dir, "flyte_dir.txt")
+        with open(write_file, "w") as file:
+            file.write("This is for flyte dir.")
+
+        return FlyteDirectory(path=str(local_dir))
+
+    @workflow
+    def flyte_dir_io_wf() -> FlyteDirectory:
+        fd = flyte_dir_task()
+        return flyte_dir_io(inputs=fd)
+
     with pytest.raises(
         AssertionError, match="FlyteFile and FlyteDirectory commands should not use the syntax: {{.inputs.infile}}"
     ):
-        ContainerTask(
-            name="flyte_dir_io",
-            input_data_dir="/var/inputs",
-            output_data_dir="/var/outputs",
-            inputs=kwtypes(inputs=FlyteDirectory),
-            outputs=kwtypes(out=FlyteDirectory),
-            image="flytekit:rawcontainer",
-            command=[
-                "python",
-                "write_flytedir.py",
-                "{{.inputs.inputs}}",
-                "/var/outputs/out",
-            ],
-        )
+        flyte_dir_io_wf()
 
 
 @pytest.mark.skipif(
