2786c5b4fc17d0c2ba91cb8e03a27ebf4b746e99
Daniel Sola <daniel.sola@union.ai>
2024-11-05 15:07:05 -0800

    wip

---
+++ plugins/flytekit-auto-cache/flytekitplugins/auto_cache/__init__.py
@@ -0,0 +1,10 @@
+"""
+.. currentmodule:: flytekitplugins.auto_cache
+
+This package contains things that are useful when extending Flytekit.
+
+.. autosummary::
+   :template: custom.rst
+   :toctree: generated/
+
+"""
