5065887e7893ef14c5682ec3d313a129e4a6fbfd
Future-Outlier <eric901201@gmail.com>
2024-11-05 11:44:53 +0800

    dict update
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- flytekit/types/structured/structured_dataset.py
+++ flytekit/types/structured/structured_dataset.py
@@ -771,6 +771,7 @@ class StructuredDatasetTransformerEngine(AsyncTypeTransformer[StructuredDataset]
     def dict_to_structured_dataset(
         self, dict_obj: typing.Dict[str, str], expected_python_type: Type[T] | StructuredDataset
     ) -> T | StructuredDataset:
+        print("@@@ input dict_obj")
         uri = dict_obj.get("uri", None)
         file_format = dict_obj.get("file_format", None)
 
