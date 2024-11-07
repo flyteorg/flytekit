dfd0fe284448b3081ac4d7d4a5e62cc4a51f7c12
Jan Fiedler <jan@union.ai>
2024-11-07 11:47:39 -0800

    adjust memray __init__.py
    Signed-off-by: Jan Fiedler <jan@union.ai>

--- plugins/flytekit-memray/flytekitplugins/memray/__init__.py
+++ plugins/flytekit-memray/flytekitplugins/memray/__init__.py
@@ -1,15 +1,3 @@
-"""
-.. currentmodule:: flytekitplugins.wandb
-
-This package contains things that are useful when extending Flytekit.
-
-.. autosummary::
-   :template: custom.rst
-   :toctree: generated/
-
-   wandb_init
-"""
-
 from .profiling import memray_profiling
 
 __all__ = ["memray_profiling"]
