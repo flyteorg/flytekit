1080eeaecb89ab097fa2f85e8f5f78589eb64b62
Future-Outlier <eric901201@gmail.com>
2024-11-09 08:11:44 +0800

    add cases
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- tests/flytekit/integration/remote/test_remote.py
+++ tests/flytekit/integration/remote/test_remote.py
@@ -108,7 +108,7 @@ def test_remote_run():
     # run twice to make sure it will register a new version of the workflow.
     run("default_lp.py", "my_wf")
 
-def test_flytetypes():
+def test_generic_idl_flytetypes():
     os.environ["FLYTE_USE_OLD_DC_FORMAT"] = "true"
     # default inputs for flyte types in dataclass
     execution_id = run("flytetypes.py", "wf")
@@ -117,6 +117,14 @@ def test_flytetypes():
     execution = remote.wait(execution=execution, timeout=datetime.timedelta(minutes=5))
     assert execution.closure.phase == WorkflowExecutionPhase.SUCCEEDED, f"Execution failed with phase: {execution.closure.phase}"
     os.environ["FLYTE_USE_OLD_DC_FORMAT"] = "false"
+
+def test_msgpack_idl_flytetypes():
+    # default inputs for flyte types in dataclass
+    execution_id = run("flytetypes.py", "wf")
+    remote = FlyteRemote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN)
+    execution = remote.fetch_execution(name=execution_id)
+    execution = remote.wait(execution=execution, timeout=datetime.timedelta(minutes=5))
+    assert execution.closure.phase == WorkflowExecutionPhase.SUCCEEDED, f"Execution failed with phase: {execution.closure.phase}"
 
 def test_fetch_execute_launch_plan(register):
     remote = FlyteRemote(Config.auto(config_file=CONFIG), PROJECT, DOMAIN)
