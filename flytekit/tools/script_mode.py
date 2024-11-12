e48f56cbef5569ca00e8e361325d67d84c543588
Bennett <bennett@gridmatic.com>
2024-11-11 16:12:16 -0800

    [BUG] Consider each site_package
    Signed-off-by: Bennett <bennett@gridmatic.com>

--- flytekit/tools/script_mode.py
+++ flytekit/tools/script_mode.py
@@ -217,7 +217,9 @@ def list_imported_modules_as_files(source_path: str, modules: List[ModuleType]) 
             if os.path.commonpath([flytekit_root, mod_file]) == flytekit_root:
                 continue
 
-            if os.path.commonpath(site_packages + [mod_file]) in site_packages_set:
+            if any(
+                (os.path.commonpath([site_package, mod_file]) == site_package for site_package in site_packages_set)
+            ):
                 # Do not upload files from site-packages
                 continue
 
