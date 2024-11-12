5243774fe1d6dd21084a8e4ddade0002bc7ae6bf
10sharmashivam <10sharmashivam@gmail.com>
2024-11-12 22:49:40 +0530

    set up the new plugins/community folder structure
    Signed-off-by: 10sharmashivam <10sharmashivam@gmail.com>

---
+++ plugins/community/flytekit-soda.io/flytekitplugins/soda/__init__.py
@@ -0,0 +1,14 @@
+"""
+.. currentmodule:: flytekitplugins.kfsoda
+
+This package contains the Soda.io plugin for Flytekit, allowing Flyte users to run data quality scans.
+
+.. autosummary::
+   :template: custom.rst
+   :toctree: generated/
+
+   SodaCheckConfig
+   SodaCheckTask
+"""
+
+from .task import SodaCheckConfig, SodaCheckTask
