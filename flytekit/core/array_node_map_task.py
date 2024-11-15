7270d18926057aea0576fee3c4737a987f186daf
Paul Dittamo <pvdittamo@gmail.com>
2024-11-14 21:59:57 -0800

    set is_original_sub_node_interface + FULL_STATE for python function task extensions
    Signed-off-by: Paul Dittamo <pvdittamo@gmail.com>

--- flytekit/core/array_node_map_task.py
+++ flytekit/core/array_node_map_task.py
@@ -8,6 +8,7 @@ from contextlib import contextmanager
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Set, Union, cast
 
 from flyteidl.core import tasks_pb2
+from flyteidl.core import workflow_pb2 as _core_workflow
 
 from flytekit.configuration import SerializationSettings
 from flytekit.core import tracker
@@ -105,6 +106,15 @@ class ArrayNodeMapTask(PythonTask):
         self._min_success_ratio: Optional[float] = min_success_ratio
         self._collection_interface = collection_interface
 
+        if (
+            type(python_function_task) in {PythonFunctionTask, PythonInstanceTask}
+            or isinstance(python_function_task, functools.partial)
+            and type(python_function_task.func) in {PythonFunctionTask, PythonInstanceTask}
+        ):
+            self._execution_mode = _core_workflow.ArrayNode.MINIMAL_STATE
+        else:
+            self._execution_mode = _core_workflow.ArrayNode.FULL_STATE
+
         if "metadata" not in kwargs and actual_task.metadata:
             kwargs["metadata"] = actual_task.metadata
         if "security_ctx" not in kwargs and actual_task.security_context:
@@ -152,6 +162,14 @@ class ArrayNodeMapTask(PythonTask):
     @property
     def bound_inputs(self) -> Set[str]:
         return self._bound_inputs
+
+    @property
+    def execution_mode(self) -> _core_workflow.ArrayNode.ExecutionMode:
+        return self._execution_mode
+
+    @property
+    def is_original_sub_node_interface(self) -> bool:
+        return False
 
     def get_extended_resources(self, settings: SerializationSettings) -> Optional[tasks_pb2.ExtendedResources]:
         return self.python_function_task.get_extended_resources(settings)
