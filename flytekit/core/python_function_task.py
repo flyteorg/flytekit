1ab62abd9a58e76a81d6ef24ea630b64da114fad
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2024-11-22 09:57:20 -0800

    add back import
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- flytekit/core/python_function_task.py
+++ flytekit/core/python_function_task.py
@@ -543,6 +543,7 @@ class EagerAsyncPythonFunctionTask(AsyncPythonFunctionTask[T], metaclass=FlyteTr
 
     def execute(self, **kwargs) -> Any:
         from flytekit.remote.remote import FlyteRemote
+        from flytekit.experimental.eager_function import _internal_demo_remote
 
         # client = ctx.flyte_client
         remote = FlyteRemote.for_sandbox(default_project="flytesnacks", default_domain="development")
