b559fc918aeeaf04b344b9e2025d1dabd934d57c
Future-Outlier <eric901201@gmail.com>
2024-11-27 22:54:06 +0800

    remove dummy deck
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- flytekit/deck/deck.py
+++ flytekit/deck/deck.py
@@ -11,41 +11,6 @@ from flytekit.tools.interactive import ipython_check
 
 OUTPUT_DIR_JUPYTER_PREFIX = "jupyter"
 DECK_FILE_NAME = "deck.html"
-DUMMY_DECK_HTML = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Flytekit Status</title>
-    <style>
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: Arial, sans-serif;
-            background-color: #f0f0f0;
-        }
-        .message {
-            background-color: #ffffff;
-            padding: 20px;
-            border: 1px solid #cccccc;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-    <div class="message">
-        <p> Flyte decks have not been created yet. </p>
-    </div>
-</body>
-</html>
-"""
-
 
 class DeckField(str, enum.Enum):
     """
