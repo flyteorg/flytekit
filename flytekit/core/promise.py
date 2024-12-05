802bae15e91f349a4e5c1609124970d8077062a7
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2024-12-04 17:01:12 -0800

    try redirecting back (#2978)
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- flytekit/core/promise.py
+++ flytekit/core/promise.py
@@ -1482,7 +1482,10 @@ def flyte_entity_call_handler(
     ctx = FlyteContextManager.current_context()
     # This handles the case where we call other entities from within a running eager task.
     if ctx.execution_state and ctx.execution_state.mode == ExecutionState.Mode.EAGER_EXECUTION:
-        raise AssertionError("shouldn't be here")
+        # call the blocking version of the async call handler
+        # This is a recursive call, the async handler also calls this function, so this conditional must match
+        # the one in the async function perfectly, otherwise you'll get infinite recursion.
+        loop_manager.run_sync(async_flyte_entity_call_handler, entity, **kwargs)
 
     if ctx.execution_state and (
         ctx.execution_state.mode == ExecutionState.Mode.TASK_EXECUTION
