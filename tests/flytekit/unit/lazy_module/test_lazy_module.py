bfffc853ca52cc2a4e24a66acb4a9f0dcc130a58
Kevin Su <pingsutw@apache.org>
2024-12-06 14:01:23 -0800

    update test
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- tests/flytekit/unit/lazy_module/test_lazy_module.py
+++ tests/flytekit/unit/lazy_module/test_lazy_module.py
@@ -1,3 +1,4 @@
+import sys
 import pytest
 from flytekit.lazy_import.lazy_module import LazyModule, lazy_module, is_imported
 
@@ -6,6 +7,7 @@ def test_lazy_module():
     mod = lazy_module("click")
     assert mod.__name__ == "click"
     mod = lazy_module("fake_module")
+    sys.modules["fake_module"] = mod
     assert not is_imported("fake_module")
     assert isinstance(mod, LazyModule)
     with pytest.raises(ImportError, match="Module fake_module is not yet installed."):
