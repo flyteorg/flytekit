cad3d33923b099ada182352f401562215bedaeea
Jan Fiedler <jan@union.ai>
2024-12-06 18:16:43 -0800

    remove optional from k8s_gpu_resource_key
    Signed-off-by: Jan Fiedler <jan@union.ai>

--- flytekit/core/resources.py
+++ flytekit/core/resources.py
@@ -106,7 +106,7 @@ def pod_spec_from_resources(
     k8s_pod_name: str,
     requests: Optional[Resources] = None,
     limits: Optional[Resources] = None,
-    k8s_gpu_resource_key: Optional[str] = "nvidia.com/gpu",
+    k8s_gpu_resource_key: str = "nvidia.com/gpu",
 ) -> dict[str, Any]:
     def _construct_k8s_pods_resources(resources: Optional[Resources], k8s_gpu_resource_key: str):
         if resources is None:
