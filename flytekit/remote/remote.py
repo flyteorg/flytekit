9a89fd39acddf71b05f10896f967fb8e763b731a
Thomas Newton <thomas.w.newton@gmail.com>
2024-12-11 16:54:43 +0000

    GH-5989: Fix task registration with `node_dependency_hints` (#2992)
    * Add a test
    
    Signed-off-by: Thomas Newton <thomas.w.newton@gmail.com>
    
    * Fix
    
    Signed-off-by: Thomas Newton <thomas.w.newton@gmail.com>
    
    * Also test workflows
    
    Signed-off-by: Thomas Newton <thomas.w.newton@gmail.com>
    
    * Fix missing import
    
    Signed-off-by: Thomas Newton <thomas.w.newton@gmail.com>
    
    * Fix via make unit_test
    
    Signed-off-by: Thomas Newton <thomas.w.newton@gmail.com>
    
    * Autoformat
    
    Signed-off-by: Thomas Newton <thomas.w.newton@gmail.com>
    
    ---------
    
    Signed-off-by: Thomas Newton <thomas.w.newton@gmail.com>

--- flytekit/remote/remote.py
+++ flytekit/remote/remote.py
@@ -524,7 +524,9 @@ class FlyteRemote(object):
 
                 if node.branch_node:
                     get_launch_plan_from_branch(node.branch_node, node_launch_plans)
-        return FlyteWorkflow.promote_from_closure(compiled_wf, node_launch_plans)
+        flyte_workflow = FlyteWorkflow.promote_from_closure(compiled_wf, node_launch_plans)
+        flyte_workflow.template._id = workflow_id
+        return flyte_workflow
 
     def _upgrade_launchplan(self, lp: launch_plan_models.LaunchPlan) -> FlyteLaunchPlan:
         """
@@ -863,13 +865,17 @@ class FlyteRemote(object):
         cp_task_entity_map = OrderedDict(filter(lambda x: isinstance(x[1], task_models.TaskSpec), m.items()))
         tasks = []
         loop = asyncio.get_running_loop()
-        for entity, cp_entity in cp_task_entity_map.items():
+        for task_entity, cp_entity in cp_task_entity_map.items():
             tasks.append(
                 loop.run_in_executor(
                     None,
-                    functools.partial(self.raw_register, cp_entity, serialization_settings, version, og_entity=entity),
+                    functools.partial(
+                        self.raw_register, cp_entity, serialization_settings, version, og_entity=task_entity
+                    ),
                 )
             )
+            if task_entity == entity:
+                registered_entity = await tasks[-1]
 
         identifiers_or_exceptions = []
         identifiers_or_exceptions.extend(await asyncio.gather(*tasks, return_exceptions=True))
@@ -882,15 +888,17 @@ class FlyteRemote(object):
                 raise ie
         # serial register
         cp_other_entities = OrderedDict(filter(lambda x: not isinstance(x[1], task_models.TaskSpec), m.items()))
-        for entity, cp_entity in cp_other_entities.items():
+        for non_task_entity, cp_entity in cp_other_entities.items():
             try:
                 identifiers_or_exceptions.append(
-                    self.raw_register(cp_entity, serialization_settings, version, og_entity=entity)
+                    self.raw_register(cp_entity, serialization_settings, version, og_entity=non_task_entity)
                 )
             except RegistrationSkipped as e:
                 logger.info(f"Skipping registration... {e}")
                 continue
-        return identifiers_or_exceptions[-1]
+            if non_task_entity == entity:
+                registered_entity = identifiers_or_exceptions[-1]
+        return registered_entity
 
     def register_task(
         self,
