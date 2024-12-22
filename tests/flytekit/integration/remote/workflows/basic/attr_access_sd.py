865c3902c58ae4554cf2d29956b59d0921f661af
JiaWei Jiang <waynechuang97@gmail.com>
2024-12-22 12:33:22 +0800

    Try imagespec with commit sha of corresponding fix
    Signed-off-by: JiaWei Jiang <waynechuang97@gmail.com>

--- tests/flytekit/integration/remote/workflows/basic/attr_access_sd.py
+++ tests/flytekit/integration/remote/workflows/basic/attr_access_sd.py
@@ -4,8 +4,22 @@ Access StructuredDataset attribute from a dataclass.
 from dataclasses import dataclass, field
 
 import pandas as pd
-from flytekit import task, workflow
+from flytekit import task, workflow, ImageSpec
 from flytekit.types.structured import StructuredDataset
+
+
+# Build docker image and push to the local registry
+flytekit_hash = "4e467b9ecd4172a77965780ea4783acea40cb502"
+flytekit = f"git+https://github.com/flyteorg/flytekit.git@{flytekit_hash}"
+image = ImageSpec(
+    packages=[
+        flytekit,
+        "pandas",
+        "pyarrow"
+    ],
+    apt_packages=["git"],
+    registry="localhost:30000",
+)
 
 
 S3_URI = "s3://my-s3-bucket/df.parquet"
@@ -16,7 +30,7 @@ class DC:
     sd: StructuredDataset = field(default_factory=lambda: StructuredDataset(uri=S3_URI, file_format="parquet"))
 
 
-@task
+@task(container_image=image)
 def t_sd_attr(sd: StructuredDataset) -> StructuredDataset:
     print("sd:", sd.open(pd.DataFrame).all())
 
