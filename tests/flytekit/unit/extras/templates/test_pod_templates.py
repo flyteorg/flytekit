067c902e5f831b9cd0792eacab3fd427e7b11b2f
Grantham Taylor <54340816+granthamtaylor@users.noreply.github.com>
2024-12-30 13:41:45 -0500

    Update tests/flytekit/unit/extras/templates/test_pod_templates.py
    Co-authored-by: Flyte Bot <admin@flyte.org>

--- tests/flytekit/unit/extras/templates/test_pod_templates.py
+++ tests/flytekit/unit/extras/templates/test_pod_templates.py
@@ -2,9 +2,13 @@ from flytekit.extras.pod_templates import attach_shm
 from flytekit.core.task import task
 
 def test_attach_shm():
-
+ 
     shm = attach_shm("SHM", "5Gi")
-
-    @task(pod_template=shm)
+    assert shm.name == "SHM"
+    assert shm.size == "5Gi"
+ 
     def my_task():
         pass
+
+    # Verify pod template is attached to task
+    assert my_task.pod_template == shm
