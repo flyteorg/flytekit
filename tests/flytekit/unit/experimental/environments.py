6761d6b883ec29873759b3fff6eb24d0756211bf
granthamtaylor <granthamtaylor@icloud.com>
2024-12-30 10:18:10 -0500

    lint unit test

--- tests/flytekit/unit/experimental/environments.py
+++ tests/flytekit/unit/experimental/environments.py
@@ -2,9 +2,9 @@ from flytekit.core.environments import Environment, inherit
 
 
 def test_basic_environment():
-    
+
     env = Environment(cache=False)
-    
+
     @env.task
     def foo():
         pass
@@ -14,11 +14,11 @@ def test_basic_environment():
         pass
 
 def test_extended_environment():
-    
+
     env = Environment(cache=False)
-    
+
     other = env.extend(cache=True)
-    
+
     @other.task
     def foo():
         pass
@@ -28,11 +28,11 @@ def test_extended_environment():
         pass
 
 def test_updated_environment():
-    
+
     env = Environment(cache=False)
-    
+
     env.update(cache=True)
-    
+
     @env.task
     def foo():
         pass
@@ -42,18 +42,18 @@ def test_updated_environment():
         pass
 
 def test_show_environment():
-    
+
     env = Environment(cache=False)
-    
+
     env.show()
 
 def test_inherit():
-    
+
     old_config = {"cache": False, "timeout": 10}
-    
+
     new_config = {"cache": True}
-    
+
     combined = inherit(old_config, new_config)
-    
+
     assert combined["cache"] == True
     assert combined["timeout"] == 10
