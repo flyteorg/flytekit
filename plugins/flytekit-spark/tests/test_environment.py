71b4c6672954baf30663bba7b02de2955ef966b5
granthamtaylor <granthamtaylor@icloud.com>
2025-01-06 12:46:18 -0500

    simplify spark unit test

--- plugins/flytekit-spark/tests/test_environment.py
+++ plugins/flytekit-spark/tests/test_environment.py
@@ -15,22 +15,6 @@ def reset_spark_session() -> None: # type: ignore
 
 
 def test_spark_task(reset_spark_session):
-    databricks_conf = {
-        "name": "flytekit databricks plugin example",
-        "new_cluster": {
-            "spark_version": "11.0.x-scala2.12",
-            "node_type_id": "r3.xlarge",
-            "aws_attributes": {"availability": "ON_DEMAND"},
-            "num_workers": 4,
-            "docker_image": {"url": "pingsutw/databricks:latest"},
-        },
-        "timeout_seconds": 3600,
-        "max_retries": 1,
-        "spark_python_task": {
-            "python_file": "dbfs:///FileStore/tables/entrypoint-1.py",
-            "parameters": "ls",
-        },
-    }
 
     env = Environment(
         task_config=Spark(
