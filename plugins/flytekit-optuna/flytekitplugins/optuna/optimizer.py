62e3123b4f75045e49fc781b9fca950313b37e26
granthamtaylor <granthamtaylor@icloud.com>
2025-01-06 13:29:37 -0500

    run linter

--- plugins/flytekit-optuna/flytekitplugins/optuna/optimizer.py
+++ plugins/flytekit-optuna/flytekitplugins/optuna/optimizer.py
@@ -1,11 +1,10 @@
 import asyncio
+import inspect
 from dataclasses import dataclass
 from types import SimpleNamespace
-from typing import Optional, Union, Any
-import inspect
+from typing import Any, Optional, Union
 
 import optuna
-
 from flytekit import PythonFunctionTask
 from flytekit.core.workflow import PythonFunctionWorkflow
 from flytekit.exceptions.eager import EagerException
@@ -74,30 +73,22 @@ class Optimizer:
             func = self.objective._workflow_function
         else:
             raise ValueError("objective must be a PythonFunctionTask or PythonFunctionWorkflow")
-        
+
         signature = inspect.signature(func)
 
         if signature.return_annotation is float:
             if len(self.study.directions) != 1:
-                raise ValueError(
-                    "the study must have a single objective if objective returns a single float"
-                )
+                raise ValueError("the study must have a single objective if objective returns a single float")
 
         elif isinstance(args := signature.return_annotation.__args__, tuple):
             if len(args) != len(self.study.directions):
-                raise ValueError(
-                    "objective must return the same number of directions in the study"
-                )
+                raise ValueError("objective must return the same number of directions in the study")
 
             if not all(arg is float for arg in args):
-                raise ValueError(
-                    "objective function must return a float or tuple of floats"
-                )
+                raise ValueError("objective function must return a float or tuple of floats")
 
         else:
-            raise ValueError(
-                "objective function must return a float or tuple of floats"
-            )
+            raise ValueError("objective function must return a float or tuple of floats")
 
     async def __call__(self, **inputs: Any):
         """
