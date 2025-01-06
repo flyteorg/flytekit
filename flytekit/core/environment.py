c26ce0aec21a51eefff4e65c4e16c157a5d57e71
granthamtaylor <granthamtaylor@icloud.com>
2025-01-06 16:44:40 -0500

    change signatures

--- flytekit/core/environment.py
+++ flytekit/core/environment.py
@@ -64,7 +64,7 @@ class Environment:
         return self.__class__(**inherit(self.overrides, overrides))
 
     @forge(task)
-    def __call__(self, _task_function: Union[Callable, None] = None, /, **overrides) -> PythonFunctionTask:
+    def __call__(self, _task_function: Union[Callable, None] = None, /, **overrides) -> Callable[..., PythonFunctionTask[T]]:
         # no additional overrides are passed
         if _task_function is not None:
             if callable(_task_function):
@@ -91,7 +91,7 @@ class Environment:
     task = __call__
 
     @forge(dynamic)
-    def dynamic(self, _task_function: Union[Callable, None] = None, /, **overrides) -> PythonFunctionTask:
+    def dynamic(self, _task_function: Union[Callable, None] = None, /, **overrides) -> Callable[..., PythonFunctionTask[T]]:
         # no additional overrides are passed
         if _task_function is not None:
             if callable(_task_function):
