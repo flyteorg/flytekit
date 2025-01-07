356944d6e5048ff625570cd2dce3e60a7e6706ac
granthamtaylor <granthamtaylor@icloud.com>
2025-01-07 18:36:54 -0500

    allow for some suggestions to be defined as kwargs during bundling

--- plugins/flytekit-optuna/flytekitplugins/optuna/optimizer.py
+++ plugins/flytekit-optuna/flytekitplugins/optuna/optimizer.py
@@ -1,10 +1,11 @@
 import asyncio
-import inspect
 from dataclasses import dataclass
 from types import SimpleNamespace
-from typing import Any, Optional, Union
+from typing import Optional, Union, Any
+import inspect
 
 import optuna
+
 from flytekit import PythonFunctionTask
 from flytekit.core.workflow import PythonFunctionWorkflow
 from flytekit.exceptions.eager import EagerException
@@ -72,29 +73,42 @@ class Optimizer:
         elif isinstance(self.objective, PythonFunctionWorkflow):
             func = self.objective._workflow_function
         else:
-            raise ValueError("objective must be a PythonFunctionTask or PythonFunctionWorkflow")
+            raise ValueError(
+                "objective must be a PythonFunctionTask or PythonFunctionWorkflow"
+            )
 
         signature = inspect.signature(func)
 
         if signature.return_annotation is float:
             if len(self.study.directions) != 1:
-                raise ValueError("the study must have a single objective if objective returns a single float")
+                raise ValueError(
+                    "the study must have a single objective if objective returns a single float"
+                )
 
         elif isinstance(args := signature.return_annotation.__args__, tuple):
             if len(args) != len(self.study.directions):
-                raise ValueError("objective must return the same number of directions in the study")
+                raise ValueError(
+                    "objective must return the same number of directions in the study"
+                )
 
             if not all(arg is float for arg in args):
-                raise ValueError("objective function must return a float or tuple of floats")
+                raise ValueError(
+                    "objective function must return a float or tuple of floats"
+                )
 
         else:
-            raise ValueError("objective function must return a float or tuple of floats")
+            raise ValueError(
+                "objective function must return a float or tuple of floats"
+            )
 
-    async def __call__(self, suggestions: Optional[dict[str, Suggestion]] = None, /, **inputs: Any):
+    async def __call__(
+        self, suggestions: Optional[dict[str, Suggestion]] = None, /, **inputs: Any
+    ):
         """
         Asynchronously executes the objective function remotely.
         Parameters:
-            **inputs: inputs to objective function
+            suggestions (Optional[dict[str, Suggestion]]): bundled suggestions to objective function
+            **inputs: other inputs to objective function
         """
 
         if suggestions is not None:
@@ -107,17 +121,13 @@ class Optimizer:
                 if not isinstance(value, Suggestion):
                     raise ValueError(f"suggestion must be of type {type(value)}")
 
-            for value in inputs.values():
-                if isinstance(value, Suggestion):
-                    raise ValueError(
-                        "you must provide all suggestions as a positional argument of type dict[str, Suggestion]"
-                    )
-
         # create semaphore to manage concurrency
         semaphore = asyncio.Semaphore(self.concurrency)
 
         # create list of async trials
-        trials = [self.spawn(semaphore, suggestions, **inputs) for _ in range(self.n_trials)]
+        trials = [
+            self.spawn(semaphore, suggestions, **inputs) for _ in range(self.n_trials)
+        ]
 
         # await all trials to complete
         await asyncio.gather(*trials)
@@ -135,8 +145,8 @@ class Optimizer:
 
             if suggestions is not None:
                 inputs["suggestions"] = self.suggest(trial, suggestions)
-            else:
-                inputs = self.suggest(trial, inputs)
+
+            inputs = self.suggest(trial, inputs)
 
             try:
                 # schedule the trial
--- plugins/flytekit-optuna/tests/test_optimizer.py
+++ plugins/flytekit-optuna/tests/test_optimizer.py
@@ -34,11 +34,11 @@ def test_local_exec():
 
 
 @fl.task(container_image=image)
-async def bundled_objective(suggestions: dict[str, Union[int, float]], power: int) -> float:
+async def bundled_objective(suggestions: dict[str, Union[int, float]], z: int, power: int) -> float:
 
     # building out a large set of typed inputs is exhausting, so we can just use a dict
 
-    x, y, z = suggestions["x"], suggestions["y"], suggestions["z"]
+    x, y = suggestions["x"], suggestions["y"]
 
     return math.log((((x - 5) ** 2) + (y + 4) ** 4 + (3 * z - 3) ** 2)) ** power
 
@@ -50,10 +50,9 @@ async def train(concurrency: int, n_trials: int) -> float:
     suggestions = {
         "x": suggest.float(low=-10, high=10),
         "y": suggest.integer(low=-10, high=10),
-        "z": suggest.category([-5, 0, 3, 6, 9]),
     }
 
-    await optimizer(suggestions, power=2)
+    await optimizer(suggestions, z=suggest.category([-5, 0, 3, 6, 9]), power=2)
 
     return optimizer.study.best_value
 
