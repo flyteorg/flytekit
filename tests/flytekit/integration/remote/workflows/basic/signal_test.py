58c2e915a2e93c5cb3a282e40c378f097f24afd2
mao3267 <chenvincent610@gmail.com>
2025-01-11 10:30:04 +0800

    fix: update comments
    Signed-off-by: mao3267 <chenvincent610@gmail.com>

--- tests/flytekit/integration/remote/workflows/basic/signal_test.py
+++ tests/flytekit/integration/remote/workflows/basic/signal_test.py
@@ -13,7 +13,5 @@ def signal_test_wf(data: typing.List[float]) -> dict:
     # Define a "review-passes" approve node so that a human can review
     # the title before finalizing it.
     approve(upstream_item=title_input, name="review-passes", timeout=timedelta(hours=1))
-    
-    # This conditional returns the finalized report if the review passes,
-    # otherwise it returns an invalid report output.
+
     return reporting_wf(title_input, data)
