d8c318cb8e6f77ae1b7cbc19153da099534d8f73
granthamtaylor <granthamtaylor@icloud.com>
2025-01-11 15:40:33 -0500

    whitespace

--- plugins/flytekit-optuna/tests/test_validation.py
+++ plugins/flytekit-optuna/tests/test_validation.py
@@ -19,55 +19,55 @@ def test_concurrency():
 
 
 def test_n_trials():
-    
+
     with pytest.raises(ValueError):
         Optimizer(objective, concurrency=3, n_trials=-10)
-    
+
     with pytest.raises(ValueError):
         Optimizer(objective, concurrency=3, n_trials="abc")
 
-    
+
 
 def test_study():
-    
+
     with pytest.raises(ValueError):
         Optimizer(objective, concurrency=3, n_trials=10, study="abc")
 
 
 
 def test_delay():
-    
+
     with pytest.raises(ValueError):
         Optimizer(objective, concurrency=3, n_trials=10, delay=-1)
-    
+
     with pytest.raises(ValueError):
         Optimizer(objective, concurrency=3, n_trials=10, delay="abc")
 
 
 
 def test_objective():
-    
+
     @fl.workflow
     def workflow(x: int, y: int, z: int, power: int) -> float:
         return 1.0
 
-    
+
     with pytest.raises(ValueError):
         Optimizer(workflow, concurrency=3, n_trials=10)
 
     @fl.task
     def mistyped_objective(x: int, y: int, z: int, power: int) -> str:
         return "abc"
-    
+
     with pytest.raises(ValueError):
         Optimizer(mistyped_objective, concurrency=3, n_trials=10)
 
 
 
 def test_callback():
-    
+
     def callback(value: float):
         return 1.0
-    
+
     with pytest.raises(ValueError):
         Optimizer(callback, concurrency=3, n_trials=10)
