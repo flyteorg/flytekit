4044c95fd28ccb2139151fe425587ee946a4dc1c
Fabio Grätz <fabiogratz@googlemail.com>
2025-01-14 21:01:10 +0100

    Fix: Always propagate pytorch task worker process exception timestamp to task exception
    Signed-off-by: Fabio Grätz <fabiogratz@googlemail.com>

--- plugins/flytekit-kf-pytorch/tests/test_elastic_task.py
+++ plugins/flytekit-kf-pytorch/tests/test_elastic_task.py
@@ -276,3 +276,37 @@ def test_omp_num_threads(start_method: str) -> None:
         assert os.environ["OMP_NUM_THREADS"] == "42"
 
     test_task_omp_set()
+
+
+def test_exception_timestamp() -> None:
+    """Test that the timestamp of the worker process exception is propagated to the task exception."""
+    @task(
+        task_config=Elastic(
+            nnodes=1,
+            nproc_per_node=2,
+        )
+    )
+    def test_task():
+        raise Exception("Test exception")
+
+    with pytest.raises(Exception) as e:
+        test_task()
+
+    assert e.value.timestamp is not None
+
+
+def test_recoverable_exception_timestamp() -> None:
+    """Test that the timestamp of the worker process exception is propagated to the task exception."""
+    @task(
+        task_config=Elastic(
+            nnodes=1,
+            nproc_per_node=2,
+        )
+    )
+    def test_task():
+        raise FlyteRecoverableException("Recoverable test exception")
+
+    with pytest.raises(Exception) as e:
+        test_task()
+
+    assert e.value.timestamp is not None
