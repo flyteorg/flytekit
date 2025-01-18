ef7ca54dba2ea3b257670c8b16ce2f1af9c28f36
taieeuu <taieeuu@gmail.com>
2025-01-19 01:37:49 +0800

    feat: add conditions for executing pyflyte-fast-execute.
    Signed-off-by: taieeuu <taieeuu@gmail.com>

--- flytekit/tools/translator.py
+++ flytekit/tools/translator.py
@@ -170,7 +170,15 @@ def get_serializable_task(
             # ly for certain kinds of tasks. Specifically,
             # tasks that rely on user code defined in the container. This should be encapsulated by the auto container
             # parent class
-            container._args = prefix_with_fast_execute(settings, container.args)
+            if (
+                (not hasattr(entity, "container_image"))
+                or (not entity.container_image)
+                or (entity.container_image and not entity.container_image.source_root)
+            ):
+                container._args = prefix_with_fast_execute(settings, container.args)
+
+            # elif not entity.container_image.source_root:
+            #     container._args = prefix_with_fast_execute(settings, container.args)
 
         # If the pod spec is not None, we have to get it again, because the one we retrieved above will be incorrect.
         # The reason we have to call get_k8s_pod again, instead of just modifying the command in this file, is because
