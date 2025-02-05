ad7e95a6f23fe3cfeb08bd30963ad52bb3964c28
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2025-02-05 15:54:32 -0800

    fmt
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- flytekit/remote/remote.py
+++ flytekit/remote/remote.py
@@ -58,6 +58,7 @@ from flytekit.core.python_function_task import PythonFunctionTask
 from flytekit.core.reference_entity import ReferenceSpec
 from flytekit.core.task import ReferenceTask
 from flytekit.core.tracker import extract_task_module
+from flytekit.core.type_cast_checking import literal_types_match
 from flytekit.core.type_engine import LiteralsResolver, TypeEngine
 from flytekit.core.workflow import PythonFunctionWorkflow, ReferenceWorkflow, WorkflowBase, WorkflowFailurePolicy
 from flytekit.exceptions import user as user_exceptions
@@ -249,6 +250,19 @@ def _get_pickled_target_dict(
             queue.append(entity.flyte_entity)
     md5_bytes = hashlib.md5(cloudpickle.dumps(pickled_target_dict)).digest()
     return md5_bytes, pickled_target_dict
+
+
+def better_guess_type_hint(input_val: typing.Any, target_literal_type: type_models.LiteralType) -> typing.Type:
+    """
+    Try to be smarter about guessing the type of the input (and hence the transformer).
+    If the literal type from the transformer for type(v), matches the literal type of the interface, then we
+    can use type(). Otherwise, fall back to guess python type from the literal type.
+    """
+    transformer = TypeEngine.get_transformer(type(input_val))
+    inferred_literal_type = transformer.get_literal_type(input_val)
+    if literal_types_match(inferred_literal_type, target_literal_type):
+        return type(input_val)
+    return TypeEngine.guess_python_type(target_literal_type)
 
 
 class FlyteRemote(object):
@@ -1514,7 +1528,7 @@ class FlyteRemote(object):
                 else:
                     if k not in type_hints:
                         try:
-                            type_hints[k] = TypeEngine.guess_python_type(input_flyte_type_map[k].type)
+                            type_hints[k] = better_guess_type_hint(v, input_flyte_type_map[k].type)
                         except ValueError:
                             logger.debug(f"Could not guess type for {input_flyte_type_map[k].type}, skipping...")
                     variable = entity.interface.inputs.get(k)
