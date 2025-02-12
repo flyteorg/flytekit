16463d29ec3043062c541096c8d376f5c853a681
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2025-02-12 12:18:37 -0800

    Apply suggestions from code review

--- tests/flytekit/unit/cli/pyflyte/workflow.py
+++ tests/flytekit/unit/cli/pyflyte/workflow.py
@@ -8,7 +8,7 @@ import pandas as pd
 from dataclasses_json import DataClassJsonMixin
 from typing_extensions import Annotated
 
-from flytekit import kwtypes, task, workflow, LaunchPlan
+from flytekit import kwtypes, task, workflow
 from flytekit.types.directory import FlyteDirectory
 from flytekit.types.file import FlyteFile
 from flytekit.types.structured import StructuredDataset
