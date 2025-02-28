d8b8e73774ba83c4f3c0811ffec75f6ffa17c784
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2025-02-28 11:25:15 -0800

    debug
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- flytekit/core/python_function_task.py
+++ flytekit/core/python_function_task.py
@@ -731,7 +731,7 @@ class EagerFailureHandlerTask(PythonAutoContainerTask, metaclass=FlyteTrackedABC
         logger.warning(f"Cleaning up potentially still running tasks for execution {name} in {project}/{domain}")
         import os
         from union._config import _get_union_api_env_var
-        api_value_tuple := _get_union_api_env_var()
+        api_value_tuple = _get_union_api_env_var()
         print(f"!!!!!!!!!!!!!!!!??????????>>>>>>>>>>>>!!!!!!!!!!! {os.environ['_UNION_EAGER_API_KEY']}", flush=True)
         print(f"!!!!!!!!!!!!!!!!??????????>>>>!!!!! {api_value_tuple}", flush=True)
         remote = get_plugin().get_remote(config=None, project=project, domain=domain)
