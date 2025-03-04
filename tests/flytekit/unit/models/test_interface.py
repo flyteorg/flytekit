15cc1b6888fbf6ebda5ca98c081c4b4ce2a9773d
Paul Dittamo <37558497+pvditt@users.noreply.github.com>
2025-03-04 10:24:39 -0800

    don't transform bound inputs to list for remote entities (#3168)
    * don't transform bound inputs to list for remote entities
    
    Signed-off-by: Paul Dittamo <pvdittamo@gmail.com>
    
    * clean up
    
    Signed-off-by: Paul Dittamo <pvdittamo@gmail.com>
    
    ---------
    
    Signed-off-by: Paul Dittamo <pvdittamo@gmail.com>

--- tests/flytekit/unit/models/test_interface.py
+++ tests/flytekit/unit/models/test_interface.py
@@ -51,8 +51,18 @@ def test_typed_interface(literal_type):
     assert len(deserialized_typed_interface.inputs) == 1
     assert len(deserialized_typed_interface.outputs) == 2
 
-    deserialized_typed_interface_list = typed_interface.transform_interface_to_list()
+    deserialized_typed_interface_list = typed_interface.transform_interface_to_list(set())
     assert deserialized_typed_interface_list.inputs["a"].type == types.LiteralType(collection_type=literal_type)
+    assert deserialized_typed_interface_list.outputs["b"].type == types.LiteralType(collection_type=literal_type)
+    assert deserialized_typed_interface_list.outputs["c"].type == types.LiteralType(collection_type=literal_type)
+    assert deserialized_typed_interface_list.inputs["a"].description == "description1"
+    assert deserialized_typed_interface_list.outputs["b"].description == "description2"
+    assert deserialized_typed_interface_list.outputs["c"].description == "description3"
+    assert len(deserialized_typed_interface_list.inputs) == 1
+    assert len(deserialized_typed_interface_list.outputs) == 2
+
+    deserialized_typed_interface_list = typed_interface.transform_interface_to_list({"a"})
+    assert deserialized_typed_interface_list.inputs["a"].type == literal_type
     assert deserialized_typed_interface_list.outputs["b"].type == types.LiteralType(collection_type=literal_type)
     assert deserialized_typed_interface_list.outputs["c"].type == types.LiteralType(collection_type=literal_type)
     assert deserialized_typed_interface_list.inputs["a"].description == "description1"
