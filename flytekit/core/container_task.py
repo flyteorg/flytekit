a1ff8b6555d7d2952ac5e52278cd0b9de2306044
Thomas J. Fan <thomasjpfan@gmail.com>
2025-03-06 12:45:05 -0500

    Add resources support to ContainerTask
    Signed-off-by: Thomas J. Fan <thomasjpfan@gmail.com>

--- flytekit/core/container_task.py
+++ flytekit/core/container_task.py
@@ -60,6 +60,7 @@ class ContainerTask(PythonTask):
         pod_template: Optional["PodTemplate"] = None,
         pod_template_name: Optional[str] = None,
         local_logs: bool = False,
+        resources: Optional[Resources] = None,
         **kwargs,
     ):
         sec_ctx = None
@@ -90,9 +91,13 @@ class ContainerTask(PythonTask):
         self._outputs = outputs
         self._md_format = metadata_format
         self._io_strategy = io_strategy
-        self._resources = ResourceSpec(
-            requests=requests if requests else Resources(), limits=limits if limits else Resources()
-        )
+        if resources is not None:
+            if limits is not None or requests is not None:
+                msg = "`resource` can not be used together with the `limits` or `requests`. Please only set `resource`."
+                raise ValueError(msg)
+            self._resources = ResourceSpec.from_multiple_resource(resources)
+        else:
+            self._resources = ResourceSpec.from_single_resources(requests=requests, limits=limits)
         self.pod_template = pod_template
         self.local_logs = local_logs
 
