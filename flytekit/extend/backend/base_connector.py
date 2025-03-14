78a4aa29ff46383cd2c0d04223d9e45ffa79aa2d
Future-Outlier <eric901201@gmail.com>
2025-03-14 15:15:08 +0800

    solve conflict
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- flytekit/extend/backend/base_connector.py
+++ flytekit/extend/backend/base_connector.py
@@ -21,7 +21,7 @@ from google.protobuf.struct_pb2 import Struct
 from rich.logging import RichHandler
 from rich.progress import Progress
 
-from flytekit import FlyteContext, PythonFunctionTask
+from flytekit import FlyteContext, PythonFunctionTask, logger
 from flytekit.configuration import ImageConfig, SerializationSettings
 from flytekit.core import utils
 from flytekit.core.base_task import PythonTask
@@ -306,6 +306,8 @@ class SyncConnectorExecutorMixin:
         ctx = FlyteContext.current_context()
         ss = ctx.serialization_settings or SerializationSettings(ImageConfig())
         task_template = get_serializable(OrderedDict(), ss, self).template
+        if task_template.metadata.timeout:
+            logger.info("Timeout is not supported for local execution.\n" "Ignoring the timeout.")
         output_prefix = ctx.file_access.get_random_remote_directory()
 
         connector = ConnectorRegistry.get_connector(task_template.type, task_template.task_type_version)
@@ -358,6 +360,8 @@ class AsyncConnectorExecutorMixin:
         from flytekit.tools.translator import get_serializable
 
         task_template = get_serializable(OrderedDict(), ss, self).template
+        if task_template.metadata.timeout:
+            logger.info("Timeout is not supported for local execution.\n" "Ignoring the timeout.")
         self._connector = ConnectorRegistry.get_connector(task_template.type, task_template.task_type_version)
 
         resource_meta = local_connector_loop.run_until_complete(
