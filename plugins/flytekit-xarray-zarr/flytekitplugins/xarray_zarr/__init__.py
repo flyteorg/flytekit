91a884a1c3c01e88bf4e5d1136ef03c1b9ba51b4
Len Strnad <ljstrnadiii@gmail.com>
2025-03-20 15:59:51 -0600

    init xarray plugin
    Signed-off-by: Len Strnad <ljstrnadiii@gmail.com>

---
+++ plugins/flytekit-xarray-zarr/flytekitplugins/xarray_zarr/__init__.py
@@ -0,0 +1,14 @@
+"""
+.. currentmodule:: flytekitplugins.geopandas
+
+This package contains things that are useful when extending Flytekit.
+
+.. autosummary::
+   :template: custom.rst
+   :toctree: generated/
+
+   GeoPandasDecodingHandler
+   GeoPandasEncodingHandler
+"""
+
+from .xarray_transformers import XarrayDaZarrTypeTransformer, XarrayZarrTypeTransformer
