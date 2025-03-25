c2bdbd3cea44e8c218a417149fe787cc9e860e11
Len Strnad <ljstrnadiii@gmail.com>
2025-03-25 12:20:52 -0600

    fix: use `get_random_remote_path` in geopandas plugin (#3208)
    * use `get_random_remote_path`
    
    Signed-off-by: Len Strnad <ljstrnadiii@gmail.com>
    
    * fix if uri is dir for tests
    
    Signed-off-by: Len Strnad <ljstrnadiii@gmail.com>
    
    * Path takes away bucket name
    
    Signed-off-by: Len Strnad <ljstrnadiii@gmail.com>
    
    ---------
    
    Signed-off-by: Len Strnad <ljstrnadiii@gmail.com>
    Signed-off-by: chmod77 <chmod777.ke@gmail.com>

--- plugins/flytekit-geopandas/flytekitplugins/geopandas/gdf_transformers.py
+++ plugins/flytekit-geopandas/flytekitplugins/geopandas/gdf_transformers.py
@@ -1,3 +1,4 @@
+import os
 import typing
 from pathlib import Path
 
@@ -39,12 +40,10 @@ class GeoPandasEncodingHandler(StructuredDatasetEncoder):
         structured_dataset: StructuredDataset,
         structured_dataset_type: StructuredDatasetType,
     ) -> literals.StructuredDataset:
-        uri = typing.cast(str, structured_dataset.uri) or ctx.file_access.join(
-            ctx.file_access.raw_output_prefix, ctx.file_access.get_random_string()
-        )
-        if not ctx.file_access.is_remote(uri):
-            Path(uri).mkdir(parents=True, exist_ok=True)
-        uri = str(Path(uri) / "data.parquet")
+        dir = ctx.file_access.get_random_remote_directory()
+        if not ctx.file_access.is_remote(dir):
+            Path(dir).mkdir(parents=True, exist_ok=True)
+        uri = os.path.join(str(dir), "data.parquet")
         df = typing.cast(gpd.GeoDataFrame, structured_dataset.dataframe)
         df.to_parquet(uri)
         structured_dataset_type.format = PARQUET
