738f88e303c9748827b644c46ee334e036b062a4
Nelson Chen <asd3431090@gmail.com>
2025-03-26 00:51:41 +0800

    adjust logic
    Signed-off-by: Nelson Chen <asd3431090@gmail.com>

--- flytekit/image_spec/default_builder.py
+++ flytekit/image_spec/default_builder.py
@@ -405,15 +405,14 @@ def create_docker_context(image_spec: ImageSpec, tmp_dir: Path):
     else:
         extra_copy_cmds = ""
 
-    if image_spec.builder_config.get("uv_image") is not None:
-        uv_image = image_spec.builder_config.get("uv_image")
-    else:
-        uv_image = "ghcr.io/astral-sh/uv:0.5.1"
+    uv_image = "ghcr.io/astral-sh/uv:0.5.1"
+    micromamba_image = "mambaorg/micromamba:2.0.3-debian12-slim"
+    if image_spec.builder_config is not None:
+        if image_spec.builder_config.get("uv_image") is not None:
+            uv_image = image_spec.builder_config.get("uv_image")
 
-    if image_spec.builder_config.get("micromamba_image") is not None:
-        micromamba_image = image_spec.builder_config.get("micromamba_image")
-    else:
-        micromamba_image = "mambaorg/micromamba:2.0.3-debian12-slim"
+        if image_spec.builder_config.get("micromamba_image") is not None:
+            micromamba_image = image_spec.builder_config.get("micromamba_image")
 
     docker_content = DOCKER_FILE_TEMPLATE.substitute(
         UV_PYTHON_INSTALL_COMMAND=uv_python_install_command,
