1108537f18b2fe00cce3769966ebd26e19726058
chmod77 <chmod777.ke@gmail.com>
2025-04-09 18:17:43 +0300

    chore: fix merge conflicts
    Signed-off-by: chmod77 <chmod777.ke@gmail.com>

--- plugins/flytekit-slurm/flytekitplugins/slurm/script/connector.py
+++ plugins/flytekit-slurm/flytekitplugins/slurm/script/connector.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 from typing import Any, Dict, List, Optional, Tuple, Type
 
 from asyncssh import SSHClientConnection
-from asyncssh.sftp import SFTPError, SFTPNoSuchFile
+from asyncssh.sftp import SFTPError
 
 import flytekit
 from flytekit.core.type_engine import TypeEngine
