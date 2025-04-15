854d54aec7c4136b9eb02ce54ae3a7379880d056
rambrus <v-rambrus@expediagroup.com>
2025-04-15 16:46:27 +0200

    Update and add unit tests
    Signed-off-by: rambrus <v-rambrus@expediagroup.com>

--- plugins/flytekit-spark/tests/test_spark_task.py
+++ plugins/flytekit-spark/tests/test_spark_task.py
@@ -4,6 +4,7 @@ from unittest import mock
 import pandas as pd
 import pyspark
 import pytest
+import tempfile
 
 from google.protobuf.json_format import MessageToDict
 from flytekit import PodTemplate
@@ -191,8 +192,46 @@ def test_spark_addPyFile(mock_add_pyfile):
     ) as new_ctx:
         my_spark.pre_execute(new_ctx.user_space_params)
         mock_add_pyfile.assert_called_once()
-        os.remove(os.path.join(os.getcwd(), "flyte_wf.zip"))
 
+@mock.patch("tempfile.mkdtemp", return_value="/tmp/123")
+@mock.patch("shutil.make_archive")
+@mock.patch("pyspark.context.SparkContext.addPyFile")
+def test_spark_archive_created_in_temp_dir(mock_add_pyfile, mock_shutil_make_archive, mock_tempfile_mkdtemp):
+    @task(
+        task_config=Spark(
+            spark_conf={"spark": "1"},
+        )
+    )
+    def my_spark(a: int) -> int:
+        return a
+
+    default_img = Image(name="default", fqn="test", tag="tag")
+    serialization_settings = SerializationSettings(
+        project="project",
+        domain="domain",
+        version="version",
+        env={"FOO": "baz"},
+        image_config=ImageConfig(default_image=default_img, images=[default_img]),
+        fast_serialization_settings=FastSerializationSettings(
+            enabled=True,
+            destination_dir="/User/flyte/workflows",
+            distribution_location="s3://my-s3-bucket/fast/123",
+        ),
+    )
+
+    ctx = context_manager.FlyteContextManager.current_context()
+    with context_manager.FlyteContextManager.with_context(
+        ctx.with_execution_state(
+            ctx.new_execution_state().with_params(
+                mode=ExecutionState.Mode.TASK_EXECUTION
+            )
+        ).with_serialization_settings(serialization_settings)
+    ) as new_ctx:
+        my_spark.pre_execute(new_ctx.user_space_params)
+
+        mock_tempfile_mkdtemp.assert_called_once()
+        mock_shutil_make_archive.assert_called_once_with("/tmp/123/flyte_wf", "zip", os.getcwd())   
+        mock_add_pyfile.assert_called_once_with("/tmp/123/flyte_wf.zip")
 
 def test_spark_with_image_spec():
     custom_image = ImageSpec(
