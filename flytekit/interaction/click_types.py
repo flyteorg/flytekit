6eea95bce5f4580f0cf420f6caf0468cf49eccc6
Kevin Su <pingsutw@apache.org>
2025-05-23 09:41:41 -0700

    click
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- flytekit/interaction/click_types.py
+++ flytekit/interaction/click_types.py
@@ -14,7 +14,9 @@ from typing import cast, get_args
 
 import rich_click as click
 import yaml
+from click import __version__ as click_version, Parameter
 from dataclasses_json import DataClassJsonMixin, dataclass_json
+from packaging.version import Version
 from pytimeparse import parse
 
 from flytekit import BlobType, FlyteContext, Literal, LiteralType, StructuredDataset
@@ -140,8 +142,16 @@ class FileParamType(click.ParamType):
 class PickleParamType(click.ParamType):
     name = "pickle"
 
-    def get_metavar(self, param: click.Parameter, *args) -> t.Optional[str]:
-        return "Python Object <Module>:<Object>"
+    # click add args "ctx" to get_metavar in version 8.2.0
+    if Version(click_version) >= Version("8.2.0"):
+        from click.core import Context
+
+        def get_metavar(self, param: Parameter, ctx: Context) -> t.Optional[str]:
+            return "Python Object <Module>:<Object>"
+    else:
+
+        def get_metavar(self, param: Parameter, *args) -> t.Optional[str]:
+            return "Python Object <Module>:<Object>"
 
     def convert(
         self, value: typing.Any, param: typing.Optional[click.Parameter], ctx: typing.Optional[click.Context]
