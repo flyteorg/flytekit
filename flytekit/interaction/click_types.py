07981f1dcaf5e305fd1150621346e1f8ffa6a040
Blake <blaketastic2@gmail.com>
2025-08-04 11:15:58 -0400

    feedback from review
    Signed-off-by: Blake <blaketastic2@gmail.com>

--- flytekit/interaction/click_types.py
+++ flytekit/interaction/click_types.py
@@ -8,6 +8,7 @@ import logging
 import os
 import pathlib
 import sys
+import types
 import typing
 import typing as t
 from typing import cast, get_args
@@ -262,27 +263,6 @@ class DateTimeType(click.DateTime):
                 value = datetime.datetime.fromisoformat(value)
 
         return self._datetime_from_format(value, param, ctx)
-
-
-class DateType(DateTimeType):
-    """
-    A specialized type for handling date types that extends DateTimeType but returns date objects.
-    """
-
-    def convert(
-        self, value: typing.Any, param: typing.Optional[click.Parameter], ctx: typing.Optional[click.Context]
-    ) -> typing.Any:
-        if isinstance(value, ArtifactQuery):
-            return value
-
-        # First convert using the parent DateTime logic
-        dt_value = super().convert(value, param, ctx)
-
-        # Then convert to date if it's a datetime object
-        if isinstance(dt_value, datetime.datetime):
-            return dt_value.date()
-
-        return dt_value
 
 
 class DurationParamType(click.ParamType):
@@ -540,13 +520,8 @@ def literal_type_to_click_type(lt: LiteralType, python_type: typing.Type) -> cli
         for i in range(len(lt.union_type.variants)):
             variant = lt.union_type.variants[i]
             variant_python_type = typing.get_args(python_type)[i]
-            # Special handling for datetime.date in union types
-            if variant_python_type is datetime.date:
-                ct = DateType()
-            else:
-                ct = literal_type_to_click_type(variant, variant_python_type)
+            ct = literal_type_to_click_type(variant, variant_python_type)
             cts.append(ct)
-
         return UnionParamType(cts)
 
     return click.UNPROCESSED
@@ -585,7 +560,14 @@ class FlyteLiteralConverter(object):
             return value
         try:
             # If the expected Python type is datetime.date, adjust the value to date
-            if self._python_type is datetime.date and isinstance(value, datetime.datetime):
+            is_union = typing.get_origin(self._python_type) in (
+                typing.Union,
+                types.UnionType,
+            )
+            args = typing.get_args(self._python_type)
+            is_date_type = (is_union and datetime.date in args) or self._python_type is datetime.date
+
+            if is_date_type and isinstance(value, datetime.datetime):
                 # Click produces datetime, so converting to date to avoid type mismatch error
                 value = value.date()
             # If the input matches the default value in the launch plan, serialization can be skipped.
