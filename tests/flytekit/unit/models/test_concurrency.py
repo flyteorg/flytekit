7e4f8c41ab9013d0360667651f83935950707f0b
thomasjhuang <thomashuang63@gmail.com>
2025-09-08 11:11:56 -0700

    Reformat with ruff
    Signed-off-by: thomasjhuang <thomashuang63@gmail.com>

--- tests/flytekit/unit/models/test_concurrency.py
+++ tests/flytekit/unit/models/test_concurrency.py
@@ -5,7 +5,7 @@ from flytekit.models.concurrency import ConcurrencyLimitBehavior, ConcurrencyPol
 
 def test_concurrency_limit_behavior():
     assert ConcurrencyLimitBehavior.SKIP == _launch_plan_idl.ConcurrencyLimitBehavior.SKIP
-    
+
     # Test enum to string conversion
     assert ConcurrencyLimitBehavior.enum_to_string(ConcurrencyLimitBehavior.SKIP) == "SKIP"
     assert ConcurrencyLimitBehavior.enum_to_string(999) == "<UNKNOWN>"
@@ -13,16 +13,16 @@ def test_concurrency_limit_behavior():
 
 def test_concurrency_policy():
     policy = ConcurrencyPolicy(max_concurrency=1, behavior=ConcurrencyLimitBehavior.SKIP)
-    
+
     assert policy.max_concurrency == 1
     assert policy.behavior == ConcurrencyLimitBehavior.SKIP
-    
+
     # Test serialization to protobuf
     pb = policy.to_flyte_idl()
     assert isinstance(pb, _launch_plan_idl.ConcurrencyPolicy)
     assert pb.max == 1
     assert pb.behavior == _launch_plan_idl.ConcurrencyLimitBehavior.SKIP
-    
+
     # Test deserialization from protobuf
     policy2 = ConcurrencyPolicy.from_flyte_idl(pb)
     assert policy2.max_concurrency == 1
@@ -33,9 +33,9 @@ def test_concurrency_policy_with_different_max():
     # Test with a higher max value
     policy = ConcurrencyPolicy(max_concurrency=5, behavior=ConcurrencyLimitBehavior.SKIP)
     assert policy.max_concurrency == 5
-    
+
     pb = policy.to_flyte_idl()
     assert pb.max == 5
-    
+
     policy2 = ConcurrencyPolicy.from_flyte_idl(pb)
     assert policy2.max_concurrency == 5
