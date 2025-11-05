b68d416258de381e62e3f991bd4b9cd4b6937bd7
Andres Gomez Ferrer <andresg@spotify.com>
2025-11-05 12:55:16 +0100

    Check metaclass error_code property properly
    Signed-off-by: Andres Gomez Ferrer <andresg@spotify.com>

--- flytekit/exceptions/user.py
+++ flytekit/exceptions/user.py
@@ -31,7 +31,7 @@ class FlyteUserRuntimeException(_FlyteException):
 
     @property
     def error_code(self):
-        return self.value.error_code if hasattr(self.value, "error_code") else self._ERROR_CODE
+        return type(self.value).error_code if hasattr(type(self.value), "error_code") else self._ERROR_CODE
 
 
 class FlyteTypeException(FlyteUserException, TypeError):
